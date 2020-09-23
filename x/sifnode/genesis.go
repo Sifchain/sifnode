ab120fe8266d61dcd8e24b12a3b5135b644438dd
utx0_ <90531+utx0@users.noreply.github.com>
2020-09-23 14:44:04 +1000

    Feature/code quality (#28)
    * Imported linter from cosmos sdk.
    
    * fixed spelling
    
    * Added github actions
    
    * Added timeout
    
    * fixed spelling
    
    * imported codecov config from cosmos sdk

--- x/sifnode/genesis.go
+++ x/sifnode/genesis.go
@@ -1,16 +1,16 @@
 package sifnode
 
 import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/Sifchain/sifnode/x/sifnode/keeper"
 	"github.com/Sifchain/sifnode/x/sifnode/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	// abci "github.com/tendermint/tendermint/abci/types"
 )
 
 // InitGenesis initialize default parameters
 // and the keeper's address to pubkey map
 func InitGenesis(ctx sdk.Context, k keeper.Keeper /* TODO: Define what keepers the module needs */, data types.GenesisState) {
-	// TODO: Define logic for when you would like to initalize a new genesis
+	// TODO: Define logic for when you would like to initialize a new genesis
 }
 
 // ExportGenesis writes the current store values
