34a5a66097319f8c57996768cdf80c305085ac0c
Ilche Bedelovski <ilche.bedelovski@gmail.com>
2020-09-24 17:11:24 +0200

    Initial version of the Terraform module for AWS

---
+++ build/terraform/providers/aws/main.tf
@@ -0,0 +1,37 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = var.cluster_name
+  subnets      = module.vpc.public_subnets
+  vpc_id       = module.vpc.vpc_id
+  tags         = merge({ "Name" = var.cluster_name }, var.tags)
+
+  node_groups_defaults = {
+    ami_type  = "AL2_x86_64"
+    disk_size = var.node_group_settings["disk_size"]
+  }
+
+  node_groups = {
+    main = {
+      desired_capacity = var.node_group_settings["desired_capacity"]
+      max_capacity     = var.node_group_settings["max_capacity"]
+      min_capacity     = var.node_group_settings["min_capacity"]
+      instance_type    = var.node_group_settings["instance_type"]
+
+      k8s_labels = {
+        Environment = "${var.cluster_name}-${var.region}"
+      }
+      additional_tags = var.tags
+    }
+  }
+
+  cluster_version  = var.cluster_version
+  write_kubeconfig = true
+}
