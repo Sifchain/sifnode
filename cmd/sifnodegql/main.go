2197af3854699897dd0dc31541704d70cc9ffa2f
utx0_ <utx0@protonmail.com>
2020-10-02 12:38:05 +1000

    Bootstrapped gqlgen

---
+++ cmd/sifnodegql/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+
+	"github.com/Sifchain/sifnode/api/graph"
+	"github.com/Sifchain/sifnode/api/graph/generated"
+)
+
+const defaultPort = "8081"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Sifchain/sifnode
 go 1.15
 
 require (
+	github.com/99designs/gqlgen v0.13.0
 	github.com/cosmos/cosmos-sdk v0.39.1
 	github.com/cucumber/godog v0.10.0
 	github.com/golang/mock v1.4.3 // indirect
@@ -13,4 +14,5 @@ require (
 	github.com/tendermint/go-amino v0.16.0
 	github.com/tendermint/tendermint v0.33.8
 	github.com/tendermint/tm-db v0.5.1
+	github.com/vektah/gqlparser/v2 v2.1.0
 )
