1f0c1a76b2d60b461d7a230be20992d609f5abf9
Rudi Yardley <contact@rudiyardley.com>
2020-10-11 18:42:26 +1100

    Fix up issues with missing about and some TS issues

--- ui/app/src/views/Home.vue
+++ ui/app/src/views/Home.vue
@@ -12,21 +12,17 @@ import Vue, { VueConstructor } from "vue";
 import { api } from "sif-core";
 import { AssetAmount } from "sif-core/dist/entities";
 
-export default (Vue as VueConstructor<
-  Vue & { balances: AssetAmount[] }
->).extend({
+export default (Vue as VueConstructor<Vue & { balances: string[] }>).extend({
   name: "Home",
   data() {
     return { balances: [] } as { balances: string[] };
   },
   async mounted() {
     const balances = await api.walletService.getAssetBalances();
-    const balanceStrings: string[] = balances.map((amount: AssetAmount) => {
+    this.balances = balances.map((amount: AssetAmount) => {
       const str = amount.asset.symbol + ":" + amount.toFixed();
       return str;
-    }) as string[];
-
-    this.balances = balanceStrings;
+    });
   },
 });
 </script>
