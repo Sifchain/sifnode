8fcd8c094eb21c26a50c03a0140325ab5abb88ed
Rudi Yardley <contact@rudiyardley.com>
2020-10-12 23:49:33 +1100

    Add instructions and tidy up

--- ui/core/fixtures/ethereum/migrations/2_deploy_mytoken.js
+++ ui/core/fixtures/ethereum/migrations/2_deploy_mytoken.js
@@ -1,21 +1,7 @@
 const AliceToken = artifacts.require("AliceToken");
 const BobToken = artifacts.require("BobToken");
-const fs = require("fs");
-const path = require("path");
+
 module.exports = function(deployer) {
   deployer.deploy(AliceToken);
   deployer.deploy(BobToken);
-  deployer.then(() => {
-    const atk = AliceToken.address;
-    const btk = BobToken.address;
-    fs.writeFileSync(
-      path.resolve(__dirname, "../ctx.json"),
-      JSON.stringify({ atk, btk })
-    );
-  });
 };
-
-// Q: How do I know the address of these contracts
-// in our dev frontend app?
-
-// A: Write a JSON file that returns them
