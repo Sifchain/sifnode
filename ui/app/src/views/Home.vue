eae52ecdf56789ff91a6af95450523ee0c305e5e
Rudi Yardley <contact@rudiyardley.com>
2020-10-14 10:18:52 +1100

    Move vue code to app and fix typings to point to actual code!

--- ui/app/src/views/Home.vue
+++ ui/app/src/views/Home.vue
@@ -1,32 +1,18 @@
 <template>
   <div class="home">
-    <Wallet/>
+    <Wallet />
   </div>
 </template>
 
 <script lang="ts">
+import Wallet from "@/components/Wallet.vue";
 
-import Wallet from '@/components/Wallet.vue'
-
-import { defineComponent, provide } from 'vue';
-import { createStore,createApi, getWeb3, createUsecases, getFakeTokens } from '../../../core';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Home',
-  setup() { 
-    const store = createStore()
-    const { state } = store;
-    const api = createApi({ getWeb3, getSupportedTokens: getFakeTokens })
-    const usecases = createUsecases({ store, state, api})
-
-    provide('api', api) 
-    provide('state', store.state)
-    provide('usecases', usecases)
-
-  },
+  name: "Home",
   components: {
-    Wallet
-  }
-  
+    Wallet,
+  },
 });
 </script>
