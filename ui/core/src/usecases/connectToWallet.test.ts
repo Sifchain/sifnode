8e7d1292d2aae0a876dfc4ff5538045c4720270b
Michael Pierce <m.edward.pierce@gmail.com>
2020-10-13 14:44:03 -0500

    core/usecases/connectToWallet: scaffold test

---
+++ ui/core/src/usecases/connectToWallet.test.ts
@@ -0,0 +1,23 @@
+import connectToWallet from "./connectToWallet";
+import { createStore } from "../store";
+
+// before all, instantiate test chain
+// cd ../
+// make install
+// sifgen node create 
+
+// starport serve (get mnemonic)
+// get mnemonic
+// "login"
+describe("connectToWallet", () => {
+
+    it("should use mnemeonic to login to a cosmos wallet", () => {
+      // get account mnemonic on 
+      // expect(store.state.userBalances.get("USDC")?.asset).toEqual(USDC);
+
+    });
+
+
+});
+
+
