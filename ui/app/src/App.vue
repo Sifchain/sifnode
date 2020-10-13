9104136b6f09055efd0c096e55d77c2a52a4badc
Rudi Yardley <contact@rudiyardley.com>
2020-10-13 23:25:28 +1100

    Refactor injection. Get working in Vue

--- ui/app/src/App.vue
+++ ui/app/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+    </div>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style>
--- ui/core/src/api/index.ts
+++ ui/core/src/api/index.ts
@@ -1,10 +1,23 @@
 // Everything here represents services that are effectively remote data storage
+export * from "./utils/getFakeTokens";
+export * from "./utils/getSupportedTokens";
+export * from "./utils/getWeb3";
 
-// export * from "./authService";
-// export * from "./bankService";
-// export * from "./sifdistService";
-// export * from "./slashingService";
-// export * from "./stakingService";
-// export * from "./tendermintService";
-// export * from "./transactionsService";
-export * from "./walletService";
+import walletService, { WalletServiceContext } from "./walletService";
+
+type ApiContext = WalletServiceContext; // add contexts from other APIs
+
+export function createApi(context: ApiContext) {
+  return {
+    walletService: walletService(context),
+  };
+}
+
+export type FullApi = ReturnType<typeof createApi>;
+
+export type Api<
+  T extends keyof FullApi = keyof FullApi,
+  U extends object = {}
+> = {
+  api: Pick<FullApi, T>;
+} & U;
