32cbdb5804b2a57c9b18aa75eaff1b3d6fee6227
Michael Pierce <m.edward.pierce@gmail.com>
2020-10-15 11:19:52 -0500

    views/CWallet

---
+++ ui/app/src/views/CWallet.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+      <c-sign-in />
+  </div>
+</template>
+
+<script lang="ts">
+import { onMounted, ref } from 'vue'
+import { api, entities } from '../../../core'
+import CSignIn from '@/components/CSignIn'
+export default {
+  name: 'CWallet',
+  components: {
+    CSignIn
+  },
+  setup() {
+  }
+};
+</script>
