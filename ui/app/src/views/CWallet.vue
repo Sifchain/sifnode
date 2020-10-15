858726eed93373472f73fcdef7b89bfe3a0f8065
Michael Pierce <m.edward.pierce@gmail.com>
2020-10-15 17:02:47 -0500

    app/CWallet.vue: localMnemonic, connectToWallet(mnemonic)

--- ui/app/src/views/CWallet.vue
+++ ui/app/src/views/CWallet.vue
@@ -1,19 +1,48 @@
 <template>
-  <div class="home">
-      <c-sign-in />
+  <div class="df fdc">
+    <textarea
+      v-if="!CWalletStore.isConnected"
+      v-model="localMnemonic"
+      placeholder="Mnemonic..."
+    ></textarea>
+    <button @click="submit">
+      Connect Wallet
+    </button>
+    {{localMnemonic}}
+    {{CWalletStore}}
+      <!-- <c-sign-in /> -->
   </div>
 </template>
 
 <script lang="ts">
-import { onMounted, ref } from 'vue'
-import { api, entities } from '../../../core'
-import CSignIn from '@/components/CSignIn'
-export default {
-  name: 'CWallet',
+import { defineComponent, ref, isRef, computed,  toRef, isReactive } from "vue";
+
+import CSignIn from "@/components/CSignIn.vue"
+import { CWalletStore, WalletStore } from "../../../core/src/store/wallet"
+import { connectToWallet } from "../../../core/src/actions/CWalletActions"
+
+export default defineComponent({
+  name: "CWallet",
   components: {
     CSignIn
   },
   setup() {
-  }
-};
+    const localMnemonic = ref()
+    async function submit() {
+      if (!localMnemonic) return
+      const parsedMnemonic = computed(() => {
+        // return (localMnemonic.value + "TRANSFORM").trim()
+        return (localMnemonic.value).trim()
+
+      })
+      await connectToWallet(parsedMnemonic.value)
+    }
+    return {CWalletStore, submit, localMnemonic}
+  },
+});
 </script>
+
+<style scoped>
+.df {display: flex}
+.fdc {flex-direction: column}
+</style>
