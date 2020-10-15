d31264fb9d96e469ef90a9908b0947e13e1a57db
Michael Pierce <m.edward.pierce@gmail.com>
2020-10-15 17:05:25 -0500

    core/store/wallet: CWalletStore

--- ui/core/src/store/wallet.ts
+++ ui/core/src/store/wallet.ts
@@ -1,12 +1,27 @@
 import { reactive } from "@vue/reactivity";
 import { Balance } from "../entities";
+import {Ref, ref} from "@vue/reactivity"
 
 export type WalletStore = {
   balances: Balance[];
-  isConnected: boolean;
+  isConnected: boolean
 };
 
 export const wallet = reactive({
   balances: [],
-  isConnected: false,
+  isConnected: false
 }) as WalletStore;
+
+
+
+export interface ICWalletStore extends  WalletStore {
+  mnemonic?: string
+} 
+
+export const CWalletStore = reactive({
+  balances: [],
+  isConnected: false,
+  mnemonic: ""
+}) as ICWalletStore;
+
+// toRefs?
