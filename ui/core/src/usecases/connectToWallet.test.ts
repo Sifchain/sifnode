004603e3a6d262a4eefa5348ba1f047bf1591097
Michael Pierce <m.edward.pierce@gmail.com>
2020-10-15 11:21:01 -0500

    usecases/connectToWallet.test.ts

---
+++ ui/core/src/usecases/connectToWallet.test.ts
@@ -0,0 +1,28 @@
+import connectToWallet from "./connectToWallet";
+import { createStore } from "../store";
+
+// before all, instantiate test chain
+// cd ../
+// make install
+// sifgen node create 
+
+
+// starport serve (get mnemonic)
+// get mnemonic
+// "get balance"
+
+const mnemonicShadowfiend = "race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow"
+
+const mnemonicAkasha = "hand inmate canvas head lunar naive increase recycle dog ecology inhale december wide bubble hockey dice worth gravity ketchup feed balance parent secret orchard"
+
+
+describe("connectToWallet", () => {
+
+    it("should use mnemeonic to get cosmos wallet", () => {
+      // get account mnemonic on 
+      // expect(store.state.userBalances.get("USDC")?.asset).toEqual(USDC);
+
+    });
+
+
+});
