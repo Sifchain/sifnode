4ff95eee8ec35c4b47a3e6206e1c5f68015ab166
Jedi2002 <tanmay@sifchain.finance>
2020-10-16 01:52:51 -0400

    added tests for double swap

--- demo.sh
+++ demo.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Creating a pool"
-yes Y | sifnodecli tx clp create-pool --from user1 --sourceChain ETHEREUM --symbol ETH --ticker ETH --nativeAmount 1000 --externalAmount 1000
+yes Y | sifnodecli tx clp create-pool --from user1 --sourceChain ETHEREUM --symbol ETH --ticker ETH --nativeAmount 10000 --externalAmount 10000
 sleep 8
-yes Y | sifnodecli tx clp create-pool --from user1 --sourceChain TEZOS --symbol XTZ --ticker XTZ --nativeAmount 1000 --externalAmount 1000
+yes Y | sifnodecli tx clp create-pool --from user1 --sourceChain TEZOS --symbol XTZ --ticker XTZ --nativeAmount 10000 --externalAmount 10000
 
 echo "Query all pools"
 sleep 8
@@ -24,12 +24,12 @@ yes Y | sifnodecli tx clp add-liquidity --from user1 --sourceChain ETHEREUM --sy
 echo "removing Liquidity"
 sleep 8
 yes Y | sifnodecli tx clp remove-liquidity --from user1 --sourceChain ETHEREUM --symbol ETH --ticker ETH --wBasis 10000 --asymmetry 1
-#
+
 
 #This swap will not work in the future as sent and received asset is the same ,but that validation will be added in a subsequent pr
 echo "swap"
 sleep 8
-yes Y |  sifnodecli tx clp swap --from user1 --sentSourceChain ETHEREUM --sentSymbol ETH --sentTicker ETH --receivedSourceChain TEZOS --receivedSymbol XTZ --receivedTicker XTZ --sentAmount 10
+yes Y |  sifnodecli tx clp swap --from user1 --sentSourceChain ETHEREUM --sentSymbol ETH --sentTicker ETH --receivedSourceChain TEZOS --receivedSymbol XTZ --receivedTicker XTZ --sentAmount 1000
 
 echo "decommission pool"
 sleep 8
