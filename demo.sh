f1ee1d85e05968b259f90f7d46802dca2cffbc33
utx0_ <90531+utx0@users.noreply.github.com>
2020-10-16 13:11:11 +1100

    Bug/minor bugs (#78)
    * Removed if a.SourceChain != NativeToken check as NativeToken is not a chain. ie Eth is a chain.
    
    * Swapped order input bug.
    
    * Updated to match ./config.yml
    
    * Updated to make more real world to current requirements

--- demo.sh
+++ demo.sh
@@ -1,35 +1,34 @@
 #!/usr/bin/env bash
 echo "Creating a pool"
-yes Y | sifnodecli tx clp create-pool --from user1 --sourceChain ROWAN --symbol EOSROWAN --ticker EOS --nativeAmount 1000 --externalAmount 1000
+yes Y | sifnodecli tx clp create-pool --from user1 --sourceChain ETH --symbol cLINK --ticker cLINK --nativeAmount 1000 --externalAmount 1
 
 echo "Query all pools"
 sleep 8
 sifnodecli query clp pools
 
 echo "Query specific pool"
-sleep 8
-sifnodecli query clp pool EOS ROWAN
+sleep 3
+sifnodecli query clp pool cLINK ETH
 
 echo "Query Liquidity Provider / Pool creator is the first lp for the pool"
-sleep 8
-sifnodecli query clp lp EOS $(sifnodecli keys show user1 -a)
+sleep 3
+sifnodecli query clp lp cLINK $(sifnodecli keys show user1 -a)
+
+echo "Adding more liquidity"
+sleep 3
+yes Y | sifnodecli tx clp add-liquidity --from user1 --sourceChain ETH --symbol cLINK --ticker cLINK --nativeAmount 1000 --externalAmount 1
 
 
-echo "adding more liquidity"
-sleep 8
-yes Y | sifnodecli tx clp add-liquidity --from user1 --sourceChain ROWAN --symbol EOSROWAN --ticker EOS --nativeAmount 1000 --externalAmount 1000
-
-
-echo "removing Liquidity"
-sleep 8
-yes Y | sifnodecli tx clp remove-liquidity --from user1 --sourceChain ROWAN --symbol EOSROWAN --ticker EOS --wBasis 10000 --asymmetry 1
+echo "Removing Liquidity"
+sleep 3
+yes Y | sifnodecli tx clp remove-liquidity --from user1 --sourceChain ETH --symbol cLINK --ticker cLINK --wBasis 1000 --asymmetry 1
 
 
 #This swap will not work in the future as sent and received asset is the same ,but that validation will be added in a subsequent pr
 echo "swap"
 sleep 8
-yes Y |  sifnodecli tx clp swap --from user1 --sentSourceChain ROWAN --sentSymbol EOSROWAN --sentTicker EOS --receivedSourceChain ROWAN --receivedSymbol EOSROWAN --receivedTicker EOS --sentAmount 1000
+yes Y |  sifnodecli tx clp swap --from user1 --sentSourceChain ETH --sentSymbol cLINK --sentTicker cLINK --receivedSourceChain ETH --receivedSymbol cLINK --receivedTicker cLINK --sentAmount 1000
 
 echo "decommission pool"
 sleep 8
-yes Y | sifnodecli tx clp decommission-pool --from user1 --ticker EOS --sourceChain ROWAN
+yes Y | sifnodecli tx clp decommission-pool --from user1 --ticker cLINK --sourceChain ETH
