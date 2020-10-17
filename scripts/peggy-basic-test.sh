ec39ec11169d2539ebc492ea6556ff18f2307b9e
utx0_ <utx0@protonmail.com>
2020-10-17 18:34:58 +1100

    Updated

--- scripts/peggy-basic-test.sh
+++ scripts/peggy-basic-test.sh
@@ -7,9 +7,11 @@ $(sifnodecli keys show user2 -a) $(sifnodecli keys show user1 -a --bech val) 5 l
 --token-contract-address=0x0000000000000000000000000000000000000000 --ethereum-chain-id=3 --from=user1 --yes
 
 # 2. query the tx
-sifnodecli q tx
+#sifnodecli q tx
 
-# 3. query the prophecy (error now)
-sifnodecli query ethbridge prophecy 0x30753E4A8aad7F8597332E813735Def5dD395028 0 eth 0x11111111262b236c9ac9a9a8c8e4276b5cf6b2c9 \
---ethereum-chain-id=3 --token-contract-address=0x0000000000000000000000000000000000000000
+# 3. check user2 account balance
+sifnodecli q auth account $(sifnodecli keys show user2 -a)
+
+# 4. query the prophecy (error now)
+#sifnodecli query ethbridge prophecy 0x30753E4A8aad7F8597332E813735Def5dD395028 0 eth 0x11111111262b236c9ac9a9a8c8e4276b5cf6b2c9 --ethereum-chain-id=3 --token-contract-address=0x0000000000000000000000000000000000000000
 
