070549c14acf74607c15c6fb0f8502e3bfaed941
utx0_ <utx0@protonmail.com>
2020-10-17 16:04:53 +1100

    Updated scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+killall sifnoded sifnodecli
+
 rm $(which sifnoded) 2> /dev/null || echo sifnoded not install yet ...
 rm $(which sifnodecli) 2> /dev/null || echo sifnodecli not install yet ...
 
@@ -8,7 +10,7 @@ rm -rf ~/.sifnodecli
 
 make install
 
-sifnoded init test --chain-id=sifnode
+sifnoded init test --chain-id=sifchain
 
 sifnodecli config output json
 sifnodecli config indent true
@@ -19,8 +21,8 @@ sifnodecli config keyring-backend test
 sifnodecli keys add user1
 sifnodecli keys add user2
 
-sifnoded add-genesis-account $(sifnodecli keys show user1 -a) 1000nametoken,100000000stake
-sifnoded add-genesis-account $(sifnodecli keys show user2 -a) 1000nametoken,100000000stake
+sifnoded add-genesis-account $(sifnodecli keys show user1 -a) 1000rwn,100000000stake
+sifnoded add-genesis-account $(sifnodecli keys show user2 -a) 1000rwn,100000000stake
 
 sifnoded gentx --name user1 --keyring-backend test
 
