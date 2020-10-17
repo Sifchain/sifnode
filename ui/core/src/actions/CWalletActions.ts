8a62a86dec91f8fcadc0347111affcc93bd3c1be
Michael Pierce <m.edward.pierce@gmail.com>
2020-10-17 17:02:45 -0500

    actions/CWallet: generateMnemonic, rm Store, clean

--- ui/core/src/actions/CWalletActions.ts
+++ ui/core/src/actions/CWalletActions.ts
@@ -1,24 +1,26 @@
-import { ActionContext } from "..";
-import { validateMnemonic } from "bip39"
+import { validateMnemonic, generateMnemonic } from "bip39"
+// import {Ref, ComputedRef} from "@vue/reactivity"
+// import { ActionContext } from "..";
+import { Mnemonic } from "../entities/Wallet"
+// import { CWalletStore, ICWalletStore } from "../store/wallet"
+import { cosmosSignin } from "../api/walletService/SifService"
 
+import {
+  Account,
+  SigningCosmosClient
+} from "@cosmjs/launchpad";
 
-import {Ref, ComputedRef} from "@vue/reactivity"
-import { CWalletStore, ICWalletStore } from "../store/wallet"
-import { cosmosSignin } from "../api/walletService/Cosmos"
-
-
-// howto not duplicate mnemonic: ICWalletStore["mnemonic"]
-export async function signInCosmosWallet(mnemonic: ICWalletStore["mnemonic"]) {
-  // set mnemonic in store (definitely won't do IRL)
-  CWalletStore.mnemonic = mnemonic
-
-  if(!mnemonicIsValid(mnemonic)) throw "Invalid Mnemonic. Not sent."
-  // "sign in" on chain
-  await cosmosSignin(mnemonic)
+export function generateMnemonicAction(): Mnemonic {
+  return generateMnemonic()
 }
 
+export async function signInCosmosWallet(mnemonic: Mnemonic): 
+  Promise<{account: Account, client: SigningCosmosClient}> {
+  if(!mnemonicIsValid(mnemonic)) throw "Invalid Mnemonic. Not sent."
+  return await cosmosSignin(mnemonic)
+}
 
-function mnemonicIsValid(mnemonic:ICWalletStore["mnemonic"]): Boolean {
-  if (!mnemonic) {throw "Mnemonic must be defined"}
+export function mnemonicIsValid(mnemonic: Mnemonic): Boolean {
+  if (!mnemonic) throw "Mnemonic must be defined"
   return validateMnemonic(mnemonic)
 }
