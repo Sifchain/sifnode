0be630c14e4793cd82b37c4d1544a336b170595c
Michael Pierce <m.edward.pierce@gmail.com>
2020-10-17 17:04:10 -0500

    connectToWallet.test: generate mnemonic, is valid, sign in

--- ui/core/src/actions/connectToWallet.test.ts
+++ ui/core/src/actions/connectToWallet.test.ts
@@ -1,6 +1,8 @@
 import connectToWallet from "./connectToWallet";
 import { createStore } from "../store";
 
+import { mnemonicIsValid, generateMnemonicAction, signInCosmosWallet  } from "./CWalletActions"
+
 // before all, instantiate test chain
 // cd ../
 // make install
@@ -11,6 +13,8 @@ import { createStore } from "../store";
 // get mnemonic
 // "get balance"
 
+const badMnemonic = "Ever have that feeling where you’re not sure if you’re awake or dreaming?"
+
 const mnemonicShadowfiend = "race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow"
 
 const mnemonicAkasha = "hand inmate canvas head lunar naive increase recycle dog ecology inhale december wide bubble hockey dice worth gravity ketchup feed balance parent secret orchard"
@@ -18,9 +22,20 @@ const mnemonicAkasha = "hand inmate canvas head lunar naive increase recycle dog
 
 describe("connectToWallet", () => {
 
-    it("should use mnemeonic to get cosmos wallet", () => {
+    it("should check if mnemonic is valid", () => {
+      expect(mnemonicIsValid(badMnemonic)).toEqual(false)
+      expect(mnemonicIsValid(mnemonicShadowfiend)).toEqual(true)
+    })
+
+    it("should check generate valid mnemonic", () => {
+      expect(mnemonicIsValid(generateMnemonicAction())).toEqual(true)
+    })
+
+    it("should use mnemeonic to sign into cosmos wallet", async () => {
       // get account mnemonic on 
-      // expect(store.state.userBalances.get("USDC")?.asset).toEqual(USDC);
+      const a = await signInCosmosWallet(mnemonicShadowfiend)
+      console.log(a)
+      // expect(signInCosmosWallet(mnemonicShadowfiend)).
 
     });
 
