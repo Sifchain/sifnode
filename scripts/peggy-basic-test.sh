78a8a73dd24da33dc6bff2b996afec35be7f1a48
juniuszhou <junius.zhou@gmail.com>
2020-10-19 20:50:32 +0800

    must use the same nonce for query.

--- scripts/peggy-basic-test.sh
+++ scripts/peggy-basic-test.sh
@@ -12,8 +12,8 @@ $(sifnodecli keys show user2 -a) $(sifnodecli keys show user1 -a --bech val) 5 l
 # 3. check user2 account balance
 sifnodecli q auth account $(sifnodecli keys show user2 -a)
 
-# 4. query the prophecy (error now)
-#sifnodecli query ethbridge prophecy 0x30753E4A8aad7F8597332E813735Def5dD395028 0 eth 0x11111111262b236c9ac9a9a8c8e4276b5cf6b2c9 --ethereum-chain-id=3 --token-contract-address=0x0000000000000000000000000000000000000000
+# 4. query the prophecy
+sifnodecli query ethbridge prophecy 0x30753E4A8aad7F8597332E813735Def5dD395028 3 eth 0x11111111262b236c9ac9a9a8c8e4276b5cf6b2c9 --ethereum-chain-id=3 --token-contract-address=0x0000000000000000000000000000000000000000
 
 ## Case 2
 ## 1. burn peggyetch for user2
