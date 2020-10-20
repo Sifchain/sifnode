7b2c718f817e00c1e7f05fe263d6216f59f36349
intl-man <theinternationalman079@gmail.com>
2020-10-20 17:54:38 +1300

    Disabled PVC. Tweaks to TF config.

--- build/terraform/providers/aws/main.tf
+++ build/terraform/providers/aws/main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = var.region
 }
 
- provider "kubernetes" {
+provider "kubernetes" {
   host                   = element(concat(data.aws_eks_cluster.cluster[*].endpoint, list("")), 0)
   cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, list("")), 0))
   token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, list("")), 0)
   load_config_file       = false
   version                = "~> 1.9"
- }
+}
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
