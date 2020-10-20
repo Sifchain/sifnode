24bee33307142b72e67435a261623df060148272
Jedi2002 <tanmay@sifchain.finance>
2020-10-20 01:52:43 -0400

    Fixed demo.sh to work with coins

--- demo.sh
+++ demo.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Creating a pool"
-yes Y | sifnodecli tx clp create-pool --from user1 --sourceChain ETHEREUM --symbol ETH --ticker ETH --nativeAmount 10000 --externalAmount 10000
+yes Y | sifnodecli tx clp create-pool --from user1 --sourceChain ETHEREUM --symbol ETH --ticker eth --nativeAmount 99 --externalAmount 99
 sleep 8
-yes Y | sifnodecli tx clp create-pool --from user1 --sourceChain TEZOS --symbol XTZ --ticker XTZ --nativeAmount 10000 --externalAmount 10000
+yes Y | sifnodecli tx clp create-pool --from user1 --sourceChain DASH --symbol DASH --ticker dash --nativeAmount 100 --externalAmount 100
 
 echo "Query all pools"
 sleep 8
@@ -10,27 +10,26 @@ sifnodecli query clp pools
 
 echo "Query specific pool"
 sleep 8
-sifnodecli query clp pool ETH
+sifnodecli query clp pool eth
 
 echo "Query Liquidity Provider / Pool creator is the first lp for the pool"
 sleep 8
-sifnodecli query clp lp ETH $(sifnodecli keys show user1 -a)
+sifnodecli query clp lp eth $(sifnodecli keys show user1 -a)
 
 echo "adding more liquidity"
 sleep 8
-yes Y | sifnodecli tx clp add-liquidity --from user1 --sourceChain ETHEREUM --symbol ETH --ticker ETH --nativeAmount 1000 --externalAmount 1000
+yes Y | sifnodecli tx clp add-liquidity --from user1 --sourceChain ETHEREUM --symbol ETH --ticker eth --nativeAmount 1 --externalAmount 1
+
+echo "swap"
+sleep 8
+yes Y |  sifnodecli tx clp swap --from user1 --sentSourceChain ETHEREUM --sentSymbol ETH --sentTicker eth --receivedSourceChain DASH --receivedSymbol DASH --receivedTicker dash --sentAmount 20
 
 
 echo "removing Liquidity"
 sleep 8
-yes Y | sifnodecli tx clp remove-liquidity --from user1 --sourceChain ETHEREUM --symbol ETH --ticker ETH --wBasis 10000 --asymmetry 1
+yes Y | sifnodecli tx clp remove-liquidity --from user1 --sourceChain ETHEREUM --symbol ETH --ticker eth --wBasis 5001 --asymmetry -1
 
-
-#This swap will not work in the future as sent and received asset is the same ,but that validation will be added in a subsequent pr
-echo "swap"
-sleep 8
-yes Y |  sifnodecli tx clp swap --from user1 --sentSourceChain ETHEREUM --sentSymbol ETH --sentTicker ETH --receivedSourceChain TEZOS --receivedSymbol XTZ --receivedTicker XTZ --sentAmount 1000
 
 echo "decommission pool"
 sleep 8
-yes Y | sifnodecli tx clp decommission-pool --from user1 --ticker ETH
+yes Y | sifnodecli tx clp decommission-pool --from user1 --ticker eth
