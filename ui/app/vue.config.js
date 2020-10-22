9337c8a8a0d4457edca34cb91f1bc56dd91dd48a
novelja <36472952+novelja@users.noreply.github.com>
2020-10-23 00:35:02 +0200

    sass loader added

--- ui/app/vue.config.js
+++ ui/app/vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  
+  css: {
+    extract: {
+      filename: "app.css"
+    },
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "normalize-scss";
+          @import "@/scss/variables.scss";
+          @import "@/scss/mixins.scss";
+        `
+      }
+    }
+  }
 }
