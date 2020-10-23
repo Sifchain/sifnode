fb761b749f517a365d9f810f68fbec87d90c62b2
novelja <36472952+novelja@users.noreply.github.com>
2020-10-23 21:45:31 +0200

    Swap panel init

---
+++ ui/app/src/components/Blocks/SwapPanel.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="swap-panel">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.swap-panel {
+  min-height: 120px;
+  margin-bottom: 10px;
+  border-radius: $br_sm;
+  background: $c_white;
+}
+</style>
