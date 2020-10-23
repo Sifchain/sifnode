009ae1f2f5b45bb4ea54792a2a105cf7c6fd27ef
novelja <36472952+novelja@users.noreply.github.com>
2020-10-23 02:28:44 +0200

    Cleanup and showcase route

---
+++ ui/app/src/views/Ui.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="mt-2">
+      <SifButton>Rowan</SifButton>
+      <SifButton disabled>Rowan</SifButton>
+    </div>
+
+    <div class="mt-2">
+      <SifButton block>Rowan</SifButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import SifButton from '../components/Elements/SifButton'
+
+export default {
+  components: {
+    SifButton,
+  }
+}
+</script>
+
+<style>
+
+</style>
