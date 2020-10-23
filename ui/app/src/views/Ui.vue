f3bf417c8f663f4a1c680dd617619305f412236e
novelja <36472952+novelja@users.noreply.github.com>
2020-10-23 20:31:19 +0200

    Main app card init

--- ui/app/src/views/Ui.vue
+++ ui/app/src/views/Ui.vue
@@ -6,18 +6,32 @@
       <SifButton disabled>Disabled</SifButton>
     </div>
 
-    <div class="mt-6">
+    <div class="mt-6 mb-6">
       <SifButton primary block>Primary Block</SifButton>
     </div>
+
+    <!-- SifCard -->
+      <SifApp>
+        <template v-slot:header>
+          HEADER
+        </template>
+        <div>
+          <p class="text--small">No wallet connected</p>
+          <SifButton primary block>Connect to Wallet</SifButton>
+        </div>
+      </SifApp>
+
   </div>
 </template>
 
 <script>
-import SifButton from '../components/Elements/SifButton'
+import SifButton from '../components/Elements/SifButton.vue';
+import SifApp from '../components/Blocks/SifApp.vue';
 
 export default {
   components: {
     SifButton,
+    SifApp,
   }
 }
 </script>
