a075b9fd177b53eddfbb63d4413bdf197bfc1672
novelja <36472952+novelja@users.noreply.github.com>
2020-10-23 13:04:59 +0200

    Main menu

---
+++ ui/app/src/components/Layout/Menu.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="menu">
+    <div class="menu__item">
+      <a class="menu__link menu__link--active" href="#">Discover</a>
+    </div>
+    <div class="menu__item">
+      <a class="menu__link" href="#">New Wallet</a>
+    </div>
+    <div class="menu__item">
+      <a class="menu__link" href="#">Convert</a>
+    </div>
+    <div class="menu__item">
+      <a class="menu__link" href="#">Deposit</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.menu {
+  display: flex;
+
+  &__item {
+    margin-right: 35px;
+  }
+
+  &__link {
+    @include resetLink;
+    font-size: $fs_lg;
+    font-weight: normal;
+    opacity: 1;
+    transition: opacity $trans_fast;
+
+    &:hover {
+      opacity: 0.5;
+    }
+
+    &--active {
+      font-weight: bold;
+      pointer-events: none;
+    }
+  }
+}
+</style>
