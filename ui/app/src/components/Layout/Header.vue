bf8527c4ddcd425fca0a3db6dc54b8db4b86218e
novelja <36472952+novelja@users.noreply.github.com>
2020-10-23 13:04:46 +0200

    Main app layout

---
+++ ui/app/src/components/Layout/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <header class="header">
+    <div class="header__left">
+      <div class="header__logo">S</div>
+      <Menu />
+    </div>
+    <div class="header__right">
+      <SifButton>Rowan</SifButton>
+    </div>
+  </header>
+</template>
+
+<script>
+import Menu from './Menu.vue';
+import SifButton from '../Elements/SifButton.vue';
+
+export default {
+  components: {
+    Menu,
+    SifButton,
+  }
+}
+</script>
+
+<style lang="scss">
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 80px;
+  padding: 0 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: white;
+  border-bottom: $divider;
+
+  &__left {
+    display: flex;
+
+  }
+
+  &__logo {
+    font-family: $f_special;
+    font-size: 40px;
+    font-weight: normal;
+    margin-right: 46px;
+  }
+}
+</style>
---
+++ ui/app/src/components/Layout/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="main">
+    <slot></slot>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.main {
+  width: 100vw;
+  min-height: calc(100vh - 81px); // account for menu height
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
