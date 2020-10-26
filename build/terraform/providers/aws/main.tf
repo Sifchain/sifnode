45b9ac96a15b0a9c7b59e8b6034c2bbef609be51
intl-man <theinternationalman079@gmail.com>
2020-10-26 14:48:17 +0800

    Rolled back the AZ config.

--- build/terraform/providers/aws/main.tf
+++ build/terraform/providers/aws/main.tf
@@ -23,7 +23,7 @@ module "vpc" {
 
   name           = var.cluster_name
   cidr           = var.vpc_cidr
-  azs            = var.az
+  azs            = [for az in var.az : format("%s%s", var.region, az)]
   public_subnets = [cidrsubnet(var.vpc_cidr, 4, 1), cidrsubnet(var.vpc_cidr, 4, 2), cidrsubnet(var.vpc_cidr, 4, 3)]
 
   enable_dns_hostnames = true
