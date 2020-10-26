f3e412d779ed496bf8e9e6ca1296a88f6ec226a3
utx0_ <utx0@protonmail.com>
2020-10-26 19:25:42 +1100

    Import'ed liangping's faucet module and got it to compile. - WIP

---
+++ x/faucet/alias.go
@@ -0,0 +1,26 @@
+package faucet
+
+import (
+	"github.com/Sifchain/sifnode/x/faucet/internal/keeper"
+	"github.com/Sifchain/sifnode/x/faucet/internal/types"
+)
+
+const (
+	ModuleName = types.ModuleName
+	RouterKey  = types.RouterKey
+	StoreKey   = types.StoreKey
+
+	MAINNET = "mainnet"
+	TESTNET = "testnet"
+)
+
+var (
+	NewKeeper     = keeper.NewKeeper
+	NewQuerier    = keeper.NewQuerier
+	ModuleCdc     = types.ModuleCdc
+	RegisterCodec = types.RegisterCodec
+)
+
+type (
+	Keeper = keeper.Keeper
+)
