0d8163fd812edad5efa1dc367f8fedc3f9f6cc30
juniuszhou <junius.zhou@gmail.com>
2020-11-02 15:16:41 +0800

    add ethereum test file.

--- cmd/ebrelayer/relayer/ethereum_test.go
+++ cmd/ebrelayer/relayer/ethereum_test.go
@@ -1,14 +1,16 @@
 package relayer
 
 import (
-	"fmt"
+	"bufio"
 	"os"
 	"testing"
 
+	"github.com/Sifchain/sifnode/app"
+	"github.com/Sifchain/sifnode/cmd/ebrelayer/relayer"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-
-	"github.com/Sifchain/sifnode/cmd/ebrelayer/relayer"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/log"
 )
 
@@ -17,16 +19,27 @@ const (
 	ethProvider     = "ws://127.0.0.1:7545/"
 	contractAddress = "0x00"
 	privateKeyStr   = "ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"
-	// privateKey              = txs.LoadPrivateKey()
-	// logger                  = tmLog.NewTMLogger(tmLog.NewSyncWriter(os.Stdout))
+	// rpc              = "http://localhost:7545"
+	rpc              = ""
+	validatorMoniker = "user1"
+	chainID          = "sifchain"
+	web3Provider     = "ws://localhost:7545/"
 )
 
-func TestNewCosmosSub(t *testing.T) {
+func TestNewEthereumSub(t *testing.T) {
 
+	rootCmd := &cobra.Command{
+		Use:          "use",
+		Short:        "short",
+		SilenceUsage: true,
+	}
+
+	cdc := app.MakeCodec()
+	inBuf := bufio.NewReader(rootCmd.InOrStdin())
 	privateKey, _ := crypto.HexToECDSA(privateKeyStr)
 	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
 	registryContractAddress := common.HexToAddress(contractAddress)
-	a := relayer.NewCosmosSub(tmProvider, ethProvider, registryContractAddress,
+	a, err := relayer.NewEthereumSub(inBuf, rpc, cdc, validatorMoniker, chainID, web3Provider, registryContractAddress,
 		privateKey, logger)
-	fmt.Println(a)
+	require.Equal(t, err, nil, "error when init Ethereum sub")
 }
