dd4f127de52cb9af8b7bd9d6516cdcf7e89e5d20
Rudi Yardley <contact@rudiyardley.com>
2020-11-10 00:11:02 +1100

    Fix up pages

--- ui/app/src/router/index.ts
+++ ui/app/src/router/index.ts
@@ -25,7 +25,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/pool/add-liquidity",
     name: "AddLiquidity",
-    component: AddLiquidity,
+    component: CreatePair,
   },
   {
     path: "/pool/create-pair",
