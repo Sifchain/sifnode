5634a79b3535a2e707569ad55c965b6743973ab9
Ilia Bukalov <ilyabukalov@gmail.com>
2020-11-25 13:08:24 +0800

    color function implemented

--- cmd/ebrelayer/siflogger/siflogger.go
+++ cmd/ebrelayer/siflogger/siflogger.go
@@ -1,9 +1,12 @@
 package siflogger
 
 import (
+	"fmt"
 	"os"
 
 	kitlog "github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/go-kit/kit/log/term"
 	"github.com/tendermint/tendermint/libs/log"
 )
 
@@ -11,8 +14,23 @@ type SifLogger struct {
 	logger log.Logger
 }
 
+func colorFn(keyvals ...interface{}) term.FgBgColor {
+	if keyvals[0] != level.Key() {
+		panic(fmt.Sprintf("expected level key to be first, got %v", keyvals[0]))
+	}
+
+	switch keyvals[1].(level.Value).String() {
+	case "debug":
+		return term.FgBgColor{Fg: term.DarkGreen}
+	case "error":
+		return term.FgBgColor{Fg: term.Red}
+	default:
+		return term.FgBgColor{}
+	}
+}
+
 func New() SifLogger {
-	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+	logger := log.NewTMLoggerWithColorFn(log.NewSyncWriter(os.Stdout), colorFn)
 	logger = logger.With("caller", kitlog.Caller(5))
 	e := SifLogger{logger}
 	return e
