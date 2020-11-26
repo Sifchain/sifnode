fc963888f93d43193f48738801b2d0a944124206
Rudi Yardley <contact@rudiyardley.com>
2020-11-26 11:21:18 +1100

    Default to hardcoded mnemonic value for testing

--- ui/chains/ethereum/run-ethereum-test-chain.sh
+++ ui/chains/ethereum/run-ethereum-test-chain.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-MNEMONIC=$(grep MNEMONIC .env | cut -d '=' -f 2-)
+ENV_MNEMONIC=$(grep MNEMONIC .env | cut -d '=' -f 2-)
 BLOCKSPEED=0
+MNEMONIC=${ENV_MNEMONIC:-race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow}
 yarn ganache-cli -m "$MNEMONIC"  -b "$BLOCKSPEED"
