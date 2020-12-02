b1eafbc0532bb1691bb56a2be5cfefc06d8b6a5d
James Moore <banshee@banshee.com>
2020-12-01 21:56:30 -0800

    [Testing] Add a github action to run integration tests (#320)
    * Add a github action to run integration tests
    
    * Adjust gas price to 300000
    
    Co-authored-by: James Moore <james@sifchain.finance>

---
+++ test/integration/integration-entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# Sifnode entrypoint.
+#
+
+#
+# Daemon.
+#
+start_daemon() {
+  sifnoded start --rpc.laddr tcp://0.0.0.0:26657
+}
+
+#
+# Rest server.
+#
+start_rest_server() {
+  sifnodecli rest-server --laddr tcp://0.0.0.0:1317 &
+}
+
+#
+# Start relayer.
+#
+start_relayer() {
+  wait_for_rpc
+  ebrelayer init tcp://0.0.0.0:26657 "$ETHEREUM_WEBSOCKET_ADDRESS" \
+                                             "$ETHEREUM_CONTRACT_ADDRESS" \
+                                             "$MONIKER" \
+                                             "$MNEMONIC" \
+                                             --chain-id "$CHAINNET" \
+                                             --gas 300000 \
+                                             --gas-adjustment 1.5
+}
+
+#
+# Wait for the RPC port to be active.
+#
+wait_for_rpc() {
+  while ! nc -z localhost 26657; do
+    sleep 15
+  done
+}
+
+# Only start the relayer if enabled.
+if [ "$RELAYER_ENABLED" = "true" ]
+then
+  start_relayer &
+fi
+
+start_rest_server
+start_daemon
