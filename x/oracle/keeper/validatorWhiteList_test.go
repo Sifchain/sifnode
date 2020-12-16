7feb01b77366c1c9f6e5e0202d8ab7c820dac3cf
juniuszhou <junius.zhou@gmail.com>
2020-12-16 09:45:03 +0800

    update test case.

--- x/oracle/keeper/validatorWhiteList_test.go
+++ x/oracle/keeper/validatorWhiteList_test.go
@@ -1,33 +1,27 @@
 package keeper_test
 
 import (
-	"log"
 	"testing"
 
 	oracleKeeper "github.com/Sifchain/sifnode/x/oracle/keeper"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestKeeper_SetValidatorWhiteList(t *testing.T) {
-	ctx, _, _, _, _, _ := oracleKeeper.CreateTestKeepers(t, 0.7, []int64{3, 7}, "")
-	log.Printf("%v", ctx)
-
-	// addresses := keeper.CreateTestAddrs(2)
-	// keeper.SetOracleWhiteList(ctx, addresses[1])
-	// vList := keeper.GetOracleWhiteList(ctx)
-	// assert.Equal(t, len(vList), 2)
-	// assert.True(t, keeper.ExistsOracleWhiteList(ctx))
+	ctx, keeper, _, _, _, _ := oracleKeeper.CreateTestKeepers(t, 0.7, []int64{3, 7}, "")
+	_, addresses := oracleKeeper.CreateTestAddrs(2)
+	keeper.SetOracleWhiteList(ctx, addresses)
+	vList := keeper.GetOracleWhiteList(ctx)
+	assert.Equal(t, len(vList), 2)
+	assert.True(t, keeper.ExistsOracleWhiteList(ctx))
 }
 
-// func TestKeeper_ValidateAddress(t *testing.T) {
-// 	signer := test.GenerateAddress("")
-// 	signer2 := test.GenerateAddress(test.AddressKey3)
-// 	v1 := test.GenerateWhitelistAddress("")
-// 	v2 := test.GenerateWhitelistAddress(test.AddressKey2)
-// 	ctx, keeper := test.CreateTestAppOracle(false)
-// 	keeper.SetOracleWhiteList(ctx, []sdk.AccAddress{v1, v2})
-// 	vList := keeper.GetOracleWhiteList(ctx)
-// 	assert.Equal(t, len(vList), 2)
-// 	assert.True(t, keeper.ExistsOracleWhiteList(ctx))
-// 	assert.True(t, keeper.ValidateAddress(ctx, signer))
-// 	assert.False(t, keeper.ValidateAddress(ctx, signer2))
-// }
+func TestKeeper_ValidateAddress(t *testing.T) {
+	ctx, keeper, _, _, _, _ := oracleKeeper.CreateTestKeepers(t, 0.7, []int64{3, 7}, "")
+	_, addresses := oracleKeeper.CreateTestAddrs(2)
+	keeper.SetOracleWhiteList(ctx, addresses)
+	assert.True(t, keeper.ValidateAddress(ctx, addresses[0]))
+	assert.True(t, keeper.ValidateAddress(ctx, addresses[1]))
+	_, addresses = oracleKeeper.CreateTestAddrs(3)
+	assert.False(t, keeper.ValidateAddress(ctx, addresses[2]))
+}
