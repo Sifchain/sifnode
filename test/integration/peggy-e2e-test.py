6bee2b4f0d7c66fec9366e256d55841c1a655848
James Moore <james@sifchain.finance>
2020-12-15 17:35:13 -0800

    Removed short delay from test2, went back to standard delay time for waiting for balance

--- test/integration/peggy-e2e-test.py
+++ test/integration/peggy-e2e-test.py
@@ -121,8 +121,8 @@ def test_case_2():
     print(f"starting user_eth_balance_before_tx {operator_balance_before_tx}, owner_sifchain_balance_before_tx {owner_sifchain_balance_before_tx}, amount {amount}")
     burn_peggy_coin(owner_addr, operatorAddress, amount)
 
-    wait_for_sifchain_addr_balance(owner_addr, SIF_ETH, owner_sifchain_balance_before_tx - amount, 7)
-    wait_for_eth_balance(operatorAddress, ETHEREUM_ETH, operator_balance_before_tx + amount, 7)
+    wait_for_sifchain_addr_balance(owner_addr, SIF_ETH, owner_sifchain_balance_before_tx - amount)
+    wait_for_eth_balance(operatorAddress, ETHEREUM_ETH, operator_balance_before_tx + amount)
     print("########## Test Case Two Over ##########")
 
 
