1003bb871530d7c6f72aa2b5ab0de76f1fa590d3
Michael Pierce <m.edward.pierce@gmail.com>
2020-12-15 22:59:20 -0600

    core/entities/Notifications: export Notification, Notifications

---
+++ ui/core/src/entities/Notifications.ts
@@ -0,0 +1,9 @@
+
+
+export type Notification = {
+  id?: String // id would be used to remove timeout, may only need to be local type
+  type: "error" | "success" | "info"
+  message: String
+}
+
+export type Notifications = Array<Notification>
