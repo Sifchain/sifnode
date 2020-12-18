9efaadfcdbb82672648051ecc6bef9caf5fd3f18
James Moore <james@sifchain.finance>
2020-12-18 14:44:05 -0800

    Add yet another sleep

--- test/integration/start-integration-env.sh
+++ test/integration/start-integration-env.sh
@@ -117,6 +117,7 @@ echo "export USER1ADDR=$USER1ADDR" >> $envexportfile
 #
 echo run python tests
 docker exec ${CONTAINER_NAME} bash -c ". /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/initial_test_balances.py /network-definition.yml"
+sleep 15
 docker exec ${CONTAINER_NAME} bash -c ". /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/peggy-basic-test-docker.py /network-definition.yml"
 docker exec ${CONTAINER_NAME} bash -c '. /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/peggy-e2e-test.py /network-definition.yml'
 
