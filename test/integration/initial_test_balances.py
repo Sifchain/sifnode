ec01c55fd983d18821ef2516a9e4b01fed8276b0
James Moore <james@sifchain.finance>
2020-12-18 14:34:14 -0800

    Be more generous with sleep length

--- test/integration/initial_test_balances.py
+++ test/integration/initial_test_balances.py
@@ -9,7 +9,7 @@ def setup_currencies():
     transact_ethereum_currency_to_sifchain_addr(owner_addr, "eth", amount_in_wei(10))
     print(f"adding eth to {user1_addr}")
     transact_ethereum_currency_to_sifchain_addr(user1_addr, "eth", amount_in_wei(13))
-    time.sleep(5)
+    time.sleep(15)
     sif_tx_send(owner_addr, user1_addr, amount_in_wei(23), "rowan", network_password)
 
 
