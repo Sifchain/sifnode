c42372d2f0befd58388071b0950c0bbca7001d11
James Moore <james@sifchain.finance>
2020-12-18 16:27:52 -0800

    Require 50 block delay instead of 30

--- cmd/ebrelayer/relayer/ethereum.go
+++ cmd/ebrelayer/relayer/ethereum.go
@@ -194,9 +194,9 @@ func (sub EthereumSub) Start(completionEvent *sync.WaitGroup) {
 			sub.EventsBuffer.AddHeader(newHead.Number, newHead.Hash(), newHead.ParentHash)
 
 			for {
-				thirty := big.NewInt(30)
-				thirty.Add(thirty, sub.EventsBuffer.MinHeight)
-				if thirty.Cmp(newHead.Number) <= 0 {
+				fifty := big.NewInt(50)
+				fifty.Add(fifty, sub.EventsBuffer.MinHeight)
+				if fifty.Cmp(newHead.Number) <= 0 {
 					events := sub.EventsBuffer.GetHeaderEvents()
 					for _, event := range events {
 						err := sub.handleEthereumEvent(event)
