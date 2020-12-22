d6c31e2b9d53778378e3cf5b797ed32d030fec40
James Moore <james@sifchain.finance>
2020-12-22 14:02:50 -0800

    Remove sudo from integration test script

--- test/integration/start-integration-env.sh
+++ test/integration/start-integration-env.sh
@@ -92,7 +92,7 @@ docker exec ${CONTAINER_NAME} bash -c '. /test/integration/vagrantenv.sh; cd /si
 
 # Rebuild sifchain, but this time don't use validators
 
-sudo rm -rf $NETWORKDIR && mkdir $NETWORKDIR
+rm -rf $NETWORKDIR && mkdir $NETWORKDIR
 ADD_VALIDATOR_TO_WHITELIST= bash ${BASEDIR}/test/integration/setup_sifchain.sh && . $envexportfile
 
 docker exec ${CONTAINER_NAME} bash -c ". /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/no_whitelisted_validators.py /network-definition.yml"
