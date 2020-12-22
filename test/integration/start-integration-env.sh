afd855e95700cc1e94e96fadade9788045cfb02e
James Moore <banshee@banshee.com>
2020-12-21 18:38:10 -0800

    Add support for calling sifnoded add-genesis-validators to integration tests (#416)
    * Add support for calling sifnoded add-genesis-validators to integration tests
    
    * Add data/sifchaintxs.txt to help diagnose sifchain problems in integration tests
    
    * Capture more data after an integration test run
    
    * Fix typo in debug output
    
    Co-authored-by: James Moore <james@sifchain.finance>

--- test/integration/start-integration-env.sh
+++ test/integration/start-integration-env.sh
@@ -3,14 +3,11 @@
 
 set -e # exit on any failure
 
-# add 18 zeros to a number
-to_wei () { echo "${1}000000000000000000" ; }
-to_json () { ruby -ryaml -rjson -e "puts YAML::load(STDIN.read).to_json"; }
-
 BASEDIR=$(pwd)/$(dirname $0)/../..
-NETWORKDIR=$BASEDIR/deploy/networks
 
-envexportfile=$BASEDIR/test/integration/vagrantenv.sh
+. ${BASEDIR}/test/integration/shell_utilities.sh
+
+export envexportfile=$BASEDIR/test/integration/vagrantenv.sh
 rm -f $envexportfile
 
 export CONTAINER_NAME="integration_sifnode1_1"
@@ -18,11 +15,13 @@ echo "export CONTAINER_NAME=$CONTAINER_NAME" >> $envexportfile
 
 echo "export BASEDIR=$BASEDIR" >> $envexportfile
 
+NETWORKDIR=$BASEDIR/deploy/networks
+echo "export NETWORKDIR=$NETWORKDIR" >> $envexportfile
+
 #
 # Remove prior generations Config
 #
-echo "apologies for this sudo, it is to delete non-persisent cryptographic keys that usually has enhanced permissions"
-sudo rm -rf $NETWORKDIR && mkdir $NETWORKDIR
+rm -rf $NETWORKDIR && mkdir $NETWORKDIR
 rm -rf ${BASEDIR}/smart-contracts/build ${BASEDIR}/smart-contracts/.openzeppelin
 make -C ${BASEDIR} install
 
@@ -31,14 +30,13 @@ cd $BASEDIR/smart-contracts
 
 # Startup ganache-cli (https://github.com/trufflesuite/ganache)
 
+cp $BASEDIR/test/integration/.env.ciExample .env
+
 yarn --cwd $BASEDIR/smart-contracts install
 export YARN_CACHE_DIR=$(yarn cache dir)
 echo "export YARN_CACHE_DIR=$YARN_CACHE_DIR" >> $envexportfile
 
-
 docker-compose --project-name genesis -f $BASEDIR/test/integration/docker-compose-ganache.yml up -d --force-recreate
-
-cp $BASEDIR/test/integration/.env.ciExample .env
 
 # https://www.trufflesuite.com/docs/truffle/overview
 # and note that truffle migrate and truffle deploy are the same command
@@ -63,46 +61,9 @@ if [ -z "BRIDGE_BANK_ADDRESS" ]; then
 fi
 echo "export BRIDGE_BANK_ADDRESS=$BRIDGE_BANK_ADDRESS" >> $envexportfile
 
-#
-# scaffold and boot the dockerized localnet
-#
-BASEDIR=${BASEDIR} rake genesis:network:scaffold['localnet']
-# see deploy/rake/genesis.rake for the description of the args to genesis:network:boot
-# :chainnet, :eth_bridge_registry_address, :eth_keys, :eth_websocket
-BASEDIR=${BASEDIR} rake genesis:network:boot["localnet,${ETHEREUM_CONTRACT_ADDRESS},c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3,ws://192.168.2.6:7545/"]
+ADD_VALIDATOR_TO_WHITELIST=1 bash ${BASEDIR}/test/integration/setup_sifchain.sh && . $envexportfile
 
-sleep 15
-
-#
-# Wait for the Websocket subscriptions to be initialized (like 10 seconds)
-#
-docker logs -f ${CONTAINER_NAME} | grep -m 1 "Subscribed"
-
-# We need to forward the port used by ganache, since adding new network didn't allow
-# using the cli
-docker exec ${CONTAINER_NAME} bash -c "bash /test/integration/start-ganache-port-forwarding.sh"
 docker exec ${CONTAINER_NAME} bash -c "cd /smart-contracts && yarn install"
-
-# those rake commands generate yaml that provides useful usernames and passwords
-# wait for it to appear
-
-NETDEF=$NETWORKDIR/network-definition.yml
-echo "export NETDEF=$NETDEF" >> $envexportfile
-while [ ! -f $NETWORKDIR/network-definition.yml ]
-do
-  sleep 2
-done
-
-export MONIKER=$(cat ${NETWORKDIR}/network-definition.yml | to_json | jq '.[0].moniker')
-echo "export MONIKER=$MONIKER" >> $envexportfile
-export MONIKER_ADDR=$(cat ${NETWORKDIR}/network-definition.yml | to_json | jq '.[0].moniker')
-echo "export MONIKER=$MONIKER" >> $envexportfile
-
-OWNER_PASSWORD=$(cat $NETDEF | yq r - ".password")
-echo "export OWNER_PASSWORD=$OWNER_PASSWORD" >> $envexportfile
-
-OWNER_ADDR=$(cat $NETDEF | yq r - ".address")
-echo "export OWNER_ADDR=$OWNER_ADDR" >> $envexportfile
 
 #
 # Add keys for a second account to test functions against
@@ -116,11 +77,16 @@ echo "export USER1ADDR=$USER1ADDR" >> $envexportfile
 # Run the python tests
 #
 echo run python tests
+
 docker exec ${CONTAINER_NAME} bash -c ". /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/initial_test_balances.py /network-definition.yml"
 sleep 15
 docker exec ${CONTAINER_NAME} bash -c ". /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/peggy-basic-test-docker.py /network-definition.yml"
 docker exec ${CONTAINER_NAME} bash -c '. /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/peggy-e2e-test.py /network-definition.yml'
 
-# killing script will not end network use stop-integration-env.sh for that
-# and note that we just allow the github actions environment to be cleaned
-# up by their scripts
+# Uncomment these tests in PR 378
+# Rebuild sifchain, but this time don't use validators
+
+#sudo rm -rf $NETWORKDIR && mkdir $NETWORKDIR
+#ADD_VALIDATOR_TO_WHITELIST= bash ${BASEDIR}/test/integration/setup_sifchain.sh && . $envexportfile
+#
+#docker exec ${CONTAINER_NAME} bash -c ". /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/no_whitelisted_validators.py /network-definition.yml"
