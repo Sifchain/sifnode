d9b3b9b074dfd6d18cea5656967b5b1c3621edd7
James Moore <james@sifchain.finance>
2020-12-22 14:30:42 -0800

    sudo is required to remove network directory for integration test

--- test/integration/start-integration-env.sh
+++ test/integration/start-integration-env.sh
@@ -28,7 +28,7 @@ echo "export NETWORKDIR=$NETWORKDIR" >> $envexportfile
 #
 # Remove prior generations Config
 #
-rm -rf $NETWORKDIR && mkdir $NETWORKDIR
+sudo rm -rf $NETWORKDIR && mkdir $NETWORKDIR
 rm -rf ${BASEDIR}/smart-contracts/build ${BASEDIR}/smart-contracts/.openzeppelin
 make -C ${BASEDIR} install
 
@@ -92,7 +92,7 @@ docker exec ${CONTAINER_NAME} bash -c '. /test/integration/vagrantenv.sh; cd /si
 
 # Rebuild sifchain, but this time don't use validators
 
-rm -rf $NETWORKDIR && mkdir $NETWORKDIR
+sudo rm -rf $NETWORKDIR && mkdir $NETWORKDIR
 ADD_VALIDATOR_TO_WHITELIST= bash ${BASEDIR}/test/integration/setup_sifchain.sh && . $envexportfile
 
 docker exec ${CONTAINER_NAME} bash -c ". /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/no_whitelisted_validators.py /network-definition.yml"
