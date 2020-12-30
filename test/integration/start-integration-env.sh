d6daff2175cc718592cd582b11880e141900288a
James Moore <banshee@banshee.com>
2020-12-29 16:35:40 -0800

    [Peggy] Integration tests - test chain rollback (#421)
    * Integration tests - stop using docker for sifchain to simplify starting and stopping individual components.  Test chain rollback.
    
    * Add block advances and debugging printfs
    
    * Better test output
    
    Co-authored-by: James Moore <james@sifchain.finance>

--- test/integration/start-integration-env.sh
+++ test/integration/start-integration-env.sh
@@ -2,6 +2,7 @@
 # must run from the root directory of the sifnode tree
 
 set -e # exit on any failure
+set -o pipefail
 
 BASEDIR=$(pwd)/$(dirname $0)/../..
 
@@ -9,27 +10,23 @@ BASEDIR=$(pwd)/$(dirname $0)/../..
 
 export envexportfile=$BASEDIR/test/integration/vagrantenv.sh
 rm -f $envexportfile
-echo "export envexportfile=$envexportfile" >> $envexportfile
 
-# datadir contains all the telemetry about the run; docker logs, etc
-export datadir=$BASEDIR/test/integration/vagrant/data
-echo "export datadir=$datadir" >> $envexportfile
+set_persistant_env_var ETHEREUM_PRIVATE_KEY c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3 $envexportfile
+set_persistant_env_var BASEDIR $(fullpath $BASEDIR) $envexportfile
+set_persistant_env_var SIFCHAIN_BIN $BASEDIR/cmd $envexportfile
+set_persistant_env_var envexportfile $(fullpath $envexportfile) $envexportfile
+set_persistant_env_var TEST_INTEGRATION_DIR ${BASEDIR}/test/integration $envexportfile
+set_persistant_env_var SMART_CONTRACTS_DIR ${BASEDIR}/smart-contracts $envexportfile
+set_persistant_env_var datadir ${TEST_INTEGRATION_DIR}/vagrant/data $envexportfile
+set_persistant_env_var CONTAINER_NAME integration_sifnode1_1 $envexportfile
+set_persistant_env_var NETWORKDIR $BASEDIR/deploy/networks $envexportfile
+set_persistant_env_var GANACHE_DB_DIR $(mktemp -d --tmpdir ganachedb.XXXX) $envexportfile
+set_persistant_env_var ETHEREUM_WEBSOCKET_ADDRESS ws://localhost:7545/ $envexportfile
+set_persistant_env_var CHAINNET localnet $envexportfile
 
-bash $BASEDIR/test/integration/start_watchers.sh
+mkdir -p $datadir
 
-export CONTAINER_NAME="integration_sifnode1_1"
-echo "export CONTAINER_NAME=$CONTAINER_NAME" >> $envexportfile
-
-echo "export BASEDIR=$BASEDIR" >> $envexportfile
-
-NETWORKDIR=$BASEDIR/deploy/networks
-echo "export NETWORKDIR=$NETWORKDIR" >> $envexportfile
-
-#
-# Remove prior generations Config
-#
-sudo rm -rf $NETWORKDIR && mkdir $NETWORKDIR
-rm -rf ${BASEDIR}/smart-contracts/build ${BASEDIR}/smart-contracts/.openzeppelin
+#rm -rf ${BASEDIR}/smart-contracts/build ${BASEDIR}/smart-contracts/.openzeppelin
 make -C ${BASEDIR} install
 
 # ===== Everything from here on down is executed in the $BASEDIR/smart-contracts directory
@@ -37,62 +34,41 @@ cd $BASEDIR/smart-contracts
 
 # Startup ganache-cli (https://github.com/trufflesuite/ganache)
 
-cp $BASEDIR/test/integration/.env.ciExample .env
+cp ${TEST_INTEGRATION_DIR}/.env.ciExample .env
 
 yarn --cwd $BASEDIR/smart-contracts install
-export YARN_CACHE_DIR=$(yarn cache dir)
-echo "export YARN_CACHE_DIR=$YARN_CACHE_DIR" >> $envexportfile
+#set_persistant_env_var YARN_CACHE_DIR $(yarn cache dir) $envexportfile
 
-docker-compose --project-name genesis -f $BASEDIR/test/integration/docker-compose-ganache.yml up -d --force-recreate
+# Uses GANACHE_DB_DIR for the --db argument to the chain
+docker-compose --project-name genesis -f ${TEST_INTEGRATION_DIR}/docker-compose-ganache.yml up -d --force-recreate
 
 # https://www.trufflesuite.com/docs/truffle/overview
 # and note that truffle migrate and truffle deploy are the same command
-truffle compile
 truffle deploy --network develop --reset
+
 # ETHEREUM_CONTRACT_ADDRESS is used for the BridgeRegistry address in many places, so we
 # set it and BRIDGE_REGISTRY_ADDRESS to the same value
-BRIDGE_REGISTRY_ADDRESS=$(cat $BASEDIR/smart-contracts/build/contracts/BridgeRegistry.json | jq '.networks["5777"].address')
-ETHEREUM_CONTRACT_ADDRESS=$BRIDGE_REGISTRY_ADDRESS
-if [ -z "$ETHEREUM_CONTRACT_ADDRESS" ]; then
-  echo ETHEREUM_CONTRACT_ADDRESS cannot be empty
-  exit 1
-fi
-echo "export ETHEREUM_CONTRACT_ADDRESS=$ETHEREUM_CONTRACT_ADDRESS" >> $envexportfile
 echo "# BRIDGE_REGISTRY_ADDRESS and ETHEREUM_CONTRACT_ADDRESS are synonyms">> $envexportfile
-echo "export BRIDGE_REGISTRY_ADDRESS=$BRIDGE_REGISTRY_ADDRESS" >> $envexportfile
+set_persistant_env_var BRIDGE_REGISTRY_ADDRESS $(cat $BASEDIR/smart-contracts/build/contracts/BridgeRegistry.json | jq -r '.networks["5777"].address') $envexportfile required
+set_persistant_env_var ETHEREUM_CONTRACT_ADDRESS $BRIDGE_REGISTRY_ADDRESS $envexportfile required
 
-export BRIDGE_BANK_ADDRESS=$(cat $BASEDIR/smart-contracts/build/contracts/BridgeBank.json | jq '.networks["5777"].address')
-if [ -z "BRIDGE_BANK_ADDRESS" ]; then
-  echo BRIDGE_BANK_ADDRESS cannot be empty
-  exit 1
-fi
-echo "export BRIDGE_BANK_ADDRESS=$BRIDGE_BANK_ADDRESS" >> $envexportfile
+set_persistant_env_var BRIDGE_BANK_ADDRESS $(cat $BASEDIR/smart-contracts/build/contracts/BridgeBank.json | jq -r '.networks["5777"].address') $envexportfile required
 
-ADD_VALIDATOR_TO_WHITELIST=1 bash ${BASEDIR}/test/integration/setup_sifchain.sh && . $envexportfile
-
-docker exec ${CONTAINER_NAME} bash -c "cd /smart-contracts && yarn install"
+ADD_VALIDATOR_TO_WHITELIST=true bash ${BASEDIR}/test/integration/setup_sifchain.sh && . $envexportfile
 
 #
 # Add keys for a second account to test functions against
-#
-docker exec ${CONTAINER_NAME} bash -c "/test/integration/add-second-account.sh"
 
-export USER1ADDR=$(cat $NETDEF | yq r - "[1].address")
-echo "export USER1ADDR=$USER1ADDR" >> $envexportfile
+yes $OWNER_PASSWORD | sifnodecli keys add user1 --home $CHAINDIR/.sifnodecli || true
+yes $OWNER_PASSWORD | sifnodecli keys show user1 --home $CHAINDIR/.sifnodecli >> $NETDEF || true
+cat $NETDEF | to_json > $NETDEF_JSON
+set_persistant_env_var USER1ADDR $(cat $NETDEF_JSON | jq -r ".[1].address") $envexportfile
 
-#
-# Run the python tests
-#
-echo run python tests
+# TODO the python tests use a lot of calls that require the owner password,
+# and they look it up every time.  Need to change that so they just have the
+# password passed in, but for now modify the user's ~/.sifnodecli directly
 
-docker exec ${CONTAINER_NAME} bash -c ". /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/initial_test_balances.py /network-definition.yml"
-sleep 15
-docker exec ${CONTAINER_NAME} bash -c ". /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/peggy-basic-test-docker.py /network-definition.yml"
-docker exec ${CONTAINER_NAME} bash -c '. /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/peggy-e2e-test.py /network-definition.yml'
+rm -rf ~/.sifnodecli
+ln -s $CHAINDIR/.sifnodecli ~
 
-# Rebuild sifchain, but this time don't use validators
-
-sudo rm -rf $NETWORKDIR && mkdir $NETWORKDIR
-ADD_VALIDATOR_TO_WHITELIST= bash ${BASEDIR}/test/integration/setup_sifchain.sh && . $envexportfile
-
-docker exec ${CONTAINER_NAME} bash -c ". /test/integration/vagrantenv.sh; cd /sifnode; SMART_CONTRACTS_DIR=/smart-contracts python3 /test/integration/no_whitelisted_validators.py /network-definition.yml"
+logecho finished $0
