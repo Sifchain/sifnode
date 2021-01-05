3290fe5be4b4832cf3e7bbb2a0fbe7857de1b2f1
Rudi Yardley <contact@rudiyardley.com>
2021-01-05 18:51:20 +1100

    Modify tests to avoid depending on magic numbers

--- ui/core/src/api/EthereumService/EthereumService.test.ts
+++ ui/core/src/api/EthereumService/EthereumService.test.ts
@@ -7,6 +7,17 @@ import JSBI from "jsbi";
 import B from "../../entities/utils/B";
 import { getTestingTokens } from "../../test/utils/getTestingToken";
 
+// ^ NOTE: we have had issues where truffle deploys contracts that cost a different amount of gas in CI versus locally.
+// These test have been altered to be less deterministic as a consequence
+
+function getBalance(balances: AssetAmount[], symbol: string) {
+  const bal = balances.find(
+    ({ asset }) => asset.symbol.toUpperCase() === symbol.toUpperCase()
+  );
+  if (!bal) throw new Error("Symbol not found in balances");
+  return bal;
+}
+
 describe("EthereumService", () => {
   let EthereumService: ReturnType<typeof createEthereumService>;
   let ATK: Asset;
@@ -37,13 +48,14 @@ describe("EthereumService", () => {
 
     const balances = EthereumService.getState().balances;
 
-    expect(balances[0].amount.toString()).toEqual("98954072680000000000");
-    expect(balances[1].toFixed()).toEqual(
-      AssetAmount(ATK, "10000.000000").toFixed()
-    );
-    expect(balances[2].toFixed()).toEqual(
-      AssetAmount(BTK, "10000.000000").toFixed()
-    );
+    const ethAmount = getBalance(balances, "eth").amount;
+    const atkAmount = getBalance(balances, "atk").amount;
+    const btkAmount = getBalance(balances, "btk").amount;
+
+    // 98xxxxxxxxxxxxxxxxxx = 98ish eth ^ (see above)
+    expect(/^98\d{18}/.test(ethAmount.toString())).toBe(true);
+    expect(atkAmount.toString()).toEqual("10000000000");
+    expect(btkAmount.toString()).toEqual("10000000000");
   });
 
   test("isConnected", async () => {
@@ -55,13 +67,11 @@ describe("EthereumService", () => {
   test("transfer ERC-20 to smart contract", async () => {
     await EthereumService.connect();
     const state = EthereumService.getState();
-    const origBalanceAccount0 = state.balances;
 
-    expect(
-      origBalanceAccount0
-        .find(({ asset: { symbol } }) => symbol.toUpperCase() === "ATK")
-        ?.toFixed()
-    ).toEqual("10000.000000");
+    const balances = state.balances;
+    const account0AtkAmount = getBalance(balances, "atk").amount;
+
+    expect(account0AtkAmount.toString()).toEqual("10000000000");
 
     await EthereumService.transfer({
       amount: B("10.000000", ATK.decimals),
@@ -69,54 +79,36 @@ describe("EthereumService", () => {
       asset: ATK,
     });
 
-    const balanceAccount0 = await EthereumService.getBalance();
-    const balanceAccount1 = await EthereumService.getBalance(state.accounts[1]);
+    const account0NewAtkAmount = getBalance(
+      await EthereumService.getBalance(),
+      "atk"
+    ).amount;
+    const account1NewAtkAmount = getBalance(
+      await EthereumService.getBalance(state.accounts[1]),
+      "atk"
+    ).amount;
 
-    expect(
-      balanceAccount0
-        .find(({ asset: { symbol } }) => symbol.toUpperCase() === "ATK")
-        ?.toFixed(2)
-    ).toEqual("9990.00");
-
-    expect(
-      balanceAccount1
-        .find(({ asset: { symbol } }) => symbol.toUpperCase() === "ATK")
-        ?.toFixed(2)
-    ).toEqual("10.00");
+    expect(account0NewAtkAmount.toString()).toEqual("9990000000");
+    expect(account1NewAtkAmount.toString()).toEqual("10000000");
   });
 
   test("transfer ETH", async () => {
     await EthereumService.connect();
     const state = EthereumService.getState();
-    const origBalanceAccount0 = await EthereumService.getBalance();
 
-    expect(
-      origBalanceAccount0
-        .find(({ asset: { symbol } }) => symbol.toUpperCase() === "ETH")
-        ?.amount.toString()
-      // ).toEqual(AssetAmount(ETH, "99.95048114").toFixed());
-    ).toEqual("98954072680000000000");
+    const TEN_ETH = JSBI.BigInt(10 * 10 ** 18);
 
     await EthereumService.transfer({
-      amount: JSBI.BigInt(10 * 10 ** 18),
+      amount: TEN_ETH,
       recipient: state.accounts[1],
     });
 
-    const balanceAccount0 = await EthereumService.getBalance();
     const balanceAccount1 = await EthereumService.getBalance(state.accounts[1]);
+    const account1NewBalance = getBalance(balanceAccount1, "ETH").amount;
 
-    expect(
-      balanceAccount0
-        .find(({ asset: { symbol } }) => symbol.toUpperCase() === "ETH")
-        ?.amount.toString()
-    ).toEqual("88953652680000000000"); // Including gas
-
-    expect(
-      balanceAccount1
-        .find(({ asset: { symbol } }) => symbol.toUpperCase() === "ETH")
-        ?.toFixed()
-    ).toEqual("110.000000000000000000");
+    expect(account1NewBalance.toString()).toEqual("110000000000000000000");
   });
+
   it("should disconnect", async () => {
     await EthereumService.disconnect();
     expect(EthereumService.getState().accounts).toEqual([]);
