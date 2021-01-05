ae61913d2935b55f11228f465fe164470e749e67
jedi2002 <tanmay@sifchain.finance>
2021-01-05 16:24:34 -0500

    renamed validator

--- scripts/init.sh
+++ scripts/init.sh
@@ -18,16 +18,15 @@ echo "race draft rival universe maid cheese steel logic crowd fork comic easy tr
 echo "Generating deterministic account - akasha"
 echo "hand inmate canvas head lunar naive increase recycle dog ecology inhale december wide bubble hockey dice worth gravity ketchup feed balance parent secret orchard" | sifnodecli keys add akasha --recover
 
-sifnoded add-genesis-account $(sifnodecli keys show shadowfiend -a) 1000000000rowan,1000000000catk,1000000000cbtk,1000000000ceth,10000000000stake,1000000000cdash
+sifnoded add-genesis-account $(sifnodecli keys show sif -a) 1000000000rowan,1000000000catk,1000000000cbtk,1000000000ceth,10000000000stake,1000000000cdash
 sifnoded add-genesis-account $(sifnodecli keys show akasha -a) 1000000000rowan,1000000000catk,1000000000cbtk,1000000000ceth,100000000000stake,1000000000cdash
-#sifnoded add-genesis-account sif17s95c5jpc6x2l3edwh4dm8yhac68yru7a7kr3x 1000000000rowan,1000000000catk,1000000000cbtk,1000000000ceth,100000000000stake,1000000000cdash
 
 sifnoded add-faucet 10000000000000000rowan,10000000000000000ceth,10000000000000000chot,10000000000000000clink,10000000000000000cusdc,10000000000000000cusdt
 
 sifnoded add-genesis-clp-admin $(sifnodecli keys show sif -a)
 sifnoded add-genesis-clp-admin $(sifnodecli keys show akasha -a)
 
-sifnoded  add-genesis-validators $(sifnodecli keys show shadowfiend -a --bech val)
+sifnoded  add-genesis-validators $(sifnodecli keys show sif -a --bech val)
 
 sifnoded gentx --name sif --keyring-backend test
 
