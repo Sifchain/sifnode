e9b20a9775cb3657947f4b5773b999013ec527a1
Rudi Yardley <contact@rudiyardley.com>
2021-01-09 23:24:42 +1100

    Wip: Added new coins for demo

--- ui/app/src/components/shared/AssetList.vue
+++ ui/app/src/components/shared/AssetList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="asset-list">
-    <div class="line" v-for="item in items" :key="item.asset.symbol">
+    <div class="line" v-for="item in filteredItems" :key="item.asset.symbol">
       <AssetItem class="token" :symbol="item.asset.symbol" />
       <div class="amount">{{ item.amount }}</div>
       <div v-if="!!item.amount" class="action">
@@ -15,12 +15,23 @@ import { PropType, defineComponent } from "vue";
 import AssetItem from "@/components/shared/AssetItem.vue";
 
 import { Asset } from "ui-core";
+import { computed } from "@vue/reactivity";
 export default defineComponent({
   components: {
     AssetItem,
   },
   props: {
     items: { type: Array as PropType<{ amount: string; asset: Asset }[]> },
+  },
+  setup(props) {
+    return {
+      filteredItems: computed(() =>
+        props.items?.filter((item) => {
+          // Remove ATK and BTK as these are test tokens
+          return !["atk", "btk", "catk", "cbtk"].includes(item.asset.symbol);
+        })
+      ),
+    };
   },
 });
 </script>
