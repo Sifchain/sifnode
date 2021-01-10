aba22b19927e2f1fcdbac3b18c4088674c5a1b52
Michael Pierce <m.edward.pierce@gmail.com>
2021-01-09 19:04:08 -0600

    ui/app/KeplrWalletPanel.vue

---
+++ ui/app/src/components/wallet/KeplrWalletPanel.vue
@@ -0,0 +1,65 @@
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+import { useCore } from "@/hooks/useCore";
+import BalanceTable from "./BalanceTable.vue";
+import SifButton from "@/components/shared/SifButton.vue";
+
+function useKeplrWallet() {
+  const { store, actions } = useCore();
+  async function handleDisconnectClicked() {
+    await actions.wallet.disconnectWallet();
+  }
+  async function handleConnectClicked() {
+    try{
+      await actions.wallet.connectToWallet();
+    } catch (error) {
+      console.log('ui', error)
+    }
+  }
+  const address = computed(() => store.wallet.sif.address);
+  const balances = computed(() => store.wallet.sif.balances);
+  const connected = computed(() => store.wallet.sif.isConnected);
+  return {
+    address,
+    balances,
+    connected,
+    handleConnectClicked,
+    handleDisconnectClicked,
+  };
+}
+export default defineComponent({
+  name: "KeplrWalletController",
+  components: { BalanceTable, SifButton },
+  setup() {
+    const {
+      address,
+      balances,
+      connected,
+      handleConnectClicked,
+      handleDisconnectClicked,
+    } = useKeplrWallet();
+    return {
+      address,
+      balances,
+      connected,
+      handleConnectClicked,
+      handleDisconnectClicked,
+    };
+  },
+});
+</script>
+
+<template>
+  <div class="wrapper">
+    <div v-if="connected">
+      <p>{{ address }}</p>
+      <BalanceTable :balances="balances" />
+      <SifButton secondary @click="handleDisconnectClicked"
+        >Disconnect Keplr</SifButton
+      >
+    </div>
+    <SifButton secondary v-else @click="handleConnectClicked"
+      >Connect to Keplr</SifButton
+    >
+  </div>
+</template>
