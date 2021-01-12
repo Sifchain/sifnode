50471597c51be0415bd31a3ab507f566a8d3982d
juniuszhou <junius.zhou@gmail.com>
2021-01-12 13:46:43 +0800

    fix linter issue.

--- x/ethbridge/keeper/keeper_test.go
+++ x/ethbridge/keeper/keeper_test.go
@@ -191,7 +191,8 @@ func TestProcessBurn(t *testing.T) {
 
 	receiverCoins := bankKeeper.GetCoins(ctx, cosmosReceivers[0])
 	require.Equal(t, receiverCoins, sdk.Coins{})
-	err := keeper.ProcessBurn(ctx, cosmosReceivers[0], cosmosSenderSequence++, coins)
+	cosmosSenderSequence++
+	err := keeper.ProcessBurn(ctx, cosmosReceivers[0], cosmosSenderSequence, coins)
 	require.Error(t, err)
 	require.True(t, strings.Contains(err.Error(), "insufficient account funds"))
 
@@ -206,8 +207,8 @@ func TestProcessBurn(t *testing.T) {
 	claimString := string(claimBytes)
 	err = keeper.ProcessSuccessfulClaim(ctx, claimString)
 	require.NoError(t, err)
-
-	err = keeper.ProcessBurn(ctx, cosmosReceivers[0], cosmosSenderSequence++, coins)
+	cosmosSenderSequence++
+	err = keeper.ProcessBurn(ctx, cosmosReceivers[0], cosmosSenderSequence, coins)
 	require.NoError(t, err)
 	// lock stake
 
@@ -224,7 +225,8 @@ func TestProcessLock(t *testing.T) {
 	require.Equal(t, receiverCoins, sdk.Coins{})
 
 	coins := sdk.NewCoins(sdk.NewCoin("stake", amount))
-	err := keeper.ProcessLock(ctx, cosmosReceivers[0], cosmosSenderSequence++, coins)
+	cosmosSenderSequence++
+	err := keeper.ProcessLock(ctx, cosmosReceivers[0], cosmosSenderSequence, coins)
 	require.True(t, strings.Contains(err.Error(), "insufficient account funds"))
 
 	//process successful claim to get stake
@@ -241,8 +243,8 @@ func TestProcessLock(t *testing.T) {
 
 	receiverCoins = bankKeeper.GetCoins(ctx, cosmosReceivers[0])
 	require.Equal(t, receiverCoins.String(), "10stake")
-
-	err = keeper.ProcessLock(ctx, cosmosReceivers[0], cosmosSenderSequence++, coins)
+	cosmosSenderSequence++
+	err = keeper.ProcessLock(ctx, cosmosReceivers[0], cosmosSenderSequence, coins)
 	require.NoError(t, err)
 
 	receiverCoins = bankKeeper.GetCoins(ctx, cosmosReceivers[0])
