3f5587f4adb0846b16c68ad5334671673a1d8e83
Tanmay <tanmay@sifchain.finance>
2021-01-18 23:35:33 -0500

    fixing swaps

--- x/clp/handler.go
+++ x/clp/handler.go
@@ -294,30 +294,17 @@ func handleMsgSwap(ctx sdk.Context, keeper Keeper, msg MsgSwap) (*sdk.Result, er
 	receivedAsset := msg.ReceivedAsset
 	// Get native asset
 	nativeAsset := types.GetSettlementAsset()
-	// If its one swap , this pool would be RWN:RWN ( Ex User sends RWN wants ETH)
-	// If its two swap . this pool would be RWN:EXTERNAL1 ( Ex User sends ETH wants XCT , ETH is EXTERNAL1)
-	//CASE 1 : RWN:ETH
-	//CASE 2 : RWN:ETH
+
 	inPool, outPool := types.Pool{}, types.Pool{}
-	err := errors.New("Blank")
+	err := errors.New("Swap Error")
+	// If sending rowan ,deduct directly from the Native balance  instead of fetching from rowan pool
 	if msg.SentAsset != types.GetSettlementAsset() {
 		inPool, err = keeper.GetPool(ctx, msg.SentAsset.Symbol)
 		if err != nil {
 			return nil, errors.Wrap(types.ErrPoolDoesNotExist, msg.SentAsset.String())
 		}
 	}
-	// If its one swap , this pool would be RWN:EXTERNAL ( Ex User sends RWN wants ETH , ETH IS EXTERNAL )
-	// If its two swap . this pool would be RWN:EXTERNAL2 ( Ex User sends ETH wants XCT , XCT is EXTERNAL2)
-	//CASE 1 : RWN:ETH
-	//CASE 2 : RWN:XCT
-	//outPool, err := keeper.GetPool(ctx, msg.ReceivedAsset.Symbol)
-	//if err != nil {
-	//	return nil, errors.Wrap(types.ErrPoolDoesNotExist, msg.ReceivedAsset.String())
-	//}
 
-	// Deducting Balance from the user , Sent Asset is the asset the user is sending to the Pool
-	// Case 1 . Deducting his RWN and adding to RWN:ETH pool
-	// Case 2 , Deduction his ETH and adding to RWN:ETH pool
 	sentAmountInt, ok := keeper.ParseToInt(sentAmount.String())
 
 	if !ok {
@@ -345,7 +332,7 @@ func handleMsgSwap(ctx sdk.Context, keeper Keeper, msg MsgSwap) (*sdk.Result, er
 		liquidityFee = liquidityFee.Add(lp)
 		tradeSlip = tradeSlip.Add(ts)
 	}
-
+	// If receiving  rowan , deduct directly from the Native balance  instead of fetching from rowan pool
 	if msg.ReceivedAsset == types.GetSettlementAsset() {
 		outPool, err = keeper.GetPool(ctx, msg.SentAsset.Symbol)
 		if err != nil {
