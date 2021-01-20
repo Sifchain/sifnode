2e2a387ba02d998933a263869f73d3843b553c88
intl-man <theinternationalman079@gmail.com>
2021-01-20 16:12:37 +1300

    Cleanup/fixes.

--- deploy/release/setup-go.sh
+++ deploy/release/setup-go.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -eux
 
-GO_LINUX_PACKAGE_URL="https://dl.google.com/go/go1.15.1.linux-amd64.tar.gz"
+GO_LINUX_PACKAGE_URL="https://dl.google.com/go/go1.14.linux-amd64.tar.gz"
+if [[ ${INPUT_GOVERSION} == "1.13" ]]; then
+    GO_LINUX_PACKAGE_URL="https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz"
+elif [[ ${INPUT_GOVERSION} == http* ]]; then
+    GO_LINUX_PACKAGE_URL=${INPUT_GOVERSION}
+fi
 
 wget --progress=dot:mega ${GO_LINUX_PACKAGE_URL} -O go-linux.tar.gz
 tar -zxf go-linux.tar.gz
