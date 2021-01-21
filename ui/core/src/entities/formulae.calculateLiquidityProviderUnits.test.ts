5ad63d9871ddbac2e386944e196a2660eba83ca5
Rudi Yardley <contact@rudiyardley.com>
2021-01-21 15:34:09 +1100

    Remove comments

--- ui/core/src/entities/formulae.calculateLiquidityProviderUnits.test.ts
+++ ui/core/src/entities/formulae.calculateLiquidityProviderUnits.test.ts
@@ -1,15 +1,10 @@
-// import { getTestingTokens } from "../test/utils/getTestingToken";
-// import { AssetAmount } from "./AssetAmount";
 import { calculatePoolUnits } from "./formulae";
 import { Fraction } from "./fraction/Fraction";
 
 import tables from "../../../../test/test-tables/sample_pool_units.json";
 
-// const [ATK, ROWAN] = getTestingTokens(["ATK", "ROWAN"]);
-
 // Use this list to only run specific tests
-const filterList: number[] = [59];
-// const filterList: number[] = [];
+const filterList: number[] = [];
 
 tables.PoolUnits.forEach(({ r, a, R, A, P, expected }, index) => {
   if (filterList.length === 0 || filterList.includes(index)) {
