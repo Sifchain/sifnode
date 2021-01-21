17cf1f43f9d014f45f62a5a0d58947003c497d40
Elliot <34463580+ElliotFriedman@users.noreply.github.com>
2021-01-21 02:41:14 -0500

    Feature/capital letters in token (#544)
    * Integration tests for new tokens
    
    * Add test_transfer_tokens_with_a_capital_letter_in_the_name to integration test
    
    * Integration test DEBUG logging level is now less verbose
    
    * Add test for "Foo" currency
    
    * Coin denom fix.
    
    * work in progress
    
    * Integration tests need to use a lower-case symbol
    
    * fix for capitalizations ready
    
    * Revert "Coin denom fix."
    
    This reverts commit f78e25629d31d93485e204b63f709408e41804a2.
    
    * test_transfer_tokens_with_a_capital_letter_in_the_name moved to its own file
    
    * Test was using the string erowan instead of the token address, fixed
    
    * test_foo should run as part of execute_integration_tests_against_test_chain.sh
    
    Co-authored-by: James Moore <james@sifchain.finance>
    Co-authored-by: intl-man <theinternationalman079@gmail.com>

---
+++ smart-contracts/scripts/test/enableNewToken.js
@@ -0,0 +1,65 @@
+const BN = require('bn.js');
+
+module.exports = async (cb) => {
+    const Web3 = require("web3");
+
+    const sifchainUtilities = require('./sifchainUtilities')
+    const contractUtilites = require('./contractUtilities');
+
+    const logging = sifchainUtilities.configureLogging(this);
+
+    const argv = sifchainUtilities.processArgs(this, {
+        ...sifchainUtilities.sharedYargOptions,
+        ...sifchainUtilities.bridgeBankAddressYargOptions,
+        ...sifchainUtilities.symbolYargOption,
+        ...sifchainUtilities.amountYargOption,
+        'limit_amount': {
+            describe: 'an amount',
+            demandOption: true
+        },
+    });
+
+    const ethMultiplier = (new BN("10", 10)).pow(new BN(18));
+
+    const amount = new BN(argv.amount, 10);
+    const limitAmount = new BN(argv.limit_amount, 10);
+
+    const BridgeToken = artifacts.require("BridgeToken");
+    const newToken = await BridgeToken.new(argv.symbol);
+
+    const bridgeBankContract = await contractUtilites.buildContract(this, argv, "BridgeBank", argv.bridgebank_address);
+
+    const accounts = await web3.eth.getAccounts();
+    const operator_address = accounts[0]; // operator
+
+    logging.info(`bridgeBankContract.updateEthWhiteList ${newToken.address}, true, from ${operator_address}`);
+    const updateWhiteListResult = await bridgeBankContract.updateEthWhiteList(newToken.address, true, {
+        from: operator_address
+    });
+    logging.info(`bridgeBankContract.updateEthWhiteList result ${JSON.stringify(updateWhiteListResult)}`);
+
+    // Update the lock/burn limit for this token
+    await bridgeBankContract.updateTokenLockBurnLimit(newToken.address, limitAmount, {
+        from: operator_address
+    });
+
+    const token_destination = accounts[0];
+        
+    await newToken.mint(token_destination, amount.toString(), {
+        from: operator_address
+    });
+
+    await newToken.approve(bridgeBankContract.address, amount.toString(), {
+        from: token_destination
+    });
+
+    const result = {
+        destination: token_destination,
+        "amount": amount.toString(),
+        "newtoken_address": newToken.address,
+        "newtoken_symbol": argv.symbol,
+    }
+    console.log(JSON.stringify(result));
+
+    return cb();
+};
--- test/integration/execute_integration_tests_against_test_chain.sh
+++ test/integration/execute_integration_tests_against_test_chain.sh
@@ -15,6 +15,8 @@ logecho $0 starting
 # TODO we should get this from a script, not hardcoded
 operator_address=0xf17f52151EbEF6C7334FAD080c5704D77216b732
 
-ETHEREUM_ADDRESS=$operator_address python3 -m pytest -olog_level=$loglevel -olog_cli=true -v -olog_file=/tmp/log.txt -v \
+ETHEREUM_ADDRESS=$operator_address python3 -m pytest -olog_level=$loglevel -v -olog_file=/tmp/log.txt -v \
   ${TEST_INTEGRATION_PY_DIR}/test_rowan_transfers.py \
+  ${TEST_INTEGRATION_PY_DIR}/test_foo.py \
+  ${TEST_INTEGRATION_PY_DIR}/test_new_currency_transfers.py \
   ${TEST_INTEGRATION_PY_DIR}/test_rollback_chain.py
