39336967e93cbeb36e465fe382f0ce650d6db775
Michael Pierce <m.edward.pierce@gmail.com>
2021-01-21 11:20:08 -0600

    app/router: change to hash based

--- ui/app/src/router/index.ts
+++ ui/app/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 import Swap from "@/views/SwapPage.vue";
 import Pool from "@/views/PoolPage.vue";
@@ -55,7 +55,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
