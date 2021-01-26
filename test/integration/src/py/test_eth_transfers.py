7206f56742317752f487a0ef46b2ed3c80e38001
James Moore <james@sifchain.finance>
2021-01-26 10:14:07 -0800

    Remove bad parameter

--- test/integration/src/py/test_eth_transfers.py
+++ test/integration/src/py/test_eth_transfers.py
@@ -56,4 +56,4 @@ def test_transfer_eth_to_ceth_over_limit():
     invalid_request = copy(request)
     invalid_request.amount = test_utilities.amount_in_wei(35)
     with pytest.raises(JSONDecodeError):
-        burn_lock_functions.transfer_ethereum_to_sifchain(invalid_request, credentials)
+        burn_lock_functions.transfer_ethereum_to_sifchain(invalid_request)
