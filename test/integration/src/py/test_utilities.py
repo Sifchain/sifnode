3043bc4dff2eb956141bd20e73f197a29c78c497
James Moore <james@sifchain.finance>
2021-01-25 16:29:27 -0800

    Added test_parallel_eth_transfers_with_timed_ganache_block.py

--- test/integration/src/py/test_utilities.py
+++ test/integration/src/py/test_utilities.py
@@ -260,6 +260,8 @@ def send_from_sifchain_to_ethereum(transfer_request: EthereumToSifchainTransferR
 # this does not wait for the transaction to complete
 def send_from_ethereum_to_sifchain(transfer_request: EthereumToSifchainTransferRequest):
     direction = "sendBurnTx" if transfer_request.sifchain_symbol == "rowan" else "sendLockTx"
+    # 60000 as the gas number lets us get multiple transactions into a single block
+    # for test/integration/src/py/test_parallel_eth_transfers_with_timed_ganache_block.py
     command_line = f"yarn -s --cwd {transfer_request.smart_contracts_dir} integrationtest:{direction} " \
                    f"--sifchain_address {transfer_request.sifchain_address} " \
                    f"--symbol {transfer_request.ethereum_symbol} " \
@@ -267,7 +269,7 @@ def send_from_ethereum_to_sifchain(transfer_request: EthereumToSifchainTransferR
                    f"--bridgebank_address {transfer_request.bridgebank_address} " \
                    f"--ethereum_address {transfer_request.ethereum_address} " \
                    f"--ethereum_private_key_env_var \"{transfer_request.ethereum_private_key_env_var}\" " \
-                   f""
+                   f"--gas 60000"
     command_line += f"--ethereum_network {transfer_request.ethereum_network} " if transfer_request.ethereum_network else ""
     return run_yarn_command(command_line)
 
