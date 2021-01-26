841128bbd8a197f4988689bde9cddf6c56aeaf8a
Rudi Yardley <contact@rudiyardley.com>
2021-01-26 18:59:54 +1100

    Dont bother with 18 decimal places

--- ui/app/src/views/CreatePoolPage.vue
+++ ui/app/src/views/CreatePoolPage.vue
@@ -210,7 +210,7 @@ export default defineComponent({
           (balance) => balance.asset.symbol === fromSymbol.value
         );
         if (!accountBalance) return;
-        fromAmount.value = accountBalance.toFixed(18);
+        fromAmount.value = accountBalance.toFixed(8);
       },
       shareOfPoolPercent,
       connectedText,
