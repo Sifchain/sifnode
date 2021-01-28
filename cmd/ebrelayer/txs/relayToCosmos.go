53fd8fb2cc803ad942034011ebfcc68fd2c754fa
juniuszhou <junius.zhou@gmail.com>
2021-01-28 08:23:10 +0800

    roll back multiple message solution.

--- cmd/ebrelayer/txs/relayToCosmos.go
+++ cmd/ebrelayer/txs/relayToCosmos.go
@@ -7,41 +7,34 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/Sifchain/sifnode/x/ethbridge"
 	"github.com/Sifchain/sifnode/x/ethbridge/types"
 )
 
 // RelayToCosmos applies validator's signature to an EthBridgeClaim message containing
 // information about an event on the Ethereum blockchain before relaying to the Bridge
-func RelayToCosmos(cdc *codec.Codec, moniker, password string, claims []types.EthBridgeClaim, cliCtx context.CLIContext,
+func RelayToCosmos(cdc *codec.Codec, moniker, password string, claim *types.EthBridgeClaim, cliCtx context.CLIContext,
 	txBldr authtypes.TxBuilder) error {
-	var messages []sdk.Msg
 
-	for _, claim := range claims {
-		// Packages the claim as a Tendermint message
-		msg := ethbridge.NewMsgCreateEthBridgeClaim(claim)
+	msg := ethbridge.NewMsgCreateEthBridgeClaim(*claim)
 
-		err := msg.ValidateBasic()
-		if err != nil {
-			fmt.Println("failed to get message from claim with:", err.Error())
-			continue
-		} else {
-			messages = append(messages, msg)
-		}
+	err := msg.ValidateBasic()
+	if err != nil {
+		fmt.Println("failed to get message from claim with:", err.Error())
+		return err
 	}
 
 	// Prepare tx
-	txBldr, err := utils.PrepareTxBuilder(txBldr, cliCtx)
+	txBldr, err = utils.PrepareTxBuilder(txBldr, cliCtx)
 	if err != nil {
 		return err
 	}
 
 	// Build and sign the transaction
-	txBytes, err := txBldr.BuildAndSign(moniker, password, messages)
+	txBytes, err := txBldr.BuildAndSign(moniker, password, []sdk.Msg{msg})
 	if err != nil {
 		return err
 	}
