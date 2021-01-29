b36d05306d2e35ed716361448df778a3ffadabce
juniuszhou <junius.zhou@gmail.com>
2021-01-29 13:32:32 +0800

    add two more unit test.

---
+++ cmd/ebrelayer/relayer/ethereum_test.go
@@ -0,0 +1,53 @@
+package relayer
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/Sifchain/sifnode/cmd/ebrelayer/types"
+	ethbridge "github.com/Sifchain/sifnode/x/ethbridge/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+)
+
+// EventProcessed check if the event processed by relayer
+func TestEventProcessed(t *testing.T) {
+	var bridgeClaims []types.EthereumBridgeClaim
+	valAddress, _ := sdk.ValAddressFromBech32("sifvaloper1l7hypmqk2yc334vc6vmdwzp5sdefygj250dmpy")
+	bridgeClaims = append(bridgeClaims, types.EthereumBridgeClaim{
+		EthereumSender: common.HexToAddress("0xd88159878c50e4B2b03BB701DD436e4A98D6fBe2"),
+		CosmosSender:   valAddress,
+		Nonce:          sdk.NewInt(int64(1)),
+	})
+
+	processedEvent := types.EthereumEvent{
+		EthereumChainID:       big.NewInt(0),
+		BridgeContractAddress: common.HexToAddress("0xd88159878c50e4B2b03BB701DD436e4A98D6fBe2"),
+		ID:                    [32]byte{},
+		From:                  common.HexToAddress("0xd88159878c50e4B2b03BB701DD436e4A98D6fBe2"),
+		To:                    []byte("sifvaloper1l7hypmqk2yc334vc6vmdwzp5sdefygj250dmpy"),
+		Token:                 common.HexToAddress("0xd88159878c50e4B2b03BB701DD436e4A98D6fBe2"),
+		Symbol:                "rewan",
+		Value:                 big.NewInt(1),
+		Nonce:                 big.NewInt(1),
+		ClaimType:             ethbridge.LockText,
+	}
+
+	require.Equal(t, true, EventProcessed(bridgeClaims, processedEvent))
+
+	notProcessedEvent := types.EthereumEvent{
+		EthereumChainID:       big.NewInt(0),
+		BridgeContractAddress: common.HexToAddress("0xd88159878c50e4B2b03BB701DD436e4A98D6fBe2"),
+		ID:                    [32]byte{},
+		From:                  common.HexToAddress("0xd88159878c50e4B2b03BB701DD436e4A98D6fBe2"),
+		To:                    []byte("sifvaloper1l7hypmqk2yc334vc6vmdwzp5sdefygj250dmpy"),
+		Token:                 common.HexToAddress("0xd88159878c50e4B2b03BB701DD436e4A98D6fBe2"),
+		Symbol:                "rewan",
+		Value:                 big.NewInt(1),
+		Nonce:                 big.NewInt(10),
+		ClaimType:             ethbridge.LockText,
+	}
+
+	require.Equal(t, false, EventProcessed(bridgeClaims, notProcessedEvent))
+}
