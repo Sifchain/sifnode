fefafa87a921f5d6d1b702f115bf80be7a363d98
juniuszhou <junius.zhou@gmail.com>
2021-01-29 13:50:52 +0800

    one more test case.

--- cmd/ebrelayer/relayer/ethereum_test.go
+++ cmd/ebrelayer/relayer/ethereum_test.go
@@ -51,3 +51,15 @@ func TestEventProcessed(t *testing.T) {
 
 	require.Equal(t, false, EventProcessed(bridgeClaims, notProcessedEvent))
 }
+
+// TestNewKeybase test if we can get keybase from moniker, mnemonic and password
+func TestNewKeybase(t *testing.T) {
+	validatorMoniker := "akasha"
+	mnemonic := "race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow"
+	password := ""
+
+	base, info, err := NewKeybase(validatorMoniker, mnemonic, password)
+	require.NotEqual(t, base, nil)
+	require.NotEqual(t, info, nil)
+	require.Equal(t, err, nil)
+}
