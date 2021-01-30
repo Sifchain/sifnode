477b87cc547c3131f5cb313ad2cd0fc207b0e6e0
Rudi Yardley <contact@rudiyardley.com>
2021-01-30 11:57:14 +1100

    Add better debugging for window store

--- ui/app/src/hooks/useCore.ts
+++ ui/app/src/hooks/useCore.ts
@@ -20,7 +20,14 @@ const poolFinder = createPoolFinder(store);
 // expose store on the window so it is easy to inspect
 Object.defineProperty(window, "store", {
   get: function() {
-    return JSON.parse(JSON.stringify(store));
+    return JSON.parse(
+      JSON.stringify(store, function replacer(key, value) {
+        if (value.amount && value.quotient) {
+          return value.toString();
+        }
+        return value;
+      })
+    );
   },
 });
 
