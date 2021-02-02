8c0233605bd8100a316def8ebf1eb71856904be9
Thomas Davis <thomasalwyndavis@gmail.com>
2021-02-02 14:08:14 +1100

    [ui] removed redundant changes to single pool

--- ui/app/src/views/SinglePool.vue
+++ ui/app/src/views/SinglePool.vue
@@ -1,20 +1,17 @@
 <script lang="ts">
 import { defineComponent, PropType, ComputedRef, ref, watch } from "vue";
-import { useRoute } from 'vue-router';
 import { computed, toRefs } from "@vue/reactivity";
-import ArrowIconButton from "@/components/shared/ArrowIconButton.vue";
 import Layout from "@/components/layout/Layout.vue";
 import SifButton from "@/components/shared/SifButton.vue";
 import { getAssetLabel, useAssetItem } from "@/components/shared/utils";
 import { Fraction, LiquidityProvider, Pool, usePoolCalculator, AssetAmount, Asset } from "ui-core";
 import { useCore } from "@/hooks/useCore";
-import Tooltip from "@/components/shared/Tooltip.vue";
-import Icon from "@/components/shared/Icon.vue";
+import { useRoute } from 'vue-router';
 
 const DECIMALS = 5
 
 export default defineComponent({
-  components: { Layout, SifButton, Icon, Tooltip },
+  components: { Layout, SifButton },
   props: {
     accountPool: Object as PropType<{
       lp: LiquidityProvider;
@@ -167,7 +164,6 @@ export default defineComponent({
       </div>
       <div class="section">
         <div class="info">
-          <Tooltip icon="tick"><Icon icon="info-box-grey" /></Tooltip>
           <h3 class="mb-2">Liquidity provider rewards</h3>
           <p class="text--small mb-2">
             Liquidity providers earn a percentage fee on all trades proportional
