d73e89bc7db57e9028347ecfde5458a986c6fd1f
utx0 <utx0@protonmail.com>
2021-02-05 10:56:54 +1100

    Added script header

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-./test/integration/start-integration-env.sh 
+#!/usr/bin/env bash
+./test/integration/start-integration-env.sh
 source test/integration/vagrantenv.sh
 ./test/integration/ganache_start.sh 10
