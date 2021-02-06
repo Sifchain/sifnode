45e735593068067a8496f4212739326e29f2185b
James Moore <james@sifchain.finance>
2021-02-06 11:53:59 -0800

    Integration tests added sifchain_blocks.sh to capture output of sifnodecli q block X for every block

--- test/integration/assemble_data.sh
+++ test/integration/assemble_data.sh
@@ -8,6 +8,7 @@ cp $TEST_INTEGRATION_DIR/vagrantenv.sh ${datadir}/vagrantenv.sh
 ( cd $SMART_CONTRACTS_DIR && npx truffle networks ) > ${datadir}/trufflenetworks.txt
 rsync --delete -a $GANACHE_DB_DIR/ ${datadir}/ganachedb/
 $basedir/sifchain_logs.sh vagrant/data/logs/* > ${datadir}/siftransactions.json
+$basedir/sifchain_blocks.sh vagrant/data/logs/* > ${datadir}/sifblocks.json
 
 tar Ccfz $datadir datafiles.tar.gz .
 cp datafiles.tar.gz datafiles.$(date +%m-%d-%H-%M-%S).tar.gz
