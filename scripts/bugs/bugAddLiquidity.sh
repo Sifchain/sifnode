20340ff36d1e3867aeb07eb39028c36ba2836364
Tanmay <tanmay@sifchain.finance>
2021-02-08 23:43:55 -0500

    added script to replicate bug

--- scripts/bugs/bugAddLiquidity.sh
+++ scripts/bugs/bugAddLiquidity.sh
@@ -11,15 +11,16 @@ rm -rf testlog.log
 cd "$(dirname "$0")"
 
 ./init.sh
-sleep 8
+sleep 5
 sifnoded start >> sifnode.log 2>&1  &
-sleep 8
+sleep 5
 
-yes Y | sifnodecli tx clp create-pool --from sif --symbol cacoin --nativeAmount 1000 --externalAmount 1000 --fees 13000rowan
-sleep 8
+yes Y | sifnodecli tx clp create-pool --from sif --symbol cacoin --nativeAmount 144219482657918838950052 --externalAmount 95306982476314799709783920 --fees 1300000rowan
+sleep 5
 
+sifnodecli q clp pools
 
 echo "adding new liquidity provider"
-sleep 8
-yes Y | sifnodecli tx clp add-liquidity --from akasha --symbol cacoin --nativeAmount 1000000000000000000000 --externalAmount 999990445060966787230000000000 --fees 13000rowan
+sleep 5
+yes Y | sifnodecli tx clp add-liquidity --from akasha --symbol cacoin --nativeAmount 1000000000000000000000 --externalAmount 999990445060966787235986082005 --fees 1300000rowan
 
