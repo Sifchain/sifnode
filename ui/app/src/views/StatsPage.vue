c909330695a72da0f60379d48e2fa6cfa351dec5
Ivan <ivanverch@gmail.com>
2021-02-10 10:14:38 +0200

    Add swap page, add menu item

---
+++ ui/app/src/views/StatsPage.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import Layout from "@/components/layout/Layout.vue";
+import PoolStatsList from "@/components/poolStats/PoolStatsList.vue";
+import PoolStatsListHeader from "@/components/poolStats/PoolStatsListHeader.vue";
+
+export default defineComponent({
+  components: {
+    Layout,
+    PoolStatsList,
+    PoolStatsListHeader,
+  },
+  data() {
+    return {
+      poolData: {},
+    };
+  },
+  async mounted() {
+    const data = await fetch("http://localhost:8010/proxy/default/tokenstats");
+    this.poolData = await data.json();
+  },
+});
+</script>
+
+<template>
+  <div class="layout">
+    <PoolStatsList class="mb-2" :poolData="poolData" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.layout {
+  background: url("../assets/World_Background_opt.jpg");
+  background-size: cover;
+  background-position: bottom center;
+  box-sizing: border-box;
+  padding-top: $header_height;
+  padding-right: 32px;
+  padding-left: 32px;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
