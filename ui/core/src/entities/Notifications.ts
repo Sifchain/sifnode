071e504e5a12fa35da4a95cd3c03f0345bc0ff51
59023g <5235538+59023g@users.noreply.github.com>
2021-02-13 01:45:48 -0600

    core/entitities/Notification: optional loader and detail as TxHash

--- ui/core/src/entities/Notifications.ts
+++ ui/core/src/entities/Notifications.ts
@@ -1,8 +1,11 @@
+import { TxHash } from "./Transaction";
+
 export type Notification = {
   id?: string; // id would be used to remove timeout, may only need to be local type
   type: "error" | "success" | "info";
   message: string;
-  detail?: string;
+  detail?: TxHash;
+  loader?: boolean
 };
 
 export type Notifications = Array<Notification>;
