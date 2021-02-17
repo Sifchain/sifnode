d91dec40c31e9d4d35e8bfedb2ef0010dc080c2f
Ivan <ivanverch@gmail.com>
2021-02-17 22:14:06 +0200

    Fix icon size

--- ui/app/src/components/poolStats/PoolStatsListItem.vue
+++ ui/app/src/components/poolStats/PoolStatsListItem.vue
@@ -113,7 +113,7 @@ export default defineComponent({
   align-items: center;
 
   .image {
-    height: 22px;
+    height: 20px;
     margin-right: 8px;
 
     & > * {
@@ -139,8 +139,8 @@ export default defineComponent({
     background: #aaa;
     box-sizing: border-box;
     border-radius: 16px;
-    height: 22px;
-    width: 22px;
+    height: 20px;
+    width: 20px;
     text-align: center;
   }
 
