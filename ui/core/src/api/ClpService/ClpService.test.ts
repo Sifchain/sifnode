00edc83759d013c4974bac114757e2febb66903d
Rudi Yardley <contact@rudiyardley.com>
2021-02-27 17:14:31 +1100

    Remove mock websocket from test

--- ui/core/src/api/ClpService/ClpService.test.ts
+++ ui/core/src/api/ClpService/ClpService.test.ts
@@ -1,9 +1,3 @@
-// HACK: This mocks a websocket connection. Cannot connect correctly to websockets in Jest
-// TODO: Remove this and connect tothe live websocket
-import WS from "jest-websocket-mock";
-new WS("ws://localhost:26667/websocket");
-// end HACK
-
 import createClpService from ".";
 import { AssetAmount } from "../../entities";
 import { getTestingTokens } from "../../test/utils/getTestingToken";
