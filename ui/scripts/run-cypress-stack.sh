4b96e0a4f698c42a7f66432a5af469946937fc0d
Rudi Yardley <contact@rudiyardley.com>
2021-03-02 22:34:34 +1100

    Try building cypress stack before test

---
+++ ui/scripts/run-cypress-stack.sh
@@ -0,0 +1,7 @@
+
+yarn concurrently -r -k \
+ "yarn chain:eth" \
+ "yarn chain:sif" \
+ "yarn wait-on http-get://localhost:1317/node_info && yarn chain:migrate && yarn chain:peggy" \
+ "yarn wait-on http-get://localhost:1317/node_info tcp:localhost:7545 node_modules/.migrate-complete && yarn app:serve"
+
