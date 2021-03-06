31112cbbc86c6a906cf3ba391073291503f2a96d
Tanmay <tanmay@sifchain.finance>
2021-03-06 00:06:16 -0500

    fixed command

--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -4,7 +4,7 @@ pkill sifnoded
 sleep 5
 sifnoded export --height -1 > exported_state.json
 sleep 1
-sifnoded migrate v0.38 exported_state.json --chain-id new-chain > new-genesis.json
+sifnoded migrate v0.38 exported_state.json --chain-id new-chain > new-genesis.json  2>&1
 sleep 1
 sifnoded unsafe-reset-all
 sleep 1
