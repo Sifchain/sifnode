7fb422e7a466215761091d455115cbbd63af43c8
59023g <5235538+59023g@users.noreply.github.com>
2021-03-09 09:45:08 -0800

    app/RewardsPageNew: copy, button

--- ui/app/src/views/RewardsPageNew.vue
+++ ui/app/src/views/RewardsPageNew.vue
@@ -1,26 +1,31 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Layout from "@/components/layout/Layout.vue";
-
-
-type AccountPool = { lp: LiquidityProvider; pool: Pool };
+import SifButton from "@/components/shared/SifButton.vue";
 
 export default defineComponent({
   components: {
-    Layout
+    Layout,
+    SifButton,
   },
 
-  setup() {
-  },
+  setup() {},
 });
 </script>
 
 <template>
-  <Layout :header="true" title="Rewards"  backLink="/peg">
+  <Layout :header="true" title="Rewards" backLink="/peg">
     <div class="liquidity-container">
       <div class="info">
-        <p class="text--small mb-2">
-          Rewards from Liquidity Mining Program: 49 Rowan YTD
+        <p class="mb-5">
+          Rewards from Liquidity Mining Program:
+        </p>
+        <p class="mb-2">
+          <span class="amount">49 Rowan YTD</span>
+        </p>
+        <SifButton disabled primary class="mb-8">Claim</SifButton>
+        <p class="text--small">
+          At present, you are unable to claim Liquidity Mining rewards.
         </p>
       </div>
     </div>
@@ -29,16 +34,20 @@ export default defineComponent({
 
 <style scoped lang="scss">
 .info {
-    text-align: left;
+  text-align: left;
 }
-.ul {text-decoration: underline}
-.liquidity-container { 
+.ul {
+  text-decoration: underline;
+}
+.amount {font-size: $fs_lg}
+.liquidity-container {
   text-align: left;
   color: $c_gray_700;
   border-top: 1px solid $c_gray_400;
   min-height: 145px;
-  background: white; 
+  background: white;
   padding: 15px;
-  border-radius: 0 0 6px 6px
+  border-radius: 0 0 6px 6px;
+  padding-bottom: 60px
 }
 </style>
