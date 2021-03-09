74df2fcdaa510c6f3ba347967046bbbf35e193d9
59023g <5235538+59023g@users.noreply.github.com>
2021-03-09 09:02:43 -0800

    ui/e2e/gitignore, wipe test

--- ui/e2e/index.test.js
+++ ui/e2e/index.test.js
@@ -1,32 +1,14 @@
-const {chromium} = require("playwright");
+const { chromium } = require("playwright");
 const expect = require("expect");
 
-// todo some kind of config file env mapping
-const targetURL = "https://dex.sifchain.finance"
-const sifAddress = "sif1k3ldrhrtkr6ncgjlu2ltyls9rxqtu9643lknrk"
-const ethAddress = "0xc3b2058C07A865c50eA456131c739E305b000e7D"
-
-let browser;
-let page;
+// https://github.com/NodeFactoryIo/dappeteer/tree/master/src
+const METAMASK_PATH = "./extensions/dmkamcknogkgcdfhhbddcghachkejeap/0.8.1_0";
+const KEPLR_PATH = "./extensions/nkbihfbeogaeaoehlefnkodbefgpgknn/9.1.1_0";
 
 describe("connect to page", () => {
-  beforeAll(async () => {
-    browser = await chromium.launch();
-  });
-  afterAll(async () => {
-    await browser.close();
-  });
-  beforeEach(async () => {
-    page = await browser.newPage();
-  });
-  afterEach(async () => {
-    await page.close();
-  });
-  
-  it("should work", async () => {
-    await page.goto(targetURL);
-    expect(await page.title()).toBe("Peg Listing - Sifchain");
-  });
-
-
-})
+  beforeAll(async () => {});
+  afterAll(async () => {});
+  beforeEach(async () => {});
+  afterEach(async () => {});
+  it("should open new tab", async () => {});
+});
