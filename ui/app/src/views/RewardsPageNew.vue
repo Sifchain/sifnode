a738d9bff752851def602ce862f40c40bb3efe2e
59023g <5235538+59023g@users.noreply.github.com>
2021-03-10 11:43:34 -0800

    app/RewardsPageNew:

--- ui/app/src/views/RewardsPageNew.vue
+++ ui/app/src/views/RewardsPageNew.vue
@@ -2,52 +2,86 @@
 import { defineComponent } from "vue";
 import Layout from "@/components/layout/Layout.vue";
 import SifButton from "@/components/shared/SifButton.vue";
+import AssetItem from "@/components/shared/AssetItem.vue";
 
 export default defineComponent({
   components: {
     Layout,
     SifButton,
+    AssetItem,
   },
-
   setup() {},
 });
 </script>
 
 <template>
   <Layout :header="true" title="Rewards" backLink="/peg">
-    <div class="liquidity-container">
-      <div class="info">
-        <p class="mb-5">
-          Rewards from Liquidity Mining Program:
-        </p>
-        <p class="mb-2">
-          <span class="amount">49 Rowan YTD</span>
-        </p>
-        <SifButton disabled primary class="mb-8">Claim</SifButton>
-        <p class="text--small">
-          At present, you are unable to claim Liquidity Mining rewards.
-        </p>
+    <div class="info mb-8" style="line-height: 1.4">
+      <p class="text--small mb-2">
+        Earn rewards by participating in of our rewards-earning programs. Please
+        see additional information of our current rewards programs and how to
+        become eligible for them
+        <a
+          target="_blank"
+          href="https://docs.sifchain.finance/resources/rewards-programs"
+          >here</a
+        >.
+      </p>
+    </div>
+    <div class="list-container">
+      <div class="item">
+        <div class="title">
+          Liquidity Mining
+        </div>
+
+        <div class="detail">
+          <!-- future: slot -->
+          <div class="amount">
+            <AssetItem symbol="Rowan" :label="false" />
+            <span class="mr-6">49.212</span>
+          </div>
+          <SifButton primary>Claim</SifButton>
+        </div>
       </div>
     </div>
   </Layout>
 </template>
 
 <style scoped lang="scss">
+/* this pattern is generic. for component lib */
 .info {
   text-align: left;
+  font-weight: 400;
 }
-.ul {
-  text-decoration: underline;
-}
-.amount {font-size: $fs_lg}
-.liquidity-container {
+.list-container {
   text-align: left;
   color: $c_gray_700;
   border-top: 1px solid $c_gray_400;
   min-height: 145px;
   background: white;
-  padding: 15px;
   border-radius: 0 0 6px 6px;
-  padding-bottom: 60px
+  padding-bottom: 60px;
+  .item {
+    padding: 14px 16px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border-bottom: $divider;
+    align-items: center;
+    &:hover {
+      cursor: pointer;
+      background: $c_gray_50;
+    }
+    .title {
+      color: $c_text;
+    }
+    .detail {
+      display: flex;
+      align-items: center;
+      .amount {
+        display: flex;
+      }
+    }
+  }
 }
 </style>
