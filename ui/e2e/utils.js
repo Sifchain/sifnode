877597e38fd2a660b6c96448709945a7b51a3b1a
59023g <5235538+59023g@users.noreply.github.com>
2021-03-10 10:16:22 -0800

    e2e/utils: extractFile, downloadFile

---
+++ ui/e2e/utils.js
@@ -0,0 +1,32 @@
+// utils.js
+import StreamZip from 'node-stream-zip';
+import axios from "axios"
+import fs from "fs"
+import path from "path"
+
+// Not in use, don't have good place to get the extension zips, for now
+export async function downloadFile (name, url, dir) { 
+  const write = path.resolve(dir, `${name}.zip`)
+  const writer = fs.createWriteStream(write)
+
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  })
+
+  response.data.pipe(writer)
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve)
+    writer.on('error', reject)
+  })
+}
+
+export async function extractFile (downloadedFile, extractDestination) {
+    const zip = new StreamZip.async({ file: downloadedFile });
+    if (!fs.existsSync(extractDestination)) {
+      fs.mkdirSync(extractDestination);
+    }
+    await zip.extract(null, extractDestination);
+  }
