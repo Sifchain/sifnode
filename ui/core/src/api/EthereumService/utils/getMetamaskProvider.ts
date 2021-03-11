ee2365cfb4397fca8a733975ccd3b79071643406
Rudi Yardley <contact@rudiyardley.com>
2021-03-11 16:31:14 +1030

    Remove redudant comment

--- ui/core/src/api/EthereumService/utils/getMetamaskProvider.ts
+++ ui/core/src/api/EthereumService/utils/getMetamaskProvider.ts
@@ -16,32 +16,9 @@ type WindowWithPossibleMetaMask = typeof window & {
 export const getMetamaskProvider = async (): Promise<provider> => {
   const mmp = await detectMetaMaskProvider();
   const win = window as WindowWithPossibleMetaMask;
-  if (!mmp) {
-    // XXX: Should not have access to sideeffects here surface in business layer
-    // TODO: Trigger this notifications in usecases
-    // This is tricky because of the way the ethereumService is designed and this needs to be overhauled
-    // Usecase Layer should do something along the lines of:
 
-    // ```ts
-    // const provider:IBlockchainProvider | null = MetamaskProvider.create()
-    // if(!provider){
-    //   services.events.dispatch({type:'MetamaskNotFoundErrorEvent'}) }
-    //   return
-    // }
-    // services.eth.setProvider(provider)
-    // ```
+  if (!mmp || !win) return null;
 
-    // notify({
-    //   type: "error",
-    //   message: "Metamask not found.",
-    //   detail: {
-    //     type: "info",
-    //     message: "Check if extension enabled for this URL.",
-    //   },
-    // });
-    return null;
-  }
-  if (!win) return null;
   if (mmp) {
     return mmp as provider;
   }
