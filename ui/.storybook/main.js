7e2159d08f3fb2a700ae6c3758d6b1f6f6efc267
Thomas Davis <thomasalwyndavis@gmail.com>
2021-03-11 16:32:53 +1100

    storybook init

---
+++ ui/.storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../app/**/*.stories.mdx",
+    "../app/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
---
+++ ui/app/src/components/shared/Icon.stories.ts
@@ -0,0 +1,23 @@
+import Icon from './Icon.vue';
+
+export default {
+  title: 'Icon',
+  component: Icon,
+};
+
+const Template = (args, { argTypes }) => ({
+  props: ['icon'],
+  components: { Icon },
+  setup() { return { args }; },
+  template: '<icon :icon="icon" v-bind="args" />',
+});
+
+export const InfoBoxBlack = Template.bind({});
+InfoBoxBlack.args = {
+  icon: "info-box-black",
+};
+
+export const Back = Template.bind({});
+Back.args = {
+  icon: "back",
+};
