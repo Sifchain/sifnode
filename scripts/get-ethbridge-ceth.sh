a9770cc4980f2f47ae34837c4111f5d2fc950dca
juniuszhou <junius.zhou@gmail.com>
2021-03-18 12:54:43 +0800

    add script to get ceth of ethbridge.

---
+++ scripts/get-ethbridge-ceth.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# the script get the ceth amount of ethbridge module account.
+sifnodecli  q account sif1l3dftf499u4gvdeuuzdl2pgv4f0xdtnuuwlzp8 | jq '. | {coins: .value.coins} ' | jq '.[] | map(select(.denom == "ceth"))' | jq '.[]'
