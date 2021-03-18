a971d1546e924993ce0296c4e78681cd852f5168
James Moore <james@sifchain.finance>
2021-03-18 10:56:56 -0700

    Integration tests - added test for sifchain transactions while the relayers are down

--- test/integration/src/py/test_ebrelayer_restart.py
+++ test/integration/src/py/test_ebrelayer_restart.py
@@ -1,12 +1,13 @@
 import copy
 import json
 import logging
-
 import time
+
+import pytest
 
 import test_utilities
 from pytest_utilities import create_new_sifaddr
-from pytest_utilities import generate_minimal_test_account
+from pytest_utilities import generate_test_account, generate_minimal_test_account
 from test_utilities import EthereumToSifchainTransferRequest
 
 
@@ -25,16 +26,17 @@ def test_ebrelayer_restart(
     test_utilities.get_shell_output(f"{integration_dir}/sifchain_start_ebrelayer.sh")
     test_utilities.advance_n_ethereum_blocks(test_utilities.n_wait_blocks, request.smart_contracts_dir)
     time.sleep(5)
-    assert balance == test_utilities.get_sifchain_addr_balance(request.sifchain_address, request.sifnodecli_node, "ceth")
+    assert balance == test_utilities.get_sifchain_addr_balance(request.sifchain_address, request.sifnodecli_node,
+                                                               "ceth")
 
 
+@pytest.mark.usefixtures("ensure_relayer_restart")
 def test_ethereum_transactions_with_offline_relayer(
         basic_transfer_request: EthereumToSifchainTransferRequest,
         smart_contracts_dir,
         source_ethereum_address,
         bridgebank_address,
         integration_dir,
-        ensure_relayer_restart,
 ):
     logging.info("shut down ebrelayer")
     test_utilities.get_shell_output(f"pkill -9 ebrelayer || true")
@@ -73,3 +75,56 @@ def test_ethereum_transactions_with_offline_relayer(
     for a in new_addresses:
         test_utilities.wait_for_sif_account(a, basic_transfer_request.sifnodecli_node, 90)
         test_utilities.wait_for_sifchain_addr_balance(a, "ceth", amount, basic_transfer_request.sifnodecli_node, 90)
+
+
+@pytest.mark.usefixtures("ensure_relayer_restart")
+def test_sifchain_transactions_with_offline_relayer(
+        basic_transfer_request: EthereumToSifchainTransferRequest,
+        rowan_source_integrationtest_env_credentials: test_utilities.SifchaincliCredentials,
+        rowan_source_integrationtest_env_transfer_request: EthereumToSifchainTransferRequest,
+        rowan_source,
+        smart_contracts_dir,
+        source_ethereum_address,
+        integration_dir,
+):
+    basic_transfer_request.ethereum_address = source_ethereum_address
+    request, credentials = generate_test_account(
+        basic_transfer_request,
+        rowan_source_integrationtest_env_transfer_request,
+        rowan_source_integrationtest_env_credentials,
+        target_ceth_balance=10 ** 19,
+        target_rowan_balance=10 ** 19,
+    )
+    logging.info("shut down ebrelayer")
+    test_utilities.get_shell_output(f"pkill -9 ebrelayer || true")
+
+    logging.info("prepare transactions to be sent while ebrelayer is offline")
+    amount = 9000
+
+    new_eth_addrs = test_utilities.create_ethereum_addresses(
+        smart_contracts_dir,
+        basic_transfer_request.ethereum_network,
+        2
+    )
+
+    request.amount = amount
+    request.sifchain_symbol = "ceth"
+    request.ethereum_symbol = "eth"
+
+    for a in new_eth_addrs:
+        request.ethereum_address = a["address"]
+        sifchain_balance = test_utilities.get_sifchain_addr_balance(request.sifchain_address, request.sifnodecli_node,
+                                                                    "ceth")
+        logging.info(f"sifchain balance is {sifchain_balance}, request is {request}")
+        test_utilities.send_from_sifchain_to_ethereum(
+            transfer_request=request,
+            credentials=credentials
+        )
+        time.sleep(5)
+
+    logging.info("restart ebrelayer")
+    test_utilities.get_shell_output(f"{integration_dir}/sifchain_start_ebrelayer.sh")
+    test_utilities.advance_n_ethereum_blocks(test_utilities.n_wait_blocks, request.smart_contracts_dir)
+    for a in new_eth_addrs:
+        request.ethereum_address = a["address"]
+        test_utilities.wait_for_eth_balance(request, amount, 600)
