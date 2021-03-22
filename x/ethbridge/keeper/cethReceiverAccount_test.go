5f6e0c270b1f2bdf7f451b5479fb8a5672461e19
juniuszhou <junius.zhou@gmail.com>
2021-03-23 00:28:20 +0800

    add unit test.

--- x/ethbridge/keeper/cethReceiverAccount_test.go
+++ x/ethbridge/keeper/cethReceiverAccount_test.go
@@ -35,3 +35,9 @@ func TestIsCethReceiverAccount(t *testing.T) {
 
 	assert.False(t, keeper.IsCethReceiverAccount(ctx, testCosmosAddress))
 }
+
+func TestIsCethReceiverAccountSet(t *testing.T) {
+	ctx, keeper, _, _, _, _ := CreateTestKeepers(t, 0.7, []int64{3, 7}, "")
+	accountSet := keeper.IsCethReceiverAccountSet(ctx)
+	require.Equal(t, accountSet, true)
+}
