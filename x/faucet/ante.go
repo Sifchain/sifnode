2bbab53c313712bc982ca4414480a4cfe7a83d50
Tanmay <tanmay@sifchain.finance>
2021-03-24 01:42:23 -0400

    renamed types

--- x/faucet/ante.go
+++ x/faucet/ante.go
@@ -18,7 +18,7 @@ var _ types.AnteDecorator = RemoveFaucetFeeDecorator{}
 //AnteHandle for the RemoveFaucetFeeDecorator removes the MinGasPrice for a request token transaction
 //This Function only works ith the profile is set as TestNet and the chain-ID is not sifchain
 func (r RemoveFaucetFeeDecorator) AnteHandle(ctx types.Context, tx types.Tx, simulate bool, next types.AnteHandler) (newCtx types.Context, err error) {
-	if ctx.ChainID() != "sifchain" && types2.Profile == types2.TESTNET {
+	if ctx.ChainID() != "sifchain" && ftypes.Profile == ftypes.TESTNET {
 		newDecCoin := types.NewDecCoinFromDec(clp.GetSettlementAsset().Symbol, types.NewDec(0))
 		ctx = ctx.WithMinGasPrices(types.NewDecCoins(newDecCoin))
 	}
