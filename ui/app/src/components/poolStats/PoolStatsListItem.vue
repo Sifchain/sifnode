5118c225006c1d624c08ea82d25bf88c6d04c30f
59023g <5235538+59023g@users.noreply.github.com>
2021-03-28 16:55:24 -0500

    app/poolStats/PoolStatsList{Header,Item}: pool arbitrage

--- ui/app/src/components/poolStats/PoolStatsListItem.vue
+++ ui/app/src/components/poolStats/PoolStatsListItem.vue
@@ -35,6 +35,10 @@ export default defineComponent({
     const priceToken = formatNumberString(
       parseFloat(props.pool?.priceToken).toFixed(2),
     );
+    const arb =
+      props.pool?.arb !== ""
+        ? formatNumberString(parseFloat(props.pool?.arb).toFixed(2)) + "%"
+        : "N/A";
     const poolDepth = formatNumberString(
       parseFloat(props.pool?.poolDepth).toFixed(2),
     );
@@ -56,6 +60,7 @@ export default defineComponent({
       volume,
       poolAPY,
       formatNumberString,
+      arb,
     };
   },
 });
@@ -75,13 +80,16 @@ export default defineComponent({
           }}</span>
         </div>
       </div>
-      <div class="col-sm">
+      <div class="col-lg">
         <span>${{ priceToken }}</span>
       </div>
-      <div class="col-sm">
+      <div class="col-lg">
+        <span>{{ arb }}</span>
+      </div>
+      <div class="col-lg">
         <span>${{ poolDepth }}</span>
       </div>
-      <div class="col-sm">
+      <div class="col-lg">
         <span>${{ volume }}</span>
       </div>
       <div class="col-sm">
@@ -151,7 +159,7 @@ export default defineComponent({
 
   .col-sm-s {
     padding-left: 12px;
-    min-width: 102px;
+    min-width: 112px;
     width: 10%;
     display: flex;
     justify-content: start;
@@ -175,6 +183,7 @@ export default defineComponent({
     min-width: 168px;
     font-size: $fs_md;
     color: $c_text;
+    text-align: center;
   }
 }
 </style>
