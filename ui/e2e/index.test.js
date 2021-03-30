67f37c8e359c80e9feeaecba4dcc6ec7bb8f58d7
59023g <5235538+59023g@users.noreply.github.com>
2021-03-30 12:33:14 -0500

    ui/e2e/index.test: metamask init

--- ui/e2e/index.test.js
+++ ui/e2e/index.test.js
@@ -18,77 +18,98 @@ const { chromium } = require("playwright");
 const { importKeplrAccount, connectKeplrAccount } = require("./keplr");
 const keplrConfig = require("../core/src/config.localnet.json");
 
-const { extractFile } = require("./utils");
 const { getSifchainBalances } = require("./sifchain.js");
-
+const { extractFile } = require("./utils");
+const { MetaMask } = require("./metamask.js")
 const DEX_TARGET = "localhost:8080";
 
-const KEPLR_EXT_ID = "dmkamcknogkgcdfhhbddcghachkejeap";
-const KEPLR_VERSION = "0.8.1_0";
-const KEPLR_PATH = `./extensions/${KEPLR_EXT_ID}/${KEPLR_VERSION}`;
-const KEPLR_OPTIONS = {
-  address: "sif1m625hcmnkc84cgmef6upzzyfu6mxd4jkpnfwwl",
-  name: "juniper",
-  mnemonic:
-    "clump genre baby drum canvas uncover firm liberty verb moment access draft erupt fog alter gadget elder elephant divide biology choice sentence oppose avoid",
-};
-// const METAMASK_PATH = "./extensions/nkbihfbeogaeaoehlefnkodbefgpgknn/9.1.1_0";
+const KEPLR_CONFIG = {
+  id: "dmkamcknogkgcdfhhbddcghachkejeap",
+  ver: "0.8.1_0",
+  get path() {
+    return `./extensions/${this.id}/${this.ver}`
+  },
+  options: {
+    address: "sif1m625hcmnkc84cgmef6upzzyfu6mxd4jkpnfwwl",
+    name: "juniper",
+    mnemonic:
+      "clump genre baby drum canvas uncover firm liberty verb moment access draft erupt fog alter gadget elder elephant divide biology choice sentence oppose avoid",
+  }
+}
+
+const MM_CONFIG = {
+  id: "nkbihfbeogaeaoehlefnkodbefgpgknn",
+  ver: "9.1.1_0",
+  get path() {
+    return `./extensions/${this.id}/${this.ver}`
+  },
+  options: {
+    address: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
+    mnemonic: "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat",
+    password: "coolguy21"
+  }
+}
 
 let browserContext;
 
 describe("connect to page", () => {
   beforeAll(async () => {
-    // const pathToMetamaskExtension = path.join(__dirname, METAMASK_PATH);
-    await extractFile(`downloads/${KEPLR_EXT_ID}.zip`, "./extensions");
-    const pathToKeplrExtension = path.join(__dirname, KEPLR_PATH);
+
+    await extractExtensionPackages();
+    const pathToKeplrExtension = path.join(__dirname, KEPLR_CONFIG.path);
+    const pathToMmExtension = path.join(__dirname, MM_CONFIG.path);
     const userDataDir = path.join(__dirname, "./playwright");
     // need to rm userDataDir or else will store extension state
     if (fs.existsSync(userDataDir)) {
       fs.rmdirSync(userDataDir, { recursive: true });
     }
+
     browserContext = await chromium.launchPersistentContext(userDataDir, {
-      // may be able to run within remote xvfb server
+      // headless required with extensions. xvfb used for ci/cd
       headless: false,
       args: [
-        `--disable-extensions-except=${pathToKeplrExtension}`,
-        `--load-extension=${pathToKeplrExtension}`,
+        `--disable-extensions-except=${pathToKeplrExtension},${pathToMmExtension}`,
+        `--load-extension=${pathToKeplrExtension},${pathToMmExtension}`,
       ],
     });
-    const keplrPage = await browserContext.newPage();
-    await keplrPage.goto(
-      "chrome-extension://dmkamcknogkgcdfhhbddcghachkejeap/popup.html#/register",
-    );
-    await importKeplrAccount(keplrPage, KEPLR_OPTIONS);
+    
+    const MM = new MetaMask(browserContext, MM_CONFIG)
+    await MM.setup(browserContext)
+
+    // setup keplr account
+    // const keplrPage = await browserContext.newPage();
+    // await keplrPage.goto(
+    //   "chrome-extension://dmkamcknogkgcdfhhbddcghachkejeap/popup.html#/register",
+    // );
+    // await importKeplrAccount(keplrPage, KEPLR_CONFIG.options);
+    
+    // setup metamask account
+
   });
 
   afterAll(async () => {
     browserContext.close();
   });
 
-  it("connect to keplr, check balance", async () => {
+  it.skip("connect to keplr, check balance", async () => {
     const keplrcEthBalance = await getSifchainBalances(
       keplrConfig.sifApiUrl,
-      KEPLR_OPTIONS.address,
+      KEPLR_CONFIG.options.address,
       "ceth",
     ); //"100.000000"; // Fetch balance
     const dexPage = await browserContext.newPage();
     await dexPage.goto(DEX_TARGET, { waitUntil: "domcontentloaded" });
 
     await connectKeplrAccount(dexPage, browserContext);
-    await dexPage.waitForTimeout(1000);
-    // await dexPage.pause();
+    await dexPage.waitForTimeout(1000); // this is only necessary bc popup
+    await dexPage.pause();
     expect(
-      await dexPage.innerText(
-        // cETH
-        "#app > div > div.layout > div > div.body > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(2) > div.amount",
+      await dexPage.innerText("[data-handle='ceth-row-amount']",
       ),
     ).toBe(keplrcEthBalance);
   });
 
   it("connects to metamask, check balance", async () => {
-    // unzip downloads/nkbih...zip
-    // place into extensions folder
-    // load it on launch persistent context
     // import account
     // see https://github.com/NodeFactoryIo/dappeteer/blob/master/src/index.ts#L57
 
@@ -96,3 +117,9 @@ describe("connect to page", () => {
   it("pegs", async () => {})
 
 });
+
+async function extractExtensionPackages() {
+  await extractFile(`downloads/${KEPLR_CONFIG.id}.zip`, "./extensions");
+  await extractFile(`downloads/${MM_CONFIG.id}.zip`, "./extensions");
+  return
+} 
