d695987a1ced97db416d440e3253eb32cab447a4
Tanmay <tanmay@sifchain.finance>
2021-04-01 02:12:38 -0400

    added tests to increase coverage

---
+++ x/dispensation/keeper/executors_test.go
@@ -0,0 +1,48 @@
+package keeper_test
+
+import (
+	"github.com/Sifchain/sifnode/x/dispensation/test"
+	"github.com/Sifchain/sifnode/x/dispensation/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestKeeper_AccumulateDrops(t *testing.T) {
+	app, ctx := test.CreateTestApp(false)
+	keeper := app.DispensationKeeper
+	inputList := test.GenerateInputList("15000000000000000000")
+	//outputList := test.GenerateOutputList("10000000000000000000")
+
+	for _, in := range inputList {
+		_, err := keeper.GetBankKeeper().AddCoins(ctx, in.Address, in.Coins)
+		assert.NoError(t, err)
+	}
+	err := keeper.AccumulateDrops(ctx, inputList)
+	assert.NoError(t, err)
+	moduleBalance, _ := sdk.NewIntFromString("30000000000000000000")
+	assert.True(t, keeper.GetBankKeeper().HasCoins(ctx, types.GetDistributionModuleAddress(), sdk.Coins{sdk.NewCoin("rowan", moduleBalance)}))
+
+}
+
+func TestKeeper_CreateAndDistributeDrops(t *testing.T) {
+	app, ctx := test.CreateTestApp(false)
+	keeper := app.DispensationKeeper
+	inputList := test.GenerateInputList("15000000000000000000")
+	outputList := test.GenerateOutputList("10000000000000000000")
+	for _, in := range inputList {
+		_, err := keeper.GetBankKeeper().AddCoins(ctx, in.Address, in.Coins)
+		assert.NoError(t, err)
+	}
+	err := keeper.AccumulateDrops(ctx, inputList)
+	assert.NoError(t, err)
+	moduleBalance, _ := sdk.NewIntFromString("30000000000000000000")
+	assert.True(t, keeper.GetBankKeeper().HasCoins(ctx, types.GetDistributionModuleAddress(), sdk.Coins{sdk.NewCoin("rowan", moduleBalance)}))
+
+	err = keeper.CreateAndDistributeDrops(ctx, outputList, "ar1")
+	assert.NoError(t, err)
+	recipientBalance, _ := sdk.NewIntFromString("10000000000000000000")
+	for _, out := range outputList {
+		assert.True(t, keeper.GetBankKeeper().HasCoins(ctx, out.Address, sdk.Coins{sdk.NewCoin("rowan", recipientBalance)}))
+	}
+}
