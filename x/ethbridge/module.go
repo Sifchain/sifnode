e7141b45d180acf099890d26985f9e8157bbda61
Tim Lind <tim@incremental.co>
2021-04-02 14:18:06 +0200

    x/ethbridge: Fix cosmos API breaks on ethbridge module.go

--- x/ethbridge/module.go
+++ x/ethbridge/module.go
@@ -2,17 +2,19 @@ package ethbridge
 
 import (
 	"encoding/json"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 
 	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
 
 	abci "github.com/tendermint/tendermint/abci/types"
 
-	"github.com/cosmos/cosmos-sdk/client/context"
+	sdkclient "github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	"github.com/cosmos/cosmos-sdk/x/supply"
 
 	"github.com/Sifchain/sifnode/x/ethbridge/client"
 	"github.com/Sifchain/sifnode/x/ethbridge/types"
@@ -26,6 +28,10 @@ var (
 // AppModuleBasic defines the basic application module used by the ethbridge module.
 type AppModuleBasic struct{}
 
+func (b AppModuleBasic) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	panic("implement me")
+}
+
 var _ module.AppModuleBasic = AppModuleBasic{}
 
 // Name returns the ethbridge module's name.
@@ -34,34 +40,38 @@ func (AppModuleBasic) Name() string {
 }
 
 // RegisterCodec registers the ethbridge module's types for the given codec.
-func (AppModuleBasic) RegisterCodec(cdc *codec.Codec) {
+func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	types.RegisterCodec(cdc)
 }
 
 // DefaultGenesis returns default genesis state as raw bytes for the ethbridge
 // module.
-func (AppModuleBasic) DefaultGenesis() json.RawMessage {
+func (b AppModuleBasic) DefaultGenesis(marshaler codec.JSONMarshaler) json.RawMessage {
 	return nil
 }
 
 // ValidateGenesis performs genesis state validation for the ethbridge module.
-func (AppModuleBasic) ValidateGenesis(_ json.RawMessage) error {
+func (b AppModuleBasic) ValidateGenesis(marshaler codec.JSONMarshaler, config sdkclient.TxEncodingConfig, message json.RawMessage) error {
 	return nil
 }
 
+func (b AppModuleBasic) RegisterGRPCGatewayRoutes(c sdkclient.Context, serveMux *runtime.ServeMux) {
+	panic("implement me")
+}
+
 // RegisterRESTRoutes registers the REST routes for the ethbridge module.
-func (AppModuleBasic) RegisterRESTRoutes(ctx context.CLIContext, rtr *mux.Router) {
-	client.RegisterRESTRoutes(ctx, rtr, StoreKey)
+func (b AppModuleBasic) RegisterRESTRoutes(ctx sdkclient.Context, router *mux.Router) {
+	client.RegisterRESTRoutes(ctx, router, StoreKey)
 }
 
 // GetTxCmd returns the root tx command for the ethbridge module.
-func (AppModuleBasic) GetTxCmd(cdc *codec.Codec) *cobra.Command {
-	return client.GetTxCmd(StoreKey, cdc)
+func (b AppModuleBasic) GetTxCmd() *cobra.Command {
+	return client.GetTxCmd(StoreKey)
 }
 
 // GetQueryCmd returns no root query command for the ethbridge module.
-func (AppModuleBasic) GetQueryCmd(cdc *codec.Codec) *cobra.Command {
-	return client.GetQueryCmd(StoreKey, cdc)
+func (AppModuleBasic) GetQueryCmd() *cobra.Command {
+	return client.GetQueryCmd(StoreKey)
 }
 
 //____________________________________________________________________________
@@ -78,14 +88,54 @@ type AppModule struct {
 	SupplyKeeper  types.SupplyKeeper
 	AccountKeeper types.AccountKeeper
 	BridgeKeeper  Keeper
-	Codec         *codec.Codec
+	Codec         *codec.Marshaler
+}
+
+func (am AppModule) RegisterLegacyAminoCodec(amino *codec.LegacyAmino) {
+	panic("implement me")
+}
+
+func (am AppModule) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	panic("implement me")
+}
+
+func (am AppModule) DefaultGenesis(marshaler codec.JSONMarshaler) json.RawMessage {
+	panic("implement me")
+}
+
+func (am AppModule) ValidateGenesis(marshaler codec.JSONMarshaler, config sdkclient.TxEncodingConfig, message json.RawMessage) error {
+	panic("implement me")
+}
+
+func (am AppModule) RegisterRESTRoutes(c sdkclient.Context, router *mux.Router) {
+	panic("implement me")
+}
+
+func (am AppModule) RegisterGRPCGatewayRoutes(c sdkclient.Context, serveMux *runtime.ServeMux) {
+	panic("implement me")
+}
+
+func (am AppModule) GetTxCmd() *cobra.Command {
+	panic("implement me")
+}
+
+func (am AppModule) GetQueryCmd() *cobra.Command {
+	panic("implement me")
+}
+
+func (am AppModule) LegacyQuerierHandler(amino *codec.LegacyAmino) sdk.Querier {
+	panic("implement me")
+}
+
+func (am AppModule) RegisterServices(configurator module.Configurator) {
+	panic("implement me")
 }
 
 // NewAppModule creates a new AppModule object
 func NewAppModule(
 	oracleKeeper types.OracleKeeper, supplyKeeper types.SupplyKeeper,
 	accountKeeper types.AccountKeeper, bridgeKeeper Keeper,
-	cdc *codec.Codec) AppModule {
+	cdc *codec.Marshaler) AppModule {
 
 	return AppModule{
 		AppModuleBasic:      AppModuleBasic{},
@@ -109,8 +159,8 @@ func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {
 }
 
 // Route returns the message routing key for the ethbridge module.
-func (AppModule) Route() string {
-	return RouterKey
+func (am AppModule) Route() sdk.Route {
+	return sdk.NewRoute(RouterKey, am.NewHandler())
 }
 
 // NewHandler returns an sdk.Handler for the ethbridge module.
@@ -130,15 +180,15 @@ func (am AppModule) NewQuerierHandler() sdk.Querier {
 
 // InitGenesis performs genesis initialization for the ethbridge module. It returns
 // no validator updates.
-func (am AppModule) InitGenesis(ctx sdk.Context, _ json.RawMessage) []abci.ValidatorUpdate {
-	bridgeAccount := supply.NewEmptyModuleAccount(ModuleName, supply.Burner, supply.Minter)
+func (am AppModule) InitGenesis(ctx sdk.Context, marshaler codec.JSONMarshaler, _ json.RawMessage) []abci.ValidatorUpdate {
+	bridgeAccount := authtypes.NewEmptyModuleAccount(ModuleName, authtypes.Burner, authtypes.Minter)
 	am.SupplyKeeper.SetModuleAccount(ctx, bridgeAccount)
 	return nil
 }
 
 // ExportGenesis returns the exported genesis state as raw bytes for the ethbridge
 // module.
-func (am AppModule) ExportGenesis(ctx sdk.Context) json.RawMessage {
+func (am AppModule) ExportGenesis(s sdk.Context, marshaler codec.JSONMarshaler) json.RawMessage {
 	return nil
 }
 
