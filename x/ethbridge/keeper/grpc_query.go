b51b7e87ff6d5d8132fe71e47abd5c210c66f72f
Tim Lind <tim@incremental.co>
2021-04-06 10:45:25 +0200

    Move eth prophecy query to query server.
    Legacy querier calls query server.

--- x/ethbridge/keeper/grpc_query.go
+++ x/ethbridge/keeper/grpc_query.go
@@ -3,7 +3,10 @@ package keeper
 import (
 	"context"
 	"github.com/Sifchain/sifnode/x/ethbridge/types"
+	oracletypes "github.com/Sifchain/sifnode/x/oracle/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"strconv"
 )
 
 var _ types.QueryServer = queryServer{}
@@ -20,10 +23,33 @@ func NewQueryServer(keeper Keeper) types.QueryServer {
 	}
 }
 
-func (k Keeper) EthProphecy(ctx context.Context, request *types.QueryEthProphecyRequest) (*types.QueryEthProphecyResponse, error) {
+func (srv queryServer) EthProphecy(ctx context.Context, req *types.QueryEthProphecyRequest) (*types.QueryEthProphecyResponse, error) {
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
 
-	return &types.QueryEthProphecyResponse{}, nil
+	id := strconv.FormatInt(req.EthereumChainId, 10) + strconv.FormatInt(req.Nonce, 10) + req.EthereumSender
+
+	prophecy, found := srv.Keeper.oracleKeeper.GetProphecy(sdkCtx, id)
+	if !found {
+		return nil, sdkerrors.Wrap(oracletypes.ErrProphecyNotFound, id)
+	}
+
+	bridgeClaims, err := types.MapOracleClaimsToEthBridgeClaims(
+		req.EthereumChainId,
+		types.NewEthereumAddress(req.BridgeContractAddress),
+		req.Nonce,
+		req.Symbol,
+		types.NewEthereumAddress(req.TokenContractAddress),
+		types.NewEthereumAddress(req.EthereumSender),
+		prophecy.ValidatorClaims,
+		types.CreateEthClaimFromOracleString,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	res := types.NewQueryEthProphecyResponse(prophecy.ID, prophecy.Status, bridgeClaims)
+
+	return &res, nil
 }
 
 
--- x/ethbridge/keeper/querier.go
+++ x/ethbridge/keeper/querier.go
@@ -2,15 +2,11 @@ package keeper
 
 import (
 	"fmt"
-	"strconv"
-
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-
-	oracletypes "github.com/Sifchain/sifnode/x/oracle/types"
 
 	"github.com/Sifchain/sifnode/x/ethbridge/types"
 )
@@ -37,28 +33,11 @@ func legacyQueryEthProphecy(ctx sdk.Context, cdc *codec.LegacyAmino, query abci.
 		return nil, sdkerrors.Wrap(types.ErrJSONMarshalling, fmt.Sprintf("failed to parse req: %s", err.Error()))
 	}
 
-	id := strconv.FormatInt(req.EthereumChainId, 10) + strconv.FormatInt(req.Nonce, 10) + req.EthereumSender
-
-	prophecy, found := keeper.oracleKeeper.GetProphecy(ctx, id)
-	if !found {
-		return nil, sdkerrors.Wrap(oracletypes.ErrProphecyNotFound, id)
-	}
-
-	bridgeClaims, err := types.MapOracleClaimsToEthBridgeClaims(
-		req.EthereumChainId,
-		types.NewEthereumAddress(req.BridgeContractAddress),
-		req.Nonce,
-		req.Symbol,
-		types.NewEthereumAddress(req.TokenContractAddress),
-		types.NewEthereumAddress(req.EthereumSender),
-		prophecy.ValidatorClaims,
-		types.CreateEthClaimFromOracleString,
-	)
+	queryServer := NewQueryServer(keeper)
+	response, err := queryServer.EthProphecy(sdk.WrapSDKContext(ctx), &req)
 	if err != nil {
 		return nil, err
 	}
-
-	response := types.NewQueryEthProphecyResponse(prophecy.ID, prophecy.Status, bridgeClaims)
 
 	return cdc.MarshalJSONIndent(response, "", "  ")
 }
