7cc0994c7a22139824eff6328a525b0bf35b0ad3
Tanmay <tanmay@sifchain.finance>
2021-04-07 00:44:37 -0400

    change comments in module.go

--- x/dispensation/module.go
+++ x/dispensation/module.go
@@ -23,7 +23,7 @@ var (
 	_ module.AppModuleBasic = AppModuleBasic{}
 )
 
-// AppModuleBasic defines the basic application module used by the clp module.
+// AppModuleBasic defines the basic application module used by the dispensation module.
 type AppModuleBasic struct{}
 
 // Name returns the clp module's name.
@@ -31,18 +31,18 @@ func (AppModuleBasic) Name() string {
 	return ModuleName
 }
 
-// RegisterCodec registers the clp module's types for the given codec.
+// RegisterCodec registers the dispensation module's types for the given codec.
 func (AppModuleBasic) RegisterCodec(cdc *codec.Codec) {
 	RegisterCodec(cdc)
 }
 
-// DefaultGenesis returns default genesis state as raw bytes for the clp
+// DefaultGenesis returns default genesis state as raw bytes for the dispensation
 // module.
 func (AppModuleBasic) DefaultGenesis() json.RawMessage {
 	return ModuleCdc.MustMarshalJSON(DefaultGenesisState())
 }
 
-// ValidateGenesis performs genesis state validation for the clp module.
+// ValidateGenesis performs genesis state validation for the dispensation module.
 func (AppModuleBasic) ValidateGenesis(bz json.RawMessage) error {
 	var data GenesisState
 	err := ModuleCdc.UnmarshalJSON(bz, &data)
@@ -52,23 +52,23 @@ func (AppModuleBasic) ValidateGenesis(bz json.RawMessage) error {
 	return ValidateGenesis(data)
 }
 
-// RegisterRESTRoutes registers the REST routes for the clp module.
+// RegisterRESTRoutes registers the REST routes for the dispensation module.
 func (AppModuleBasic) RegisterRESTRoutes(_ context.CLIContext, _ *mux.Router) {
 }
 
-// GetTxCmd returns the root tx command for the clp module.
+// GetTxCmd returns the root tx command for the dispensation module.
 func (AppModuleBasic) GetTxCmd(cdc *codec.Codec) *cobra.Command {
 	return cli.GetTxCmd(cdc)
 }
 
-// GetQueryCmd returns no root query command for the clp module.
+// GetQueryCmd returns no root query command for the dispensation module.
 func (AppModuleBasic) GetQueryCmd(cdc *codec.Codec) *cobra.Command {
 	return cli.GetQueryCmd(StoreKey, cdc)
 }
 
 //____________________________________________________________________________
 
-// AppModule implements an application module for the clp module.
+// AppModule implements an application module for the dispensation module.
 type AppModule struct {
 	AppModuleBasic
 
@@ -87,35 +87,35 @@ func NewAppModule(k keeper.Keeper, bankKeeper types.BankKeeper, supplyKeeper typ
 	}
 }
 
-// Name returns the clp module's name.
+// Name returns the dispensation module's name.
 func (AppModule) Name() string {
 	return ModuleName
 }
 
-// RegisterInvariants registers the clp module invariants.
+// RegisterInvariants registers the dispensation module invariants.
 func (am AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
 
-// Route returns the message routing key for the clp module.
+// Route returns the message routing key for the dispensation module.
 func (AppModule) Route() string {
 	return RouterKey
 }
 
-// NewHandler returns an sdk.Handler for the clp module.
+// NewHandler returns an sdk.Handler for the dispensation module.
 func (am AppModule) NewHandler() sdk.Handler {
 	return NewHandler(am.keeper)
 }
 
-// QuerierRoute returns the clp module's querier route name.
+// QuerierRoute returns the dispensation module's querier route name.
 func (AppModule) QuerierRoute() string {
 	return QuerierRoute
 }
 
-// NewQuerierHandler returns the clp module sdk.Querier.
+// NewQuerierHandler returns the dispensation module sdk.Querier.
 func (am AppModule) NewQuerierHandler() sdk.Querier {
 	return NewQuerier(am.keeper)
 }
 
-// InitGenesis performs genesis initialization for the clp module. It returns
+// InitGenesis performs genesis initialization for the dispensation module. It returns
 // no validator updates.
 func (am AppModule) InitGenesis(ctx sdk.Context, data json.RawMessage) []abci.ValidatorUpdate {
 	var genesisState GenesisState
@@ -123,19 +123,19 @@ func (am AppModule) InitGenesis(ctx sdk.Context, data json.RawMessage) []abci.Va
 	return InitGenesis(ctx, am.keeper, genesisState)
 }
 
-// ExportGenesis returns the exported genesis state as raw bytes for the clp
+// ExportGenesis returns the exported genesis state as raw bytes for the dispensation
 // module.
 func (am AppModule) ExportGenesis(ctx sdk.Context) json.RawMessage {
 	gs := ExportGenesis(ctx, am.keeper)
 	return ModuleCdc.MustMarshalJSON(gs)
 }
 
-// BeginBlock returns the begin blocker for the clp module.
+// BeginBlock returns the begin blocker for the dispensation module.
 func (am AppModule) BeginBlock(ctx sdk.Context, req abci.RequestBeginBlock) {
 	BeginBlocker(ctx, req, am.keeper)
 }
 
-// EndBlock returns the end blocker for the clp module. It returns no validator
+// EndBlock returns the end blocker for the dispensation module. It returns no validator
 // updates.
 func (AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
 	return []abci.ValidatorUpdate{}
