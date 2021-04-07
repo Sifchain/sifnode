2eeed907a5731aed7d20f4e28f1e3a65cefc629e
Tim Lind <tim@incremental.co>
2021-04-07 20:39:32 +0200

    TX cmd fixes

--- x/ethbridge/client/cli/tx.go
+++ x/ethbridge/client/cli/tx.go
@@ -98,7 +98,7 @@ func GetCmdCreateEthBridgeClaim() *cobra.Command {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, flags, &msg)
 		},
 	}
 }
@@ -218,7 +218,7 @@ func GetCmdLock() *cobra.Command {
 				return err
 			}
 
-			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, flags, &msg)
 		},
 	}
 }
