3c6422a8b68ee666420e32a31c193ecd0ff86c46
59023g <5235538+59023g@users.noreply.github.com>
2021-04-08 17:11:39 -0500

    ui/rewards: copied from feature/rewards branch

---
+++ ui/app/src/components/shared/Text/Copy.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  font-size: 12px; /* make $variable */
+  line-height: 14px; /* make $variable */
+  text-align: left;
+  font-weight: 400; /* make $variable */
+}
+</style>
