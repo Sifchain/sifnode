50c97e4791c6e6c10264da0c2dfecaaf79f5e921
Thomas Davis <thomasalwyndavis@gmail.com>
2021-04-14 10:38:38 +1000

    fix for e2e test on select token dialog

--- ui/app/src/components/tokenSelector/SelectTokenDialog.vue
+++ ui/app/src/components/tokenSelector/SelectTokenDialog.vue
@@ -93,7 +93,7 @@ export default defineComponent({
       <p>No tokens available.</p>
     </div>
     <button
-      :data-handle="tb.symbol + '-select-button'"
+      :data-handle="tb.asset.symbol + '-select-button'"
       class="option"
       v-for="tb in tokenBalances"
       :disabled="tb.asset.disabled"
