3aded8b08e184af08e85f45b53ff207ad162521e
juniuszhou <junius.zhou@gmail.com>
2021-04-16 06:59:35 +0800

    put the mint outside of case.

--- x/ethbridge/keeper/keeper.go
+++ x/ethbridge/keeper/keeper.go
@@ -70,17 +70,20 @@ func (k Keeper) ProcessSuccessfulClaim(ctx sdk.Context, claim string, sugaredLog
 	case types.LockText:
 		symbol := fmt.Sprintf("%v%v", types.PeggedCoinPrefix, oracleClaim.Symbol)
 		k.AddPeggyToken(ctx, symbol)
-
 		coins = sdk.Coins{sdk.NewCoin(symbol, oracleClaim.Amount)}
-		err = k.supplyKeeper.MintCoins(ctx, types.ModuleName, coins)
 	case types.BurnText:
 		coins = sdk.Coins{sdk.NewCoin(oracleClaim.Symbol, oracleClaim.Amount)}
-		// not mint if burn token is rowan
-		if oracleClaim.Symbol != types.NativeSymbol {
-			err = k.supplyKeeper.MintCoins(ctx, types.ModuleName, coins)
-		}
+
 	default:
 		err = types.ErrInvalidClaimType
+		sugaredLogger.Errorw("failed to process successful claim.",
+			errorMessageKey, err.Error())
+		return err
+	}
+
+	// not mint if burn token is rowan
+	if oracleClaim.Symbol != types.NativeSymbol {
+		err = k.supplyKeeper.MintCoins(ctx, types.ModuleName, coins)
 	}
 
 	if err != nil {
