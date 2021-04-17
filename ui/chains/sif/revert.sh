da8cc88bb375346190742ff2af2c2039f80f7d8a
Rudi Yardley <contact@rudiyardley.com>
2021-04-17 21:18:22 +1000

    Add logging

--- ui/chains/sif/revert.sh
+++ ui/chains/sif/revert.sh
@@ -2,10 +2,12 @@
 . ./config.sh
 
 # replace chain data with archive if exists
+echo "extracting..." 
 if [[ -f "$snapshot_loc" ]]; then
   rm -rf $db_loc
   mkdir -p $db_loc
   tar -zxf $snapshot_loc --directory $db_loc
+  echo "extracted '$snapshot_loc' to '$db_loc'"
 fi
 
 # restart sifnode
