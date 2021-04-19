f9c64df0563a108c9720c9be031051b7f5ed2383
Thomas Davis <thomasalwyndavis@gmail.com>
2021-04-19 22:49:47 +1000

    [ui] change rowan fee to a constant

--- ui/app/src/views/utils/getMaxAmount.ts
+++ ui/app/src/views/utils/getMaxAmount.ts
@@ -2,6 +2,10 @@ import { TransactionStatus } from "ui-core";
 import { effect, Ref, ref, ComputedRef } from "@vue/reactivity";
 import { IAssetAmount, IAmount, Amount } from "ui-core";
 
+// We set this static fee to minus from some ROWAN transactions such
+// that users don't have to manually minus it from KEPLR
+const ROWAN_GASS_FEE = Amount("0.5"); // 0.5 ROWAN
+
 export function getMaxAmount(
   symbol: Ref<string | null>,
   accountBalance: IAssetAmount,
@@ -12,8 +16,8 @@ export function getMaxAmount(
   if (symbol.value !== "rowan") {
     return accountBalance;
   } else {
-    if (accountBalance.greaterThan(Amount("0.5"))) {
-      const fee = 5 * Math.pow(10, accountBalance.decimals - 1); // 0.5 ROWAN
+    if (accountBalance.greaterThan(ROWAN_GASS_FEE)) {
+      const fee = 5 * Math.pow(10, accountBalance.decimals - 1);
       return accountBalance.subtract(Amount(fee.toString()));
     } else {
       return Amount("0");
