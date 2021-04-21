4b890bef60997adac41d6883fb6dbcb2be7fe5db
59023g <5235538+59023g@users.noreply.github.com>
2021-04-21 15:57:40 -0500

    ui/rewardsPage/DetailItem

---
+++ ui/app/src/components/rewardsPage/DetailItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="df fdr w100 item">
+      <div class="df fdr detail-item-amount">
+        <AssetItem
+          v-if="copyMap[pkey].icon"
+          :symbol="copyMap[pkey].icon"
+          :label="false"
+        />
+        <!-- TODO - Pass in type for Date vs Number -->
+        <span>{{ format(+item[pkey]) }}</span>
+        <span v-if="pkey === 'multiplier'">x</span>
+      </div>
+      <span class="mr-3">{{ copyMap[pkey].title }}</span>
+      <Tooltip :message="copyMap[pkey].tooltip">
+        <Icon icon="info-box-black" />
+      </Tooltip>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import AssetItem from "@/components/shared/AssetItem.vue";
+import Tooltip from "@/components/shared/Tooltip.vue";
+import Icon from "@/components/shared/Icon.vue";
+
+// NOTE - This will be removed and replaced with Amount API
+function format(amount: number) {
+  if (amount < 1) {
+    return amount.toFixed(6);
+  } else if (amount < 1000) {
+    return amount.toFixed(4);
+  } else if (amount < 100000) {
+    return amount.toFixed(2);
+  } else {
+    return amount.toFixed(0);
+  }
+}
+
+export default {
+  components: { AssetItem, Tooltip, Icon },
+  props: {
+    item: { type: Object, default: null },
+    copyMap: { type: Object, default: null },
+    pkey: { type: String, default: null },
+  },
+  methods: {
+    format,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.item {
+  margin-top: 8px;
+}
+.detail-item-amount {
+  width: 120px;
+}
+</style>
