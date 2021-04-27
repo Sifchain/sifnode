2d727b339c2ec6b98eaded59f32e3e2f24c9ca9d
Rudi Yardley <contact@rudiyardley.com>
2021-04-28 09:54:39 +1000

    Add comment; remove workflow

--- ui/scripts/build-stack-backend-docker.sh
+++ ui/scripts/build-stack-backend-docker.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-cd .. && docker build -f ./ui/scripts/stack.Dockerfile -t ryardley/sifdevstack01:experimental .
+
+
+# this is temporarily being published on rudis docker hub account - @ryarfley
+# TODO: investigate publishing on sifchain repository (possibly GH repo)
+cd .. && docker build -f ./ui/scripts/stack.Dockerfile -t ryardley/sifdevstack01:experimental1 .
+docker push ryardley/sifdevstack01:experimental1
