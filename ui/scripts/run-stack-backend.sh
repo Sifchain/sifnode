cff85eab5f3fd9843d4f0a668a7bb39d11a2d82d
Rudi Yardley <contact@rudiyardley.com>
2021-04-28 10:45:40 +1000

    Add better stopping

--- ui/scripts/run-stack-backend.sh
+++ ui/scripts/run-stack-backend.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker stop sifdevstack01 && docker rm sifdevstack01
+docker ps -q -f name=sifdevstack01 && docker stop sifdevstack01 && docker rm sifdevstack01
 
 # this runs a docker image built by the build command
 # the image temporarily will be pulled from dockerhub 
 # but at a point soon will be transferred to our GH actions repo
-docker run -it \
+docker run \
   -p 1317:1317 \
   -p 7545:7545 \
   -p 5000:5000 \
