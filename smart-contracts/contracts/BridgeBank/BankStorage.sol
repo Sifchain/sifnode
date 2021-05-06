4d2d858a6657c304aa89642f212498b4790bbf2a
Elliot Friedman <elliotfriedman3@gmail.com>
2021-05-06 17:18:26 -0400

    update bank storage

--- smart-contracts/contracts/BridgeBank/BankStorage.sol
+++ smart-contracts/contracts/BridgeBank/BankStorage.sol
@@ -1,11 +1,23 @@
 pragma solidity 0.8.0;
 
+import "./CosmosBankStorage.sol";
 import "./EthereumBankStorage.sol";
 import "./CosmosWhiteListStorage.sol";
 
 contract BankStorage is 
+    CosmosBankStorage,
     EthereumBankStorage,
     CosmosWhiteListStorage {
+
+    /**
+    * @notice DEPRECATED operator address that can update the smart contract
+    */
+    address public operator;
+
+    /**
+    * @notice DEPRECATED address of the Oracle smart contract
+    */
+    address public oracle;
 
     /**
     * @notice address of the Cosmos Bridge smart contract
@@ -16,6 +28,11 @@ contract BankStorage is
     * @notice owner address that can use the admin API
     */
     address public owner;
+
+    /**
+    * @notice DEPRECATED token limit
+    */
+    mapping (string => uint256) public maxTokenAmount;
 
     /*
     * @notice gap of storage for future upgrades
