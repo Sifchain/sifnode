13f7721fb3675b55612b7caa37cd859e4958fe83
Rudi Yardley <contact@rudiyardley.com>
2021-05-06 22:53:16 +1000

    Add script information

--- ui/scripts/build-stack-backend-docker.sh
+++ ui/scripts/build-stack-backend-docker.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+if  [[ $(cat ~/.docker/config.json  | jq '.auths["ghcr.io"].auth') == 'null' ]]; then
+  echo "In order to run this script and push a new container to the github registry you need to create a personal access token and use it to login to ghcr with docker"
+  echo ""
+  echo "  echo \$MY_PAT | docker login ghcr.io -u USERNAME --password-stdin"
+  echo ""
+  echo "For more information see https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry#authenticating-to-the-container-registry"
+  echo ""
+  echo "Create a personal access token and log into docker using the above link then try running this script again.s"
+  exit 1
+fi
 
-# this is temporarily being published on rudis docker hub account - @ryarfley
-# TODO: investigate publishing on sifchain repository (possibly GH repo)
-
+echo "Github Registry Login found."
+echo "Building new container..."
 
 NOW=$(date +%s)
 IMAGE_NAME=ghcr.io/sifchain/sifnode/ui-stack:$NOW
