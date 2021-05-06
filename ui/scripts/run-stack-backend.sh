be5ad4787a6dcf7bc717f0fbcbfbea5da1d2f590
Rudi Yardley <contact@rudiyardley.com>
2021-05-06 23:35:19 +1000

    Only use tty when running script locally

--- ui/scripts/run-stack-backend.sh
+++ ui/scripts/run-stack-backend.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# If CI don't use tty - this is so we can use Ctrl C to cancel the stack script localy
+[ -z "$CI" ] && IT="-it"
+
 # get latest image name from latest file
 IMAGE_NAME=$(cat ./scripts/latest)
 
@@ -9,7 +12,7 @@ docker ps -q -f name=sif-ui-stack && docker stop sif-ui-stack && docker rm sif-u
 # this runs a docker image built by the build command
 # the image temporarily will be pulled from dockerhub 
 # but at a point soon will be transferred to our GH actions repo
-docker run -it \
+docker run $IT \
   -p 1317:1317 \
   -p 7545:7545 \
   -p 26656:26656 \
