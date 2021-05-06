6c47faa1185ea2482619404e670fce6a552c34f3
Rudi Yardley <contact@rudiyardley.com>
2021-05-06 22:16:22 +1000

    Use generated image for instant stack

--- ui/scripts/build-stack-backend-docker.sh
+++ ui/scripts/build-stack-backend-docker.sh
@@ -3,5 +3,11 @@
 
 # this is temporarily being published on rudis docker hub account - @ryarfley
 # TODO: investigate publishing on sifchain repository (possibly GH repo)
-cd .. && docker build -f ./ui/scripts/stack.Dockerfile -t ryardley/sifdevstack01:experimental1 .
-docker push ryardley/sifdevstack01:experimental1
+
+
+NOW=$(date +%s)
+IMAGE_NAME=ghcr.io/sifchain/sifnode/ui-stack:$NOW
+cd .. && docker build -f ./ui/scripts/stack.Dockerfile -t $IMAGE_NAME .
+docker push $IMAGE_NAME
+
+echo $IMAGE_NAME > ./scripts/latest
