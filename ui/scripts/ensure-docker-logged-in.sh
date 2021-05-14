b16f5ee535e3f8cc6ad1bc37dd50fa8132787e72
Rudi Yardley <contact@rudiyardley.com>
2021-05-14 12:53:03 +1000

    Warn when not logged in to ghcr registry

---
+++ ui/scripts/ensure-docker-logged-in.sh
@@ -0,0 +1,10 @@
+if  [[ $(cat ~/.docker/config.json  | jq '.auths["ghcr.io"].auth') == 'null' ]]; then
+  echo "In order to run this script and push a new container to the github registry you need to create a personal access token and use it to login to ghcr with docker"
+  echo ""
+  echo "  echo \$MY_PAT | docker login ghcr.io -u USERNAME --password-stdin"
+  echo ""
+  echo "For more information see https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry#authenticating-to-the-container-registry"
+  echo ""
+  echo "Create a personal access token and log into docker using the above link then try running this script again."
+  exit 1
+fi
