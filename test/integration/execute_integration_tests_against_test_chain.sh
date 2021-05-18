82055cf7d0a20f829a38e69f71ddd5750c2aeced
James Moore <james@sifchain.finance>
2021-05-18 10:33:33 -0700

    Added passing tests to integration tests

--- test/integration/execute_integration_tests_against_test_chain.sh
+++ test/integration/execute_integration_tests_against_test_chain.sh
@@ -14,9 +14,9 @@ logecho $0 starting
 
 python3 -m pytest -olog_level=$loglevel -v -olog_file=/tmp/log.txt -v \
   ${TEST_INTEGRATION_PY_DIR}/test_new_currency_transfers.py \
+  ${TEST_INTEGRATION_PY_DIR}/test_random_currency_roundtrip.py \
 
 echo these tests do not run yet: \
-  ${TEST_INTEGRATION_PY_DIR}/test_random_currency_roundtrip.py \
   ${TEST_INTEGRATION_PY_DIR}/test_peggy_fees.py \
   ${TEST_INTEGRATION_PY_DIR}/test_rollback_chain.py \
   ${TEST_INTEGRATION_PY_DIR}/test_liquidity_pools.py \
