2556b71fad80330deb1e264fa9feadf8ab5c2b82
Pradeep Sheokand <pradeepk.sheokand@gmail.com>
2021-05-24 15:33:15 +0100

    Update test_dispensation_onlinetxn.py
    Fixed CICD errors in this version

--- test/integration/src/py/test_dispensation_onlinetxn.py
+++ test/integration/src/py/test_dispensation_onlinetxn.py
@@ -28,7 +28,7 @@ def create_online_singlekey_txn(
         sifnodecli_node
     ):
     logging.debug(f"create_online_dispensation")
-    sifchain_fees_entry = f"--gas 200064128"
+    sifchain_fees_entry = f"--gas auto"
     output = 'output.json'
     cmd = " ".join([
         "sifnodecli tx dispensation create",
@@ -36,6 +36,7 @@ def create_online_singlekey_txn(
         f"{claimType}",
         output,
         sifchain_fees_entry,
+        f"--fees 50000rowan",
         f"--from {signing_address}", 
         f"{chain_id}",
         f"{sifnodecli_node}",
@@ -57,7 +58,7 @@ def create_new_sifaddr_and_key():
 #CODE TO SEND SOME SAMPLE TOKEN TO NEW ADDRESS
 def send_sample_rowan(from_address,to_address,amount,keyring_backend,chain_id):
     logging.debug(f"transfer_rowan")
-    sifchain_fees_entry = f"--fees 10000rowan"
+    sifchain_fees_entry = f"--fees 150000rowan"
     keyring_backend_entry = f"--keyring-backend {keyring_backend}"     
     output = 'output.json'
     cmd = " ".join([
@@ -103,23 +104,42 @@ def balance_check(address,currency):
 @pytest.mark.parametrize("claimType", ['ValidatorSubsidy','LiquidityMining'])
 def test_create_online_singlekey_txn(claimType):
     sifchain_address = str(create_new_sifaddr_and_key())
-    from_address = 'sif'
-    #claimType = 'ValidatorSubsidy'
+    destaddress1 = str(create_new_sifaddr_and_key())
+    destaddress2 = str(create_new_sifaddr_and_key())
+    from_address = 'sifnodeadmin'
     dispensation_name = id_generator()
     keyring_backend = 'test'
     chain_id = 'localnet'
     sifnodecli_node = 'tcp://127.0.0.1:1317'
     amount = '10000000rowan'
+    fee='50000'
     currency = 'rowan'
+    sampleamount = '1000rowan'
 
-    #THIS TXN IS TO REGISTER A NEW ACCOUNT ON CHAIN
+    #THESE 3 TXNS ARE TO REGISTER NEW ACCOUNTS ON CHAIN
     send_sample_rowan(from_address,sifchain_address,amount,keyring_backend,chain_id)
     time.sleep(5)
+    send_sample_rowan(from_address,destaddress1,sampleamount,keyring_backend,chain_id)
+    time.sleep(5)
+    send_sample_rowan(from_address,destaddress2,sampleamount,keyring_backend,chain_id)
+    time.sleep(5)
+
+    #CREATING TEST DATA HERE MIMICKING OUTPUT.JSON TO BE SUPPLIED BY NIKO'S API
+    dict1 = {"denom": "rowan","amount": "5000"}
+    dict2 = {"denom": "rowan","amount": "7000"}
+    dict3 = {"address": destaddress1,"coins":[dict1]}
+    dict4 = {"address": destaddress2,"coins":[dict2]}
+    dict5 = {"Output":[dict3,dict4]}
+    data = json.dumps(dict5)
+    with open("output.json","w") as f:
+        f.write(data)
 
     #READ OUTPUT.JSON WITH CLAIMING ADDRESSES AND AMOUNT
-    with open('output.json') as f:
-        opjson = json.load(f)
-    one_claiming_address = str(opjson['Output'][0]['address'])
+    with open("output.json","r") as f:
+        data = f.read()
+    d = json.loads(data)
+    
+    one_claiming_address = str(d['Output'][0]['address'])
     logging.info(f"one claiming address = {one_claiming_address}")
     
      #SENDER AND RECIPENT INITIAL BALANCE 
@@ -128,94 +148,109 @@ def test_create_online_singlekey_txn(claimType):
     logging.info(f"sender initial balance = {sender_initial_balance}")
     logging.info(f"one claiming address initial balance = {claiming_address_initial_balance}")
     
-    try:
-        #ACTUAL DISPENSATION TXN; GET TXN HASH
-        txhash = str((create_online_singlekey_txn(claimType,dispensation_name,sifchain_address,chain_id,sifnodecli_node)))
-        logging.info(f"txn hash = {txhash}")
-        time.sleep(5)
+    #ACTUAL DISPENSATION TXN; GET TXN HASH
+    txhash = str((create_online_singlekey_txn(claimType,dispensation_name,sifchain_address,chain_id,sifnodecli_node)))
+    logging.info(f"txn hash = {txhash}")
+    time.sleep(5)
 
-        #QUERY BLOCK USING TXN HASH
-        resp = query_block_claim(txhash)
+    #QUERY BLOCK USING TXN HASH
+    resp = query_block_claim(txhash)
 
-        #READ SPECIFIC DISPENSATION TXN JSON TAGS
-        distbstart = resp['logs'][0]['events'][0]['type']
-        dispattb = resp['logs'][0]['events'][0]['attributes'][0]
-        distype = resp['tx']['value']['msg'][0]['type']
-        disvals = resp['tx']['value']['msg'][0]['value']
-        account_key = str((dispattb['key']))
-        bcasttags = list(disvals.keys())
-        list_of_values = [disvals[key] for key in disvals]
+    #READ DISPENSATION TXN JSON TAGS
+    distributionstartedtag = resp['logs'][0]['events'][0]['type']
+    distributionattributesttags = resp['logs'][0]['events'][0]['attributes'][0]
+    distributiontypetag = resp['tx']['value']['msg'][0]['type']
+    distributionvaluetags = resp['tx']['value']['msg'][0]['value']
+    account_key = str((distributionattributesttags['key']))
+    chaintags = list(distributionvaluetags.keys())
+    list_of_values = [distributionvaluetags[key] for key in distributionvaluetags]
 
-        #DISTRIBUTION TXN JSON TAGS ASSERTIONS
-        assert str(distbstart) == 'distribution_started'  
-        assert str(account_key) == 'module_account'
-        assert str(distype) == 'dispensation/create'
-        assert bcasttags[0] == 'distributor'
-        assert bcasttags[1] == 'distribution_name'
-        assert bcasttags[2] == 'distribution_type'
-        assert bcasttags[3] == 'Output'
-        assert list_of_values[0] == sifchain_address
-        assert list_of_values[1] == dispensation_name
+    #DISTRIBUTION TXN JSON TAGS ASSERTIONS
+    assert str(distributionstartedtag) == 'distribution_started'  
+    assert str(account_key) == 'module_account'
+    assert str(distributiontypetag) == 'dispensation/create'
+    assert chaintags[0] == 'distributor'
+    assert chaintags[1] == 'distribution_name'
+    assert chaintags[2] == 'distribution_type'
+    assert chaintags[3] == 'output'
+    assert list_of_values[0] == sifchain_address
+    assert list_of_values[1] == dispensation_name
 
-        txn_signer_sender_address = resp['tx']['value']['msg'][0]['value']['distributor']
-        distributionaddresslist = resp['tx']['value']['msg'][0]['value']['Output']
-        recipient_dispensation_addresses = [] 
-        amount_distributed = []
-        for dic in distributionaddresslist:
-            recipient_dispensation_addresses.append(dic['address']) 
-            for val in dic['coins']:
-                amount_distributed.append(val['amount'])
+    txn_signer_sender_address = resp['tx']['value']['msg'][0]['value']['distributor']
+    distributionaddresslist = resp['tx']['value']['msg'][0]['value']['output']
+    recipient_dispensation_addresses = [] 
+    amount_distributed = []
+    for dic in distributionaddresslist:
+        recipient_dispensation_addresses.append(dic['address']) 
+        for val in dic['coins']:
+            amount_distributed.append(val['amount'])
 
-        logging.info(f"dispensation txn addresses = {recipient_dispensation_addresses}")  
-        logging.info(f"amount distributed = {amount_distributed}") 
+    logging.info(f"dispensation txn addresses = {recipient_dispensation_addresses}")  
+    logging.info(f"amount distributed = {amount_distributed}") 
     
-        total_amount_distributed = sum(int(i) for i in amount_distributed)
-        recipient_with_respective_distributed_amount = dict(zip(recipient_dispensation_addresses, amount_distributed))
+    total_amount_distributed = sum(int(i) for i in amount_distributed)
+    recipient_with_respective_distributed_amount = dict(zip(recipient_dispensation_addresses, amount_distributed))
 
-        logging.info(f"recipients and their respective distributed amounts = {recipient_with_respective_distributed_amount}") 
-        logging.info(f"total amount distributed = {total_amount_distributed}") 
+    logging.info(f"recipients and their respective distributed amounts = {recipient_with_respective_distributed_amount}") 
+    logging.info(f"total amount distributed = {total_amount_distributed}") 
 
-        sender_final_balance = int(balance_check(sifchain_address,currency))
-        recipient_address_final_balance = int(balance_check(one_claiming_address,currency))  
+    sender_final_balance = int(balance_check(sifchain_address,currency))
+    recipient_address_final_balance = int(balance_check(one_claiming_address,currency))  
     
-        logging.info(f"sender initial balance = {sender_initial_balance}") 
-        logging.info(f"sender final balance = {sender_final_balance}") 
+    logging.info(f"sender initial balance = {sender_initial_balance}") 
+    logging.info(f"sender final balance = {sender_final_balance}") 
     
-        claimed_amount_single_recipient = int(recipient_with_respective_distributed_amount[one_claiming_address]) 
+    claimed_amount_single_recipient = int(recipient_with_respective_distributed_amount[one_claiming_address]) 
         
-        #BALANCES ASSERTIONS
-        assert int(total_amount_distributed) == int(sender_initial_balance - sender_final_balance)
-        assert int(claimed_amount_single_recipient) == (recipient_address_final_balance - claiming_address_initial_balance) 
-        logging.info(f"balance transferred from sender's address  = {(sender_initial_balance - sender_final_balance)}")  
-        logging.info(f"total amount distributed  = {total_amount_distributed}")
+    #BALANCES ASSERTIONS
+    assert int(total_amount_distributed) == int((sender_initial_balance - sender_final_balance) - int(fee))
+    assert int(claimed_amount_single_recipient) == (recipient_address_final_balance - claiming_address_initial_balance) 
+    logging.info(f"balance transferred including fee from sender's address  = {(sender_initial_balance - sender_final_balance)}")  
+    logging.info(f"total amount distributed  = {total_amount_distributed}")
         
-        logging.info(f"amount claimed by one recipient  = {claimed_amount_single_recipient}")
-        logging.info(f"balance transferred in one recipient address  = {(recipient_address_final_balance - claiming_address_initial_balance)}")
+    logging.info(f"amount claimed by one recipient  = {claimed_amount_single_recipient}")
+    logging.info(f"balance transferred in one recipient address  = {(recipient_address_final_balance - claiming_address_initial_balance)}")
 
-    except Exception as e:
-            logging.error(f"error: {e}")   
-
-#AUTOMTED TEST TO VALIDATE IF FUNDING ADDRESS DOESN'T HAVE ENOUGH BALANCE
+ #AUTOMTED TEST TO VALIDATE IF FUNDING ADDRESS DOESN'T HAVE ENOUGH BALANCE
 @pytest.mark.parametrize("claimType", ['ValidatorSubsidy','LiquidityMining'])
 def test_insufficient_funds_dispensation_txn(claimType):
     sifchain_address = str(create_new_sifaddr_and_key())
-    from_address = 'sif'
-    claimType = 'ValidatorSubsidy'
+    destaddress1 = str(create_new_sifaddr_and_key())
+    destaddress2 = str(create_new_sifaddr_and_key())
+    from_address = 'sifnodeadmin'
     dispensation_name = id_generator()
     keyring_backend = 'test'
     chain_id = 'localnet'
     sifnodecli_node = 'tcp://127.0.0.1:1317'
-    amount = '100rowan'
+    amount = '70000rowan'
+    fee='50000'
     currency = 'rowan'
+    sampleamount = '1000rowan'
 
-    #THIS TXN IS TO REGISTER A NEW ACCOUNT ON CHAIN
+    #THESE 3 TXNS ARE TO REGISTER NEW ACCOUNTS ON CHAIN
     send_sample_rowan(from_address,sifchain_address,amount,keyring_backend,chain_id)
     time.sleep(5)
+    send_sample_rowan(from_address,destaddress1,sampleamount,keyring_backend,chain_id)
+    time.sleep(5)
+    send_sample_rowan(from_address,destaddress2,sampleamount,keyring_backend,chain_id)
+    time.sleep(5)
+
+    #CREATING TEST DATA HERE MIMICKING OUTPUT.JSON TO BE SUPPLIED BY NIKO'S API
+    dict1 = {"denom": "rowan","amount": "5000"}
+    dict2 = {"denom": "rowan","amount": "19000"}
+    dict3 = {"address": destaddress1,"coins":[dict1]}
+    dict4 = {"address": destaddress2,"coins":[dict2]}
+    dict5 = {"Output":[dict3,dict4]}
+    data = json.dumps(dict5)
+    with open("output.json","w") as f:
+        f.write(data)
 
     #READ OUTPUT.JSON WITH CLAIMING ADDRESSES AND AMOUNT
-    with open('output.json') as f:
-        opjson = json.load(f)
-    one_claiming_address = str(opjson['Output'][0]['address'])
+    with open("output.json","r") as f:
+        data = f.read()
+    d = json.loads(data)
+    
+    one_claiming_address = str(d['Output'][0]['address'])
     logging.info(f"one claiming address = {one_claiming_address}")
     
      #SENDER AND RECIPENT INITIAL BALANCE 
@@ -224,18 +259,8 @@ def test_insufficient_funds_dispensation_txn(claimType):
     logging.info(f"sender initial balance = {sender_initial_balance}")
     logging.info(f"one claiming address initial balance = {claiming_address_initial_balance}")
     
-    try:
-        #ACTUAL DISPENSATION TXN; GET TXN HASH
+    #ACTUAL DISPENSATION TXN; TXN RAISES AN EXCEPTION ABOUT INSUFFICIENT FUNDS, CAPTURED HERE AND TEST IS MARKED PASS
+    with pytest.raises(Exception) as execinfo:
         txhash = str((create_online_singlekey_txn(claimType,dispensation_name,sifchain_address,chain_id,sifnodecli_node)))
-        logging.info(f"txn hash = {txhash}")
-        time.sleep(5)
-
-        #QUERY BLOCK USING TXN HASH
-        resp = query_block_claim(txhash)
-        assert resp['raw_log'] == f"for address  : {sifchain_address}: Failed in collecting funds for airdrop: failed to execute message; message index: 0"
-        logging.info(f"Insufficient Funds Message = {resp['raw_log']}")
-    except Exception as e:
-            logging.error(f"error: {e}")
-
-    
-    
+        assert str(execinfo.value) == f"for address  : {sifchain_address}: Failed in collecting funds for airdrop: failed to execute message; message index: 0: failed to simulate tx"
+        logging.info(f"Insufficient Funds Message = {resp['raw_log']}") 
