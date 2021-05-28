0d746ee79dd72a0e91c73b2bed528554e6755c7a
James Moore <james@sifchain.finance>
2021-05-28 14:39:36 -0700

    Added restart tests to run list for github CI

--- test/integration/execute_integration_tests_against_test_chain.sh
+++ test/integration/execute_integration_tests_against_test_chain.sh
@@ -17,6 +17,7 @@ python3 -m pytest -olog_level=$loglevel -v -olog_file=/tmp/log.txt -v \
   ${TEST_INTEGRATION_PY_DIR}/test_random_currency_roundtrip.py \
   ${TEST_INTEGRATION_PY_DIR}/test_rollback_chain.py \
   ${TEST_INTEGRATION_PY_DIR}/test_ebrelayer_replay.py \
+  ${TEST_INTEGRATION_PY_DIR}/test_ebrelayer_restart.py \
 
 echo these tests do not run yet: \
   ${TEST_INTEGRATION_PY_DIR}/test_peggy_fees.py \
