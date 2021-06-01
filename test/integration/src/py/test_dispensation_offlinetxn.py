8f2163f4fbd2aa524a76b239918b111a270185c6
Pradeep Sheokand <pradeepk.sheokand@gmail.com>
2021-06-01 22:41:54 +0100

    Integration tests updates
    1) Added a new file that contains all helper/set-up functions for dispensation
    2) Updated onlinetxn and offlinetxn files to remove dispensation_name from CLI parameters after Tanmay's code refactoring

--- test/integration/src/py/test_dispensation_offlinetxn.py
+++ test/integration/src/py/test_dispensation_offlinetxn.py
@@ -5,82 +5,9 @@ import json
 import pytest
 import string
 import random
+from dispensation_envutils import create_offline_singlekey_txn, create_new_sifaddr_and_key, send_sample_rowan, balance_check, \
+     query_block_claim,sign_txn,broadcast_txn,broadcast_async_txn
 
-import burn_lock_functions
-from burn_lock_functions import EthereumToSifchainTransferRequest
-import test_utilities
-from pytest_utilities import generate_test_account
-from test_dispensation_onlinetxn import create_new_sifaddr_and_key, send_sample_rowan
-from test_utilities import get_required_env_var, SifchaincliCredentials, get_optional_env_var, ganache_owner_account, \
-    get_shell_output_json, get_shell_output, detect_errors_in_sifnodecli_output, get_transaction_result, amount_in_wei
-
-#CODE TO GENERATE RANDOM STRING FOR DISPENSATION NAME
-def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-#CODE TO GENERATE OFFLINE DISPENSATION TXN
-def create_offline_singlekey_txn(
-        claimType,
-        dispensation_name,
-        signing_address,
-        chain_id,
-        sifnodecli_node
-    ):
-    logging.debug(f"create_unsigned_offline_dispensation_txn")
-    sifchain_fees_entry = f"--gas auto"
-    output = 'output.json'
-    cmd = " ".join([
-        "sifnodecli tx dispensation create",
-        f"{claimType}",
-        output,
-        f"--from {signing_address}",
-        f"--chain-id={chain_id}",
-        f"{sifnodecli_node}",
-        f"--fees 150000rowan",
-        f"--generate-only", 
-        f"--yes -o json"
-        
-    ])
-    json_str = get_shell_output_json(cmd)
-    assert(json_str.get("code", 0) == 0)
-    return json_str
-
-#CODE TO SIGN DISPENSATION BY A USER
-def sign_txn(signingaddress, offlinetx):
-    keyring_backend_entry = f"--keyring-backend test"
-    cmd = " ".join([
-        "sifnodecli tx sign",
-        f"--from {signingaddress}",
-        f"{offlinetx}",
-        keyring_backend_entry,
-        "--chain-id localnet",
-        f"--yes -o json"
-    ])
-    json_str = get_shell_output_json(cmd)
-    return json_str
-
-
-#CODE TO BROADCAST SINGLE SIGNED TXN ON BLOCK
-def broadcast_txn(signedtx):
-    cmd = " ".join([
-        "sifnodecli tx broadcast",
-        f"{signedtx}",
-        f"--yes -o json"
-    ])
-    json_str = get_shell_output_json(cmd)
-    txn = json_str["txhash"]
-    return txn
-
-#CODE TO QUERY BLOCK FOR NEW DISPENSATION TXN
-def query_block_claim(txnhash):
-    cmd = " ".join([
-        "sifnodecli q tx",
-        f"{txnhash}",
-        "--chain-id localnet",
-        f"-o json"
-    ])
-    json_str = get_shell_output_json(cmd)
-    return json_str
 
 #TEST CODE TO ASSERT TAGS GENERATED ON A BLOCK WHEN A NEW UNSIGNED DISPENSATION IS CREATED
 @pytest.mark.parametrize("claimType", ['ValidatorSubsidy','LiquidityMining'])
@@ -92,7 +19,6 @@ def test_create_offline_singlekey_txn(claimType):
     destaddress2, destname2 = create_new_sifaddr_and_key()
     logging.info(f"destaddress2 = {destaddress2}, destname2 = {destname2}")
     from_address = 'sifnodeadmin'
-    dispensation_name = id_generator()
     keyring_backend = 'test'
     chain_id = 'localnet'
     sifnodecli_node = 'tcp://127.0.0.1:1317'
@@ -125,7 +51,7 @@ def test_create_offline_singlekey_txn(claimType):
     d = json.loads(data)
     
   
-    response = (create_offline_singlekey_txn(claimType,dispensation_name,sifchain_address,chain_id,sifnodecli_node))
+    response = (create_offline_singlekey_txn(claimType,sifchain_address,chain_id,sifnodecli_node))
     
     distributiontypetag = response['value']['msg'][0]['type']
     distributionvaluetags = response['value']['msg'][0]['value']
@@ -135,13 +61,14 @@ def test_create_offline_singlekey_txn(claimType):
         
     assert str(distributiontypetag) == 'dispensation/create'
     assert actuallisttags[0] == 'distributor'
+    #assert actuallisttags[1] == 'distribution_name'
     assert actuallisttags[1] == 'distribution_type'
-    assert actuallisttags[2] == 'distribution_name'
-    assert actuallisttags[3] == 'output'
+    assert actuallisttags[2] == 'output'
     try:
         os.remove('output.json')
     except OSError as e:
         print ("Error: %s - %s." % (e.filename, e.strerror))
+
         
 #TEST CODE TO ASSERT TAGS GENERATED ON A BLOCK WHEN A NEW SIGNED DISPENSATION IS BROADCASTED on BLOCKCHAIN
 @pytest.mark.parametrize("claimType", ['ValidatorSubsidy','LiquidityMining'])
@@ -151,7 +78,6 @@ def test_broadcast_txn(claimType):
     destaddress1, destname1 = create_new_sifaddr_and_key()
     destaddress2, destname2 = create_new_sifaddr_and_key()
     from_address = 'sifnodeadmin'
-    dispensation_name = id_generator()
     keyring_backend = 'test'
     chain_id = 'localnet'
     sifnodecli_node = 'tcp://127.0.0.1:1317'
@@ -183,15 +109,15 @@ def test_broadcast_txn(claimType):
         data = f.read()
     d = json.loads(data)
       
-    response = (create_offline_singlekey_txn(claimType,dispensation_name,sifchain_address,chain_id,sifnodecli_node))
+    response = (create_offline_singlekey_txn(claimType,sifchain_address,chain_id,sifnodecli_node))
     with open("sample.json", "w") as outfile: 
         json.dump(response, outfile)
     
     sigresponse = sign_txn(sifchain_name, 'sample.json')
     with open("signed.json", "w") as sigfile: 
         json.dump(sigresponse, sigfile)
-        
-    txhashbcast = broadcast_txn('signed.json')
+      
+    txhashbcast = broadcast_async_txn('signed.json')
     time.sleep(5)
     resp = query_block_claim(txhashbcast)
     distypebcast = (resp['tx']['value']['msg'][0]['type'])
@@ -201,13 +127,13 @@ def test_broadcast_txn(claimType):
     assert str(distypebcast) == 'dispensation/create'
     assert broadcasttags[0] == 'distributor'
     assert broadcasttags[1] == 'distribution_type'
-    assert broadcasttags[2] == 'distribution_name'
-    assert broadcasttags[3] == 'output'
+    assert broadcasttags[2] == 'output'
     assert list_of_values[0] == sifchain_address
-
     try:
         os.remove('signed.json')
         os.remove('sample.json')
-        os.remove('sample.json')
+        os.remove('output.json')
     except OSError as e:
             print ("Error: %s - %s." % (e.filename, e.strerror)) 
+
+
--- test/integration/src/py/test_dispensation_onlinetxn.py
+++ test/integration/src/py/test_dispensation_onlinetxn.py
@@ -5,120 +5,16 @@ import json
 import pytest
 import string
 import random
-
-import burn_lock_functions
-from burn_lock_functions import EthereumToSifchainTransferRequest
-import test_utilities
-from pytest_utilities import generate_test_account
-from integration_env_credentials import sifchain_cli_credentials_for_test
-from test_utilities import get_required_env_var, SifchaincliCredentials, get_optional_env_var, ganache_owner_account, \
-    get_shell_output_json, get_shell_output, detect_errors_in_sifnodecli_output, get_transaction_result, amount_in_wei
-
-
-# CODE TO GENERATE RANDOM STRING FOR DISPENSATION NAME AS DISPENSATION NAME IS A UNIQUE KEY
-def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-
-# CODE TO CREATE A NEW SINGLE-KEY ONLINE TXN
-# FROM ADDRESS IS THE SIGNING/FUNDING ADDRESS; OUTPUT.JSON CONTAINS CLAIM RECIPIENT ADDRESSES
-def create_online_singlekey_txn(
-        claimType,
-        dispensation_name,
-        signing_address,
-        chain_id,
-        sifnodecli_node
-):
-    logging.debug(f"create_online_dispensation")
-    sifchain_fees_entry = f"--gas auto"
-    keyring_backend_entry = f"--keyring-backend test"
-    output = 'output.json'
-    cmd = " ".join([
-        "sifnodecli tx dispensation create",
-        f"{claimType}",
-        output,
-        sifchain_fees_entry,
-        f"--fees 50000rowan",
-        f"--from {signing_address}",
-        f"--chain-id={chain_id}",
-        f"{sifnodecli_node}",
-        keyring_backend_entry,
-        f"--yes -o json"
-
-    ])
-    json_str = get_shell_output_json(cmd)
-    assert (json_str.get("code", 0) == 0)
-    txn = json_str["txhash"]
-    return txn
-
-
-# CODE TO GENERATE NEW ADDRESS
-def create_new_sifaddr_and_key():
-    new_account_key = test_utilities.get_shell_output("uuidgen").replace("-", "")
-    credentials = sifchain_cli_credentials_for_test(new_account_key)
-    new_addr = burn_lock_functions.create_new_sifaddr(credentials=credentials, keyname=new_account_key)
-    return new_addr["address"], new_addr["name"]
-
-
-# CODE TO SEND SOME SAMPLE TOKEN TO NEW ADDRESS
-def send_sample_rowan(from_address, to_address, amount, keyring_backend, chain_id):
-    logging.debug(f"transfer_rowan")
-    sifchain_fees_entry = f"--fees 150000rowan"
-    keyring_backend_entry = f"--keyring-backend {keyring_backend}"
-    output = 'output.json'
-    cmd = " ".join([
-        "sifnodecli tx send",
-        f"{from_address}",
-        f"{to_address}",
-        f"{amount}",
-        keyring_backend_entry,
-        sifchain_fees_entry,
-        f"--chain-id {chain_id}",
-        f"--yes -o json"
-
-    ])
-    json_str = get_shell_output_json(cmd)
-    assert (json_str.get("code", 0) == 0)
-    return json_str
-
-
-# CODE TO QUERY BLOCK FOR NEW DISPENSATION TXN
-def query_block_claim(txnhash):
-    cmd = " ".join([
-        "sifnodecli q tx",
-        f"{txnhash}",
-        "--chain-id localnet",
-        "-o json"
-    ])
-    json_str = get_shell_output_json(cmd)
-    return json_str
-
-
-# CODE TO CHECK ACCOUNT BALANCE
-def balance_check(address, currency):
-    logging.debug(f"check_balance")
-    cmd = " ".join([
-        "sifnodecli query account",
-        f"{address}",
-        f"-o json"
-    ])
-    json_str = get_shell_output_json(cmd)
-    amountbalance = json_str['value']['coins']
-    for i in amountbalance:
-        if i['denom'] == currency:
-            balance = i['amount']
-    return (balance)
-
+from dispensation_envutils import create_online_singlekey_txn, create_new_sifaddr_and_key, send_sample_rowan, balance_check, query_block_claim
 
 # AUTOMATED TEST TO VALIDATE ONLINE TXN
-@pytest.mark.parametrize("claimType", ['ValidatorSubsidy', 'LiquidityMining'])
+@pytest.mark.parametrize("claimType", ['ValidatorSubsidy','LiquidityMining'])
 def test_create_online_singlekey_txn(claimType):
     sifchain_address, sifchain_name = create_new_sifaddr_and_key()
     logging.info(f"sifchain_address = {sifchain_address}, sifchain_name = {sifchain_name}")
     destaddress1, destname1 = create_new_sifaddr_and_key()
     destaddress2, destname2 = create_new_sifaddr_and_key()
     from_address = 'sifnodeadmin'
-    dispensation_name = id_generator()
     keyring_backend = 'test'
     chain_id = 'localnet'
     sifnodecli_node = 'tcp://127.0.0.1:1317'
@@ -126,7 +22,7 @@ def test_create_online_singlekey_txn(claimType):
     fee = '50000'
     currency = 'rowan'
     sampleamount = '1000rowan'
-
+    
     # THESE 3 TXNS ARE TO REGISTER NEW ACCOUNTS ON CHAIN
     send_sample_rowan(from_address, sifchain_address, amount, keyring_backend, chain_id)
     time.sleep(5)
@@ -160,14 +56,15 @@ def test_create_online_singlekey_txn(claimType):
     logging.info(f"one claiming address initial balance = {claiming_address_initial_balance}")
 
     # ACTUAL DISPENSATION TXN; GET TXN HASH
-    txhash = str((create_online_singlekey_txn(claimType, dispensation_name, sifchain_name, chain_id, sifnodecli_node)))
+    txhash = str((create_online_singlekey_txn(claimType, sifchain_name, chain_id, sifnodecli_node)))
     logging.info(f"txn hash = {txhash}")
     time.sleep(5)
 
     # QUERY BLOCK USING TXN HASH
     resp = query_block_claim(txhash)
+    logging.info(f"valid hash response = {txhash}")
 
-    # READ DISPENSATION TXN JSON TAGS
+     # READ DISPENSATION TXN JSON TAGS
     distributionstartedtag = resp['logs'][0]['events'][0]['type']
     distributionattributesttags = resp['logs'][0]['events'][0]['attributes'][0]
     distributiontypetag = resp['tx']['value']['msg'][0]['type']
@@ -175,17 +72,17 @@ def test_create_online_singlekey_txn(claimType):
     account_key = str((distributionattributesttags['key']))
     chaintags = list(distributionvaluetags.keys())
     list_of_values = [distributionvaluetags[key] for key in distributionvaluetags]
-
+    print(chaintags)
+    print(list_of_values)
     # DISTRIBUTION TXN JSON TAGS ASSERTIONS
     assert str(distributionstartedtag) == 'distribution_started'
     assert str(account_key) == 'module_account'
     assert str(distributiontypetag) == 'dispensation/create'
     assert chaintags[0] == 'distributor'
     assert chaintags[1] == 'distribution_type'
-    assert chaintags[2] == 'distribution_name'
-    assert chaintags[3] == 'output'
+    assert chaintags[2] == 'output'
     assert list_of_values[0] == sifchain_address
-
+    
     txn_signer_sender_address = resp['tx']['value']['msg'][0]['value']['distributor']
     distributionaddresslist = resp['tx']['value']['msg'][0]['value']['output']
     recipient_dispensation_addresses = []
@@ -232,7 +129,6 @@ def test_insufficient_funds_dispensation_txn(claimType):
     destaddress1, destname1 = create_new_sifaddr_and_key()
     destaddress2, destname2 = create_new_sifaddr_and_key()
     from_address = 'sifnodeadmin'
-    dispensation_name = id_generator()
     keyring_backend = 'test'
     chain_id = 'localnet'
     sifnodecli_node = 'tcp://127.0.0.1:1317'
@@ -276,7 +172,7 @@ def test_insufficient_funds_dispensation_txn(claimType):
     # ACTUAL DISPENSATION TXN; TXN RAISES AN EXCEPTION ABOUT INSUFFICIENT FUNDS, CAPTURED HERE AND TEST IS MARKED PASS
     with pytest.raises(Exception) as execinfo:
         txhash = str(
-            (create_online_singlekey_txn(claimType, dispensation_name, sifchain_address, chain_id, sifnodecli_node)))
+            (create_online_singlekey_txn(claimType, sifchain_address, chain_id, sifnodecli_node)))
         assert str(
             execinfo.value) == f"for address  : {sifchain_address}: Failed in collecting funds for airdrop: failed to execute message; message index: 0: failed to simulate tx"
         logging.info(f"Insufficient Funds Message = {resp['raw_log']}")
