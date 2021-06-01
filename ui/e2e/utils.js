961c5e2b0d2e13ec33d46884c97ce8238a9d96a1
Piotr Gesicki <piotr.gesicki@gmail.com>
2021-06-01 15:32:20 +0200

    add on demand screenshot method

--- ui/e2e/utils.js
+++ ui/e2e/utils.js
@@ -3,6 +3,7 @@ import StreamZip from "node-stream-zip";
 import axios from "axios";
 import fs from "fs";
 import path from "path";
+import mkdirp from "mkdirp";
 
 const retry = require("retry-assert");
 
@@ -45,8 +46,8 @@ export async function getExtensionPage(extensionId, suffixUrl = undefined) {
   } else {
     matchingUrl = `chrome-extension://${extensionId}${suffixUrl}`;
   }
-  const pages = await context.pages();
 
+  const pages = await context.pages();
   const foundPages = pages.filter((page) => page.url().includes(matchingUrl));
 
   if (foundPages.length > 0) return foundPages[0];
@@ -82,4 +83,30 @@ export async function assertWaitedValue(
     .fn(() => getInputValue(selector))
     .withTimeout(timeout)
     .until((value) => expect(value).toBe(expectedValue));
+}
+
+export async function takeScreenshot(name) {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getUTCMonth() + 1;
+  const dateOfMonth = date.getUTCDate();
+  const hour = date.getUTCHours();
+  const minute = date.getUTCMinutes();
+  const sec = date.getUTCSeconds();
+  const dateString = `${year}-${month}-${dateOfMonth}-${hour}-${minute}-${sec}`;
+
+  const screenshotPath = `screenshots/${browserName}-${dateString}-${name.replace(
+    / /g,
+    "_",
+  )}`;
+
+  await mkdirp("screenshots");
+
+  const pages = await context.pages();
+  await pages.forEach(async (page) => {
+    const title = await page.title();
+    await page.screenshot({
+      path: `${screenshotPath}_${title}.png`,
+    });
+  });
 }
