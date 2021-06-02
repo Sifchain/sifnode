35c5877e4e96955576f9f2e39c16ec74e03dfa44
59023g <5235538+59023g@users.noreply.github.com>
2021-06-01 19:37:26 -0600

    ui/app/DetailsPanelRemove: include rewards opportunity, pairtable

--- ui/app/src/components/shared/DetailsPanelRemove.vue
+++ ui/app/src/components/shared/DetailsPanelRemove.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="details">
-    <div class="details-header">
-      <h4 class="text--left">You Should Receive:</h4>
+  <div>
+    <Copy class="mb-8">
+      Are you sure you want to claim your rewards? Once you claim these rewards,
+      your multiplier will reset to 1x for all remaining amounts and will
+      continue to accumulate if within the reward eligibility timeframe.
+    </Copy>
+    <Copy class="mb-8">
+      Please note that the rewards will be released at the end of the week.
+    </Copy>
+    <Copy class="mb-8">
+      Find out <a href="">additional information here</a>.
+    </Copy>
+    <PairTable class="mb-8" :items="computedPairPanel" />
+    <div class="details mb-8">
+      <div class="details-header">
+        <h4 class="text--left">You Should Receive:</h4>
 
-      <div class="details-row">
-        <AssetItem :symbol="nativeAssetSymbol" /> {{ nativeAssetAmount }}
-      </div>
-      <div class="details-row">
-        <AssetItem :symbol="externalAssetSymbol" /> {{ externalAssetAmount }}
+        <div class="details-row">
+          <AssetItem :symbol="nativeAssetSymbol" /> {{ nativeAssetAmount }}
+        </div>
+        <div class="details-row">
+          <AssetItem :symbol="externalAssetSymbol" /> {{ externalAssetAmount }}
+        </div>
       </div>
     </div>
+    <br /><br />
   </div>
 </template>
 
@@ -53,16 +68,36 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import AssetItem from "@/components/shared/AssetItem.vue";
+import { Copy } from "@/components/shared/Text";
+import PairTable from "@/components/shared/PairTable.vue";
 
 export default defineComponent({
   components: {
     AssetItem,
+    Copy,
+    PairTable,
   },
   props: {
+    rewardsData: { type: Object, default: {} },
     nativeAssetSymbol: { type: String, default: "" },
     nativeAssetAmount: { type: String, default: "" },
     externalAssetAmount: { type: String, default: "" },
     externalAssetSymbol: { type: String, default: "" },
+  },
+  computed: {
+    computedPairPanel(): Array<Object> {
+      return [
+        {
+          key: "Claimable  Rewards",
+          value: this.$props.rewardsData
+            .totalClaimableCommissionsAndClaimableRewards,
+        },
+        {
+          key: "Projected Full Amount",
+          value: this.$props.rewardsData.totalCommissionsAndRewardsAtMaturity,
+        },
+      ];
+    },
   },
 });
 </script>
