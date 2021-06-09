8fc820320124e36311b7b8116c8db68956a04bf3
Rudi Yardley <contact@rudiyardley.com>
2021-06-09 16:34:36 +1000

    Delete all the things!

--- ui/core/src/services/utils/SifClient/SifClient.test.ts
+++
@@ -1,6 +0,0 @@
-import { SifUnSignedClient } from ".";
-
-test("SifUnSignedClient can be constructed", async () => {
-  const client = new SifUnSignedClient("http://localhost:1317");
-  expect(client).not.toBeNull();
-});
--- ui/core/src/services/utils/SifClient/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./SifClient";
-export * from "./SifUnsignedClient";
