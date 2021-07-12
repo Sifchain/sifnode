356bd66c404b010151663d95a0b5d7acd645af7a
intl-man <theinternationalman079@gmail.com>
2021-07-12 14:23:20 +0800

    Imports cleanup.

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -3,7 +3,6 @@ package app
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	// dispensationkeeper "github.com/Sifchain/sifnode/x/dispensation/keeper"
 )
 
 func SetupHandlers(app *SifchainApp) {
