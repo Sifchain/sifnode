e78e4bf838eb80c071192da0eb38f69a70897251
intl-man <theinternationalman079@gmail.com>
2021-07-12 14:21:51 +0800

    Version update.

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,5 +7,5 @@ import (
 )
 
 func SetupHandlers(app *SifchainApp) {
-	app.UpgradeKeeper.SetUpgradeHandler("0.9.0-rc.8", func(ctx sdk.Context, plan types.Plan) {})
+	app.UpgradeKeeper.SetUpgradeHandler("0.9.0-rc.7", func(ctx sdk.Context, plan types.Plan) {})
 }
