e4cc42285f96315628790ad2b445c2302baf845b
Tanmay <tanmay@sifchain.finance>
2021-07-16 00:09:05 -0400

    changed shell script to use -o flag

--- scripts/demos/dispensation/airdrop.sh
+++ scripts/demos/dispensation/airdrop.sh
@@ -9,7 +9,7 @@
 # input.json list of funding addresses  -  Input address must be part of the multisig key
 # output.json list of airdrop receivers.
 sifnoded tx dispensation create ValidatorSubsidy output.json sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd --from sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd --yes --fees 150000rowan --chain-id=localnet --keyring-backend=test
-sifnoded tx dispensation run 32_sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd --from sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd --yes --fees 150000rowan --chain-id=localnet --keyring-backend=test
+sifnoded tx dispensation run 29_sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd ValidatorSubsidy--from sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd --yes --fees 150000rowan --chain-id=localnet --keyring-backend=test
 sleep 8
 sifnoded q dispensation distributions-all -chain-id localnet
 #sifnoded q dispensation records-by-name-all ar1 >> all.json
@@ -19,7 +19,7 @@ sifnoded q dispensation distributions-all -chain-id localnet
 
 sifnoded tx dispensation create Airdrop output.json --gas 90128 --from $(sifnoded keys show sif -a) --yes --broadcast-mode async --sequence 26 --account-number 3 --chain-id localnet
 sifnoded tx dispensation create Airdrop output.json --gas 90128 --from $(sifnoded keys show sif -a) --yes --broadcast-mode async --sequence 27 --account-number 3 --chain-id localnet
-sifnoded tx dispensation run 10_sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd ValidatorSubsidy --from sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd --yes --gas auto --gas-adjustment=1.5 --gas-prices 1.0rowan --chain-id=localnet --keyring-backend=test
+sifnoded tx dispensation run 29_sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd ValidatorSubsidy --from sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd --yes --gas auto --gas-adjustment=1.5 --gas-prices 1.0rowan --chain-id=localnet --keyring-backend=test
 
 
 
