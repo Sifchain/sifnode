ef8d2028ec045e0453c254bca844d69434f0372b
James Moore <james@sifchain.finance>
2021-07-21 13:26:35 -0700

    Remove unused hardhat code

--- smart-contracts/test/test_foo.js
+++
@@ -1,7 +0,0 @@
-const { expect } = require('chai');
-
-describe("Greeter", function () {
-    it("Should return the new greeting once it's changed", async function () {
-        console.log("hellome")
-    });
-});
