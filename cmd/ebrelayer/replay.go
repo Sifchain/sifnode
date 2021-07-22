abe0a62ce4ef32c2a841b307e11a594a03b74c04
James Moore <james@sifchain.finance>
2021-07-21 18:58:08 -0700

    Revert "generate cmd docs, minor refactoring and fixes"
    This reverts commit d9fa344dfb7aa7b2f5d8c062c03527b9846d4681.

--- cmd/ebrelayer/replay.go
+++ cmd/ebrelayer/replay.go
@@ -1,4 +1,4 @@
-package cmd
+package ebrelayer
 
 import (
 	"log"
--- cmd/gen_docs.go
+++ cmd/gen_docs.go
@@ -3,8 +3,8 @@ package main
 import (
 	"log"
 
-	ebrelayer "github.com/Sifchain/sifnode/cmd/ebrelayer/cmd"
-	sifgen "github.com/Sifchain/sifnode/cmd/sifgen/cmd"
+	ebrelayer "github.com/Sifchain/sifnode/cmd/ebrelayer"
+	sifgen "github.com/Sifchain/sifnode/cmd/sifgen"
 	sifnoded "github.com/Sifchain/sifnode/cmd/sifnoded/cmd"
 	"github.com/spf13/cobra/doc"
 )
@@ -14,17 +14,17 @@ func main() {
 	sifgenCmd := sifgen.NewRootCmd()
 	ebrelayerCmd := ebrelayer.NewRootCmd()
 
-	err := doc.GenMarkdownTree(sifnodedCmd, "../docs/cmd/sifnoded")
+	err := doc.GenMarkdownTree(sifnodedCmd, "docs/cmd/sifnoded/")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = doc.GenMarkdownTree(sifgenCmd, "../docs/cmd/sifgen")
+	err = doc.GenMarkdownTree(sifgenCmd, "docs/cmd/sifgen/")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = doc.GenMarkdownTree(ebrelayerCmd, "../docs/cmd/ebrelayer")
+	err = doc.GenMarkdownTree(ebrelayerCmd, "docs/cmd/ebrelayer/")
 	if err != nil {
 		log.Fatal(err)
 	}
