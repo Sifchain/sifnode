4000ea62408ad9ae617536149b702e499941f7ea
intl-man <theinternationalman079@gmail.com>
2021-07-30 19:47:17 +0800

    Pipeline test.

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,4 +7,5 @@ import (
 
 func SetupHandlers(app *SifchainApp) {
 	app.UpgradeKeeper.SetUpgradeHandler("0.9.0", func(ctx sdk.Context, plan types.Plan) {})
+	app.UpgradeKeeper.SetUpgradeHandler("0.9.1-rc.1", func(ctx sdk.Context, plan types.Plan) {})
 }
