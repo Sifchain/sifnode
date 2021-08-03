4accdad2e13fe340591e9d7ca096c55886341d8d
Tim Lind <tim@incremental.co>
2021-08-03 18:02:55 +0200

    Increment version for deploy to testnet

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -10,7 +10,8 @@ import (
 func SetupHandlers(app *SifchainApp) {
 	app.UpgradeKeeper.SetUpgradeHandler("0.9.0", func(ctx sdk.Context, plan types.Plan) {})
 	app.UpgradeKeeper.SetUpgradeHandler("0.9.2-ibc.1", func(ctx sdk.Context, plan types.Plan) {})
-	app.UpgradeKeeper.SetUpgradeHandler("0.9.2-ibc.2", func(ctx sdk.Context, plan types.Plan) {
+	app.UpgradeKeeper.SetUpgradeHandler("0.9.2-ibc.2", func(ctx sdk.Context, plan types.Plan) {})
+	app.UpgradeKeeper.SetUpgradeHandler("0.9.2-ibc.3", func(ctx sdk.Context, plan types.Plan) {
 		tokenregistrymigrations.Init(ctx, app.TokenRegistryKeeper)
 	})
 }
