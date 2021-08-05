633a5b5746764c11a605d5dc622a2a97e957c203
Tanmay <tanmay@sifchain.finance>
2021-08-05 01:34:29 -0400

    refactored ReduceGasPriceDecorator

--- app/ante/ante.go
+++ app/ante/ante.go
@@ -12,54 +12,23 @@ import (
 )
 
 func NewAnteHandler(ak authkeeper.AccountKeeper, bk bankkeeper.Keeper, gasConsumer ante.SignatureVerificationGasConsumer, signModeHandler signing.SignModeHandler) sdk.AnteHandler {
-	return func(
-		ctx sdk.Context, tx sdk.Tx, sim bool,
-	) (newCtx sdk.Context, err error) {
-		var anteHandler sdk.AnteHandler
-		msgs := tx.GetMsgs()
-		// If number of messages is greater than one ,the second message will be able to get away with a lower tx fee
-		if len(msgs) > 1 && msgs[0].Type() == disptypes.MsgTypeCreateDistribution {
-			return ctx, errors.New("Create Dispensation cannot be part of a multi message transaction")
-		}
-		switch msgs[0].Type() {
-		case disptypes.MsgTypeCreateDistribution:
-			anteHandler = sdk.ChainAnteDecorators(
-				ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
-				NewReduceGasPriceDecorator(),
-				ante.NewRejectExtensionOptionsDecorator(),
-				ante.NewMempoolFeeDecorator(),
-				ante.NewValidateBasicDecorator(),
-				ante.TxTimeoutHeightDecorator{},
-				ante.NewValidateMemoDecorator(ak),
-				ante.NewConsumeGasForTxSizeDecorator(ak),
-				ante.NewRejectFeeGranterDecorator(),
-				ante.NewSetPubKeyDecorator(ak), // SetPubKeyDecorator must be called before all signature verification decorators
-				ante.NewValidateSigCountDecorator(ak),
-				ante.NewDeductFeeDecorator(ak, bk),
-				ante.NewSigGasConsumeDecorator(ak, gasConsumer),
-				ante.NewSigVerificationDecorator(ak, signModeHandler),
-				ante.NewIncrementSequenceDecorator(ak),
-			)
-		default:
-			anteHandler = sdk.ChainAnteDecorators(
-				ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
-				ante.NewRejectExtensionOptionsDecorator(),
-				ante.NewMempoolFeeDecorator(),
-				ante.NewValidateBasicDecorator(),
-				ante.TxTimeoutHeightDecorator{},
-				ante.NewValidateMemoDecorator(ak),
-				ante.NewConsumeGasForTxSizeDecorator(ak),
-				ante.NewRejectFeeGranterDecorator(),
-				ante.NewSetPubKeyDecorator(ak), // SetPubKeyDecorator must be called before all signature verification decorators
-				ante.NewValidateSigCountDecorator(ak),
-				ante.NewDeductFeeDecorator(ak, bk),
-				ante.NewSigGasConsumeDecorator(ak, gasConsumer),
-				ante.NewSigVerificationDecorator(ak, signModeHandler),
-				ante.NewIncrementSequenceDecorator(ak),
-			)
-		}
-		return anteHandler(ctx, tx, sim)
-	}
+	return sdk.ChainAnteDecorators(
+		ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
+		NewReduceGasPriceDecorator(),    // Custom decorator to reduce gas price for specific msg types
+		ante.NewRejectExtensionOptionsDecorator(),
+		ante.NewMempoolFeeDecorator(),
+		ante.NewValidateBasicDecorator(),
+		ante.TxTimeoutHeightDecorator{},
+		ante.NewValidateMemoDecorator(ak),
+		ante.NewConsumeGasForTxSizeDecorator(ak),
+		ante.NewRejectFeeGranterDecorator(),
+		ante.NewSetPubKeyDecorator(ak), // SetPubKeyDecorator must be called before all signature verification decorators
+		ante.NewValidateSigCountDecorator(ak),
+		ante.NewDeductFeeDecorator(ak, bk),
+		ante.NewSigGasConsumeDecorator(ak, gasConsumer),
+		ante.NewSigVerificationDecorator(ak, signModeHandler),
+		ante.NewIncrementSequenceDecorator(ak),
+	)
 }
 
 // ReduceGasPriceDecorator is a custom decorator to reduce fee prices .
@@ -73,13 +42,24 @@ func NewReduceGasPriceDecorator() ReduceGasPriceDecorator {
 
 // AnteHandle reduces the gas price to a lower value which is hardcoded.The ReduceGasPriceDecorator should only be used for specific transaction types to lower the fee cost.
 func (r ReduceGasPriceDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
-	loweredGasPrice := sdk.DecCoin{
-		Denom:  "rowan",
-		Amount: sdk.MustNewDecFromStr("0.00000005"),
+	msgs := tx.GetMsgs()
+	// If number of messages is greater than one ,the second message will be able to get away with a lower tx fee
+	if len(msgs) > 1 && msgs[0].Type() == disptypes.MsgTypeCreateDistribution {
+		return ctx, errors.New("Create Dispensation cannot be part of a multi message transaction")
 	}
-	if !loweredGasPrice.IsValid() {
-		return ctx, sdkerrors.Wrap(sdkerrors.ErrLogic, "unable to lower gas price")
+	if len(msgs) == 0 {
+		return ctx, errors.New("Transaction must have atleast one message")
 	}
-	ctx = ctx.WithMinGasPrices(sdk.NewDecCoins(loweredGasPrice))
+
+	if msgs[0].Type() == disptypes.MsgTypeCreateDistribution {
+		loweredGasPrice := sdk.DecCoin{
+			Denom:  "rowan",
+			Amount: sdk.MustNewDecFromStr("0.00000005"),
+		}
+		if !loweredGasPrice.IsValid() {
+			return ctx, sdkerrors.Wrap(sdkerrors.ErrLogic, "unable to lower gas price")
+		}
+		ctx = ctx.WithMinGasPrices(sdk.NewDecCoins(loweredGasPrice))
+	}
 	return next(ctx, tx, simulate)
 }
--- app/ante/ante_test.go
+++ app/ante/ante_test.go
@@ -3,10 +3,13 @@ package ante_test
 import (
 	sifapp "github.com/Sifchain/sifnode/app"
 	"github.com/Sifchain/sifnode/app/ante"
+	dispensationtypes "github.com/Sifchain/sifnode/x/dispensation/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/tendermint/tendermint/crypto/ed25519"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	"testing"
 )
@@ -27,9 +30,21 @@ func TestReduceGasPriceDecorator_AnteHandle(t *testing.T) {
 		Amount: sdk.MustNewDecFromStr("0.00000005"),
 	}
 	ctx = ctx.WithMinGasPrices(sdk.NewDecCoins(highGasPrice))
-	next := func(ctx sdk.Context, tx sdk.Tx, simulate bool) (newCtx sdk.Context, err error){return ctx, nil}
+	next := func(ctx sdk.Context, tx sdk.Tx, simulate bool) (newCtx sdk.Context, err error) { return ctx, nil }
 	//Error will always be nil
-	newCtx,_ := decorator.AnteHandle(ctx,legacytx.StdTx{},false,next)
-	assert.Equal(t, loweredGasPrice.String(),newCtx.MinGasPrices().String())
-	assert.Equal(t, highGasPrice.String(),ctx.MinGasPrices().String())
+	msg := dispensationtypes.NewMsgCreateDistribution(sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address()), dispensationtypes.DistributionType_DISTRIBUTION_TYPE_AIRDROP, []banktypes.Output{}, "")
+	tx := legacytx.StdTx{
+		Msgs:          []sdk.Msg{&msg},
+		Fee:           legacytx.StdFee{},
+		Signatures:    nil,
+		Memo:          "",
+		TimeoutHeight: 0,
+	}
+	newCtx, _ := decorator.AnteHandle(ctx, tx, false, next)
+	assert.Equal(t, loweredGasPrice.String(), newCtx.MinGasPrices().String())
+	assert.Equal(t, highGasPrice.String(), ctx.MinGasPrices().String())
+
+	newCtx, _ = decorator.AnteHandle(ctx, legacytx.StdTx{}, false, next)
+	assert.Equal(t, highGasPrice.String(), newCtx.MinGasPrices().String())
+
 }
