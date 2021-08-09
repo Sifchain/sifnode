97b188a7913cc0e8dda6d5b5dff03c1de4911a15
Daniel Portugal <daniportugal@gmail.com>
2021-08-09 20:29:05 -0300

    generating go files and keeping folder structure

--- smart-contracts/scripts/generateAbi.js
+++ smart-contracts/scripts/generateAbi.js
@@ -8,119 +8,125 @@
  * smart-contracts/build/generated/abi
  * And save each contract's binary to the folder 
  * smart-contracts/build/generated/bin
+ * And save the generated go files to the folder 
+ * smart-contracts/build/generated/go
  */
 
-const fs = require('fs');
-const path = require('path');
-//const util = require('util');
-//const exec = util.promisify(require('child_process').exec);
-
-// where to fetch artifacts from (hardhat should save compiled contracts in this folder)
-const HARDHAT_ARTIFACTS_DIR = './artifacts/contracts';
-
-// where to save the generated files (BASE_TARGET_DIR/TARGET_XXX_FOLDER):
-const BASE_TARGET_DIR = './build/generated'
-
-// where to save the ABI files (BASE_TARGET_DIR/TARGET_ABI_DIR):
-const TARGET_ABI_DIR = 'abi';
-
-// where to save the BIN files (BASE_TARGET_DIR/TARGET_BIN_DIR):
-const TARGET_BIN_DIR = 'bin';
-
-// where to save the GO files (BASE_TARGET_DIR/TARGET_GO_DIR):
-//const TARGET_GO_DIR = 'go';
-
-async function main() {
-  // creates the target directory if it doesn't already exist
-  createDirectories();
-
-  // get only the files that we care about
-  const files = getArtifacts();
-
-  // For each file...
-  for (let i = 0; i < files.length; i++) {
-    // get the name of the file without its path
-    const strippedFilename = files[i].split('/').slice(-1)[0].replace('.json', '');
-    console.log(`Processing ${strippedFilename}...`);
-
-    // read what's in the file
-    const data = fs.readFileSync(files[i], 'utf8');
-
-    // parse the JSON data
-    const parsed = JSON.parse(data);
-
-    // write the abi to a file
-    const targetAbiFileName = `${BASE_TARGET_DIR}/${TARGET_ABI_DIR}/${strippedFilename}.abi`;
-    fs.writeFileSync(targetAbiFileName, JSON.stringify(parsed.abi));
-
-    // write the binary data to a file
-    const targetBinFileName = `${BASE_TARGET_DIR}/${TARGET_BIN_DIR}/${strippedFilename}.bin`;
-    fs.writeFileSync(targetBinFileName, JSON.stringify(parsed.bytecode));
-
-    // create go bindings for this contract
-    // const targetGoFileName = `${BASE_TARGET_DIR}/${TARGET_GO_DIR}/${strippedFilename}.go`;
-    // const { stderr } = await exec(`abigen --abi ${targetAbiFileName} --bin ${targetBinFileName} --pkg ${strippedFilename} --out ${targetGoFileName}`);
-    // console.log(`stderr: ${stderr}`);
-  }
-
-  printSuccess();
-}
-
-function createDirectories() {
-  if (!fs.existsSync(BASE_TARGET_DIR)) {
-    fs.mkdirSync(BASE_TARGET_DIR);
-  }
-
-  if (!fs.existsSync(`${BASE_TARGET_DIR}/${TARGET_ABI_DIR}`)) {
-    fs.mkdirSync(`${BASE_TARGET_DIR}/${TARGET_ABI_DIR}`);
-  }
-
-  if (!fs.existsSync(`${BASE_TARGET_DIR}/${TARGET_BIN_DIR}`)) {
-    fs.mkdirSync(`${BASE_TARGET_DIR}/${TARGET_BIN_DIR}`);
-  }
-
-  /*
-  if (!fs.existsSync(`${BASE_TARGET_DIR}/${TARGET_GO_DIR}`)) {
-    fs.mkdirSync(`${BASE_TARGET_DIR}/${TARGET_GO_DIR}`);
-  }
-  */
-}
-
-// Get all files that end with .json and not .dbg.json
-function getArtifacts() {
-  const allContractsArtifacts = [];
-  getFilesRecursively(HARDHAT_ARTIFACTS_DIR, allContractsArtifacts);
-
-  const filtered = allContractsArtifacts.filter((file) => {
-    return file.endsWith('.json') && !file.endsWith('.dbg.json');
-  });
-
-  return filtered;
-}
-
-// Traverse through a directory and get all files recursively
-// Save the result in `targetList`
-function getFilesRecursively(directory, targetList) {
-  const filesInDirectory = fs.readdirSync(directory);
-
-  for (const file of filesInDirectory) {
-    const absolute = path.join(directory, file);
-
-    if (fs.statSync(absolute).isDirectory()) {
-      getFilesRecursively(absolute, targetList);
-    } else {
-      targetList.push(absolute);
-    }
-  }
-};
-
-function printSuccess() {
-  console.log(`-> Generated ABI files in ${BASE_TARGET_DIR}/${TARGET_ABI_DIR}/`);
-  console.log(`-> Generated BIN files in ${BASE_TARGET_DIR}/${TARGET_BIN_DIR}/`);
-}
-
-main()
-  .catch((error) => {
-    console.error({ error });
-  })
-  .finally(() => process.exit(0))
+ const fs = require('fs');
+ const path = require('path');
+ const util = require('util');
+ const exec = util.promisify(require('child_process').exec);
+ 
+ // where to fetch artifacts from (hardhat should save compiled contracts in this folder)
+ const HARDHAT_ARTIFACTS_DIR = './artifacts/contracts';
+ 
+ // where to save the generated files (BASE_TARGET_DIR/TARGET_XXX_FOLDER):
+ const BASE_TARGET_DIR = '../cmd/ebrelayer/contract/generated'
+ //const BASE_TARGET_DIR = './build/generated'
+ 
+ // where to save the ABI files (BASE_TARGET_DIR/TARGET_ABI_DIR):
+ const TARGET_ABI_DIR = 'abi';
+ 
+ // where to save the BIN files (BASE_TARGET_DIR/TARGET_BIN_DIR):
+ const TARGET_BIN_DIR = 'bin';
+ 
+ // where to save the GO files (BASE_TARGET_DIR/TARGET_GO_DIR):
+ const TARGET_GO_DIR = 'bindings';
+ 
+ async function main() {
+   // creates the target directory if it doesn't already exist
+   createDirectories();
+ 
+   // get only the files that we care about
+   const files = getArtifacts();
+ 
+   // For each file...
+   for (let i = 0; i < files.length; i++) {
+     // get the name of the file without its path
+     const strippedFilename = files[i].split('/').slice(-1)[0].replace('.json', '');
+     
+     // get the normalized path
+     const internalPath = path.dirname(files[i]).split('artifacts/contracts/')[1].replace('.sol', '').split('/')[0];
+     
+     console.log(`Processing ${internalPath}/${strippedFilename}...`);
+ 
+     // read what's in the file
+     const data = fs.readFileSync(files[i], 'utf8');
+ 
+     // parse the JSON data
+     const parsed = JSON.parse(data);
+ 
+     // write the abi to a file
+     const targetAbiDirectory = `${BASE_TARGET_DIR}/${TARGET_ABI_DIR}/${internalPath}`;
+     createDir(targetAbiDirectory);
+     const targetAbiFileName = `${targetAbiDirectory}/${strippedFilename}.abi`;
+     fs.writeFileSync(targetAbiFileName, JSON.stringify(parsed.abi));
+ 
+     // write the binary data to a file
+     const targetBinDirectory = `${BASE_TARGET_DIR}/${TARGET_BIN_DIR}/${internalPath}`;
+     createDir(targetBinDirectory);
+     const targetBinFileName = `${targetBinDirectory}/${strippedFilename}.bin`;
+     fs.writeFileSync(targetBinFileName, JSON.stringify(parsed.bytecode));
+ 
+     // create go bindings for this contract
+     const targetGoDirectory = `${BASE_TARGET_DIR}/${TARGET_GO_DIR}/${internalPath}`;
+     createDir(targetGoDirectory);
+     const targetGoFileName = `${targetGoDirectory}/${strippedFilename}.go`;
+     await exec(`abigen --abi ${targetAbiFileName} --pkg ${strippedFilename} --type ${strippedFilename} --out ${targetGoFileName}`);
+   }
+ 
+   printSuccess();
+ }
+ 
+ function createDirectories() {
+   createDir(BASE_TARGET_DIR);
+   createDir(`${BASE_TARGET_DIR}/${TARGET_ABI_DIR}`);
+   createDir(`${BASE_TARGET_DIR}/${TARGET_BIN_DIR}`);
+   createDir(`${BASE_TARGET_DIR}/${TARGET_GO_DIR}`);
+ }
+ 
+ function createDir(directory) {
+   if (!fs.existsSync(directory)) {
+     fs.mkdirSync(directory);
+   }
+ }
+ 
+ // Get all files that end with .json and not .dbg.json
+ function getArtifacts() {
+   const allContractsArtifacts = [];
+   getFilesRecursively(HARDHAT_ARTIFACTS_DIR, allContractsArtifacts);
+ 
+   const filtered = allContractsArtifacts.filter((file) => {
+     return file.endsWith('.json') && !file.endsWith('.dbg.json');
+   });
+ 
+   return filtered;
+ }
+ 
+ // Traverse through a directory and get all files recursively
+ // Save the result in `targetList`
+ function getFilesRecursively(directory, targetList) {
+   const filesInDirectory = fs.readdirSync(directory);
+ 
+   for (const file of filesInDirectory) {
+     const absolute = path.join(directory, file);
+ 
+     if (fs.statSync(absolute).isDirectory()) {
+       getFilesRecursively(absolute, targetList);
+     } else {
+       targetList.push(absolute);
+     }
+   }
+ };
+ 
+ function printSuccess() {
+   console.log(`-> Generated ABI files in ${BASE_TARGET_DIR}/${TARGET_ABI_DIR}/`);
+   console.log(`-> Generated BIN files in ${BASE_TARGET_DIR}/${TARGET_BIN_DIR}/`);
+   console.log(`-> Generated GO files in ${BASE_TARGET_DIR}/${TARGET_GO_DIR}/`);
+ }
+ 
+ main()
+   .catch((error) => {
+     console.error({ error });
+   })
+   .finally(() => process.exit(0))
