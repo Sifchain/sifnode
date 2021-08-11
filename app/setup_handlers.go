30e4c7153006ea2c06b7553eadab55f05d64f6f6
Tim Lind <tim@incremental.co>
2021-08-11 12:55:21 +0200

    0.9.2-ibc.7

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -10,7 +10,7 @@ import (
 func SetupHandlers(app *SifchainApp) {
 	app.UpgradeKeeper.SetUpgradeHandler("0.9.0", func(ctx sdk.Context, plan types.Plan) {})
 	app.UpgradeKeeper.SetUpgradeHandler("0.9.1", func(ctx sdk.Context, plan types.Plan) {})
-	app.UpgradeKeeper.SetUpgradeHandler("0.9.1-rc.11", func(ctx sdk.Context, plan types.Plan) {
+	app.UpgradeKeeper.SetUpgradeHandler("0.9.2-ibc.7", func(ctx sdk.Context, plan types.Plan) {
 		tokenregistrymigrations.Init(ctx, app.TokenRegistryKeeper)
 	})
 }
