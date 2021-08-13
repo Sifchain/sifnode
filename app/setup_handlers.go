00b8447b1db9733807b5eb1ecb3f37478918deb6
Tanmay <tanmay@sifchain.finance>
2021-08-13 04:24:38 -0400

    added store loaded

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	tokenregistrytypes "github.com/Sifchain/sifnode/x/tokenregistry/types"
+	upgradetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
@@ -12,5 +14,10 @@ func SetupHandlers(app *SifchainApp) {
 	app.UpgradeKeeper.SetUpgradeHandler("0.9.1", func(ctx sdk.Context, plan types.Plan) {})
 	app.UpgradeKeeper.SetUpgradeHandler("0.9.2", func(ctx sdk.Context, plan types.Plan) {
 		tokenregistrymigrations.Init(ctx, app.TokenRegistryKeeper)
+	})
+	app.BaseApp.SetStoreLoader(func(ms sdk.CommitMultiStore) error {
+		return ms.LoadLatestVersionAndUpgrade(&upgradetypes.StoreUpgrades{
+			Added: []string{tokenregistrytypes.ModuleName},
+		})
 	})
 }
