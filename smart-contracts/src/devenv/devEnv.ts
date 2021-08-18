b83df9e420765f149f09a552f274cd285bd2388b
James Moore <james@sifchain.finance>
2021-08-18 14:25:54 -0700

    moving files around

--- smart-contracts/src/devenv/devEnv.ts
+++ smart-contracts/src/devenv/devEnv.ts
@@ -18,22 +18,6 @@ export abstract class ShellCommand {
     }
 }
 
-@registry([{
-    token: EthereumArguments,
-    useValue: new EthereumArguments("localhost", 8545, 1, 1, 1)
-}])
-export class EthereumArguments {
-    constructor(
-        readonly host: string,
-        readonly port: number,
-        readonly nValidators: number,
-        readonly networkId: number,
-        readonly chainId: number,
-    ) {
-    }
-}
-
-
 export interface EthereumAccount {
     address: string
     privateKey: string
@@ -53,64 +37,4 @@ export interface EthereumResults {
     httpPort: number,
     chainId: number,  // note that hardhat doesn't believe networkId exists...
     accounts: EthereumAccounts
-}
-
-@singleton()
-export class HardhatNodeRunner extends ShellCommand {
-    constructor(
-        readonly args: EthereumArguments
-    ) {
-        super();
-    }
-
-    cmd(): [string, string[]] {
-        return ["node_modules/.bin/hardhat", [
-            "node",
-            "--hostname", this.args.host,
-            "--port", this.args.port.toString()
-        ]]
-    }
-
-    override run(): Promise<void> {
-        return new Promise((resolve, reject) => {
-            const [c, args] = this.cmd()
-            const process = childProcess.spawn(c, args, {stdio: "inherit"})
-            process.on("error", err => {
-                reject(err)
-            })
-            process.on("exit", e => {
-                if (e && e != 0) {
-                    reject(e)
-                } else
-                    resolve()
-            })
-        })
-    }
-
-    override async results(): Promise<EthereumResults> {
-        const hardhatSigners = await hre.ethers.getSigners()
-        let ethereumAccounts = signerArrayToEthereumAccounts(hardhatSigners, this.args.nValidators)
-        if (hre.network.config.chainId) {
-            return {
-                accounts: ethereumAccounts,
-                httpHost: this.args.host,
-                httpPort: this.args.port,
-                chainId: hre.network.config.chainId
-            }
-        } else throw "unknown chainId"
-    }
-}
-
-function signerArrayToEthereumAccounts(accounts: SignerWithAddress[], nValidators: number): EthereumAccounts {
-    const [operator, owner, pauser, ...moreAccounts] = accounts.map(x => x.address)
-    const validators = moreAccounts.slice(0, nValidators)
-    const available = moreAccounts.slice(nValidators)
-    return {
-        proxyAdmin: operator,
-        operator,
-        owner,
-        pauser,
-        validators: validators,
-        available,
-    }
 }
---
+++ smart-contracts/src/devenv/hardhatNode.ts
@@ -0,0 +1,105 @@
+import {registry, singleton} from "tsyringe";
+import * as childProcess from "child_process"
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import * as hre from "hardhat"
+import {EthereumAccounts, EthereumResults, ShellCommand} from "./devEnv";
+import {EthereumAddress} from "../ethereumAddress";
+import {Wallet} from "ethers";
+
+@registry([{
+    token: EthereumArguments,
+    useValue: new EthereumArguments("localhost", 8545, 1, 1, 1)
+}])
+export class EthereumArguments {
+    constructor(
+        readonly host: string,
+        readonly port: number,
+        readonly nValidators: number,
+        readonly networkId: number,
+        readonly chainId: number,
+    ) {
+    }
+}
+
+@singleton()
+export class HardhatNodeRunner extends ShellCommand {
+    constructor(
+        readonly args: EthereumArguments
+    ) {
+        super();
+    }
+
+    cmd(): [string, string[]] {
+        return ["node_modules/.bin/hardhat", [
+            "node",
+            "--hostname", this.args.host,
+            "--port", this.args.port.toString()
+        ]]
+    }
+
+    override run(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const [c, args] = this.cmd()
+            const process = childProcess.spawn(c, args, {stdio: "inherit"})
+            process.on("error", err => {
+                reject(err)
+            })
+            process.on("exit", e => {
+                if (e && e != 0) {
+                    reject(e)
+                } else
+                    resolve()
+            })
+        })
+    }
+
+    override async results(): Promise<EthereumResults> {
+        const hardhatSigners = await hre.ethers.getSigners()
+        let ethereumAccounts = signerArrayToEthereumAccounts(hardhatSigners, this.args.nValidators)
+        if (hre.network.config.chainId) {
+            return {
+                accounts: ethereumAccounts,
+                httpHost: this.args.host,
+                httpPort: this.args.port,
+                chainId: hre.network.config.chainId
+            }
+        } else throw "unknown chainId"
+    }
+}
+
+interface EthereumAddressAndKey {
+    privateKey: string
+    address: string
+}
+
+function isWallet(s: SignerWithAddress | Wallet): s is Wallet {
+    return 'privateKey' in s;
+}
+
+// should get this from hardhat, but it's nontrivial, so I just copied it
+const HARDHAT_NETWORK_MNEMONIC =
+    "test test test test test test test test test test test junk";
+
+function signerWithAddressToEthereumAddress(s: SignerWithAddress | Wallet): EthereumAddressAndKey | null {
+    if (isWallet(s)) {
+        return {
+            address: s.address,
+            privateKey: s.privateKey
+        }
+    } else return null
+}
+
+function signerArrayToEthereumAccounts(accounts: SignerWithAddress[], nValidators: number): EthereumAccounts {
+    const xs = accounts.map(signerWithAddressToEthereumAddress)
+    const [operator, owner, pauser, ...moreAccounts] = accounts.map(x => x.address)
+    const validators = moreAccounts.slice(0, nValidators)
+    const available = moreAccounts.slice(nValidators)
+    return {
+        proxyAdmin: operator,
+        operator,
+        owner,
+        pauser,
+        validators: validators,
+        available,
+    }
+}
