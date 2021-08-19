429d61c361c6b95bac61053060613d9efa651f7f
James Moore <james@sifchain.finance>
2021-08-18 17:41:47 -0700

    go builder and hardhat ethereum server working
    Signed-off-by: James Moore <james@sifchain.finance>

--- smart-contracts/scripts/devenv.ts
+++ smart-contracts/scripts/devenv.ts
@@ -1,12 +1,25 @@
 import {container} from "tsyringe";
 import {HardhatNodeRunner} from "../src/devenv/hardhatNode";
+import {GolangBuilder} from "../src/devenv/golangBuilder";
 
-async function main() {
+async function startHardhat() {
     const node = container.resolve(HardhatNodeRunner)
     const [process, resultsPromise] = node.go()
     const results = await resultsPromise
     console.log(`rsltis: ${JSON.stringify(results, undefined, 2)}`)
-    await process
+    return process
+}
+
+async function golangBuilder() {
+    const node = container.resolve(GolangBuilder)
+    const [process, resultsPromise] = node.go()
+    const results = await resultsPromise
+    console.log(`golangBuilder: ${JSON.stringify(results, undefined, 2)}`)
+    return process
+}
+
+async function main() {
+    await Promise.all([startHardhat(), golangBuilder()])
 }
 
 main()
