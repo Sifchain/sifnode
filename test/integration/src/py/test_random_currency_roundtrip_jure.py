4a8d2f0c61213541bf33803184d62cd45ed1c481
Jure Žvikart <jure@sifchain.finance>
2021-08-23 22:03:37 +0200

    Cleanup

--- test/integration/src/py/test_random_currency_roundtrip_jure.py
+++ test/integration/src/py/test_random_currency_roundtrip_jure.py
@@ -5,57 +5,16 @@ import burn_lock_functions
 import test_utilities
 from burn_lock_functions import EthereumToSifchainTransferRequest
 from pytest_utilities import generate_test_account
-from test_utilities import get_required_env_var, get_shell_output, amount_in_wei, \
-    SifchaincliCredentials
+from test_utilities import get_shell_output, amount_in_wei, SifchaincliCredentials
 
-# smart_contracts_dir = get_required_env_var("SMART_CONTRACTS_DIR")
-# bridgebank_address = get_required_env_var("BRIDGE_BANK_ADDRESS")
-# bridgetoken_address = get_required_env_var("BRIDGE_TOKEN_ADDRESS")
-
-# smart_contracts_dir = None
-# bridgebank_address = None
-# bridgetoken_address = None
-
-# @pytest.mark.skip
-@pytest.mark.usefixtures("with_snapshot")
-@pytest.mark.parametrize("snapshot_name", ["s1"])
-def test_simple(ctx):
-    ctx.set_operator_private_key_env_var()
-    basic_transfer_request = ctx.basic_transfer_request
-    source_ethereum_address = ctx.source_ethereum_address
-    rowan_source_integrationtest_env_credentials = ctx.rowan_source_integrationtest_env_credentials
-    rowan_source_integrationtest_env_transfer_request = ctx.rowan_source_integrationtest_env_transfer_request(basic_transfer_request)
-    new_currency_symbol = ("a" + get_shell_output("uuidgen").replace("-", ""))[:4]
-    # ethereum_network = ctx.ethereum_network
-    # solidity_json_path = ctx.solidity_json_path
-
-    # global smart_contracts_dir
-    # global bridgebank_address
-    # global bridgetoken_address
-    # smart_contracts_dir = get_required_env_var("SMART_CONTRACTS_DIR")
-    # bridgebank_address = get_required_env_var("BRIDGE_BANK_ADDRESS")
-    # bridgetoken_address = get_required_env_var("BRIDGE_TOKEN_ADDRESS")
-
-    # assert ctx.smart_contracts_dir == smart_contracts_dir
-    # assert ctx.bridgebank_address == bridgebank_address
-    # assert ctx.bridgetoken_address == bridgetoken_address
-
-    do_currency_test(
-        ctx,
-        new_currency_symbol,
-        basic_transfer_request,
-        source_ethereum_address,
-        rowan_source_integrationtest_env_credentials,
-        rowan_source_integrationtest_env_transfer_request,
-    )
 
 def do_currency_test(
-        ctx,
-        new_currency_symbol,
-        basic_transfer_request: EthereumToSifchainTransferRequest,
-        source_ethereum_address: str,
-        rowan_source_integrationtest_env_credentials: SifchaincliCredentials,
-        rowan_source_integrationtest_env_transfer_request: EthereumToSifchainTransferRequest,
+    ctx,
+    new_currency_symbol,
+    basic_transfer_request: EthereumToSifchainTransferRequest,
+    source_ethereum_address: str,
+    rowan_source_integrationtest_env_credentials: SifchaincliCredentials,
+    rowan_source_integrationtest_env_transfer_request: EthereumToSifchainTransferRequest,
 ):
     amount = amount_in_wei(9)
     logging.info(f"create new currency")
@@ -66,8 +25,7 @@ def do_currency_test(
         18,
         smart_contracts_dir=ctx.smart_contracts_dir,
         bridgebank_address=ctx.bridgebank_address,
-        solidity_json_path=ctx.solidity_json_path
-    )
+        solidity_json_path=ctx.solidity_json_path)
 
     logging.info(f"create test account to use with new currency {new_currency_symbol}")
     basic_transfer_request.ethereum_address = source_ethereum_address
@@ -75,9 +33,8 @@ def do_currency_test(
         basic_transfer_request,
         rowan_source_integrationtest_env_transfer_request,
         rowan_source_integrationtest_env_credentials,
-        target_ceth_balance=10 ** 17,
-        target_rowan_balance=10 ** 18
-    )
+        target_ceth_balance=10**17,
+        target_rowan_balance=10**18)
     test_amount = 39000
     logging.info(f"transfer some of the new currency {new_currency_symbol} to the test sifchain address")
     request.ethereum_symbol = new_currency["newtoken_address"]
@@ -87,53 +44,42 @@ def do_currency_test(
 
     logging.info("send some new currency to ethereum")
     request.ethereum_address, _ = test_utilities.create_ethereum_address(
-        ctx.smart_contracts_dir, ctx.ethereum_network
-    )
+        ctx.smart_contracts_dir, ctx.ethereum_network)
     request.amount = test_amount - 1
     burn_lock_functions.transfer_sifchain_to_ethereum(request, credentials)
 
 
-@pytest.mark.skip
 @pytest.mark.usefixtures("with_snapshot")
 @pytest.mark.parametrize("snapshot_name", ["s1"])
-@pytest.mark.usefixtures("operator_private_key")
-def test_transfer_tokens_with_some_currency(
-        basic_transfer_request: EthereumToSifchainTransferRequest,
-        source_ethereum_address: str,
-        rowan_source_integrationtest_env_credentials: SifchaincliCredentials,
-        rowan_source_integrationtest_env_transfer_request: EthereumToSifchainTransferRequest,
-        ethereum_network,
-        solidity_json_path,
-):
+def test_transfer_tokens_with_some_currency(ctx):
+    ctx.set_operator_private_key_env_var()  # TODO Instead of @pytest.mark.usefixtures("operator_private_key")
+    basic_transfer_request = ctx.basic_transfer_request
+    source_ethereum_address = ctx.source_ethereum_address
+    rowan_source_integrationtest_env_credentials = ctx.rowan_source_integrationtest_env_credentials
+    rowan_source_integrationtest_env_transfer_request = ctx.rowan_source_integrationtest_env_transfer_request(basic_transfer_request)
     new_currency_symbol = ("a" + get_shell_output("uuidgen").replace("-", ""))[:4]
     do_currency_test(
+        ctx,
         new_currency_symbol,
         basic_transfer_request,
         source_ethereum_address,
         rowan_source_integrationtest_env_credentials,
-        rowan_source_integrationtest_env_transfer_request,
-        ethereum_network,
-        solidity_json_path=solidity_json_path
-    )
+        rowan_source_integrationtest_env_transfer_request)
 
 
-@pytest.mark.skip
-@pytest.mark.usefixtures("operator_private_key")
-def test_three_letter_currency_with_capitals_in_name(
-        basic_transfer_request: EthereumToSifchainTransferRequest,
-        source_ethereum_address: str,
-        rowan_source_integrationtest_env_credentials: SifchaincliCredentials,
-        rowan_source_integrationtest_env_transfer_request: EthereumToSifchainTransferRequest,
-        ethereum_network,
-        solidity_json_path,
-):
+@pytest.mark.usefixtures("with_snapshot")
+@pytest.mark.parametrize("snapshot_name", ["s1"])
+def test_three_letter_currency_with_capitals_in_name(ctx):
+    ctx.set_operator_private_key_env_var()  # TODO Instead of @pytest.mark.usefixtures("operator_private_key")
+    basic_transfer_request = ctx.basic_transfer_request
+    source_ethereum_address = ctx.source_ethereum_address
+    rowan_source_integrationtest_env_credentials = ctx.rowan_source_integrationtest_env_credentials
+    rowan_source_integrationtest_env_transfer_request = ctx.rowan_source_integrationtest_env_transfer_request(basic_transfer_request)
     new_currency_symbol = ("F" + get_shell_output("uuidgen").replace("-", ""))[:3]
     do_currency_test(
+        ctx,
         new_currency_symbol,
         basic_transfer_request,
         source_ethereum_address,
         rowan_source_integrationtest_env_credentials,
-        rowan_source_integrationtest_env_transfer_request,
-        ethereum_network,
-        solidity_json_path=solidity_json_path
-    )
+        rowan_source_integrationtest_env_transfer_request)
