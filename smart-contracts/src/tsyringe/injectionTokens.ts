e5a9722545ac0fca158c8f7230d401a473174a43
James Moore <james@sifchain.finance>
2021-08-25 14:26:00 -0700

    Pull in changes from develop at d4a247a48107d94044a7b0ef14cfc0eb3ec59933
    Signed-off-by: James Moore <james@sifchain.finance>

--- smart-contracts/src/tsyringe/injectionTokens.ts
+++ smart-contracts/src/tsyringe/injectionTokens.ts
@@ -1,4 +1,12 @@
 // Used for tstringe registration of the HRE
 export const HardhatRuntimeEnvironmentToken = Symbol("HardhatRuntimeEnvironmentToken")
 
-export const NetworkDescriptorToken = Symbol("NetworkDescriptorToken")
+export const DeploymentName = Symbol("DeploymentName")
+
+export const DeploymentDirectory = Symbol("DeploymentDirectory")
+
+export const DeploymentChainId = Symbol("DeploymentChainId")
+
+export const BridgeBankMainnetUpgradeAdmin = Symbol("BridgeBankMainnetUpgradeAdmin")
+
+export const CosmosBridgeMainnetUpgradeAdmin = Symbol("CosmosBridgeMainnetUpgradeAdmin")
