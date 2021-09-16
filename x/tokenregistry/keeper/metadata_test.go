06148716f3a168fdc4a105ecb51cdfc149ffc82b
juniuszhou <junius.zhou@gmail.com>
2021-09-17 07:04:56 +0800

    add unit test

--- x/tokenregistry/keeper/metadata_test.go
+++ x/tokenregistry/keeper/metadata_test.go
@@ -3,7 +3,10 @@ package keeper_test
 import (
 	"testing"
 
+	"github.com/Sifchain/sifnode/x/ethbridge/test"
+	ethbridgetypes "github.com/Sifchain/sifnode/x/ethbridge/types"
 	oracletypes "github.com/Sifchain/sifnode/x/oracle/types"
+	"github.com/stretchr/testify/require"
 
 	"github.com/Sifchain/sifnode/x/tokenregistry/types"
 )
@@ -19,19 +22,31 @@ var testTokenMetadata = types.TokenMetadata{
 }
 
 func TestGetAddTokenMetadata(t *testing.T) {
-	// ctx, keeper, _, _, _, _, _, _ := test.CreateTestKeepers(t, 0.7, []int64{3, 3}, "")
-	// expected := types.TokenMetadata{}
-	// result, _ := keeper.GetTokenMetadata(ctx, invalidDenom)
-	// require.Equal(t, expected, result)
-	// resultDenom := keeper.AddTokenMetadata(ctx, testTokenMetadata)
-	// expectedDenom := ethbridgetypes.GetDenomHash(
-	// 	testTokenMetadata.NetworkDescriptor,
-	// 	testTokenMetadata.TokenAddress,
-	// 	testTokenMetadata.Decimals,
-	// 	testTokenMetadata.Name,
-	// 	testTokenMetadata.Symbol,
-	// )
-	// require.Equal(t, expectedDenom, resultDenom)
-	// result, _ = keeper.GetTokenMetadata(ctx, resultDenom)
-	// require.Equal(t, testTokenMetadata, result)
+	ctx, keeper, _, _, _, _, _, _ := test.CreateTestKeepers(t, 0.7, []int64{3, 3}, "")
+	expected := types.TokenMetadata{}
+	result, _ := keeper.GetTokenMetadata(ctx, invalidDenom)
+	require.Equal(t, expected, result)
+
+	expectedDenom := ethbridgetypes.GetDenomHash(
+		testTokenMetadata.NetworkDescriptor,
+		testTokenMetadata.TokenAddress,
+		testTokenMetadata.Decimals,
+		testTokenMetadata.Name,
+		testTokenMetadata.Symbol,
+	)
+
+	entry := types.RegistryEntry{
+		Denom:         expectedDenom,
+		DisplaySymbol: testTokenMetadata.Symbol,
+		Decimals:      testTokenMetadata.Decimals,
+		IsWhitelisted: true,
+		Permissions:   []types.Permission{types.Permission_PERMISSION_CLP},
+	}
+	keeper.GetTokenRegistryKeeper().SetToken(ctx, &entry)
+
+	resultDenom := keeper.AddTokenMetadata(ctx, testTokenMetadata)
+
+	require.Equal(t, expectedDenom, resultDenom)
+	result, _ = keeper.GetTokenMetadata(ctx, resultDenom)
+	require.Equal(t, testTokenMetadata, result)
 }
