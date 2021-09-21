51e8341c32c9a28c036f4476a07194e5d6f5971d
Ivan <ivanverch@gmail.com>
2021-09-22 00:49:36 +0300

    fix keeper test

--- x/tokenregistry/keeper/keeper_test.go
+++ x/tokenregistry/keeper/keeper_test.go
@@ -1,19 +1,14 @@
 package keeper_test
 
 import (
-	"fmt"
 	"github.com/Sifchain/sifnode/x/tokenregistry/test"
 	"github.com/Sifchain/sifnode/x/tokenregistry/types"
-	whitelistmocks "github.com/Sifchain/sifnode/x/tokenregistry/types/mock"
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestKeeper_CheckDenomPermissions(t *testing.T) {
 	app, ctx, _ := test.CreateTestApp(false)
-	ctrl := gomock.NewController(t)
-	wl := whitelistmocks.NewMockKeeper(ctrl)
 	app.TokenRegistryKeeper.SetToken(ctx, &types.RegistryEntry{
 		Denom:       "rowan",
 		Decimals:    18,
@@ -30,13 +25,9 @@ func TestKeeper_CheckDenomPermissions(t *testing.T) {
 		Decimals:    18,
 		Permissions: []types.Permission{types.Permission_IBCEXPORT, types.Permission_IBCEXPORT},
 	})
-	wl.EXPECT().GetDenomWhitelist(ctx)
-	registry := wl.GetDenomWhitelist(ctx)
-	fmt.Println(registry)
-	wl.EXPECT().GetDenom(&registry, "rowan")
-	wl.EXPECT().GetDenom(&registry, "t2")
-	entry := wl.GetDenom(&registry, "rowan")
-	entry2 := wl.GetDenom(&registry, "t2")
+	registry := app.TokenRegistryKeeper.GetDenomWhitelist(ctx)
+	entry := app.TokenRegistryKeeper.GetDenom(registry, "rowan")
+	entry2 := app.TokenRegistryKeeper.GetDenom(registry, "t2")
 	assert.True(t, app.TokenRegistryKeeper.CheckDenomPermissions(entry, []types.Permission{types.Permission_CLP}))
 	assert.False(t, app.TokenRegistryKeeper.CheckDenomPermissions(entry, []types.Permission{types.Permission_IBCIMPORT}))
 	assert.False(t, app.TokenRegistryKeeper.CheckDenomPermissions(entry, []types.Permission{types.Permission_CLP, types.Permission_IBCIMPORT}))
