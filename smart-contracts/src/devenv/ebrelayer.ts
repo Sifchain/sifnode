a04f745273d94027d508461e4b1dd4be973db98f
James Moore <james@sifchain.finance>
2021-09-30 12:17:36 -0700

    change log file for devenv to evmrelayer.log from ebrelayer.log

--- smart-contracts/src/devenv/ebrelayer.ts
+++ smart-contracts/src/devenv/ebrelayer.ts
@@ -121,7 +121,7 @@ export class RelayerRunner extends ShellCommand<EbrelayerResults> {
     readonly ebrelayerDB = `levelDB.db`,
     readonly relayerdbPath = "./relayerdb",
     readonly symbolTranslatorFile = "../test/integration/config/symbol_translator.json",
-    readonly logFile = "/tmp/sifnode/ebrelayer.log"
+    readonly logFile = "/tmp/sifnode/evmrelayer.log"
   ) {
     super();
     this.output = new Promise<EbrelayerResults>((res, rej) => {
