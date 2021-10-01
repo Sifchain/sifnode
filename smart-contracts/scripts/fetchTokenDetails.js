6f67092947cd53f3f9d6eeaf5a8699a331870f7a
Daniel Portugal <daniportugal@gmail.com>
2021-10-01 16:46:10 -0300

    wip: sifnode file

--- smart-contracts/scripts/fetchTokenDetails.js
+++ smart-contracts/scripts/fetchTokenDetails.js
@@ -5,25 +5,54 @@
  */
 
 require("dotenv").config();
-const fs = require('fs');
-const axios = require('axios');
+const fs = require("fs");
+const axios = require("axios");
 const { ethers } = require("hardhat");
 
 const addressListFile = process.env.ADDRESS_LIST_SOURCE;
-const destinationFolder = 'data';
+const destinationFolder = "data";
 const destinationFile = generateDestinationFilename();
 
+/**
+ * This is what a token looks like at this point to the sifnode team
+ * We'll generate a file for them too
+ */
+const SIFNODE_MODEL = {
+  is_whitelisted: true,
+  decimals: "",
+  denom: "",
+  base_denom: "",
+  path: "",
+  ibc_channel_id: "",
+  ibc_counterparty_channel_id: "",
+  display_name: "",
+  display_symbol: "",
+  network: "",
+  address: "",
+  external_symbol: "",
+  transfer_limit: "",
+  permissions: ["CLP"],
+  unit_denom: "",
+  ibc_counterparty_denom: "",
+  ibc_counterparty_chain_id: "",
+};
+
 async function main() {
-  print('yellow', 'Starting...', true);
+  print("yellow", "Starting...", true);
 
   const ERC20Factory = await ethers.getContractFactory("BridgeToken");
 
-  const data = fs.readFileSync(addressListFile, 'utf8');
+  const data = fs.readFileSync(addressListFile, "utf8");
   const addressList = JSON.parse(data);
-  
-  print('yellow', `Will fetch data for the following addresses:\n${addressList.join(', ')}`, true);
+
+  print(
+    "yellow",
+    `Will fetch data for the following addresses:\n${addressList.join(", ")}`,
+    true
+  );
 
   const finalList = [];
+  const sifnodeList = [];
 
   let address;
   for (let i = 0; i < addressList.length; i++) {
@@ -34,14 +63,18 @@ async function main() {
       const name = await instance.name();
       const decimals = await instance.decimals();
       const symbol = await instance.symbol();
+      const v1Denom = generateDenom(symbol);
 
-      if(!isValidSymbol(symbol)) {
-        print('red', `Skipping token ${address} (${name}) because it's symbol has spaces or special characters: ${symbol}`);
+      if (!isValidSymbol(symbol)) {
+        print(
+          "red",
+          `Skipping token ${address} (${name}) because it's symbol has spaces or special characters: ${symbol}`
+        );
         continue;
       }
 
       const iconUrl = await getTokenMetadata(address);
-      
+
       finalList.push({
         address,
         name,
@@ -53,56 +86,78 @@ async function main() {
         imageUrl: iconUrl,
       });
 
-      print('green', `--> Processed token "${name}" (${symbol}) successfully: ${decimals} decimals.`, true);
-    } catch(e) {
-      print('red', `--> Failed to fetch details of token ${address}: ${e.message}`);
+      const sifnodeObj = { ...SIFNODE_MODEL };
+      sifnodeObj.denom = v1Denom;
+      sifnodeObj.base_denom = v1Denom;
+      sifnodeObj.decimals = decimals;
+      sifnodeList.push(sifnodeObj);
+
+      print(
+        "green",
+        `--> Processed token "${name}" (${symbol}) successfully: ${decimals} decimals.`,
+        true
+      );
+    } catch (e) {
+      print(
+        "red",
+        `--> Failed to fetch details of token ${address}: ${e.message}`
+      );
     }
   }
 
   // The output file expects this format:
   const output = {
-    array: finalList
-  }
+    array: finalList,
+  };
 
   fs.writeFileSync(destinationFile, JSON.stringify(output, null, 2));
 
-  print('cyan', `DONE! These results have been written to ${destinationFile}:`);
-  print('cyan', JSON.stringify(finalList, null, 2));
+  print("cyan", `DONE! These results have been written to ${destinationFile}:`);
+  print("cyan", JSON.stringify(finalList, null, 2));
 }
 
 const colors = {
-  green: '\x1b[42m\x1b[37m',
-  red: '\x1b[41m\x1b[37m',
-  yellow: '\x1b[33m',
-  cyan: '\x1b[36m',
-  close: '\x1b[0m'
-}
+  green: "\x1b[42m\x1b[37m",
+  red: "\x1b[41m\x1b[37m",
+  yellow: "\x1b[33m",
+  cyan: "\x1b[36m",
+  close: "\x1b[0m",
+};
 function print(color, message, breakLine) {
-  const lb = breakLine ? '\n' : '';
+  const lb = breakLine ? "\n" : "";
   console.log(`${colors[color]}${message}${colors.close}${lb}`);
 }
 
 /**
-* Will return false for a symbol that has spaces and/or special characters in it
-* @param {string} symbol 
-* @returns {bool} does the symbol match the RegExp?
-*/
+ * Will return false for a symbol that has spaces and/or special characters in it
+ * @param {string} symbol
+ * @returns {bool} does the symbol match the RegExp?
+ */
 function isValidSymbol(symbol) {
-  const regexp = new RegExp('^[a-zA-Z0-9]+$');
+  const regexp = new RegExp("^[a-zA-Z0-9]+$");
   return regexp.test(symbol);
 }
-
 
 function generateDestinationFilename() {
   // setup month names
   const monthNames = [
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
   ];
 
   // get current date (we do it manually so that it's not dependant on user's locale)
   const today = new Date();
-  const day = String(today.getDate()).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, "0");
   const month = monthNames[today.getMonth()];
   const year = today.getFullYear();
 
@@ -114,21 +169,28 @@ function generateDestinationFilename() {
 }
 
 async function getTokenMetadata(address) {
-  const response = await axios.post(process.env.MAINNET_URL, {
-    "jsonrpc":"2.0",
-    "method":"alchemy_getTokenMetadata",
-    "params":[address],
-    "id":1
-  }).catch(e => {
-    print('red', `-> Cannot find imageUrl. Setting imageUrl to null.`);
-    return null;
-  });
+  const response = await axios
+    .post(process.env.MAINNET_URL, {
+      jsonrpc: "2.0",
+      method: "alchemy_getTokenMetadata",
+      params: [address],
+      id: 1,
+    })
+    .catch((e) => {
+      print("red", `-> Cannot find imageUrl. Setting imageUrl to null.`);
+      return null;
+    });
 
   return response?.data?.result?.logo;
+}
+
+function generateV1Denom(symbol) {
+  const denom = "c" + symbol.toLowerCase();
+  return denom;
 }
 
 main()
   .catch((error) => {
     console.error({ error });
   })
-  .finally(() => process.exit(0))
+  .finally(() => process.exit(0));
