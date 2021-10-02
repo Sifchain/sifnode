87e3b2f56b89ba64dc183cc389cdc317a6792322
Ivan <ivanverch@gmail.com>
2021-10-02 07:33:12 +0300

    bump version to 0.9.9

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -1,32 +1,12 @@
 package app
 
 import (
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const upgradeName = "0.9.8"
+const upgradeName = "0.9.9"
 
 func SetupHandlers(app *SifchainApp) {
-	app.UpgradeKeeper.SetUpgradeHandler(upgradeName, func(ctx sdk.Context, plan types.Plan) {
-		app.Logger().Info("Running upgrade handler for " + upgradeName)
-	})
-
-	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
-	if err != nil {
-		panic(err)
-	}
-
-	if upgradeInfo.Name == upgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		storeUpgrades := storetypes.StoreUpgrades{
-			Added: []string{},
-		}
-
-		// Use upgrade store loader for the initial loading of all stores when app starts,
-		// it checks if version == upgradeHeight and applies store upgrades before loading the stores,
-		// so that new stores start with the correct version (the current height of chain),
-		// instead the default which is the latest version that store last committed i.e 0 for new stores.
-		app.SetStoreLoader(types.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
-	}
+	app.UpgradeKeeper.SetUpgradeHandler(upgradeName, func(ctx sdk.Context, plan types.Plan) {})
 }
