3fcf02e54195dc0531171a9c5a03b6af028ffba7
Daniel Portugal <daniportugal@gmail.com>
2021-10-04 13:25:39 -0300

    ofacParser

--- smart-contracts/scripts/helpers/ofacParser.js
+++ smart-contracts/scripts/helpers/ofacParser.js
@@ -1,25 +1,47 @@
+/**
+ * This will parse the OFAC list, extracting EVM addresses
+ * It will also convert addresses to their checksum version
+ * And remove any duplicate addresses found in OFAC's list
+ */
+
+const Web3 = require("web3");
+const web3 = new Web3();
 const axios = require("axios");
-const { print, cacheBuster } = require("./utils");
+const { print, cacheBuster, removeDuplicates } = require("./utils");
 
 const OFAC_URL = "https://www.treasury.gov/ofac/downloads/sdnlist.txt";
 
 async function getList() {
+  print("highlight", "Fetching and parsing OFAC blocklist. Please wait...");
+
   const finalUrl = cacheBuster(OFAC_URL);
   const response = await axios.get(finalUrl).catch((e) => {
     throw e;
   });
 
   const addresses = extractAddresses(response.data);
+
   return addresses;
 }
 
-async function extractAddresses(rawFileContents) {
-  const list = rawFileContents.match();
+function extractAddresses(rawFileContents) {
+  const list = rawFileContents.match(/0x[a-fA-F0-9]{40}/g);
+  const checksumList = list.map(web3.utils.toChecksumAddress);
+
+  print(
+    "yellow",
+    `Found ${checksumList.length} EVM addresses. Removing duplicates...`
+  );
+
+  const finalList = removeDuplicates(checksumList);
+
+  print("yellow", `The final list has ${finalList.length} unique addresses:`);
+  print("yellow", `${finalList}`);
+
+  return finalList;
 }
 
 module.exports = {
   getList,
   extractAddresses,
 };
-
-getList();
