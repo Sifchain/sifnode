af23834d6d0aa427fdf66d6c1208d6b22d925a9c
Daniel Portugal <daniportugal@gmail.com>
2021-10-07 19:04:20 -0300

    fetching tokenData from file

--- smart-contracts/scripts/deploy_ibc_tokens.js
+++ smart-contracts/scripts/deploy_ibc_tokens.js
@@ -1,8 +1,40 @@
 /**
- * This script will deploy a new BridgeToken to an EVM network.
- // TODO runbook
- // TODO get list from file
- // TODO write the attach script
+ * This script will deploy N new BridgeTokens to an EVM network.
+ *
+ * Before executing this script, add the following variables to your .env,
+ * changing the values to your actual mainnet Alchemy URL and Private Key:
+ * MAINNET_URL=https://eth-mainnet.alchemyapi.io/v2/XXXXXXXXXXXXXXXXXXXXXXXX
+ * MAINNET_PRIVATE_KEY=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ *
+ * Then, create or edit the file data/ibc_tokens_to_deploy.json so that it
+ * has only the IbcTokens that you want to deploy.
+ * Example:
+ * [
+ *   {
+ *     "name": "Alice Token",
+ *     "symbol": "ALI",
+ *     "decimals": 10,
+ *     "denom": ""
+ *   },
+ *   {
+ *     "name": "Bob Token",
+ *     "symbol": "BOB",
+ *     "decimals": 18,
+ *     "denom": "Bob denom"
+ *   }
+ * ]
+ *
+ * Note that the `denom` field is optional. If you don't have that information,
+ * you may leave it as an empty string.
+ *
+ * Finally, run the command `yarn deployIbcTokens:run`.
+ *
+ * A new file will be created with the results. It's name will be something like
+ * data/deployed_ibc_tokens_07_Oct_2021.json, but with today's date.
+ *
+ * @dev If you want to TEST this script, run the command `yarn deployIbcTokens:test`
+ *
+ * That's it.
  */
 
 require("dotenv").config();
@@ -28,40 +60,36 @@ const DEPLOYMENT_NAME = process.env.DEPLOYMENT_NAME || "sifchain-1";
 // If there is no FORKING_CHAIN_ID env var, we'll use the mainnet id
 const CHAIN_ID = process.env.FORKING_CHAIN_ID || 1;
 
+// Will get the token data from this file
+const TOKEN_DATA_SOURCE_FILENAME = "./data/ibc_tokens_to_deploy.json";
+
+// Safeguard: if there is an error estimating gasPrice, it cannot go below this value
 const MINIMUM_GAS_PRICE_IN_GWEI = 100;
 
 const log = {
   tokens: [],
 };
 
-const tokensToDeploy = [
-  {
-    name: "Dan Token",
-    symbol: "DAN",
-    decimals: 6,
-    denom: "",
-  },
-  {
-    name: "Alice Token",
-    symbol: "ALI",
-    decimals: 10,
-    denom: "Alice denom",
-  },
-  {
-    name: "Bruce Token",
-    symbol: "BRU",
-    decimals: 18,
-    denom: "Bruce denom",
-  },
-];
-
 async function main() {
   print("highlight", "~~~ DEPLOY IBC TOKENS ~~~");
 
+  // get tokens from file:
+  print("yellow", `📢 Getting token data from ${TOKEN_DATA_SOURCE_FILENAME}`);
+  const data = fs.readFileSync(TOKEN_DATA_SOURCE_FILENAME, "utf8");
+  const tokensToDeploy = JSON.parse(data);
+
   print("yellow", `📢 Will deploy ${tokensToDeploy.length} tokens.`);
 
+  // Get the current account
+  const accounts = await ethers.getSigners();
+  const activeAccount = accounts[0];
+
   // If we're forking, we want to impersonate the owner account
-  if (USE_FORKING) await setupForking();
+  if (USE_FORKING) {
+    await setupForking();
+  } else {
+    print("cyan", `🤵 Active account is ${activeAccount.address}`);
+  }
 
   // Create an instance of BridgeBank from the deployed code, to have access to its address
   const { instance: bridgeBank } = await support.getDeployedContract(
@@ -69,11 +97,6 @@ async function main() {
     "BridgeBank",
     CHAIN_ID
   );
-
-  // Get the current account
-  const accounts = await ethers.getSigners();
-  const activeAccount = accounts[0];
-  print("cyan", `🤵 Active account is ${activeAccount.address}`);
 
   const startingBalance = await ethers.provider.getBalance(
     activeAccount.address
@@ -116,7 +139,7 @@ async function deployToken({
 }) {
   sanityCheck({ name, symbol, decimals, deployer });
 
-  print("yellow", `🔑 Deploying ${name} token. Please wait...`);
+  print("yellow", `🔑 Deploying ${name}. Please wait...`);
 
   // Deploy the token
   const bridgeTokenFactory = await ethers.getContractFactory("BridgeToken");
@@ -207,7 +230,9 @@ async function logTotalCost(account, startingBalance) {
   );
 
   log.totalCost = web3.utils.fromWei(totalCost.toString());
-  print("cyan", `Total ETH spent: ${log.totalCost}`);
+  print("cyan", "----");
+  print("cyan", `💵 Total ETH spent: ${log.totalCost}`);
+  print("cyan", "----");
 }
 
 function saveToFile() {
