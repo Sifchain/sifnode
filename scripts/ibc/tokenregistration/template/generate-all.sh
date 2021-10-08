1595cd8012f76d5fcf7321536a032dc629e875ad
Ivan <ivanverch@gmail.com>
2021-10-08 19:15:14 +0300

    update template and testnet scripts

--- scripts/ibc/tokenregistration/template/generate-all.sh
+++ scripts/ibc/tokenregistration/template/generate-all.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ./$SIFCHAIN_ID
 rm -f ./$SIFCHAIN_ID/temp.json
-rm -f ./$SIFCHAIN_ID/temp2.json
+#rm -f ./$SIFCHAIN_ID/temp2.json
 rm -f ./$SIFCHAIN_ID/tokenregistry.json
 
 sifnoded q tokenregistry add-all ./$SIFCHAIN_ID/registry.json | jq > $SIFCHAIN_ID/temp.json
@@ -22,7 +22,9 @@ sifnoded q tokenregistry add ./$SIFCHAIN_ID/temp.json ./$SIFCHAIN_ID/regen.json 
 rm ./$SIFCHAIN_ID/temp.json
 sifnoded q tokenregistry add ./$SIFCHAIN_ID/tokenregistry.json ./$SIFCHAIN_ID/terra.json | jq > $SIFCHAIN_ID/temp.json
 rm ./$SIFCHAIN_ID/tokenregistry.json
-sifnoded q tokenregistry add ./$SIFCHAIN_ID/temp.json ./$SIFCHAIN_ID/osmosis.json | jq > $SIFCHAIN_ID/temp2.json
+sifnoded q tokenregistry add ./$SIFCHAIN_ID/temp.json ./$SIFCHAIN_ID/osmosis.json | jq > $SIFCHAIN_ID/tokenregistry.json
 rm ./$SIFCHAIN_ID/temp.json
-sifnoded q tokenregistry add ./$SIFCHAIN_ID/temp2.json ./$SIFCHAIN_ID/juno.json | jq > $SIFCHAIN_ID/tokenregistry.json
-rm ./$SIFCHAIN_ID/temp2.json
+sifnoded q tokenregistry add ./$SIFCHAIN_ID/tokenregistry.json ./$SIFCHAIN_ID/juno.json | jq > $SIFCHAIN_ID/temp.json
+rm ./$SIFCHAIN_ID/tokenregistry.json
+sifnoded q tokenregistry add ./$SIFCHAIN_ID/temp.json ./$SIFCHAIN_ID/ixo.json | jq > $SIFCHAIN_ID/tokenregistry.json
+rm ./$SIFCHAIN_ID/temp.json
