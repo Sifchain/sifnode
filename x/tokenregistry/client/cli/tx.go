e4c9dddb54577fde0b353b26c7b5defc376104c0
Brian Woods <me@brian.biz>
2021-10-13 21:41:22 -0700

    Needs more thorough testing and protobuf compilation

--- x/tokenregistry/client/cli/tx.go
+++ x/tokenregistry/client/cli/tx.go
@@ -98,6 +98,39 @@ func GetCmdRegister() *cobra.Command {
 	return cmd
 }
 
+func GetCmdSetRegistry() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "set-registry [registry.json]",
+		Short: "Reset the registry to the registry.json",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			err = cobra.ExactArgs(1)(cmd, args)
+			if err != nil {
+				return err
+			}
+			registry, err := whitelistutils.ParseDenoms(clientCtx.JSONMarshaler, args[0])
+			if err != nil {
+				return err
+			} else if len(registry.Entries) != 1 {
+				return errors.New("exactly one token entry must be specified in input file")
+			}
+			msg := types.MsgSetRegistry{
+				From:  clientCtx.GetFromAddress().String(),
+				Registry: registry,
+			}
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
 func GetCmdDeregister() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "deregister [denom]",
--- x/tokenregistry/handler/handler_test.go
+++ x/tokenregistry/handler/handler_test.go
@@ -118,6 +118,108 @@ func TestHandleRegister(t *testing.T) {
 	}
 }
 
+func TestHandleSetRegistry(t *testing.T) {
+	app, ctx, admin := test.CreateTestApp(false)
+	h := handler.NewHandler(app.TokenRegistryKeeper)
+	registry := app.TokenRegistryKeeper.GetRegistry(ctx)
+	require.Empty(t, registry)
+	tests := []struct {
+		name           string
+		msg            types.MsgSetRegistry
+		errorAssertion assert.ErrorAssertionFunc
+		valueAssertion require.ValueAssertionFunc
+	}{
+		{
+			name: "Successful Registration",
+			msg: types.MsgSetRegistry{
+				From: admin,
+				Registry: &types.Registry {
+					Entries: [1]*types.RegistryEntry{&types.RegistryEntry{Denom: "TestDenom", Decimals: 18}},
+				},
+			},
+			errorAssertion: assert.NoError,
+			valueAssertion: func(t require.TestingT, res interface{}, i ...interface{}) {
+				registry = app.TokenRegistryKeeper.GetRegistry(ctx)
+				require.Len(t, registry.Entries, 1)
+				d, err := app.TokenRegistryKeeper.GetEntry(registry, "TestDenom")
+				require.NoError(t, err)
+				require.Equal(t, "TestDenom", d.Denom)
+			},
+		},
+		{
+			name: "Successful IBC Registration",
+			msg: types.MsgSetRegistry{
+				From: admin,
+				Registry: &types.Registry {
+					Entries: [1]*types.RegistryEntry{&types.RegistryEntry{Denom: "TestDenomIBC", Decimals: 18}},
+				},
+			},
+			errorAssertion: assert.NoError,
+			valueAssertion: func(t require.TestingT, res interface{}, i ...interface{}) {
+				registry = app.TokenRegistryKeeper.GetRegistry(ctx)
+				require.Len(t, registry.Entries, 2)
+				d, err := app.TokenRegistryKeeper.GetEntry(registry, "TestDenomIBC")
+				require.NoError(t, err)
+				require.Equal(t, "TestDenomIBC", d.Denom)
+			},
+		},
+		{
+			name: "Successful IBC Registration 2",
+			msg: types.MsgSetRegistry{
+				From: admin,
+				Registry: &types.Registry{
+					Entries: [1]*types.RegistryEntry{&types.RegistryEntry{Denom: "TestDenomIBC2", Decimals: 8}},
+				},
+			},
+			errorAssertion: assert.NoError,
+			valueAssertion: func(t require.TestingT, res interface{}, i ...interface{}) {
+				registry = app.TokenRegistryKeeper.GetRegistry(ctx)
+				require.Len(t, registry.Entries, 3)
+				d, err := app.TokenRegistryKeeper.GetEntry(registry, "TestDenomIBC2")
+				require.NoError(t, err)
+				require.Equal(t, "TestDenomIBC2", d.Denom)
+			},
+		},
+		{
+			name: "Registration Ignore Duplicate Tokens",
+			msg: types.MsgSetRegistry{
+				From: admin,
+				Registry: &types.Registry{
+					Entries: [1]*types.RegistryEntry{&types.RegistryEntry{Denom: "TestDenomIBC", Decimals: 18}},
+				},
+			},
+			errorAssertion: assert.NoError,
+			valueAssertion: func(t require.TestingT, res interface{}, i ...interface{}) {
+				// Denom whitelist size is still 3, duplicate denoms are ignored.
+				registry = app.TokenRegistryKeeper.GetRegistry(ctx)
+				require.Len(t, registry.Entries, 3)
+				d, err := app.TokenRegistryKeeper.GetEntry(registry, "TestDenomIBC")
+				require.NoError(t, err)
+				require.Equal(t, "TestDenomIBC", d.Denom)
+			},
+		},
+		{
+			name: "Non Admin Account",
+			msg: types.MsgSetRegistry{
+				From: sdk.AccAddress("addr2_______________").String(),
+				Registry: &types.Registry{
+					Entries: [1]*types.RegistryEntry{&types.RegistryEntry{Denom: "TestDenom", Decimals: 18}},
+				},
+			},
+			errorAssertion: assert.Error,
+			valueAssertion: require.Nil,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := h(ctx, &tt.msg)
+			tt.errorAssertion(t, err)
+			tt.valueAssertion(t, res)
+		})
+	}
+}
+
 func TestHandleDeregister(t *testing.T) {
 	app, ctx, admin := test.CreateTestApp(false)
 	app.TokenRegistryKeeper.SetToken(ctx, &types.RegistryEntry{
