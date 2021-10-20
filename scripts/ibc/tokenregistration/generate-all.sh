880adc8ecfa36194776c1f845a2b899ecd64cea4
Ivan <ivanverch@gmail.com>
2021-10-21 00:38:24 +0300

    fix ixo entry, generate emoney, add missing records to tokenregistry for testnet

--- scripts/ibc/tokenregistration/generate-all.sh
+++ scripts/ibc/tokenregistration/generate-all.sh
@@ -6,7 +6,7 @@
 
 mkdir -p ./$SIFCHAIN_ID
 rm -f ./$SIFCHAIN_ID/temp.json
-#rm -f ./$SIFCHAIN_ID/temp2.json
+rm -f ./$SIFCHAIN_ID/temp2.json
 rm -f ./$SIFCHAIN_ID/tokenregistry.json
 
 sifnoded q tokenregistry add-all ./$SIFCHAIN_ID/registry.json | jq > $SIFCHAIN_ID/temp.json
@@ -31,4 +31,12 @@ rm ./$SIFCHAIN_ID/temp.json
 sifnoded q tokenregistry add ./$SIFCHAIN_ID/tokenregistry.json ./$SIFCHAIN_ID/juno.json | jq > $SIFCHAIN_ID/temp.json
 rm ./$SIFCHAIN_ID/tokenregistry.json
 sifnoded q tokenregistry add ./$SIFCHAIN_ID/temp.json ./$SIFCHAIN_ID/ixo.json | jq > $SIFCHAIN_ID/tokenregistry.json
+rm ./$SIFCHAIN_ID/temp.json
+sifnoded q tokenregistry add ./$SIFCHAIN_ID/tokenregistry.json ./$SIFCHAIN_ID/emoney.json | jq > $SIFCHAIN_ID/temp.json
+rm ./$SIFCHAIN_ID/tokenregistry.json
+sifnoded q tokenregistry add ./$SIFCHAIN_ID/temp.json ./$SIFCHAIN_ID/likecoin.json | jq > $SIFCHAIN_ID/tokenregistry.json
+rm ./$SIFCHAIN_ID/temp.json
+sifnoded q tokenregistry add ./$SIFCHAIN_ID/tokenregistry.json ./$SIFCHAIN_ID/bitsong.json | jq > $SIFCHAIN_ID/temp.json
+rm ./$SIFCHAIN_ID/tokenregistry.json
+sifnoded q tokenregistry add ./$SIFCHAIN_ID/temp.json ./$SIFCHAIN_ID/band.json | jq > $SIFCHAIN_ID/tokenregistry.json
 rm ./$SIFCHAIN_ID/temp.json
