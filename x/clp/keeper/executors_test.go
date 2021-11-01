af1c04530fc4a42936399601c553a82d8df8deac
wilsonc2020 <wilson@sifchain.finance>
2021-11-01 15:13:09 -0700

    add unit tests for keeper/executors

--- x/clp/keeper/executors_test.go
+++ x/clp/keeper/executors_test.go
@@ -70,56 +70,87 @@ func TestKeeper_AddLiquidity(t *testing.T) {
 func TestKeeper_RemoveLiquidityProvider(t *testing.T) {
 	ctx, app := test.CreateTestAppClp(false)
 	asset := types.NewAsset("eth")
+	initialBalance := sdk.NewUintFromString("1")
+	newAssetCoin := sdk.NewCoin(asset.Symbol, sdk.Int(initialBalance))
 	lpAddess, err := sdk.AccAddressFromBech32("sif1azpar20ck9lpys89r8x7zc8yu0qzgvtp48ng5v")
 	if err != nil {
 		fmt.Println("Error Creating Liquidity Provider :", err)
 	}
 	lp := app.ClpKeeper.CreateLiquidityProvider(ctx, &asset, sdk.NewUint(1), lpAddess)
-	app.ClpKeeper.DestroyLiquidityProvider(ctx, lp.Asset.Symbol, lp.LiquidityProviderAddress)
+	app.ClpKeeper.RemoveLiquidityProvider(ctx, sdk.Coins{newAssetCoin}.Sort(), lp)
 	assert.False(t, app.ClpKeeper.GetLiquidityProviderIterator(ctx).Valid())
 }
 
 func TestKeeper_DecommissionPool(t *testing.T) {
-	pool := test.GenerateRandomPool(1)[0]
+
 	ctx, app := test.CreateTestAppClp(false)
-	clpKeeper := app.ClpKeeper
-	err := clpKeeper.SetPool(ctx, &pool)
-	assert.NoError(t, err)
-	getpool, err := clpKeeper.GetPool(ctx, pool.ExternalAsset.Symbol)
-	assert.NoError(t, err, "Error in get pool")
-	assert.Equal(t, getpool, pool)
-	err = clpKeeper.DestroyPool(ctx, pool.ExternalAsset.Symbol)
-	assert.NoError(t, err)
-	_, err = clpKeeper.GetPool(ctx, pool.ExternalAsset.Symbol)
-	assert.Error(t, err, "Pool should be deleted")
-	// This should do nothing.
-	err = clpKeeper.DestroyPool(ctx, pool.ExternalAsset.Symbol)
-	assert.Error(t, err)
+	signer := test.GenerateAddress(test.AddressKey1)
+	//Parameters for create pool
+	nativeAssetAmount := sdk.NewUintFromString("998")
+	externalAssetAmount := sdk.NewUintFromString("998")
+	asset := types.NewAsset("eth")
+	externalCoin := sdk.NewCoin(asset.Symbol, sdk.Int(sdk.NewUint(10000)))
+	nativeCoin := sdk.NewCoin(types.NativeSymbol, sdk.Int(sdk.NewUint(10000)))
+	_ = app.ClpKeeper.GetBankKeeper().AddCoins(ctx, signer, sdk.NewCoins(externalCoin, nativeCoin))
+	msgCreatePool := types.NewMsgCreatePool(signer, asset, nativeAssetAmount, externalAssetAmount)
+	// Create Pool
+	pool, err := app.ClpKeeper.CreatePool(ctx, sdk.NewUint(1), &msgCreatePool)
+	if err != nil {
+		fmt.Println("Error Generating new pool :", err)
+	}
+	app.ClpKeeper.DecommissionPool(ctx, *pool)
+	require.NoError(t, err)
 }
 
 func TestKeeper_RemoveLiquidity(t *testing.T) {
-
+	// TODO:
 }
 
 func TestKeeper_InitiateSwap(t *testing.T) {
-	ctx, keeper, bankKeeper, _, _, _, _ := test.CreateTestKeepers(t, 0.7, []int64{3, 3}, "")
-
-	msg := types.NewMsgBurn(1, cosmosReceivers[0], ethereumSender, amount, "stake", amount)
-	coins := sdk.NewCoins(sdk.NewCoin("stake", amount), sdk.NewCoin(types.CethSymbol, amount))
-	_ = bankKeeper.MintCoins(ctx, types.ModuleName, coins)
-	_ = bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, cosmosReceivers[0], coins)
-
-	err := keeper.ProcessBurn(ctx, cosmosReceivers[0], &msg)
+	ctx, app := test.CreateTestAppClp(false)
+	senderAddress, _ := sdk.AccAddressFromBech32(test.AddressKey1)
+	asset := types.NewAsset("eth")
+	externalCoin := sdk.NewCoin(asset.Symbol, sdk.Int(sdk.NewUint(10000)))
+	err := app.ClpKeeper.InitiateSwap(ctx, externalCoin, senderAddress)
+	if err != nil {
+		fmt.Println("Error doing initialSwap :", err)
+	}
 	require.NoError(t, err)
 
-	receiverCoins := bankKeeper.GetAllBalances(ctx, cosmosReceivers[0])
-	require.Equal(t, receiverCoins.String(), string(""))
 }
 
 func TestKeeper_FinalizeSwap(t *testing.T) {
-
+	ctx, app := test.CreateTestAppClp(false)
+	signer := test.GenerateAddress("")
+	assetEth := types.NewAsset("eth")
+	assetDash := types.NewAsset("dash")
+	nativeAssetAmount := sdk.NewUintFromString("998")
+	externalAssetAmount := sdk.NewUintFromString("998")
+	asset := types.NewAsset("eth")
+	msgCreatePool := types.NewMsgCreatePool(signer, asset, nativeAssetAmount, externalAssetAmount)
+	// Create Pool
+	pool, err := app.ClpKeeper.CreatePool(ctx, sdk.NewUint(1), &msgCreatePool)
+	if err != nil {
+		fmt.Println("Error Generating new pool :", err)
+	}
+	// Test Parameters for swap
+	// initialBalance: Initial account balance for all assets created.
+	initialBalance := sdk.NewUintFromString("1000000000000000000000")
+	// poolBalance: Amount funded to pool. The same amount is used both for native and external asset.
+	externalCoin1 := sdk.NewCoin(assetEth.Symbol, sdk.Int(initialBalance))
+	externalCoin2 := sdk.NewCoin(assetDash.Symbol, sdk.Int(initialBalance))
+	nativeCoin := sdk.NewCoin(types.NativeSymbol, sdk.Int(initialBalance))
+	// Signer is given ETH and RWN (Signer will creat pool and become LP)
+	_ = app.ClpKeeper.GetBankKeeper().AddCoins(ctx, signer, sdk.NewCoins(externalCoin1, nativeCoin))
+	_ = app.ClpKeeper.GetBankKeeper().AddCoins(ctx, signer, sdk.NewCoins(externalCoin2))
+	msg := types.NewMsgSwap(signer, assetEth, assetDash, sdk.NewUint(1), sdk.NewUint(10))
+	app.ClpKeeper.FinalizeSwap(ctx, "1", *pool, msg)
+	require.NoError(t, err)
 }
 
 func TestKeeper_ParseToInt(t *testing.T) {
-
+	_, app := test.CreateTestAppClp(false)
+	res, boolean := app.ClpKeeper.ParseToInt("1")
+	assert.True(t, boolean)
+	assert.Equal(t, res, sdk.NewUint(1))
 }
