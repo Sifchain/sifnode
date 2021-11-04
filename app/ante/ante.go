487a932c82939c3f4b35106ea86fe2d88b0c445a
Ivan <ivanverch@gmail.com>
2021-11-04 21:46:51 +0200

    handle multiple fee denoms, improve fee checks

--- app/ante/ante.go
+++ app/ante/ante.go
@@ -1,6 +1,8 @@
 package ante
 
 import (
+	"strings"
+
 	disptypes "github.com/Sifchain/sifnode/x/dispensation/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -54,25 +56,34 @@ func (r AdjustGasPriceDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate
 		ctx = ctx.WithMinGasPrices(sdk.NewDecCoins(minGasPrice))
 		return next(ctx, tx, simulate)
 	}
-	minFees := sdk.ZeroInt()
+	minFee := sdk.ZeroInt()
 	for i := range msgs {
 		if msgs[i].Type() == banktypes.TypeMsgSend || msgs[i].Type() == banktypes.TypeMsgMultiSend ||
 			msgs[i].Type() == "createUserClaim" || msgs[i].Type() == "swap" ||
 			msgs[i].Type() == "remove_liquidity" || msgs[i].Type() == "add_liquidity" {
-			minFees = sdk.NewInt(100000000000000000) // 0.1
-		} else if msgs[i].Type() == "transfer" && minFees.LTE(sdk.NewInt(10000000000000000)) {
-			minFees = sdk.NewInt(10000000000000000) // 0.01
+			minFee = sdk.NewInt(100000000000000000) // 0.1
+		} else if msgs[i].Type() == "transfer" && minFee.LTE(sdk.NewInt(10000000000000000)) {
+			minFee = sdk.NewInt(10000000000000000) // 0.01
 		}
 	}
-	if minFees.Equal(sdk.ZeroInt()) {
+	if minFee.Equal(sdk.ZeroInt()) {
 		return next(ctx, tx, simulate)
 	}
 	feeTx, ok := tx.(sdk.FeeTx)
 	if !ok {
-		return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "Tx must be a FeeTx")
+		return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "tx must be a FeeTx")
 	}
 	fees := feeTx.GetFee()
-	if len(fees) == 1 && fees[0].Amount.LT(minFees) {
+	rowanFee := sdk.ZeroInt()
+	for j := range fees {
+		if strings.EqualFold("rowan", fees[j].Denom) {
+			rowanFee = fees[j].Amount
+		}
+	}
+	if rowanFee.LTE(sdk.ZeroInt()) {
+		return ctx, sdkerrors.Wrap(sdkerrors.ErrLogic, "unsupported fee asset")
+	}
+	if rowanFee.LT(minFee) {
 		return ctx, sdkerrors.Wrap(sdkerrors.ErrLogic, "tx fee is too low")
 	}
 	return next(ctx, tx, simulate)
