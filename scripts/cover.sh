7cd012ff862d989c745f97edf80d362b9df56222
Kevin DeGraaf <kevin@sifchain.finance>
2021-11-15 18:07:13 -0800

    wip3

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -10,7 +10,7 @@ for filename in ${excludelist_dontcover} ${excludelist_pb} ${excludelist_pb_gw};
 done
 # Probably a better way to do this, but doesn't work if done like above.
 for filename in x/*/client/*/*.go; do
-  filename=$(echo $filename | sed 's/^../github.com\/Sifchain\/sifnode/g')
+  rem filename=$(echo $filename | sed 's/^../github.com\/Sifchain\/sifnode/g')
   echo "Excluding ${filename} from coverage report..."
   sed -i.bak "/$(echo $filename | sed 's/\//\\\//g')/d" coverage.txt
 done
