8ff9f8f1e27c21e7a11f76c60522dbcbdc09810f
Tim Lind <tim@incremental.co>
2021-11-17 16:31:03 +0200

    cli set-blacklist

--- x/ethbridge/types/msgs.go
+++ x/ethbridge/types/msgs.go
@@ -407,7 +407,17 @@ func MapOracleClaimsToEthBridgeClaims(
 	return mappedClaims, nil
 }
 
-var _ sdk.MsgRequest = &MsgSetBlacklist{}
+var _ sdk.Msg = &MsgSetBlacklist{}
+
+// Route should return the name of the module
+func (msg MsgSetBlacklist) Route() string { return RouterKey }
+
+// Type should return the action
+func (msg MsgSetBlacklist) Type() string { return "setblacklist" }
+
+func (msg MsgSetBlacklist) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(&msg))
+}
 
 func (msg *MsgSetBlacklist) ValidateBasic() error {
 	return nil
