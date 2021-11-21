36fbb21ac5b04f5e33877dc749ff8df4a6f8e34c
Tanmay <tanmay@sifchain.finance>
2021-11-20 23:03:37 -0500

    rebased develop

--- app/ante/ante.go
+++ app/ante/ante.go
@@ -1,13 +1,13 @@
 package ante
 
 import (
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"strings"
 
 	disptypes "github.com/Sifchain/sifnode/x/dispensation/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/x/auth/ante"
-	"github.com/pkg/errors"
 )
 
 func NewAnteHandler(options ante.HandlerOptions) (sdk.AnteHandler, error) {
@@ -30,7 +30,7 @@ func NewAnteHandler(options ante.HandlerOptions) (sdk.AnteHandler, error) {
 	}
 	return sdk.ChainAnteDecorators(
 		ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
-		NewReduceGasPriceDecorator(),    // Custom decorator to reduce gas price for specific msg types
+		NewAdjustGasPriceDecorator(),    // Custom decorator to reduce gas price for specific msg types
 		ante.NewRejectExtensionOptionsDecorator(),
 		ante.NewMempoolFeeDecorator(),
 		ante.NewValidateBasicDecorator(),
@@ -47,48 +47,65 @@ func NewAnteHandler(options ante.HandlerOptions) (sdk.AnteHandler, error) {
 
 }
 
-// ReduceGasPriceDecorator is a custom decorator to reduce fee prices .
-type ReduceGasPriceDecorator struct {
+// AdjustGasPriceDecorator is a custom decorator to reduce fee prices .
+type AdjustGasPriceDecorator struct {
 }
 
-// NewReduceGasPriceDecorator create a new instance of ReduceGasPriceDecorator
-func NewReduceGasPriceDecorator() ReduceGasPriceDecorator {
-	return ReduceGasPriceDecorator{}
+// NewAdjustGasPriceDecorator create a new instance of AdjustGasPriceDecorator
+func NewAdjustGasPriceDecorator() AdjustGasPriceDecorator {
+	return AdjustGasPriceDecorator{}
 }
 
-// AnteHandle reduces the gas price to a lower value which is hardcoded.The ReduceGasPriceDecorator should only be used for specific transaction types to lower the fee cost.
-func (r ReduceGasPriceDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
+// AnteHandle adjusts the gas price based on the tx type.
+func (r AdjustGasPriceDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
 	msgs := tx.GetMsgs()
+	if len(msgs) == 1 && (strings.Contains(strings.ToLower(sdk.MsgTypeURL(msgs[0])), strings.ToLower(disptypes.MsgTypeCreateDistribution)) ||
+		strings.Contains(strings.ToLower(sdk.MsgTypeURL(msgs[0])), strings.ToLower(disptypes.MsgTypeRunDistribution))) {
 
-	var found bool
-	for i := range msgs {
-		msgTypeURLLower := strings.ToLower(sdk.MsgTypeURL(msgs[i]))
-		if strings.Contains(msgTypeURLLower, strings.ToLower(disptypes.MsgTypeCreateDistribution)) ||
-			strings.Contains(msgTypeURLLower, strings.ToLower(disptypes.MsgTypeRunDistribution)) {
-			found = true
+		minGasPrice := sdk.DecCoin{
+			Denom:  "rowan",
+			Amount: sdk.MustNewDecFromStr("0.00000005"),
 		}
-	}
-
-	// Pass earlier if not a dispensation tx.
-	if !found {
+		if !minGasPrice.IsValid() {
+			return ctx, sdkerrors.Wrap(sdkerrors.ErrLogic, "invalid gas price")
+		}
+		ctx = ctx.WithMinGasPrices(sdk.NewDecCoins(minGasPrice))
 		return next(ctx, tx, simulate)
 	}
+	minFee := sdk.ZeroInt()
+	for i := range msgs {
+		msgTypeURLLower := strings.ToLower(sdk.MsgTypeURL(msgs[i]))
 
-	// If number of messages is greater than one, the other messages will be able to get away with a lower tx fee
-	if len(msgs) != 1 {
-		return ctx, errors.New("transaction for dispensation create / run must have exactly one message")
+		if strings.Contains(msgTypeURLLower, strings.ToLower(banktypes.TypeMsgSend)) ||
+			strings.Contains(msgTypeURLLower, strings.ToLower(banktypes.TypeMsgMultiSend)) ||
+			strings.Contains(msgTypeURLLower, "createuserclaim") ||
+			strings.Contains(msgTypeURLLower, "swap") ||
+			strings.Contains(msgTypeURLLower, "removeliquidity") ||
+			strings.Contains(msgTypeURLLower, "addliquidity") {
+			minFee = sdk.NewInt(100000000000000000) // 0.1
+		} else if strings.Contains(msgTypeURLLower, "transfer") && minFee.LTE(sdk.NewInt(10000000000000000)) {
+			minFee = sdk.NewInt(10000000000000000) // 0.01
+		}
 	}
-
-	loweredGasPrice := sdk.DecCoin{
-		Denom:  "rowan",
-		Amount: sdk.MustNewDecFromStr("0.00000005"),
+	if minFee.Equal(sdk.ZeroInt()) {
+		return next(ctx, tx, simulate)
 	}
-
-	if !loweredGasPrice.IsValid() {
-		return ctx, sdkerrors.Wrap(sdkerrors.ErrLogic, "unable to lower gas price")
+	feeTx, ok := tx.(sdk.FeeTx)
+	if !ok {
+		return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "tx must be a FeeTx")
 	}
-
-	ctx = ctx.WithMinGasPrices(sdk.NewDecCoins(loweredGasPrice))
-
+	fees := feeTx.GetFee()
+	rowanFee := sdk.ZeroInt()
+	for j := range fees {
+		if strings.EqualFold("rowan", fees[j].Denom) {
+			rowanFee = fees[j].Amount
+		}
+	}
+	if rowanFee.LTE(sdk.ZeroInt()) {
+		return ctx, sdkerrors.Wrap(sdkerrors.ErrLogic, "unsupported fee asset")
+	}
+	if rowanFee.LT(minFee) {
+		return ctx, sdkerrors.Wrap(sdkerrors.ErrLogic, "tx fee is too low")
+	}
 	return next(ctx, tx, simulate)
 }
