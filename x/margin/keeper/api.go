9b287ae35ac314efd5657c5775052198986a33aa
Tim Lind <tim@incremental.co>
2021-12-01 17:58:41 +0200

    add mtp functions to keeper interface

--- x/margin/keeper/api.go
+++ x/margin/keeper/api.go
@@ -9,4 +9,12 @@ import (
 type KeeperI interface {
 	InitGenesis(sdk.Context, types.GenesisState) []abci.ValidatorUpdate
 	ExportGenesis(sdk.Context) *types.GenesisState
+
+	SetMTP(ctx sdk.Context, mtp *types.MTP) error
+	GetMTP(ctx sdk.Context, symbol string, mtpAddress string) (types.MTP, error)
+	GetMTPIterator(ctx sdk.Context) sdk.Iterator
+	GetMTPs(ctx sdk.Context) []*types.MTP
+	GetMTPsForAsset(ctx sdk.Context, asset string) []*types.MTP
+	GetAssetsForMTP(ctx sdk.Context, mtpAddress sdk.Address) []string
+	DestroyMTP(ctx sdk.Context, symbol string, mtpAddress string) error
 }
