0e5b14e144a90acf3ed5e65cc0fae16bf5680344
Tim Lind <tim@incremental.co>
2021-12-06 13:04:15 +0200

    try fix build by implementing msg interface without pointer reference

--- x/margin/types/msgs.go
+++ x/margin/types/msgs.go
@@ -4,11 +4,11 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 
 var _ sdk.Msg = &MsgOpenLong{}
 
-func (m *MsgOpenLong) ValidateBasic() error {
+func (m MsgOpenLong) ValidateBasic() error {
 	return nil
 }
 
-func (m *MsgOpenLong) GetSigners() []sdk.AccAddress {
+func (m MsgOpenLong) GetSigners() []sdk.AccAddress {
 	signer, err := sdk.AccAddressFromBech32(m.Signer)
 	if err != nil {
 		panic(err)
