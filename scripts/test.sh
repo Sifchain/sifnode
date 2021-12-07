30853f550d90d384a64a921375e80b0445a78993
wilsonc2020 <wilson@sifchain.finance>
2021-12-07 14:36:02 -0800

    remove test.sh

--- scripts/test.sh
+++
@@ -1,6 +0,0 @@
-# Probably a better way to do this, but doesn't work if done like above.
-for filename in x/*/client/*/*.go; do
-  rem filename=$(echo $filename | sed 's/^../github.com\/Sifchain\/sifnode/g')
-  echo "Excluding ${filename} from coverage report..."
-  sed -i.bak "/$(echo $filename | sed 's/\//\\\//g')/d" coverage.txt
-done
