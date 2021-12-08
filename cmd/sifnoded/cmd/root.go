d9d952f95b8095d78560914e7de72de7cb6e22cf
Tanmay <tanmay@sifchain.finance>
2021-12-08 18:22:55 +0530

    debug network.go

--- cmd/sifnoded/cmd/root.go
+++ cmd/sifnoded/cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/cosmos/cosmos-sdk/client/config"
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	"io"
 	"os"
@@ -32,40 +33,6 @@ import (
 	"github.com/Sifchain/sifnode/app"
 )
 
-//func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
-//	encodingConfig := app.MakeTestEncodingConfig()
-//	initClientCtx := client.Context{}.
-//		WithCodec(encodingConfig.Marshaler).
-//		WithInterfaceRegistry(encodingConfig.InterfaceRegistry).
-//		WithTxConfig(encodingConfig.TxConfig).
-//		WithLegacyAmino(encodingConfig.Amino).
-//		WithInput(os.Stdin).
-//		WithAccountRetriever(types.AccountRetriever{}).
-//		WithBroadcastMode(flags.BroadcastBlock).
-//		WithHomeDir(app.DefaultNodeHome).
-//		WithViper("")
-//	rootCmd := &cobra.Command{
-//		Use:   "sifnoded",
-//		Short: "app Daemon (server)",
-//		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
-//			initClientCtx, err := client.ReadPersistentCommandFlags(initClientCtx, cmd.Flags())
-//			if err != nil {
-//				return err
-//			}
-//			initClientCtx, err = config.ReadFromClientConfig(initClientCtx)
-//			if err != nil {
-//				return err
-//			}
-//			if err := client.SetCmdClientContextHandler(initClientCtx, cmd); err != nil {
-//				return err
-//			}
-//			return server.InterceptConfigsPreRunHandler(cmd, "", nil)
-//		},
-//	}
-//	initRootCmd(rootCmd, encodingConfig)
-//	return rootCmd, encodingConfig
-//}
-
 func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 	encodingConfig := app.MakeTestEncodingConfig()
 	initClientCtx := client.Context{}.
@@ -76,22 +43,27 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 		WithInput(os.Stdin).
 		WithAccountRetriever(types.AccountRetriever{}).
 		WithBroadcastMode(flags.BroadcastBlock).
-		WithHomeDir(app.DefaultNodeHome)
-
+		WithHomeDir(app.DefaultNodeHome).
+		WithViper("")
 	rootCmd := &cobra.Command{
 		Use:   "sifnoded",
 		Short: "app Daemon (server)",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
+			initClientCtx, err := client.ReadPersistentCommandFlags(initClientCtx, cmd.Flags())
+			if err != nil {
+				return err
+			}
+			initClientCtx, err = config.ReadFromClientConfig(initClientCtx)
+			if err != nil {
+				return err
+			}
 			if err := client.SetCmdClientContextHandler(initClientCtx, cmd); err != nil {
 				return err
 			}
-
 			return server.InterceptConfigsPreRunHandler(cmd, "", nil)
 		},
 	}
-
 	initRootCmd(rootCmd, encodingConfig)
-
 	return rootCmd, encodingConfig
 }
 
