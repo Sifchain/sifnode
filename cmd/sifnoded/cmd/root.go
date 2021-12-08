1fcc7e821b18357828328bd7757d19de1d73a2ee
Tanmay <tanmay@sifchain.finance>
2021-12-08 18:10:23 +0530

    debug network.go

--- cmd/sifnoded/cmd/root.go
+++ cmd/sifnoded/cmd/root.go
@@ -1,13 +1,13 @@
 package cmd
 
 import (
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	"io"
 	"os"
 	"path/filepath"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/config"
 	"github.com/cosmos/cosmos-sdk/client/debug"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/keys"
@@ -20,7 +20,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
-	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/crisis"
 	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
@@ -33,37 +32,66 @@ import (
 	"github.com/Sifchain/sifnode/app"
 )
 
+//func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
+//	encodingConfig := app.MakeTestEncodingConfig()
+//	initClientCtx := client.Context{}.
+//		WithCodec(encodingConfig.Marshaler).
+//		WithInterfaceRegistry(encodingConfig.InterfaceRegistry).
+//		WithTxConfig(encodingConfig.TxConfig).
+//		WithLegacyAmino(encodingConfig.Amino).
+//		WithInput(os.Stdin).
+//		WithAccountRetriever(types.AccountRetriever{}).
+//		WithBroadcastMode(flags.BroadcastBlock).
+//		WithHomeDir(app.DefaultNodeHome).
+//		WithViper("")
+//	rootCmd := &cobra.Command{
+//		Use:   "sifnoded",
+//		Short: "app Daemon (server)",
+//		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
+//			initClientCtx, err := client.ReadPersistentCommandFlags(initClientCtx, cmd.Flags())
+//			if err != nil {
+//				return err
+//			}
+//			initClientCtx, err = config.ReadFromClientConfig(initClientCtx)
+//			if err != nil {
+//				return err
+//			}
+//			if err := client.SetCmdClientContextHandler(initClientCtx, cmd); err != nil {
+//				return err
+//			}
+//			return server.InterceptConfigsPreRunHandler(cmd, "", nil)
+//		},
+//	}
+//	initRootCmd(rootCmd, encodingConfig)
+//	return rootCmd, encodingConfig
+//}
+
 func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 	encodingConfig := app.MakeTestEncodingConfig()
 	initClientCtx := client.Context{}.
-		WithCodec(encodingConfig.Marshaler).
+		WithJSONCodec(encodingConfig.Marshaler).
 		WithInterfaceRegistry(encodingConfig.InterfaceRegistry).
 		WithTxConfig(encodingConfig.TxConfig).
 		WithLegacyAmino(encodingConfig.Amino).
 		WithInput(os.Stdin).
 		WithAccountRetriever(types.AccountRetriever{}).
 		WithBroadcastMode(flags.BroadcastBlock).
-		WithHomeDir(app.DefaultNodeHome).
-		WithViper("")
+		WithHomeDir(app.DefaultNodeHome)
+
 	rootCmd := &cobra.Command{
 		Use:   "sifnoded",
 		Short: "app Daemon (server)",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
-			initClientCtx, err := client.ReadPersistentCommandFlags(initClientCtx, cmd.Flags())
-			if err != nil {
-				return err
-			}
-			initClientCtx, err = config.ReadFromClientConfig(initClientCtx)
-			if err != nil {
-				return err
-			}
 			if err := client.SetCmdClientContextHandler(initClientCtx, cmd); err != nil {
 				return err
 			}
+
 			return server.InterceptConfigsPreRunHandler(cmd, "", nil)
 		},
 	}
+
 	initRootCmd(rootCmd, encodingConfig)
+
 	return rootCmd, encodingConfig
 }
 
