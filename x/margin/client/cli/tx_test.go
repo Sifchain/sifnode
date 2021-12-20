b518872761736657b51b28486acbb552197af595
Tim Lind <tim@incremental.co>
2021-12-20 15:15:04 +0200

    integration test for open long

---
+++ x/margin/client/cli/tx_test.go
@@ -0,0 +1,69 @@
+package cli_test
+
+import (
+	"os"
+	"testing"
+
+	sifapp "github.com/Sifchain/sifnode/app"
+	"github.com/Sifchain/sifnode/x/margin/client/cli"
+	servertypes "github.com/cosmos/cosmos-sdk/server/types"
+	testutilcli "github.com/cosmos/cosmos-sdk/testutil/cli"
+	testnetwork "github.com/cosmos/cosmos-sdk/testutil/network"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
+)
+
+type IntegrationTestSuite struct {
+	suite.Suite
+
+	cfg     testnetwork.Config
+	network *testnetwork.Network
+}
+
+func (s *IntegrationTestSuite) SetupSuite() {
+	s.T().Log("setting up integration test suite")
+
+	cfg := testnetwork.DefaultConfig()
+	cfg.NumValidators = 1
+	cfg.AppConstructor = func(val testnetwork.Validator) servertypes.Application {
+		db := dbm.NewMemDB()
+		return sifapp.NewSifApp(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, val.Dir, 0, sifapp.MakeTestEncodingConfig(), sifapp.EmptyAppOptions{})
+	}
+
+	s.cfg = cfg
+	s.network = testnetwork.New(s.T(), cfg)
+
+	_, err := s.network.WaitForHeight(1)
+	s.Require().NoError(err)
+}
+
+func (s *IntegrationTestSuite) TearDownSuite() {
+	s.T().Log("tearing down integration test suite")
+
+	// This is important and must be called to ensure other tests can create
+	// a network!
+	s.network.Cleanup()
+}
+
+func (s *IntegrationTestSuite) TestOpenLongCmd() {
+	val := s.network.Validators[0]
+	// baseURL := val.APIAddress
+
+	// Use baseURL to make API HTTP requests or use val.RPCClient to make direct
+	// Tendermint RPC calls.
+	// ...
+	args := []string{
+		"--collateral_asset=rowan",
+		"--borrow_asset=atom",
+		"--collateral_amount=1000",
+		"--from=" + val.Address.String(),
+	}
+	_, err := testutilcli.ExecTestCLICmd(val.ClientCtx, cli.GetOpenLongCmd(), args)
+	require.NoError(s.T(), err)
+}
+
+func TestIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(IntegrationTestSuite))
+}
