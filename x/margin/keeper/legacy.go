663e23623e66abcc88123c598d77d66f4aa5eb22
Tim Lind <tim@incremental.co>
2021-12-28 13:16:45 +0200

    legacy querier and handler

--- x/margin/keeper/legacy.go
+++ x/margin/keeper/legacy.go
@@ -3,12 +3,29 @@ package keeper
 import (
 	"github.com/Sifchain/sifnode/x/margin/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	abci "github.com/tendermint/tendermint/abci/types"
 )
 
 func NewLegacyQuerier(k types.Keeper) sdk.Querier {
-	return nil
+	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, error) {
+		return nil, sdkerrors.Wrap(types.ErrUnknownRequest, "unknown request")
+	}
 }
 
 func NewLegacyHandler(k types.Keeper) sdk.Handler {
-	return nil
+	msgServer := NewMsgServerImpl(k)
+
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		switch msg := msg.(type) {
+		case *types.MsgOpenLong:
+			res, err := msgServer.OpenLong(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgCloseLong:
+			res, err := msgServer.CloseLong(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		default:
+			return nil, sdkerrors.Wrap(types.ErrUnknownRequest, "unknown request")
+		}
+	}
 }
