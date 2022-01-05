fab2d6737a11d2653810dfd8ff85906920385f5d
Tim Lind <tim@incremental.co>
2022-01-05 14:12:48 +0200

    grpc query for mtp

--- x/margin/types/query.pb.go
+++ x/margin/types/query.pb.go
@@ -11,7 +11,11 @@ import (
 	proto "github.com/gogo/protobuf/proto"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
+	io "io"
 	math "math"
+	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -25,22 +29,139 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
+type MTPRequest struct {
+	Address         string `protobuf:"bytes,1,opt,name=address,proto3" json:"address,omitempty"`
+	CustodyAsset    string `protobuf:"bytes,2,opt,name=custody_asset,json=custodyAsset,proto3" json:"custody_asset,omitempty"`
+	CollateralAsset string `protobuf:"bytes,3,opt,name=collateral_asset,json=collateralAsset,proto3" json:"collateral_asset,omitempty"`
+}
+
+func (m *MTPRequest) Reset()         { *m = MTPRequest{} }
+func (m *MTPRequest) String() string { return proto.CompactTextString(m) }
+func (*MTPRequest) ProtoMessage()    {}
+func (*MTPRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_73c14070fed1f663, []int{0}
+}
+func (m *MTPRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MTPRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MTPRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MTPRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MTPRequest.Merge(m, src)
+}
+func (m *MTPRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *MTPRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_MTPRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MTPRequest proto.InternalMessageInfo
+
+func (m *MTPRequest) GetAddress() string {
+	if m != nil {
+		return m.Address
+	}
+	return ""
+}
+
+func (m *MTPRequest) GetCustodyAsset() string {
+	if m != nil {
+		return m.CustodyAsset
+	}
+	return ""
+}
+
+func (m *MTPRequest) GetCollateralAsset() string {
+	if m != nil {
+		return m.CollateralAsset
+	}
+	return ""
+}
+
+type MTPResponse struct {
+	Mtp *MTP `protobuf:"bytes,1,opt,name=mtp,proto3" json:"mtp,omitempty"`
+}
+
+func (m *MTPResponse) Reset()         { *m = MTPResponse{} }
+func (m *MTPResponse) String() string { return proto.CompactTextString(m) }
+func (*MTPResponse) ProtoMessage()    {}
+func (*MTPResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_73c14070fed1f663, []int{1}
+}
+func (m *MTPResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MTPResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MTPResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MTPResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MTPResponse.Merge(m, src)
+}
+func (m *MTPResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *MTPResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_MTPResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MTPResponse proto.InternalMessageInfo
+
+func (m *MTPResponse) GetMtp() *MTP {
+	if m != nil {
+		return m.Mtp
+	}
+	return nil
+}
+
+func init() {
+	proto.RegisterType((*MTPRequest)(nil), "sifnode.margin.v1.MTPRequest")
+	proto.RegisterType((*MTPResponse)(nil), "sifnode.margin.v1.MTPResponse")
+}
+
 func init() { proto.RegisterFile("sifnode/margin/v1/query.proto", fileDescriptor_73c14070fed1f663) }
 
 var fileDescriptor_73c14070fed1f663 = []byte{
-	// 181 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x2d, 0xce, 0x4c, 0xcb,
-	0xcb, 0x4f, 0x49, 0xd5, 0xcf, 0x4d, 0x2c, 0x4a, 0xcf, 0xcc, 0xd3, 0x2f, 0x33, 0xd4, 0x2f, 0x2c,
-	0x4d, 0x2d, 0xaa, 0xd4, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x84, 0x4a, 0xeb, 0x41, 0xa4,
-	0xf5, 0xca, 0x0c, 0xa5, 0x44, 0xd2, 0xf3, 0xd3, 0xf3, 0xc1, 0xb2, 0xfa, 0x20, 0x16, 0x44, 0xa1,
-	0x94, 0x4c, 0x7a, 0x7e, 0x7e, 0x7a, 0x4e, 0xaa, 0x7e, 0x62, 0x41, 0xa6, 0x7e, 0x62, 0x5e, 0x5e,
-	0x7e, 0x49, 0x62, 0x49, 0x66, 0x7e, 0x5e, 0x31, 0x54, 0x16, 0x8b, 0x2d, 0x25, 0x95, 0x05, 0xa9,
-	0x50, 0x69, 0x23, 0x76, 0x2e, 0xd6, 0x40, 0x90, 0xa5, 0x4e, 0x2e, 0x27, 0x1e, 0xc9, 0x31, 0x5e,
-	0x78, 0x24, 0xc7, 0xf8, 0xe0, 0x91, 0x1c, 0xe3, 0x84, 0xc7, 0x72, 0x0c, 0x17, 0x1e, 0xcb, 0x31,
-	0xdc, 0x78, 0x2c, 0xc7, 0x10, 0xa5, 0x95, 0x9e, 0x59, 0x92, 0x51, 0x9a, 0xa4, 0x97, 0x9c, 0x9f,
-	0xab, 0x1f, 0x9c, 0x99, 0x96, 0x9c, 0x91, 0x98, 0x99, 0xa7, 0x0f, 0x33, 0xb5, 0x02, 0x66, 0x2e,
-	0xd8, 0xd0, 0x24, 0x36, 0xb0, 0xa9, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x54, 0x3b, 0x59,
-	0x42, 0xdc, 0x00, 0x00, 0x00,
+	// 314 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x90, 0x41, 0x4b, 0xc3, 0x30,
+	0x18, 0x86, 0x5b, 0x87, 0x13, 0x33, 0x45, 0x0d, 0x22, 0x63, 0xb8, 0x20, 0xf3, 0x32, 0x3d, 0x24,
+	0x6c, 0x1e, 0x3c, 0x2b, 0x82, 0x78, 0x18, 0xcc, 0xb9, 0x93, 0x17, 0xc9, 0xda, 0x2c, 0x0b, 0x74,
+	0xf9, 0xba, 0x26, 0x1d, 0xee, 0x5f, 0xf8, 0xb3, 0x3c, 0xee, 0xe8, 0x51, 0xd6, 0x3f, 0x22, 0x4d,
+	0x5b, 0x3c, 0xe8, 0x6e, 0xc9, 0xfb, 0x3c, 0xf0, 0xf2, 0xbd, 0xa8, 0x6d, 0xd4, 0x54, 0x43, 0x28,
+	0xd8, 0x9c, 0x27, 0x52, 0x69, 0xb6, 0xec, 0xb1, 0x45, 0x2a, 0x92, 0x15, 0x8d, 0x13, 0xb0, 0x80,
+	0x4f, 0x4a, 0x4c, 0x0b, 0x4c, 0x97, 0xbd, 0xd6, 0xa9, 0x04, 0x09, 0x8e, 0xb2, 0xfc, 0x55, 0x88,
+	0xad, 0x73, 0x09, 0x20, 0x23, 0xc1, 0x78, 0xac, 0x18, 0xd7, 0x1a, 0x2c, 0xb7, 0x0a, 0xb4, 0x29,
+	0xe9, 0x3f, 0x2d, 0x76, 0x15, 0x8b, 0x12, 0x77, 0x96, 0x08, 0x0d, 0xc6, 0xc3, 0x91, 0x58, 0xa4,
+	0xc2, 0x58, 0xdc, 0x44, 0x7b, 0x3c, 0x0c, 0x13, 0x61, 0x4c, 0xd3, 0xbf, 0xf0, 0xbb, 0xfb, 0xa3,
+	0xea, 0x8b, 0x2f, 0xd1, 0x61, 0x90, 0x1a, 0x0b, 0xe1, 0xea, 0x8d, 0x1b, 0x23, 0x6c, 0x73, 0xc7,
+	0xf1, 0x83, 0x32, 0xbc, 0xcb, 0x33, 0x7c, 0x85, 0x8e, 0x03, 0x88, 0x22, 0x6e, 0x45, 0xc2, 0xa3,
+	0xd2, 0xab, 0x39, 0xef, 0xe8, 0x37, 0x77, 0x6a, 0xe7, 0x16, 0x35, 0x5c, 0xaf, 0x89, 0x41, 0x1b,
+	0x81, 0xbb, 0xa8, 0x36, 0xb7, 0xb1, 0x2b, 0x6d, 0xf4, 0xcf, 0xe8, 0x9f, 0xd3, 0x69, 0x2e, 0xe7,
+	0x4a, 0x7f, 0x84, 0x76, 0x9f, 0xf3, 0x95, 0xf0, 0x13, 0xaa, 0x3f, 0x0a, 0x3b, 0x18, 0x0f, 0x71,
+	0x7b, 0x8b, 0x5f, 0x1c, 0xd5, 0x22, 0xdb, 0x70, 0xd1, 0xdd, 0xf1, 0xee, 0x1f, 0x3e, 0x37, 0xc4,
+	0x5f, 0x6f, 0x88, 0xff, 0xbd, 0x21, 0xfe, 0x47, 0x46, 0xbc, 0x75, 0x46, 0xbc, 0xaf, 0x8c, 0x78,
+	0xaf, 0xd7, 0x52, 0xd9, 0x59, 0x3a, 0xa1, 0x01, 0xcc, 0xd9, 0x8b, 0x9a, 0x06, 0x33, 0xae, 0x34,
+	0xab, 0x16, 0x7d, 0xaf, 0x36, 0x75, 0x83, 0x4e, 0xea, 0x6e, 0xd1, 0x9b, 0x9f, 0x00, 0x00, 0x00,
+	0xff, 0xff, 0x1c, 0xd8, 0x3c, 0x10, 0xd8, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -55,6 +176,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type QueryClient interface {
+	GetMTP(ctx context.Context, in *MTPRequest, opts ...grpc.CallOption) (*MTPResponse, error)
 }
 
 type queryClient struct {
@@ -65,22 +187,506 @@ func NewQueryClient(cc grpc1.ClientConn) QueryClient {
 	return &queryClient{cc}
 }
 
+func (c *queryClient) GetMTP(ctx context.Context, in *MTPRequest, opts ...grpc.CallOption) (*MTPResponse, error) {
+	out := new(MTPResponse)
+	err := c.cc.Invoke(ctx, "/sifnode.margin.v1.Query/GetMTP", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // QueryServer is the server API for Query service.
 type QueryServer interface {
+	GetMTP(context.Context, *MTPRequest) (*MTPResponse, error)
 }
 
 // UnimplementedQueryServer can be embedded to have forward compatible implementations.
 type UnimplementedQueryServer struct {
 }
 
+func (*UnimplementedQueryServer) GetMTP(ctx context.Context, req *MTPRequest) (*MTPResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetMTP not implemented")
+}
+
 func RegisterQueryServer(s grpc1.Server, srv QueryServer) {
 	s.RegisterService(&_Query_serviceDesc, srv)
+}
+
+func _Query_GetMTP_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(MTPRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(QueryServer).GetMTP(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/sifnode.margin.v1.Query/GetMTP",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(QueryServer).GetMTP(ctx, req.(*MTPRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "sifnode.margin.v1.Query",
 	HandlerType: (*QueryServer)(nil),
-	Methods:     []grpc.MethodDesc{},
-	Streams:     []grpc.StreamDesc{},
-	Metadata:    "sifnode/margin/v1/query.proto",
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "GetMTP",
+			Handler:    _Query_GetMTP_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "sifnode/margin/v1/query.proto",
 }
+
+func (m *MTPRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MTPRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MTPRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.CollateralAsset) > 0 {
+		i -= len(m.CollateralAsset)
+		copy(dAtA[i:], m.CollateralAsset)
+		i = encodeVarintQuery(dAtA, i, uint64(len(m.CollateralAsset)))
+		i--
+		dAtA[i] = 0x1a
+	}
+	if len(m.CustodyAsset) > 0 {
+		i -= len(m.CustodyAsset)
+		copy(dAtA[i:], m.CustodyAsset)
+		i = encodeVarintQuery(dAtA, i, uint64(len(m.CustodyAsset)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.Address) > 0 {
+		i -= len(m.Address)
+		copy(dAtA[i:], m.Address)
+		i = encodeVarintQuery(dAtA, i, uint64(len(m.Address)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *MTPResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MTPResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MTPResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.Mtp != nil {
+		{
+			size, err := m.Mtp.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintQuery(dAtA, i, uint64(size))
+		}
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func encodeVarintQuery(dAtA []byte, offset int, v uint64) int {
+	offset -= sovQuery(v)
+	base := offset
+	for v >= 1<<7 {
+		dAtA[offset] = uint8(v&0x7f | 0x80)
+		v >>= 7
+		offset++
+	}
+	dAtA[offset] = uint8(v)
+	return base
+}
+func (m *MTPRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Address)
+	if l > 0 {
+		n += 1 + l + sovQuery(uint64(l))
+	}
+	l = len(m.CustodyAsset)
+	if l > 0 {
+		n += 1 + l + sovQuery(uint64(l))
+	}
+	l = len(m.CollateralAsset)
+	if l > 0 {
+		n += 1 + l + sovQuery(uint64(l))
+	}
+	return n
+}
+
+func (m *MTPResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.Mtp != nil {
+		l = m.Mtp.Size()
+		n += 1 + l + sovQuery(uint64(l))
+	}
+	return n
+}
+
+func sovQuery(x uint64) (n int) {
+	return (math_bits.Len64(x|1) + 6) / 7
+}
+func sozQuery(x uint64) (n int) {
+	return sovQuery(uint64((x << 1) ^ uint64((int64(x) >> 63))))
+}
+func (m *MTPRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MTPRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MTPRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Address", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Address = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field CustodyAsset", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.CustodyAsset = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field CollateralAsset", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.CollateralAsset = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MTPResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MTPResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MTPResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Mtp", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Mtp == nil {
+				m.Mtp = &MTP{}
+			}
+			if err := m.Mtp.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func skipQuery(dAtA []byte) (n int, err error) {
+	l := len(dAtA)
+	iNdEx := 0
+	depth := 0
+	for iNdEx < l {
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return 0, ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return 0, io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= (uint64(b) & 0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		wireType := int(wire & 0x7)
+		switch wireType {
+		case 0:
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return 0, ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return 0, io.ErrUnexpectedEOF
+				}
+				iNdEx++
+				if dAtA[iNdEx-1] < 0x80 {
+					break
+				}
+			}
+		case 1:
+			iNdEx += 8
+		case 2:
+			var length int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return 0, ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return 0, io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				length |= (int(b) & 0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if length < 0 {
+				return 0, ErrInvalidLengthQuery
+			}
+			iNdEx += length
+		case 3:
+			depth++
+		case 4:
+			if depth == 0 {
+				return 0, ErrUnexpectedEndOfGroupQuery
+			}
+			depth--
+		case 5:
+			iNdEx += 4
+		default:
+			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
+		}
+		if iNdEx < 0 {
+			return 0, ErrInvalidLengthQuery
+		}
+		if depth == 0 {
+			return iNdEx, nil
+		}
+	}
+	return 0, io.ErrUnexpectedEOF
+}
+
+var (
+	ErrInvalidLengthQuery        = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowQuery          = fmt.Errorf("proto: integer overflow")
+	ErrUnexpectedEndOfGroupQuery = fmt.Errorf("proto: unexpected end of group")
+)
