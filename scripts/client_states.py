6b1dac704c96eb42b4357a50ce52f180ff5b5c22
Brian Woods <me@brian.biz>
2022-01-10 16:06:12 -0800

    checkpoint

---
+++ scripts/client_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import subprocess
+from dateutil.parser import parse
+
+output = subprocess.check_output(["sifnoded", "q", "ibc", "client", "states", "--output", "json", "--node", sys.argv[1]])
+
+clients = json.loads(output.decode('utf-8'))
+
+# get the block time
+output = subprocess.check_output(["sifnoded", "q", "ibc", "connection", "connections", "--output", "json", "--node", sys.argv[1]])
+
+connection = json.loads(output.decode('utf-8'))
+
+current_block_number = connection['height']['revision_height']
+output = subprocess.check_output(["sifnoded", "q", "block", current_block_number, "--node", sys.argv[1]])
+current_block = json.loads(output.decode('utf-8'))
+current_block_time = parse(current_block['block']['header']['time'])
+
+print("Current block time: " + str(current_block_time))
+
+
+for client_data in clients['client_states']:
+  client_id = client_data['client_id']
+  revision_height = client_data['client_state']['latest_height']['revision_height']
+  trusting_period = client_data['client_state']['trusting_period']
+
+  # now get the time from the block at the revision height
+  # and compare to the time at the current block
+  output = subprocess.check_output(["sifnoded", "q", "block", revision_height, "--node", sys.argv[1]])
+  block = json.loads(output.decode('utf-8'))
+
+  block_time = parse(block['block']['header']['time'])
+
+  print("client_id: " + client_id)
+  print("RPC endpoint block time: " + str(current_block_time))
+  print("consensus block time: " + str(block_time))
+  print("revison height: " + revision_height)
+  print('trusting period: ' + trusting_period)
+
+
+
