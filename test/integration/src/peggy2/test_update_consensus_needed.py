06e9f95c8268f96aeec159e02b2ac417e7d55f02
Jure Žvikart <jure@sifchain.finance>
2022-01-17 12:52:59 +0100

    Add future test descriptions

---
+++ test/integration/src/peggy2/test_update_consensus_needed.py
@@ -0,0 +1,5 @@
+# Test scenario:
+#
+# We need add the test case for it. For instance, three nodes with the same voting power, the init value is 0.6, then
+# the claim finalized after two transactions. After update it as 0.8, then we need three transactions. And the update
+# for network A doesn't impact other network. -- by @juniuszhou
