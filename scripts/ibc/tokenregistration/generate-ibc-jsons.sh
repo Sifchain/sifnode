78fecece59a8584642eebfefcaf0b37875fb8403
Brian Woods <me@brian.biz>
2022-01-26 15:07:36 -0800

    first pass at making the generation script more generic

--- scripts/ibc/tokenregistration/generate-ibc-jsons.sh
+++ scripts/ibc/tokenregistration/generate-ibc-jsons.sh
@@ -8,345 +8,59 @@ echo "\n\ngenerating and storing all entries for network $SIFCHAIN_ID"
 
 mkdir -p ./$SIFCHAIN_ID
 
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=$COSMOS_BASE_DENOM \
-	--token_ibc_counterparty_chain_id=$COSMOS_CHAIN_ID \
-  --token_ibc_channel_id=$COSMOS_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$COSMOS_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/cosmos.json
+OSMOSIS_TOKEN_DECIMALS=6
+AKASH_TOKEN_DECIMALS=6
+SENTINEL_TOKEN_DECIMALS=6
+IRIS_TOKEN_DECIMALS=6
+PERSISTENCE_TOKEN_DECIMALS=6
+CRYPTO_ORG_TOKEN_DECIMALS=6
+REGEN_TOKEN_DECIMALS=6
+JUNO_TOKEN_DECIMALS=6
+IXO_TOKEN_DECIMALS=6
+LIKECOIN_TOKEN_DECIMALS=6
+BITSONG_TOKEN_DECIMALS=6
+BAND_TOKEN_DECIMALS=6
+EMONEY_TOKEN_DECIMALS=6
+EMONEY_EUR_TOKEN_DECIMALS=6
+TERRA_TOKEN_DECIMALS=6
+TERRA_UUSD_TOKEN_DECIMALS=6
+BITSONG_TOKEN_DECIMALS=6
+SECRET_TOKEN_DECIMALS=6
+COMDEX_TOKEN_DECIMALS=6
 
-echo "\n\ngenerated entry for COSMOS $COSMOS_CHAIN_ID"
+for chain in AKASH SENTINEL IRIS PERSISTENCE CRYPTO_ORG REGEN OSMOSIS JUNO IXO LIKECOIN BITSONG BAND EMONEY EMONEY_EUR TERRA TERRA_UUSD SECRET COMDEX
+do
+  # do some munging
+  chain_id=$chain"_CHAIN_ID"
+  chain_id=${!chain_id}
+  base_denom=$chain"_BASE_DENOM"
+  base_denom=${!base_denom}
+  channel_id=$chain"_CHANNEL_ID"
+  channel_id=${!channel_id}
+  counterparty_channel_id=$chain"_COUNTERPARTY_CHANNEL_ID"
+  counterparty_channel_id=${!counterparty_channel_id}
+  token_decimals=$chain"_TOKEN_DECIMALS"
+  token_decimals=${!token_decimals}
 
-cat $SIFCHAIN_ID/cosmos.json | jq
+  filename="$(tr [A-Z] [a-z] <<< "$chain")"
 
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=uakt \
-  --token_ibc_counterparty_chain_id=$AKASH_CHAIN_ID \
-  --token_ibc_channel_id=$AKASH_CHANNEL_ID \
-	--token_ibc_counterparty_channel_id=$AKASH_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="UAKT" \
-	--token_external_symbol="uakt" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/akash.json
+  # generate the IBC json
+  sifnoded q tokenregistry generate -o json \
+  	--token_base_denom=$base_denom \
+  	--token_ibc_counterparty_chain_id=$chain_id \
+        --token_ibc_channel_id=$channel_id \
+        --token_ibc_counterparty_channel_id=$counterparty_channel_id \
+  	--token_ibc_counterparty_denom="" \
+  	--token_unit_denom="" \
+  	--token_decimals=$token_decimals \
+  	--token_display_name="" \
+        --token_network=$chain_id \
+  	--token_external_symbol="" \
+  	--token_permission_clp=true \
+  	--token_permission_ibc_export=true \
+  	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/${filename}.json
+  
+  echo "\n\ngenerated entry for $chain $chain_id"
+  cat $SIFCHAIN_ID/${filename}.json | jq
+done
 
-echo "\n\ngenerated entry for AKASH $AKASH_CHAIN_ID"
-
-cat $SIFCHAIN_ID/akash.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=udvpn \
-	--token_ibc_counterparty_chain_id=$SENTINEL_CHAIN_ID \
-  --token_ibc_channel_id=$SENTINEL_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$SENTINEL_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="uDVPN" \
-	--token_external_symbol="udvpn" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/sentinel.json
-
-echo "\n\ngenerated entry for SENTINEL $SENTINEL_CHAIN_ID"
-
-cat $SIFCHAIN_ID/sentinel.json | jq
-
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=$IRIS_BASE_DENOM \
-	--token_ibc_counterparty_chain_id=$IRIS_CHAIN_ID \
-  --token_ibc_channel_id=$IRIS_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$IRIS_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/iris.json
-
-echo "\n\ngenerated entry for IRIS $IRIS_CHAIN_ID"
-
-cat $SIFCHAIN_ID/iris.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=uxprt \
-	--token_ibc_counterparty_chain_id=$PERSISTENCE_CHAIN_ID \
-  --token_ibc_channel_id=$PERSISTENCE_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$PERSISTENCE_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="uXPRT" \
-	--token_external_symbol="uxprt" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/persistence.json
-
-echo "\n\ngenerated entry for PERSISTENCE $PERSISTENCE_CHAIN_ID"
-
-cat $SIFCHAIN_ID/persistence.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=basecro \
-	--token_ibc_counterparty_chain_id=$CRYPTO_ORG_CHAIN_ID \
-  --token_ibc_channel_id=$CRYPTO_ORG_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$CRYPTO_ORG_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=8 \
-	--token_display_name="CRO" \
-	--token_external_symbol="basecro" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/crypto-org.json
-
-echo "\n\ngenerated entry for CRYPTO $CRYPTO_ORG_CHAIN_ID"
-
-cat $SIFCHAIN_ID/crypto-org.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=uregen \
-	--token_ibc_counterparty_chain_id=$REGEN_CHAIN_ID \
-  --token_ibc_channel_id=$REGEN_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$REGEN_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/regen.json
-
-echo "\n\ngenerated entry for REGEN $REGEN_CHAIN_ID"
-
-cat $SIFCHAIN_ID/regen.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=uosmo \
-	--token_ibc_counterparty_chain_id=$OSMOSIS_CHAIN_ID \
-  --token_ibc_channel_id=$OSMOSIS_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$OSMOSIS_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/osmosis.json
-
-echo "\n\ngenerated entry for OSMOSIS $OSMOSIS_CHAIN_ID"
-
-cat $SIFCHAIN_ID/osmosis.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=ujuno \
-	--token_ibc_counterparty_chain_id=$JUNO_CHAIN_ID \
-  --token_ibc_channel_id=$JUNO_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$JUNO_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/juno.json
-
-echo "\n\ngenerated entry for JUNO $JUNO_CHAIN_ID"
-
-cat $SIFCHAIN_ID/juno.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=uixo \
-	--token_ibc_counterparty_chain_id=$IXO_CHAIN_ID \
-  --token_ibc_channel_id=$IXO_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$IXO_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/ixo.json
-
-echo "\n\ngenerated entry for IXO $IXO_CHAIN_ID"
-
-cat $SIFCHAIN_ID/ixo.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=nanolike \
-	--token_ibc_counterparty_chain_id=$LIKECOIN_CHAIN_ID \
-  --token_ibc_channel_id=$LIKECOIN_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$LIKECOIN_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=9 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/likecoin.json
-
-echo "\n\ngenerated entry for LIKECOIN $LIKECOIN_CHAIN_ID"
-
-cat $SIFCHAIN_ID/likecoin.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=ubtsg \
-	--token_ibc_counterparty_chain_id=$BITSONG_CHAIN_ID \
-  --token_ibc_channel_id=$BITSONG_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$BITSONG_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/bitsong.json
-
-echo "\n\ngenerated entry for BITSONG $BITSONG_CHAIN_ID"
-
-cat $SIFCHAIN_ID/bitsong.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=uband \
-	--token_ibc_counterparty_chain_id=$BAND_CHAIN_ID \
-  --token_ibc_channel_id=$BAND_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$BAND_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/band.json
-
-echo "\n\ngenerated entry for BAND $BAND_CHAIN_ID"
-
-cat $SIFCHAIN_ID/band.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=ungm \
-	--token_ibc_counterparty_chain_id=$EMONEY_CHAIN_ID \
-  --token_ibc_channel_id=$EMONEY_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$EMONEY_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/emoney.json
-
-echo "\n\ngenerated entry for EMONEY $EMONEY_CHAIN_ID"
-
-cat $SIFCHAIN_ID/emoney.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=eeur \
-	--token_ibc_counterparty_chain_id=$EMONEY_CHAIN_ID \
-  --token_ibc_channel_id=$EMONEY_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$EMONEY_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/emoney-eeur.json
-
-echo "\n\ngenerated entry for EMONEY $EMONEY_CHAIN_ID"
-
-cat $SIFCHAIN_ID/emoney-eeur.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=uluna \
-	--token_ibc_counterparty_chain_id=$TERRA_CHAIN_ID \
-  --token_ibc_channel_id=$TERRA_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$TERRA_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/terra.json
-
-echo "\n\ngenerated entry for TERRA $TERRA_CHAIN_ID"
-
-cat $SIFCHAIN_ID/terra.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=uusd \
-	--token_ibc_counterparty_chain_id=$TERRA_CHAIN_ID \
-  --token_ibc_channel_id=$TERRA_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$TERRA_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=6 \
-	--token_display_name="" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/terra-uusd.json
-
-echo "\n\ngenerated entry for TERRA $TERRA_CHAIN_ID"
-
-cat $SIFCHAIN_ID/terra-uusd.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=scrt \
-	--token_ibc_counterparty_chain_id=$SECRET_CHAIN_ID \
-  --token_ibc_channel_id=$SECRET_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$SECRET_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=18 \
-	--token_display_name="Secret" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/scrt.json
-
-echo "\n\ngenerated entry for SECRET $SECRET_CHAIN_ID"
-
-cat $SIFCHAIN_ID/scrt.json | jq
-
-sifnoded q tokenregistry generate -o json \
-	--token_base_denom=cdmx \
-	--token_ibc_counterparty_chain_id=$COMDEX_CHAIN_ID \
-  --token_ibc_channel_id=$COMDEX_CHANNEL_ID \
-  --token_ibc_counterparty_channel_id=$COMDEX_COUNTERPARTY_CHANNEL_ID \
-	--token_ibc_counterparty_denom="" \
-	--token_unit_denom="" \
-	--token_decimals=18 \
-	--token_display_name="Comdex" \
-	--token_external_symbol="" \
-	--token_permission_clp=true \
-	--token_permission_ibc_export=true \
-	--token_permission_ibc_import=true | jq > $SIFCHAIN_ID/cdmx.json
-
-echo "\n\ngenerated entry for $COMDEX_CHAIN_ID"
-
-cat $SIFCHAIN_ID/cmdx.json | jq
