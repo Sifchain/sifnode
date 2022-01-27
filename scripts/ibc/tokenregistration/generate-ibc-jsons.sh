5de54d19eb3e52ee1b5e9101f3dba322cbf40472
Brian Woods <me@brian.biz>
2022-01-27 14:05:18 -0800

    cleaning up generation script

--- scripts/ibc/tokenregistration/generate-ibc-jsons.sh
+++ scripts/ibc/tokenregistration/generate-ibc-jsons.sh
@@ -8,39 +8,35 @@ echo "\n\ngenerating and storing all entries for network $SIFCHAIN_ID"
 
 mkdir -p ./$SIFCHAIN_ID
 
-OSMOSIS_TOKEN_DECIMALS=6
-AKASH_TOKEN_DECIMALS=6
-SENTINEL_TOKEN_DECIMALS=6
-IRIS_TOKEN_DECIMALS=6
-PERSISTENCE_TOKEN_DECIMALS=6
-CRYPTO_ORG_TOKEN_DECIMALS=6
-REGEN_TOKEN_DECIMALS=6
-JUNO_TOKEN_DECIMALS=6
-IXO_TOKEN_DECIMALS=6
-LIKECOIN_TOKEN_DECIMALS=9
-BITSONG_TOKEN_DECIMALS=6
-BAND_TOKEN_DECIMALS=6
-EMONEY_TOKEN_DECIMALS=6
-EMONEY_EUR_TOKEN_DECIMALS=6
-TERRA_TOKEN_DECIMALS=6
-TERRA_UUSD_TOKEN_DECIMALS=6
-BITSONG_TOKEN_DECIMALS=6
-SECRET_TOKEN_DECIMALS=6
-COMDEX_TOKEN_DECIMALS=6
-
-for chain in AKASH SENTINEL IRIS PERSISTENCE CRYPTO_ORG REGEN OSMOSIS JUNO IXO LIKECOIN BITSONG BAND EMONEY EMONEY_EUR TERRA TERRA_UUSD SECRET COMDEX
+for chain in AKASH SENTINEL IRIS PERSISTENCE CRYPTO_ORG REGEN OSMOSIS JUNO IXO LIKECOIN BITSONG BAND EMONEY EMONEY_EEUR TERRA TERRA_UUSD SECRET COMDEX
 do
   # do some munging
   chain_id=$chain"_CHAIN_ID"
   chain_id=${!chain_id}
+
+  # skip the empties
+  if [ -z "$chain_id" ]; then
+    continue
+  fi
+
   base_denom=$chain"_BASE_DENOM"
   base_denom=${!base_denom}
   channel_id=$chain"_CHANNEL_ID"
   channel_id=${!channel_id}
   counterparty_channel_id=$chain"_COUNTERPARTY_CHANNEL_ID"
   counterparty_channel_id=${!counterparty_channel_id}
-  token_decimals=$chain"_TOKEN_DECIMALS"
-  token_decimals=${!token_decimals}
+
+  # almost always they are 6 but this is the place to override them
+  token_decimals=0
+  case $chain in
+    LIKECOIN)
+      token_decimals=9
+      ;;
+    *)
+      token_decimals=6
+      ;;
+  esac
+
   token_address=$chain"_TOKEN_ADDRESS"
   token_address=${!token_address}
 
