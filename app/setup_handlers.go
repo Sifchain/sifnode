e80658705e857f3730623175f4df2b3846c67c55
Tanmay <tanmay@sifchain.finance>
2022-02-10 02:00:15 -0500

    added script

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -4,7 +4,6 @@ import (
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	m "github.com/cosmos/cosmos-sdk/types/module"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
 	"github.com/cosmos/cosmos-sdk/x/feegrant"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
@@ -22,7 +21,7 @@ func SetupHandlers(app *SifchainApp) {
 	app.UpgradeKeeper.SetUpgradeHandler(rc1, func(ctx sdk.Context, plan types.Plan, vm m.VersionMap) (m.VersionMap, error) {
 		app.Logger().Info("Running upgrade handler for " + rc1)
 		app.IBCKeeper.ConnectionKeeper.SetParams(ctx, ibcconnectiontypes.DefaultParams())
-		fromVM := make(map[string]uint64)
+		//fromVM := make(map[string]uint64)
 		// Old Modules can execute Migrations if needed .
 		// State migration logic should be added to a migrator function
 		// Migrating modules should increment the ConsensusVersion
@@ -31,15 +30,15 @@ func SetupHandlers(app *SifchainApp) {
 		//	fromVM[moduleName] = 1
 		//}
 		// Set to 2 , which is the same as the ConsensusVersion to disable migrate function
-		fromVM[authtypes.ModuleName] = 2
-		newVM, err := app.mm.RunMigrations(ctx, app.configurator, fromVM)
-		if err != nil {
-			panic(err)
-		}
+		//fromVM[authtypes.ModuleName] = 2
+		//newVM, err := app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		//if err != nil {
+		//	panic(err)
+		//}
 		// Set it back to 1 to run only auth migration
-		newVM[authtypes.ModuleName] = 1
+		//newVM[authtypes.ModuleName] = 1
 		// This is to make sure auth module migrates after staking
-		return app.mm.RunMigrations(ctx, app.configurator, newVM)
+		return app.mm.RunMigrations(ctx, app.configurator, vm)
 	})
 
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
