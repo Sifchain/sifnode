1357484b0ced3a3ab9a2b216755ad9bf2d426659
Tim Lind <tim@incremental.co>
2022-02-11 12:49:15 +0200

    set default genesis params

--- x/margin/types/genesis.go
+++ x/margin/types/genesis.go
@@ -4,17 +4,16 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		// TODO review default param values
 		Params: &Params{
 			LeverageMax:          sdk.NewUint(1),
 			HealthGainFactor:     sdk.NewDec(1),
-			InterestRateMin:      sdk.NewDec(1),
-			InterestRateMax:      sdk.NewDec(1),
-			InterestRateDecrease: sdk.NewDec(1),
-			InterestRateIncrease: sdk.NewDec(1),
+			InterestRateMin:      sdk.NewDec(0.005),
+			InterestRateMax:      sdk.NewDec(3),
+			InterestRateDecrease: sdk.NewDec(0.1),
+			InterestRateIncrease: sdk.NewDec(0.1),
+			ForceCloseThreshold:  sdk.NewDec(0.1),
 			EpochLength:          1,
-			// TODO start with empty slice for pools
-			Pools: []string{"ceth"},
+			Pools:                []string{},
 		},
 	}
 }
