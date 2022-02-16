837c1a29fabca4b03486fd88822d308ca339334f
Tanmay <tanmay@sifchain.finance>
2022-02-15 22:49:08 -0500

    added upgrade handler

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -4,9 +4,7 @@ import (
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	m "github.com/cosmos/cosmos-sdk/types/module"
-	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
-	"github.com/cosmos/cosmos-sdk/x/feegrant"
-	types3 "github.com/cosmos/cosmos-sdk/x/mint/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	ibcconnectiontypes "github.com/cosmos/ibc-go/v2/modules/core/03-connection/types"
 	"strings"
@@ -15,21 +13,32 @@ import (
 const releaseVersion = "0.11.0"
 
 var (
-	rc1 = GetUpgradeName(releaseVersion, "rc1")
+	rc1 = GetUpgradeName(releaseVersion, "rc.1")
 )
 
 func SetupHandlers(app *SifchainApp) {
 	app.UpgradeKeeper.SetUpgradeHandler(rc1, func(ctx sdk.Context, plan types.Plan, vm m.VersionMap) (m.VersionMap, error) {
 		app.Logger().Info("Running upgrade handler for " + rc1)
 		app.IBCKeeper.ConnectionKeeper.SetParams(ctx, ibcconnectiontypes.DefaultParams())
-		minter := types3.Minter{
+		/*
+			The exact APR depends on the total Bonded Rowan , and can thus fluctuate a little .
+
+			- Rewards Percentage Required
+			    - APR * BondRatio ( Total Bonded Rowan/ Total Supply Rowan)
+
+			- Calculations for APR 300 % , assuming the max APR to be 350 and min APR to be 250
+			    - 300% → 41.78
+			    - 350 % → 48.74
+			    - 250 % → 34.81
+		*/
+		minter := minttypes.Minter{
 			Inflation:        sdk.MustNewDecFromStr("0.417800000000000000"),
 			AnnualProvisions: sdk.ZeroDec(),
 		}
 		app.MintKeeper.SetMinter(ctx, minter)
 		params := app.MintKeeper.GetParams(ctx)
-		params.InflationMax = sdk.MustNewDecFromStr("0.500000000000000000")
-		params.InflationMin = sdk.MustNewDecFromStr("0.370000000000000000")
+		params.InflationMax = sdk.MustNewDecFromStr("0.487400000000000000")
+		params.InflationMin = sdk.MustNewDecFromStr("0.348100000000000000")
 		app.MintKeeper.SetParams(ctx, params)
 		return app.mm.RunMigrations(ctx, app.configurator, vm)
 	})
@@ -39,9 +48,7 @@ func SetupHandlers(app *SifchainApp) {
 		panic(err)
 	}
 	if upgradeInfo.Name == releaseVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		storeUpgrades := storetypes.StoreUpgrades{
-			Added: []string{feegrant.ModuleName, crisistypes.ModuleName},
-		}
+		storeUpgrades := storetypes.StoreUpgrades{}
 		// Use upgrade store loader for the initial loading of all stores when app starts,
 		// it checks if version == upgradeHeight and applies store upgrades before loading the stores,
 		// so that new stores start with the correct version (the current height of chain),
