7e4f5f4b41d2cc91a3cba048ca0127370fe422f0
marsifi <martin@sifchain.finance>
2022-02-18 17:46:55 +0000

    Update dbtool

--- cmd/dbtool/main.go
+++ cmd/dbtool/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bufio"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"os"
 
 	cfg "github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/rpc/core"
+	coretypes "github.com/tendermint/tendermint/rpc/core/types"
 	rpctypes "github.com/tendermint/tendermint/rpc/jsonrpc/types"
 	"github.com/tendermint/tendermint/state/txindex/kv"
 	"github.com/tendermint/tendermint/store"
@@ -28,7 +30,9 @@ var (
 	// "fungible_token_packet.sucess='001'"
 	// "fungible_token_packet.denom='transfer/channel-19/ungm'"
 	// "fungible_token_packet.denom='transfer/channel-18/uluna'"
-	defaultQuery   = "update_client.client_id='07-tendermint-41'"
+	// defaultQuery = "update_client.client_id='07-tendermint-41' AND fungible_token_packet.success='false'"
+	defaultQuery = "update_client.client_id='07-tendermint-41'"
+	// defaultQuery   = "send_packet.packet_dst_port='transfer'"
 	defaultPage    = 1
 	defaultPerPage = 10
 )
@@ -60,12 +64,6 @@ func main() {
 		panic(err)
 	}
 
-	f, err := openOutputFile(outfile)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
 	fmt.Printf("Getting transactions (page %d, perPage %d)...\n", page, perPage)
 	res, err := core.TxSearch(
 		&rpctypes.Context{},
@@ -81,15 +79,12 @@ func main() {
 
 	fmt.Printf("results: %d | total: %d\n", len(res.Txs), res.TotalCount)
 
-	fmt.Printf("Writing transactions to %s...\n", outfile)
-	datawriter := bufio.NewWriter(f)
-	for _, tx := range res.Txs {
-		for _, ev := range tx.TxResult.Events {
-			datawriter.WriteString(ev.String() + "\n\n")
-		}
-		datawriter.WriteString("*************************************\n\n")
+	events := filterEvents(res.Txs)
+
+	err = printEvents(events)
+	if err != nil {
+		panic(err)
 	}
-	datawriter.Flush()
 }
 
 func openDB(dataPath string) error {
@@ -143,4 +138,76 @@ func getTxIndexer(config *cfg.Config) (*kv.TxIndex, error) {
 func openOutputFile(filename string) (*os.File, error) {
 	os.Remove(filename)
 	return os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+}
+
+type EventInfo struct {
+	Type           string
+	PacketSequence int
+	ChannelID      string
+	TxHash         string
+	Denom          string
+	Amount         uint64
+	Attributes     []string
+}
+
+func filterEvents(txs []*coretypes.ResultTx) []*EventInfo {
+	infos := []*EventInfo{}
+	for _, tx := range txs {
+		txHash := hex.EncodeToString(tx.Tx.Hash())
+		for _, ev := range tx.TxResult.Events {
+			attributes := []string{}
+			if keepEvent(ev.Type) {
+				for _, attr := range ev.Attributes {
+					attributes = append(attributes, attr.String())
+				}
+				info := &EventInfo{
+					TxHash:     txHash,
+					Type:       ev.Type,
+					Attributes: attributes,
+				}
+				infos = append(infos, info)
+			}
+		}
+	}
+	return infos
+}
+
+func keepEvent(eventType string) bool {
+
+	// incoming transfers
+	// return eventType == "recv_packet" ||
+	// 	eventType == "write_acknowledgement"
+
+	// outgoing transfers
+	return eventType == "send_packet" ||
+		eventType == "acknowledge_packet" ||
+		eventType == "timeout_packet"
+
+	// return true
+
+}
+
+func printEvents(events []*EventInfo) error {
+
+	f, err := openOutputFile(outfile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	fmt.Printf("Writing events to %s...\n", outfile)
+
+	datawriter := bufio.NewWriter(f)
+	for _, info := range events {
+		fmt.Fprintf(f, "tx: %s\n", info.TxHash)
+		fmt.Fprintf(f, "type: %s\n", info.Type)
+		fmt.Fprintf(f, "attributes:\n")
+		for _, attr := range info.Attributes {
+			fmt.Fprintf(f, "	%s\n", attr)
+		}
+		fmt.Fprintf(f, "\n")
+	}
+	datawriter.Flush()
+
+	return nil
 }
