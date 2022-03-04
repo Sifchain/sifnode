07b7a7c7c55427423093fc342de8d8b97b733672
Brandon <Brandon@Sifchain.finance>
2022-03-03 17:41:16 -0700

    Added a script to update the blocklist on bridgebank.

--- smart-contracts/scripts/patchBlocklist.ts
+++ smart-contracts/scripts/patchBlocklist.ts
@@ -4,7 +4,7 @@ import {BridgeBank__factory} from "../build";
 const BridgeBankAddress = "0xB5F54ac4466f5ce7E0d8A5cB9FE7b8c0F35B7Ba8";
 const BlockListAddress = "0x9C8a2011cCb697D7EDe3c94f9FBa5686a04DeACB";
 
-async function finishUpdate() {
+async function updateBridgebankBlocklist() {
     const [admin, operator, pauser] = await ethers.getSigners();
     const factory = await ethers.getContractFactory("BridgeBank") as BridgeBank__factory;
     const bridgebank = await factory.attach(BridgeBankAddress);
@@ -12,12 +12,8 @@ async function finishUpdate() {
     // Set the blocklist variable
     console.log("Setting the blocklist variable");
     await bridgebank.connect(operator).setBlocklist(BlockListAddress);
-
-    // Unpause the bridgebank
-    console.log("Unpausing the variable");
-    await bridgebank.connect(pauser).unpause();
 }
 
 finishUpdate()
-    .then(() => {console.log("Tests completed")})
+    .then(() => {console.log("Bridge Bank Blocklist Updated")})
     .catch((err)=> console.error("Encountered an error:", err))
