5bec622ece431d7c32310415dad1be5eee44d0d3
James Moore <james@sifchain.finance>
2022-03-15 09:50:48 -0700

    Added scripts to generate bulk sifchain accounts

--- smart-contracts/bin/make_sif_accounts.sh
+++ smart-contracts/bin/make_sif_accounts.sh
@@ -4,6 +4,10 @@
 
 # Creates 1000 sif accounts in a test keychain located in /tmp/somedirectory
 
+# To output json for those accounts, use this standard tool:
+
+#   sifnoded keys list --keyring-backend test --home test_data/test_keychain --output json > test_data/test_keychain.json
+
 naccounts=$1
 shift
 home=$1
