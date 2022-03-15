e2dd826c1800ed1434c17a3c9557805749dd5c72
juniuszhou <junius.zhou@gmail.com>
2022-03-15 23:36:37 +0800

    add main func

--- smart-contracts/data/denom_mapping.py
+++ smart-contracts/data/denom_mapping.py
@@ -2,15 +2,6 @@ import subprocess
 import json 
 import os
 
-# network descriptor is 1 for ethereum
-network_descriptor = 1
-
-bridge_bank_address = '0xB5F54ac4466f5ce7E0d8A5cB9FE7b8c0F35B7Ba8'
-
-# get all entries from product
-result = subprocess.run(['sifnoded', 'query', 'tokenregistry', 'entries', '--node',  'tcp://rpc-archive.sifchain.finance:80'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-denoms = json.loads(result)['entries']
-
 # algorithm to get denom in peggy2.0
 def getPeggy2Denom(network_descriptor, token_contract_address):
     assert token_contract_address.startswith("0x")
@@ -26,64 +17,53 @@ def composeMapping(old_denom, new_denom):
         'peggy2': new_denom
     }
 
-folder = './'
+# network descriptor is 1 for ethereum
+network_descriptor = 1
 
-directory = os.fsencode(folder)
-denom_address_map = {}   
+def main():
+    # get all entries from product
+    result = subprocess.run(['sifnoded', 'query', 'tokenregistry', 'entries', '--node',  'tcp://rpc-archive.sifchain.finance:80'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    denoms = json.loads(result)['entries']
 
-# try get all contracts from whitelist_* files from same folder
-# but the contracts not completed
-# for file in os.listdir(directory):
-#      filename = os.fsdecode(file)
-#      # all deployed contract in the files with prefix whitelist
-#      if filename.startswith('whitelist'):
-#         print(filename)
-#         file = open(folder + '/' + filename)
-#         data = json.load(file)
+    denom_address_map = {}
+    data = json.load(open('denom_contracts.json'))
+    for entry in data:
+        denom_address_map[entry['symbol']] = entry['token']
 
-#         entries = data['array']
-#         for entry in entries:
-#             if 'symbol' in entry and 'address' in entry:
-#                 denom_address_map[entry['symbol']] = entry['address']
+    # map denom in peggy 1.0 to peggy 2.0
+    # if denom start with ibc, then denom is the same with peggy 2.0
+    # if denom start with c, then remove c, call getPeggy2Denom
+    # if denom start with x, then remove x, call getPeggy2Denom
 
-# denom_contracts.json got from run following command in develop branch
-# yarn integrationtest:whitelistedTokens --json_path /Users/junius/github/sifnode/smart-contracts/deployments/sifchain-1  
-# --ethereum_network mainnet --bridgebank_address 0xB5F54ac4466f5ce7E0d8A5cB9FE7b8c0F35B7Ba8 --network mainnet
+    missed_denom = []
+    result = []
+    output_file = open('denom_mapping.json', 'w', encoding='utf-8')
 
-data = json.load(open('denom_contracts.json'))
-for entry in data:
-    denom_address_map[entry['symbol']] = entry['token']
-
-# map denom in peggy 1.0 to peggy 2.0
-# if denom start with ibc, then denom is the same with peggy 2.0
-# if denom start with c, then remove c, call getPeggy2Denom
-# if denom start with x, then remove x, call getPeggy2Denom
-
-missed_denom = []
-result = []
-output_file = open('denom_mapping.json', 'w', encoding='utf-8')
-
-for item in denoms:
-    denom = (item['denom'])
-
-    if denom == 'rowan':
-        result.append(composeMapping(denom, denom))
-    elif denom.startswith('ibc/'):
-        result.append(composeMapping(denom, denom))
-    elif denom.startswith('c'):
-        tmp_denom = denom[1:].upper()
-        if tmp_denom in denom_address_map:
-            result.append(composeMapping(denom, getPeggy2Denom(network_descriptor, denom_address_map[tmp_denom])))
+    for item in denoms:
+        denom = (item['denom'])
+        if denom == 'rowan':
+            result.append(composeMapping(denom, denom))
+        elif denom.startswith('ibc/'):
+            result.append(composeMapping(denom, denom))
+        elif denom.startswith('c'):
+            tmp_denom = denom[1:].upper()
+            if tmp_denom in denom_address_map:
+                result.append(composeMapping(denom, getPeggy2Denom(network_descriptor, denom_address_map[tmp_denom])))
+            else:
+                missed_denom.append(denom)
         else:
             missed_denom.append(denom)
-    else:
-        missed_denom.append(denom)
 
-json.dump({'array': result}, output_file)
+    json.dump({'array': result}, output_file)
 
-print("-------- items not found --------")
-# sort for better find out denom
-missed_denom.sort()
-for item in missed_denom:
-    print(item, end=',')
+    print("-------- items not found --------")
+    # sort for better find out denom
+    missed_denom.sort()
+    for item in missed_denom:
+        print(item, end=',')
+
+
+if __name__ == "__main__":
+    main()
+
 
