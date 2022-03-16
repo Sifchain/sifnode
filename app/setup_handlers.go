b9279cefd2c616c0e896a1ef4c90253de2b95855
Tanmay <tanmay@sifchain.finance>
2022-03-16 01:02:32 -0400

    added check for dispensation less than 0

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	dispensation "github.com/Sifchain/sifnode/x/dispensation/types"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -13,12 +14,14 @@ const releaseVersion = "0.12.0"
 func SetupHandlers(app *SifchainApp) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm m.VersionMap) (m.VersionMap, error) {
 		app.Logger().Info("Running upgrade handler for " + releaseVersion)
-		fromVM := make(map[string]uint64)
-		for moduleName := range app.mm.Modules {
-			fromVM[moduleName] = 1
-		}
-		fromVM[dispensation.ModuleName] = 0
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		//fromVM := make(map[string]uint64)
+		//for moduleName := range app.mm.Modules {
+		//	fromVM[moduleName] = 1
+		//}
+		//fromVM[dispensation.ModuleName] = 0
+		fromVersion, exists := vm[dispensation.ModuleName]
+		fmt.Println("fromversion ", fromVersion, "| exists :", exists)
+		return app.mm.RunMigrations(ctx, app.configurator, vm)
 	})
 
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
