06ba464d4af8adb8eca41e1b66c6f6c163cf8ea6
Pradeep Sheokand <pradeep@sifchain.finance>
2022-03-16 08:42:19 +0000

    Update test_dispensation_onlinetxn.py

--- test/integration/src/py/test_dispensation_onlinetxn.py
+++ test/integration/src/py/test_dispensation_onlinetxn.py
@@ -214,7 +214,8 @@ def test_run_online_singlekey_txn(claimType):
 
     # RUN DISPENSATION TXN; GET TXN HASH
     distribution_name = f"{str(resp['height'])}_{str(distributor)}"
-    runtxnhash = run_dispensation(distribution_name, claimType, runner_address, chain_id)
+    distribution_count = 20
+    runtxnhash = run_dispensation(distribution_name, claimType, distribution_count, runner_address, chain_id)
     logging.info(f"txn hash for running dispensation = {runtxnhash}")
     time.sleep(5)
 
