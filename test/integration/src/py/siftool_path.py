1b3e0342199fedb49a63859bd31011820743d97d
Jure Žvikart <jure@sifchain.finance>
2022-03-21 13:52:21 +0100

    Bugfixes

--- test/integration/src/py/siftool_path.py
+++ test/integration/src/py/siftool_path.py
@@ -3,7 +3,7 @@ import sys
 
 # Temporary workaround to include siftool
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), *([os.path.pardir] * 4)))
-base_dir = os.path.join(project_root, "test", "integration", "framework")
+base_dir = os.path.join(project_root, "test", "integration", "framework", "src")
 enabled = False
 for p in sys.path:
     enabled = enabled or os.path.realpath(p) == os.path.realpath(base_dir)
