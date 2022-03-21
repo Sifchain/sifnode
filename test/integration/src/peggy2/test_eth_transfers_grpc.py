5e1a4cd168468f8a921e4495912da1e6077f8676
Jure Žvikart <jure@sifchain.finance>
2022-03-21 08:17:33 +0100

    Update Peggy2 tests

---
+++ test/integration/src/peggy2/test_eth_transfers_grpc.py
@@ -0,0 +1,41 @@
+import siftool_path
+from siftool import eth, test_utils, sifchain
+from siftool.common import *
+
+
+fund_amount_eth = 10 * eth.ETH
+fund_amount_sif = 10 * test_utils.sifnode_funds_for_transfer_peggy1  # TODO How much rowan do we need? (this is 10**18)
+
+
+def test_eth_to_ceth_and_back_grpc(ctx):
+    # Create/retrieve a test ethereum account
+    test_eth_account = ctx.create_and_fund_eth_account(fund_amount=fund_amount_eth)
+
+    # create/retrieve a test sifchain account
+    test_sif_account = ctx.create_sifchain_addr(fund_amounts=[[fund_amount_sif, "rowan"]])
+
+    # Verify initial balance
+    test_sif_account_initial_balance = ctx.get_sifchain_balance(test_sif_account)
+
+    # Send from ethereum to sifchain by locking
+    amount_to_send = 123456 * eth.GWEI
+    assert amount_to_send < fund_amount_eth
+
+    ctx.bridge_bank_lock_eth(test_eth_account, test_sif_account, amount_to_send)
+    ctx.advance_blocks()
+
+    # Verify final balance
+    test_sif_account_final_balance = ctx.wait_for_sif_balance_change(test_sif_account, test_sif_account_initial_balance)
+    balance_diff = sifchain.balance_delta(test_sif_account_initial_balance, test_sif_account_final_balance)
+    assert exactly_one(list(balance_diff.keys())) == ctx.ceth_symbol
+    assert balance_diff[ctx.ceth_symbol] == amount_to_send
+
+    # Send from sifchain to ethereum by burning on sifchain side,
+    # > sifnoded tx ethbridge burn
+    # Reduce amount for cross-chain fee. The same formula is used inside this function.
+    eth_balance_before = ctx.eth.get_eth_balance(test_eth_account)
+    amount_to_send = amount_to_send - ctx.eth.cross_chain_fee_base * ctx.eth.cross_chain_burn_fee
+    ctx.sifnode_client.send_from_sifchain_to_ethereum_grpc(test_sif_account, test_eth_account, amount_to_send, ctx.ceth_symbol)
+
+    # Verify final balance
+    ctx.wait_for_eth_balance_change(test_eth_account, eth_balance_before)
