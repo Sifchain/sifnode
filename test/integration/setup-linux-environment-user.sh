1744d1882c72db75a355d225de5199c60ae29f0c
Michael Ho <michaelho@sifchain.finance>
2022-04-02 11:13:24 -0500

    Restore common files. Remove go install in github action

--- test/integration/setup-linux-environment-user.sh
+++ test/integration/setup-linux-environment-user.sh
@@ -15,11 +15,11 @@ npm install -g truffle @truffle/hdwallet-provider ganache-cli || true
 sudo npm install -g dotenv
 
 # set up environment vars in .bash_profile
-# echo 'export GOPATH=$HOME/go' >> ~/.bash_profile
-# echo 'export GOBIN=$GOPATH/bin' >> ~/.bash_profile
-# echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
-# echo 'export PATH=$GOROOT/bin:$PATH' >> ~/.bash_profile
-# echo 'export PATH=$GOBIN:$PATH' >> ~/.bash_profile
+echo 'export GOPATH=$HOME/go' >> ~/.bash_profile
+echo 'export GOBIN=$GOPATH/bin' >> ~/.bash_profile
+echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
+echo 'export PATH=$GOROOT/bin:$PATH' >> ~/.bash_profile
+echo 'export PATH=$GOBIN:$PATH' >> ~/.bash_profile
 echo 'export PATH=$HOME/.npm-global/bin:$PATH' >> ~/.bash_profile
 
 echo '. ~/.bash_profile' >> ~/.bashrc
