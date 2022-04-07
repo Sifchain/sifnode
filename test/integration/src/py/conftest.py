036527c8da4d347fa9e6961b0727f6368bd8449e
Jure Žvikart <jure@sifchain.finance>
2022-04-07 19:57:00 +0200

    Merge siftool changes from future/peggy2

--- test/integration/src/py/conftest.py
+++ test/integration/src/py/conftest.py
@@ -381,8 +381,8 @@ def ctx(request):
     snapshot_name = request.node.get_closest_marker("snapshot_name")
     if snapshot_name is not None:
         snapshot_name = snapshot_name.args[0]
+        logging.debug("Context setup: snapshot_name={}".format(repr(snapshot_name)))
     from siftool import test_utils
-    logging.error("Context setup: snapshot_name={}".format(repr(snapshot_name)))
     with test_utils.get_test_env_ctx() as ctx:
         yield ctx
         logging.debug("Test context cleanup")
