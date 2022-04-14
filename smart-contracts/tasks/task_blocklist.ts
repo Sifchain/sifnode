7efb7aca1e30dfb55ee9f3f716aa81fad6a6cc6e
Brandon <Brandon@Sifchain.finance>
2022-04-14 16:45:05 -0700

    Added support for a password env variable so it does not have to be on the CLI.

--- smart-contracts/tasks/task_blocklist.ts
+++ smart-contracts/tasks/task_blocklist.ts
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 import { task, types } from "hardhat/config";
 import { AddUser, RemoveUser } from "./blocklist/blocklist_operations";
 import {SetupOFACBlocklist} from "./blocklist/deploy_ofac_contract";
@@ -25,29 +27,32 @@ interface BlocklistOperationArgs extends BlocklistGeneralArgs {
 
 task("blocklist:deploy", "Deploy and sync a new blocklist smart contract")
     .addOptionalParam("wallet", "The name of the wallet to generate/fetch. Defaults to 'blocklist'", "blocklist", types.string)
-    .addOptionalParam("password", "The password to encrypt/decrypt the wallet with", "", types.string)
+    .addOptionalParam("password", "The password to encrypt/decrypt the wallet with. Can also be set with WALLET_PASSWORD env variable.", "", types.string)
     .addOptionalParam("minimumBalance", "The minimum balance the scripts should look for before performing operations. Defaults to 2 ETH.", 2.0, types.float)
     .addOptionalParam("ofacWebsite", "The website to sync sanctioned addresses from. Defaults to `https://www.treasury.gov/ofac/downloads/sdnlist.txt`", "https://www.treasury.gov/ofac/downloads/sdnlist.txt", types.string)
     .setAction(async (args: BlocklistDeployArgs, hre) => {
-        await SetupOFACBlocklist(hre, args.wallet, args.password, hre.ethers.utils.parseEther(String(args.minimumBalance)), args.ofacWebsite);
+        const password = process.env.WALLET_PASSWORD || args.password;
+        await SetupOFACBlocklist(hre, args.wallet, password, hre.ethers.utils.parseEther(String(args.minimumBalance)), args.ofacWebsite);
     });
 
 task("blocklist:sync", "Tools related to the deploying, management and syncing of OFAC blocklists")
     .addPositionalParam("blocklistAddress", "The contract address of the Blocklist")
     .addOptionalParam("wallet", "The name of the wallet to generate/fetch. Defaults to 'blocklist'", "blocklist", types.string)
-    .addOptionalParam("password", "The password to encrypt/decrypt the wallet with", "", types.string)
+    .addOptionalParam("password", "The password to encrypt/decrypt the wallet with. Can also be set with WALLET_PASSWORD env variable.", "", types.string)
     .addOptionalParam("ofacWebsite", "The website to sync sanctioned addresses from. Defaults to `https://www.treasury.gov/ofac/downloads/sdnlist.txt`", "https://www.treasury.gov/ofac/downloads/sdnlist.txt", types.string)
     .setAction(async (args: BlocklistSyncArgs, hre) => {
-        await SyncOfacBlocklist(hre, args.blocklistAddress, args.wallet, args.password, args.ofacWebsite);
+        const password = process.env.WALLET_PASSWORD || args.password;
+        await SyncOfacBlocklist(hre, args.blocklistAddress, args.wallet, password, args.ofacWebsite);
     });
 
 task("blocklist:add", "Tools related to the deploying, management and syncing of OFAC blocklists")
     .addPositionalParam("blocklistAddress", "The contract address of the Blocklist")
     .addPositionalParam("userAddress", "The address of the user to add to the blocklist")
     .addOptionalParam("wallet", "The name of the wallet to generate/fetch. Defaults to 'blocklist'", "blocklist", types.string)
-    .addOptionalParam("password", "The password to encrypt/decrypt the wallet with", "", types.string)
+    .addOptionalParam("password", "The password to encrypt/decrypt the wallet with. Can also be set with WALLET_PASSWORD env variable.", "", types.string)
     .setAction(async (args: BlocklistOperationArgs, hre) => {
-        await AddUser(hre, args.blocklistAddress, args.userAddress, args.wallet, args.password);
+        const password = process.env.WALLET_PASSWORD || args.password;
+        await AddUser(hre, args.blocklistAddress, args.userAddress, args.wallet, password);
 
     });
 
@@ -55,7 +60,8 @@ task("blocklist:remove", "Tools related to the deploying, management and syncing
     .addPositionalParam("blocklistAddress", "The contract address of the Blocklist")
     .addPositionalParam("userAddress", "The address of the user to remove from the blocklist")
     .addOptionalParam("wallet", "The name of the wallet to generate/fetch. Defaults to 'blocklist'", "blocklist", types.string)
-    .addOptionalParam("password", "The password to encrypt/decrypt the wallet with", "", types.string)
+    .addOptionalParam("password", "The password to encrypt/decrypt the wallet with. Can also be set with WALLET_PASSWORD env variable.", "", types.string)
     .setAction(async (args: BlocklistOperationArgs, hre) => {
-        await RemoveUser(hre, args.blocklistAddress, args.userAddress, args.wallet, args.password);
+        const password = process.env.WALLET_PASSWORD || args.password;
+        await RemoveUser(hre, args.blocklistAddress, args.userAddress, args.wallet, password);
     });
