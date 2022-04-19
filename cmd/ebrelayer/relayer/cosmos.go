f463b0b5e447fb99a6dcd37067cf3c556be54325
juniuszhou <junius.zhou@gmail.com>
2022-04-19 21:25:49 +0800

    fix some issue found in audit

--- cmd/ebrelayer/relayer/cosmos.go
+++ cmd/ebrelayer/relayer/cosmos.go
@@ -232,7 +232,7 @@ func (sub CosmosSub) ProcessLockBurnWithScope(txFactory tx.Factory, client *tmcl
 								zap.Reflect("sub", sub),
 								"globalSequence", globalSequence)
 
-							sub.witnessSignProphecyID(txFactory, cosmosMsg)
+							sub.witnessSignAndBroadcastProphecy(txFactory, cosmosMsg)
 							// update expected global Sequence
 							globalSequence++
 
@@ -288,7 +288,7 @@ func tryInitRelayConfig(sub CosmosSub) (*ethclient.Client, *bind.TransactOpts, c
 }
 
 // witness node sign against prophecyID of lock and burn message and send the singnature in message back to Sifnode.
-func (sub CosmosSub) witnessSignProphecyID(
+func (sub CosmosSub) witnessSignAndBroadcastProphecy(
 	txFactory tx.Factory,
 	cosmosMsg types.CosmosMsg,
 ) {
@@ -337,7 +337,7 @@ func (sub CosmosSub) GetGlobalSequenceBlockNumberFromCosmos(
 	networkDescriptor oracletypes.NetworkDescriptor,
 	relayerValAddress string) (uint64, uint64, error) {
 
-	gRpcClientConn, err := grpc.Dial("0.0.0.0:9090", grpc.WithInsecure())
+	gRpcClientConn, err := grpc.Dial(DefaultGrpcEntryPoint, grpc.WithInsecure())
 	if err != nil {
 		return 0, 0, err
 	}
