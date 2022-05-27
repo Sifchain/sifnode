2b227c15a78b84a57c29d7d83e599a76059a3f85
JP Rupp <jp@sifchain.finance>
2022-05-27 16:03:17 +0100

    Use release version of Buf

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -13,17 +13,7 @@ protoc_gen_gocosmos() {
 
 protoc_gen_gocosmos
 
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
-  buf protoc \
-    -I "proto" \
-    -I "third_party/proto" \
-    --gocosmos_out=plugins=interfacetype+grpc,\
-Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-    --grpc-gateway_out=logtostderr=true:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
-
-done
+buf generate
 
 # command to generate docs using protoc-gen-doc
 # buf protoc \
