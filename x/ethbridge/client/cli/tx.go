fcbf06b7eee62910884512afd9b5d80e9595e16e
juniuszhou <junius.zhou@gmail.com>
2022-06-02 06:36:21 +0800

    init solution

--- x/ethbridge/client/cli/tx.go
+++ x/ethbridge/client/cli/tx.go
@@ -36,11 +36,11 @@ func parseNetworkDescriptor(networkDescriptorStr string) (oracletypes.NetworkDes
 //nolint:lll
 func GetCmdBurn() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "burn [cosmos-sender-address] [ethereum-receiver-address] [amount] [symbol] [crossChainFee] --network-descriptor [network-descriptor]",
+		Use:   "burn [ethereum-receiver-address] [amount] [symbol] [crossChainFee] --network-descriptor [network-descriptor]",
 		Short: "burn CrossChainFee or cERC20 on the Cosmos chain",
 		Long: `This should be used to burn CrossChainFee or cERC20. It will burn your coins on the Cosmos Chain, removing them from your account and deducting them from the supply.
 		It will also trigger an event on the Cosmos Chain for relayers to watch so that they can trigger the withdrawal of the original ETH/ERC20 to you from the Ethereum contract!`,
-		Args: cobra.ExactArgs(5),
+		Args: cobra.ExactArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
@@ -59,21 +59,16 @@ func GetCmdBurn() *cobra.Command {
 				return err
 			}
 
-			cosmosSender, err := sdk.AccAddressFromBech32(args[0])
-			if err != nil {
-				return err
-			}
-
 			if !common.IsHexAddress(args[1]) {
 				return errors.Errorf("invalid [ethereum-receiver-address]: %s", args[1])
 			}
-			ethereumReceiver := types.NewEthereumAddress(args[1])
+			ethereumReceiver := types.NewEthereumAddress(args[0])
 
 			var digitCheck = regexp.MustCompile(`^[0-9]+$`)
-			if !digitCheck.MatchString(args[2]) {
+			if !digitCheck.MatchString(args[1]) {
 				return types.ErrInvalidAmount
 			}
-			amount, ok := sdk.NewIntFromString(args[2])
+			amount, ok := sdk.NewIntFromString(args[1])
 			if !ok {
 				return err
 			}
@@ -82,14 +77,14 @@ func GetCmdBurn() *cobra.Command {
 				return types.ErrInvalidAmount
 			}
 
-			symbol := args[3]
+			symbol := args[2]
 
-			crossChainFee, ok := sdk.NewIntFromString(args[4])
+			crossChainFee, ok := sdk.NewIntFromString(args[3])
 			if !ok {
 				return errors.New("Error parsing cross-chain-fee amount")
 			}
 
-			msg := types.NewMsgBurn(networkDescriptor, cosmosSender, ethereumReceiver, amount, symbol, crossChainFee)
+			msg := types.NewMsgBurn(networkDescriptor, clientCtx.FromAddress, ethereumReceiver, amount, symbol, crossChainFee)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
@@ -107,22 +102,14 @@ func GetCmdBurn() *cobra.Command {
 func GetCmdLock() *cobra.Command {
 	//nolint:lll
 	cmd := &cobra.Command{
-		Use:   "lock [cosmos-sender-address] [ethereum-receiver-address] [amount] [symbol] [crossChainFee] --network-descriptor [network-descriptor]",
+		Use:   "lock [ethereum-receiver-address] [amount] [symbol] [crossChainFee] --network-descriptor [network-descriptor]",
 		Short: "This should be used to lock Cosmos-originating coins (eg: ATOM). It will lock up your coins in the supply module, removing them from your account. It will also trigger an event on the Cosmos Chain for relayers to watch so that they can trigger the minting of the pegged token on Etherum to you!",
-		Args:  cobra.ExactArgs(5),
+		Args:  cobra.ExactArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
-
-			cosmosSender, err := sdk.AccAddressFromBech32(args[0])
-			if err != nil {
-				return err
-			}
-
-			// TODO: Rather use standard --from arg in integration test, and remove first redudant param in this command.
-			clientCtx = clientCtx.WithFromAddress(cosmosSender)
 
 			flags := cmd.Flags()
 
@@ -136,17 +123,17 @@ func GetCmdLock() *cobra.Command {
 				return err
 			}
 
-			if !common.IsHexAddress(args[1]) {
+			if !common.IsHexAddress(args[0]) {
 				return errors.Errorf("invalid [ethereum-receiver-address]: %s", args[1])
 			}
 			ethereumReceiver := types.NewEthereumAddress(args[1])
 
 			var digitCheck = regexp.MustCompile(`^[0-9]+$`)
-			if !digitCheck.MatchString(args[2]) {
+			if !digitCheck.MatchString(args[1]) {
 				return types.ErrInvalidAmount
 			}
 
-			amount, ok := sdk.NewIntFromString(args[2])
+			amount, ok := sdk.NewIntFromString(args[1])
 
 			if !ok {
 				return err
@@ -155,14 +142,14 @@ func GetCmdLock() *cobra.Command {
 				return types.ErrInvalidAmount
 			}
 
-			symbol := args[3]
+			symbol := args[2]
 
-			crossChainFee, ok := sdk.NewIntFromString(args[4])
+			crossChainFee, ok := sdk.NewIntFromString(args[3])
 			if !ok {
 				return errors.New("Error parsing cross-chain-fee amount")
 			}
 
-			msg := types.NewMsgLock(networkDescriptor, cosmosSender, ethereumReceiver, amount, symbol, crossChainFee)
+			msg := types.NewMsgLock(networkDescriptor, clientCtx.FromAddress, ethereumReceiver, amount, symbol, crossChainFee)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
@@ -179,7 +166,7 @@ func GetCmdLock() *cobra.Command {
 // GetCmdUpdateWhiteListValidator is the CLI command for update the validator whitelist
 func GetCmdUpdateWhiteListValidator() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "update-whitelist-validator [cosmos-sender-address] [network-id]  [validator-address] [power] --node [node-address]",
+		Use:   "update-whitelist-validator [network-id]  [validator-address] [power] --node [node-address]",
 		Short: "This should be used to update the validator whitelist.",
 		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -188,27 +175,22 @@ func GetCmdUpdateWhiteListValidator() *cobra.Command {
 				return err
 			}
 
-			cosmosSender, err := sdk.AccAddressFromBech32(args[0])
-			if err != nil {
-				return err
-			}
-
-			networkDescriptor, err := strconv.Atoi(args[1])
+			networkDescriptor, err := strconv.Atoi(args[0])
 			if err != nil {
 				return errors.New("Error parsing network descriptor")
 			}
 
-			validatorAddress, err := sdk.ValAddressFromBech32(args[2])
+			validatorAddress, err := sdk.ValAddressFromBech32(args[1])
 			if err != nil {
 				return err
 			}
 
-			power, err := strconv.Atoi(args[3])
+			power, err := strconv.Atoi(args[2])
 			if err != nil {
 				return err
 			}
 
-			msg := types.NewMsgUpdateWhiteListValidator(oracletypes.NetworkDescriptor(networkDescriptor), cosmosSender, validatorAddress, uint32(power))
+			msg := types.NewMsgUpdateWhiteListValidator(oracletypes.NetworkDescriptor(networkDescriptor), clientCtx.FromAddress, validatorAddress, uint32(power))
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
@@ -225,16 +207,11 @@ func GetCmdUpdateWhiteListValidator() *cobra.Command {
 // GetCmdUpdateCrossChainFeeReceiverAccount is the CLI command to update the sifchain account that receives the cross-chain-fee proceeds
 func GetCmdUpdateCrossChainFeeReceiverAccount() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "update-cross-chain-fee-receiver-account [cosmos-sender-address] [cross-chain-fee-receiver-account]",
+		Use:   "update-cross-chain-fee-receiver-account  [cross-chain-fee-receiver-account]",
 		Short: "This should be used to set the crosschain fee receiver account.",
-		Args:  cobra.ExactArgs(2),
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
-			if err != nil {
-				return err
-			}
-
-			cosmosSender, err := sdk.AccAddressFromBech32(args[0])
 			if err != nil {
 				return err
 			}
@@ -244,7 +221,7 @@ func GetCmdUpdateCrossChainFeeReceiverAccount() *cobra.Command {
 				return err
 			}
 
-			msg := types.NewMsgUpdateCrossChainFeeReceiverAccount(cosmosSender, crossChainFeeReceiverAccount)
+			msg := types.NewMsgUpdateCrossChainFeeReceiverAccount(clientCtx.FromAddress, crossChainFeeReceiverAccount)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
@@ -261,33 +238,28 @@ func GetCmdUpdateCrossChainFeeReceiverAccount() *cobra.Command {
 // GetCmdRescueCrossChainFee is the CLI command to send the message to transfer cross-chain-fee from ethbridge module to account
 func GetCmdRescueCrossChainFee() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "rescue-cross-chain-fee [cosmos-sender-address] [cross-chain-fee-receiver-account] [cross-chain-fee] [cross-chain-fee-amount]",
+		Use:   "rescue-cross-chain-fee  [cross-chain-fee-receiver-account] [cross-chain-fee] [cross-chain-fee-amount]",
 		Short: "This should be used to send cross-chain-fee from ethbridge to an account.",
-		Args:  cobra.ExactArgs(4),
+		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
 
-			cosmosSender, err := sdk.AccAddressFromBech32(args[0])
+			crossChainFeeReceiverAccount, err := sdk.AccAddressFromBech32(args[0])
 			if err != nil {
 				return err
 			}
 
-			crossChainFeeReceiverAccount, err := sdk.AccAddressFromBech32(args[1])
-			if err != nil {
-				return err
-			}
-
-			crossChainFee, ok := sdk.NewIntFromString(args[2])
+			crossChainFee, ok := sdk.NewIntFromString(args[1])
 			if !ok {
 				return errors.New("Error parsing cross-chain-fee amount")
 			}
 
-			crosschainFeeSymbol := args[3]
+			crosschainFeeSymbol := args[2]
 
-			msg := types.NewMsgRescueCrossChainFee(cosmosSender, crossChainFeeReceiverAccount, crosschainFeeSymbol, crossChainFee)
+			msg := types.NewMsgRescueCrossChainFee(clientCtx.FromAddress, crossChainFeeReceiverAccount, crosschainFeeSymbol, crossChainFee)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
@@ -304,44 +276,38 @@ func GetCmdRescueCrossChainFee() *cobra.Command {
 // GetCmdSetCrossChainFee is the CLI command to send the message to set crosschain fee for network
 func GetCmdSetCrossChainFee() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "set-cross-chain-fee [cosmos-sender-address] [network-descriptor] [fee-currency] [fee-currency-gas] [minimum-lock-cost] [minimum-burn-cost]",
+		Use:   "set-cross-chain-fee  [network-descriptor] [fee-currency] [fee-currency-gas] [minimum-lock-cost] [minimum-burn-cost]",
 		Short: "This should be used to set crosschain fee for a network.",
-		Args:  cobra.ExactArgs(6),
+		Args:  cobra.ExactArgs(5),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
 
-			cosmosSender, err := sdk.AccAddressFromBech32(args[0])
-
-			if err != nil {
-				return err
-			}
-
-			networkDescriptor, err := strconv.Atoi(args[1])
+			networkDescriptor, err := strconv.Atoi(args[0])
 			if err != nil {
 				return errors.New("Error parsing network descriptor")
 			}
 
-			feeCurrency := args[2]
+			feeCurrency := args[1]
 
-			feeCurrencyGas, ok := sdk.NewIntFromString(args[3])
+			feeCurrencyGas, ok := sdk.NewIntFromString(args[2])
 			if !ok {
 				return errors.New("Error parsing feeCurrencyGas")
 			}
 
-			minimumLockCost, ok := sdk.NewIntFromString(args[4])
+			minimumLockCost, ok := sdk.NewIntFromString(args[3])
 			if !ok {
 				return errors.New("Error parsing minimumLockCost")
 			}
 
-			minimumBurnCost, ok := sdk.NewIntFromString(args[5])
+			minimumBurnCost, ok := sdk.NewIntFromString(args[4])
 			if !ok {
 				return errors.New("Error parsing minimumBurnCost")
 			}
 
-			msg := types.NewMsgSetFeeInfo(cosmosSender,
+			msg := types.NewMsgSetFeeInfo(clientCtx.FromAddress,
 				oracletypes.NetworkDescriptor(networkDescriptor),
 				feeCurrency,
 				feeCurrencyGas,
@@ -363,30 +329,25 @@ func GetCmdSetCrossChainFee() *cobra.Command {
 // GetCmdSignProphecy is the CLI command to send the message to sign a prophecy
 func GetCmdSignProphecy() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "sign signature [cosmos-sender-address] [network-id] [prophecy-id] [ethereum-address] [signature]",
+		Use:   "sign signature [network-id] [prophecy-id] [ethereum-address] [signature]",
 		Short: "This should be used to sign a prophecy.",
-		Args:  cobra.ExactArgs(5),
+		Args:  cobra.ExactArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
 
-			_, err = sdk.AccAddressFromBech32(args[0])
-			if err != nil {
-				return err
-			}
-
-			networkDescriptor, err := strconv.Atoi(args[1])
+			networkDescriptor, err := strconv.Atoi(args[0])
 			if err != nil {
 				return errors.New("Error parsing network descriptor")
 			}
 
-			prophecyID := args[2]
-			ethereumAddress := args[3]
-			signature := args[4]
+			prophecyID := args[1]
+			ethereumAddress := args[2]
+			signature := args[3]
 
-			msg := types.NewMsgSignProphecy(args[0], oracletypes.NetworkDescriptor(networkDescriptor), []byte(prophecyID), ethereumAddress, signature)
+			msg := types.NewMsgSignProphecy(clientCtx.FromAddress.String(), oracletypes.NetworkDescriptor(networkDescriptor), []byte(prophecyID), ethereumAddress, signature)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
@@ -403,26 +364,21 @@ func GetCmdSignProphecy() *cobra.Command {
 // GetCmdUpdateConsensusNeeded is the CLI command to send the message to update consensusNeeded
 func GetCmdUpdateConsensusNeeded() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "update-consensus-needed [cosmos-sender-address] [network-id] [consensus-needed]",
+		Use:   "update-consensus-needed [network-id] [consensus-needed]",
 		Short: "This should be used to update consensus-needed.",
-		Args:  cobra.ExactArgs(3),
+		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
 
-			_, err = sdk.AccAddressFromBech32(args[0])
-			if err != nil {
-				return errors.New("Error cosmos sender address")
-			}
-
-			networkDescriptor, err := strconv.Atoi(args[1])
+			networkDescriptor, err := strconv.Atoi(args[0])
 			if err != nil {
 				return errors.New("Error parsing network descriptor")
 			}
 
-			consensusNeeded, err := strconv.ParseUint(args[2], 10, 32)
+			consensusNeeded, err := strconv.ParseUint(args[1], 10, 32)
 			if err != nil {
 				return errors.New("Error parsing consensus needed")
 			}
@@ -431,7 +387,7 @@ func GetCmdUpdateConsensusNeeded() *cobra.Command {
 				return errors.New("Error consensus needed value too large")
 			}
 
-			msg := types.NewMsgUpdateConsensusNeeded(args[0], oracletypes.NetworkDescriptor(networkDescriptor), uint32(consensusNeeded))
+			msg := types.NewMsgUpdateConsensusNeeded(clientCtx.FromAddress.String(), oracletypes.NetworkDescriptor(networkDescriptor), uint32(consensusNeeded))
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
