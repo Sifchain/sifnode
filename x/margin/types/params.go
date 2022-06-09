9401eb7080b2502bec367fb507ecd2a6e1533797
Tim Lind <tim@incremental.co>
2022-06-09 14:29:59 +0200

    cleanup

--- x/margin/types/params.go
+++ x/margin/types/params.go
@@ -5,34 +5,12 @@ import (
 )
 
 // Parameter store keys
-/*var (
-	KeyLeverageMaxParam          = []byte("LeverageMax")
-	KeyInterestRateMaxParam      = []byte("InterestRateMax")
-	KeyInterestRateMinParam      = []byte("InterestRateMin")
-	KeyInterestRateIncreaseParam = []byte("InterestRateIncrease")
-	KeyInterestRateDecreaseParam = []byte("InterestRateDecrease")
-	KeyHealthGainFactorParam     = []byte("HealthGainFactor")
-	KeyEpochLengthParam          = []byte("EpochLength")
-	KeyForceCloseThresholdParam  = []byte("ForceCloseThreshold")
-	KeyPoolsParam                = []byte("Pools")
-)*/
+// var ()
 
 var _ paramtypes.ParamSet = (*Params)(nil)
 
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{
-		/*
-			paramtypes.NewParamSetPair(KeyLeverageMaxParam, &p.LeverageMax, validate),
-			paramtypes.NewParamSetPair(KeyInterestRateMaxParam, &p.InterestRateMax, validate),
-			paramtypes.NewParamSetPair(KeyInterestRateMinParam, &p.InterestRateMin, validate),
-			paramtypes.NewParamSetPair(KeyInterestRateIncreaseParam, &p.InterestRateIncrease, validate),
-			paramtypes.NewParamSetPair(KeyInterestRateDecreaseParam, &p.InterestRateDecrease, validate),
-			paramtypes.NewParamSetPair(KeyHealthGainFactorParam, &p.HealthGainFactor, validate),
-			paramtypes.NewParamSetPair(KeyEpochLengthParam, &p.EpochLength, validate),
-			paramtypes.NewParamSetPair(KeyForceCloseThresholdParam, &p.ForceCloseThreshold, validate),
-			paramtypes.NewParamSetPair(KeyPoolsParam, &p.Pools, validate),
-		*/
-	}
+	return paramtypes.ParamSetPairs{}
 }
 
 func ParamKeyTable() paramtypes.KeyTable {
