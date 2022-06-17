8075484aff27a6700f8d4200dcabb111c6e6acc9
Tim Lind <tim@incremental.co>
2022-06-17 16:53:23 +0200

    emit force close event in abci as well

--- x/margin/keeper/msg_server.go
+++ x/margin/keeper/msg_server.go
@@ -118,19 +118,7 @@ func (k msgServer) ForceClose(goCtx context.Context, msg *types.MsgForceClose) (
 		return nil, sdkerrors.Wrap(types.ErrInvalidPosition, mtpToClose.Position.String())
 	}
 
-	ctx.EventManager().EmitEvent(sdk.NewEvent(types.EventForceClose,
-		sdk.NewAttribute("position", mtp.Position.String()),
-		sdk.NewAttribute("address", mtp.Address),
-		sdk.NewAttribute("collateral_asset", mtp.CollateralAsset),
-		sdk.NewAttribute("collateral_amount", mtp.CollateralAmount.String()),
-		sdk.NewAttribute("custody_asset", mtp.CustodyAsset),
-		sdk.NewAttribute("custody_amount", mtp.CustodyAmount.String()),
-		sdk.NewAttribute("leverage", mtp.Leverage.String()),
-		sdk.NewAttribute("liabilities_p", mtp.LiabilitiesP.String()),
-		sdk.NewAttribute("liabilities_i", mtp.LiabilitiesI.String()),
-		sdk.NewAttribute("health", mtp.MtpHealth.String()),
-		sdk.NewAttribute("closer", msg.Signer),
-	))
+	k.EmitForceClose(ctx, mtp, msg.Signer)
 
 	return &types.MsgForceCloseResponse{}, nil
 }
