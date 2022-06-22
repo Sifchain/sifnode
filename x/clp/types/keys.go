2480bdce617f966240c221766c690bba7e2298c0
sifag <sifag@pm.me>
2022-06-22 11:32:42 +0200

    cache blockDistribution calculation

--- x/clp/types/keys.go
+++ x/clp/types/keys.go
@@ -36,6 +36,7 @@ var (
 	RewardParamPrefix                = []byte{0x06}
 	SymmetryThresholdPrefix          = []byte{0x07}
 	ProviderDistributionParamsPrefix = []byte{0x08}
+	RewardsBlockDistributionPrefix   = []byte{0x09}
 )
 
 // Generates a key for storing a specific pool
