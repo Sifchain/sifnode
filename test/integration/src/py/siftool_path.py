a342767cf3097ade078f96d34fdd7355457eb817
Jure Žvikart <7929905+jzvikart@users.noreply.github.com>
2022-06-28 18:30:35 +0200

    Prepend path instead of appending it

--- test/integration/src/py/siftool_path.py
+++ test/integration/src/py/siftool_path.py
@@ -2,15 +2,10 @@ import os
 import sys
 
 # Temporary workaround to include siftool
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), *([os.path.pardir] * 4)))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), *([os.path.pardir] * 2)))
 base_dir = os.path.join(project_root, "test", "integration", "framework")
 src_dir = os.path.join(base_dir, "src")
 build_generated_dir = os.path.join(base_dir, "build", "generated")
 paths = [src_dir, build_generated_dir]
-enabled = False
-paths_to_add = []
-for p in paths:
-    enabled = any([os.path.realpath(p) == os.path.realpath(s) for s in sys.path])
-    if not enabled:
-        paths_to_add.append(p)
-sys.path.extend(paths_to_add)
+paths_to_add = [p for p in paths if not any(os.path.realpath(p) == os.path.realpath(s) for s in sys.path)]
+sys.path[:0] = paths_to_add
