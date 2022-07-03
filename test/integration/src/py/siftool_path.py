00782c755fd524605d46070231cbaf5f85ba3f83
Jure Žvikart <7929905+jzvikart@users.noreply.github.com>
2022-07-03 17:16:37 +0200

    Fix path issue

--- test/integration/src/py/siftool_path.py
+++ test/integration/src/py/siftool_path.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Temporary workaround to include siftool
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), *([os.path.pardir] * 2)))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), *([os.path.pardir] * 4)))
 base_dir = os.path.join(project_root, "test", "integration", "framework")
 src_dir = os.path.join(base_dir, "src")
 build_generated_dir = os.path.join(base_dir, "build", "generated")
