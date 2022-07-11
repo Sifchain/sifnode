ee00bebfb3e57a9700cc2f1bda871608ab2f8565
Pradeep Sheokand <pradeep@sifchain.finance>
2022-07-11 18:29:28 +0100

    Create localnet-env.sh

---
+++ scripts/mptestscripts/localnet-env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -x
+
+export ADMIN_ADDRESS="sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd"
+export ADMIN_MNEMONIC="race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow"
+export SIF_ACT=sif
+export SIFNODE_CHAIN_ID=localnet
+export SIFNODE_NODE=tcp://0.0.0.0:26657
