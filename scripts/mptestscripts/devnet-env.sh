b8ecb32a63bf8268e2e0ce634d2c8182cbb5092b
Pradeep Sheokand <pradeep@sifchain.finance>
2022-07-11 18:29:59 +0100

    Create devnet-env.sh

---
+++ scripts/mptestscripts/devnet-env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -x
+
+export ADMIN_ADDRESS="sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd"
+export ADMIN_MNEMONIC="race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow"
+export SIF_ACT=sif1syavy2npfyt9tcncdtsdzf7kny9lh777yqc2nd
+export SIFNODE_CHAIN_ID="sifchain-devnet-1"
+export SIFNODE_NODE=https://rpc-devnet.sifchain.finance:443
