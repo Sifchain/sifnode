f3c42fce89f3203cff9df393fb86dd0a4de78a8c
Pradeep Sheokand <pradeep@sifchain.finance>
2022-07-11 18:48:28 +0100

    Create unbond-liquidity.sh

---
+++ scripts/mptestscripts/unbond-liquidity.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -x
+
+sifnoded tx clp unbond-liquidity \
+  --from $SIF_ACT \
+  --keyring-backend test \
+  --symbol cusdc \
+  --units 1559429888878798180197958710 \
+  --fees 100000000000000000rowan \
+  --chain-id $SIFNODE_CHAIN_ID \
+  --node ${SIFNODE_NODE} \
+  --broadcast-mode block \
+  -y
