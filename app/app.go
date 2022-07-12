2b04120f71d1dbe3131690090d5729d1fdc471fb
James Moore <james@sifchain.finance>
2022-07-12 14:56:54 -0700

    Remove unnecessary permissions to fix #2892.

--- app/app.go
+++ app/app.go
@@ -163,7 +163,7 @@ var (
 		sctransfertypes.ModuleName:     {authtypes.Minter, authtypes.Burner},
 		ethbridgetypes.ModuleName:      {authtypes.Minter, authtypes.Burner},
 		clptypes.ModuleName:            {authtypes.Burner, authtypes.Minter},
-		dispensation.ModuleName:        {authtypes.Burner, authtypes.Minter},
+		dispensation.ModuleName:        {authtypes.Minter},
 	}
 )
 
