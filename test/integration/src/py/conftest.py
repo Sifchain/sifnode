f6a2df7d1ecce5f8d899162a0619967935a87d38
Jure Žvikart <7929905+jzvikart@users.noreply.github.com>
2022-07-14 09:06:27 +0200

    Cleanup

--- test/integration/src/py/conftest.py
+++ test/integration/src/py/conftest.py
@@ -349,31 +349,6 @@ def restore_default_rescue_location(
     )
 
 
-threadlocals = threading.local()
-
-@pytest.fixture
-def ctx_legacy(snapshot_name):
-    logging.debug("Before ctx()")
-    yield threadlocals.ctx
-    logging.debug("After ctx()")
-
-@pytest.fixture(scope="function")
-def with_snapshot(snapshot_name):
-    main = integration_test_context.main
-    cmd = main.Integrator()
-    project = cmd.project
-    project.cleanup_and_reset_state()
-    ctx = integration_test_context.IntegrationTestContext(snapshot_name)
-    logging.info("Started processes: {}".format(repr(ctx.processes)))
-    threadlocals.ctx = ctx
-    os.environ["VAGRANT_ENV_JSON"] = main.project_dir("test/integration/vagrantenv.json")  # TODO HACK
-    logging.debug("Before with_snapshot()")
-    yield
-    logging.debug("After with_snapshot()")
-    main.project.killall(ctx.processes)  # TODO Ensure this is called even in the case of exception
-    logging.info("Terminated processes: {}".format(repr(ctx.processes)))
-    del threadlocals.ctx
-
 @pytest.fixture(scope="function")
 def ctx(request):
     # To pass the "snapshot_name" as a parameter with value "foo" from test, annotate the test function like this:
