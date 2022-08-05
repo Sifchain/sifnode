32a99b834239f830b2bb3394c1da05396fdd9dea
Caner Candan <caner@candan.fr>
2022-08-05 22:23:02 +0200

    fix: 🐛 update migration logic

--- x/margin/keeper/migrations.go
+++ x/margin/keeper/migrations.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"github.com/Sifchain/sifnode/x/margin/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -13,14 +14,24 @@ func NewMigrator(keeper Keeper) Migrator {
 }
 
 func (m Migrator) MigrateToVer2(ctx sdk.Context) error {
-	params := m.keeper.GetParams(ctx)
-
-	params.ForceCloseFundPercentage = sdk.NewDecWithPrec(1, 1)
-	params.InsuranceFundAddress = ""
-	params.SqModifier = sdk.MustNewDecFromStr("10000000000000000000000000")
-	params.SafetyFactor = sdk.MustNewDecFromStr("1.05")
-
-	m.keeper.SetParams(ctx, &params)
+	m.keeper.SetParams(ctx, &types.Params{
+		LeverageMax:              sdk.NewDec(2),
+		HealthGainFactor:         sdk.NewDec(1),
+		InterestRateMin:          sdk.NewDecWithPrec(5, 3),
+		InterestRateMax:          sdk.NewDec(3),
+		InterestRateDecrease:     sdk.NewDecWithPrec(1, 1),
+		InterestRateIncrease:     sdk.NewDecWithPrec(1, 1),
+		ForceCloseThreshold:      sdk.NewDecWithPrec(1, 1),
+		ForceCloseFundPercentage: sdk.NewDecWithPrec(1, 1),
+		InsuranceFundAddress:     "",
+		PoolOpenThreshold:        sdk.NewDecWithPrec(1, 1),
+		RemovalQueueThreshold:    sdk.NewDecWithPrec(1, 1),
+		EpochLength:              1,
+		MaxOpenPositions:         10000,
+		Pools:                    []string{},
+		SqModifier:               sdk.MustNewDecFromStr("10000000000000000000000000"),
+		SafetyFactor:             sdk.MustNewDecFromStr("1.05"),
+	})
 
 	return nil
 }
