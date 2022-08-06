6b4982c12f3ab6e8de730da9499dc492fc377396
Caner Candan <caner@candan.fr>
2022-08-06 23:19:12 +0200

    fix: 🐛 add missing fields to export genesis

--- x/margin/keeper/genesis.go
+++ x/margin/keeper/genesis.go
@@ -18,17 +18,22 @@ func (k Keeper) InitGenesis(ctx sdk.Context, data types.GenesisState) []abci.Val
 func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
 	return &types.GenesisState{
 		Params: &types.Params{
-			LeverageMax:           k.GetMaxLeverageParam(ctx),
-			InterestRateMax:       k.GetInterestRateMax(ctx),
-			InterestRateMin:       k.GetInterestRateMin(ctx),
-			InterestRateIncrease:  k.GetInterestRateIncrease(ctx),
-			InterestRateDecrease:  k.GetInterestRateDecrease(ctx),
-			HealthGainFactor:      k.GetHealthGainFactor(ctx),
-			EpochLength:           k.GetEpochLength(ctx),
-			ForceCloseThreshold:   k.GetForceCloseThreshold(ctx),
-			PoolOpenThreshold:     k.GetPoolOpenThreshold(ctx),
-			RemovalQueueThreshold: k.GetRemovalQueueThreshold(ctx),
-			MaxOpenPositions:      k.GetMaxOpenPositions(ctx),
+			LeverageMax:              k.GetMaxLeverageParam(ctx),
+			InterestRateMax:          k.GetInterestRateMax(ctx),
+			InterestRateMin:          k.GetInterestRateMin(ctx),
+			InterestRateIncrease:     k.GetInterestRateIncrease(ctx),
+			InterestRateDecrease:     k.GetInterestRateDecrease(ctx),
+			HealthGainFactor:         k.GetHealthGainFactor(ctx),
+			EpochLength:              k.GetEpochLength(ctx),
+			ForceCloseThreshold:      k.GetForceCloseThreshold(ctx),
+			ForceCloseFundPercentage: k.GetForceCloseFundPercentage(ctx),
+			InsuranceFundAddress:     k.GetInsuranceFundAddress(ctx).String(),
+			PoolOpenThreshold:        k.GetPoolOpenThreshold(ctx),
+			RemovalQueueThreshold:    k.GetRemovalQueueThreshold(ctx),
+			MaxOpenPositions:         k.GetMaxOpenPositions(ctx),
+			Pools:                    k.GetEnabledPools(ctx),
+			SqModifier:               k.GetSqModifier(ctx),
+			SafetyFactor:             k.GetSafetyFactor(ctx),
 		},
 	}
 }
