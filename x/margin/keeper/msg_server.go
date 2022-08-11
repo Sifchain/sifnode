9fd2785938c421735c36f5b026b1a1f376f71787
Tim Lind <tim@incremental.co>
2022-08-11 11:48:39 +0200

    restrcict opens to whitelist

--- x/margin/keeper/msg_server.go
+++ x/margin/keeper/msg_server.go
@@ -31,6 +31,10 @@ func NewMsgServerImpl(k types.Keeper) types.MsgServer {
 func (k msgServer) Open(goCtx context.Context, msg *types.MsgOpen) (*types.MsgOpenResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
+	if !k.IsWhitelisted(ctx, msg.Signer) {
+		return nil, sdkerrors.Wrap(types.ErrUnauthorised, "unauthorised")
+	}
+
 	if k.GetOpenMTPCount(ctx) >= k.GetMaxOpenPositions(ctx) {
 		return nil, sdkerrors.Wrap(types.ErrMaxOpenPositions, "cannot open new positions")
 	}
