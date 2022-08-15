0c9602765f9e99b7ac7a9c56bfa01518960b007b
Jure Žvikart <7929905+jzvikart@users.noreply.github.com>
2022-08-15 21:49:13 +0200

    Tests

--- test/integration/framework/test/test_environments.py
+++ test/integration/framework/test/test_environments.py
@@ -1,11 +1,18 @@
 from siftool.common import *
-from siftool import command, environments, project
+from siftool import command, environments, project, sifchain
 
 
 def get_validators(env):
     sifnoded = env._sifnoded_for(env.node_info[0])
     return {v["description"]["moniker"]: v for v in sifnoded.query_staking_validators()}
 
+
+def assert_validators_working(env, expected_monikers):
+    assert set(get_validators(env)) == set("sifnoded-{}".format(i) for i in range(3))
+    for i in range(len(env.node_info)):
+        node_info = env.node_info[i]
+        sifnoded = env._sifnoded_for(node_info)
+        sifnoded.send_and_check(node_info["admin_addr"], sifnoded.create_addr(), {sifchain.ROWAN: 10**sifchain.ROWAN_DECIMALS})
 
 def test_environment_1():
     cmd = command.Command()
@@ -20,9 +27,7 @@ def test_environment_1():
     env.init()
     env.start()
     env.add_validator()
-    sifnoded = env._sifnoded_for(env.node_info[0])
-    assert set(get_validators(env)) == set("sifnoded-{}".format(i) for i in range(3))
-    return
+    assert_validators_working(env, set("sifnoded-{}".format(i) for i in range(3)))
 
 def test_environment_2():
     cmd = command.Command()
@@ -33,10 +38,6 @@ def test_environment_2():
     prj.pkill()
     env = environments.SifnodedEnvironment(cmd, sifnoded_home_root=sifnoded_home_root)
     env.add_validator()
-    env.init()
-    env.add_validator()
     env.start()
     env.add_validator()
-    sifnoded = env._sifnoded_for(env.node_info[0])
-    assert set(get_validators(env)) == set("sifnoded-{}".format(i) for i in range(3))
-    return
+    assert_validators_working(env, set("sifnoded-{}".format(i) for i in range(3)))
