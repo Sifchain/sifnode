93e3cec38f976c7e6844138e24a5bfc4cbb6a5b0
Michael Ho <michaelho@sifchain.finance>
2022-08-23 03:49:15 -0500

    Update make createState testhelper func private. Add test to test export import on non-empty EthereumLockBurn, and GlobalSequence

--- x/ethbridge/genesis_test.go
+++ x/ethbridge/genesis_test.go
@@ -8,6 +8,10 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/assert"
+
+	oracletypes "github.com/Sifchain/sifnode/x/oracle/types"
+
+	"math/rand"
 )
 
 // const
@@ -20,57 +24,113 @@ const (
 
 var valAddress, _ = sdk.ValAddressFromBech32(testAddress)
 
-func TestExportGenesis(t *testing.T) {
+func TestExportGenesisExportsCorrectValue(t *testing.T) {
 	ctx, keeper := test.CreateTestAppEthBridge(false)
 	// Generate State
-	receiver := CreateState(ctx, keeper, t)
+	receiver := createState(ctx, keeper, t)
 	state := ethbridge.ExportGenesis(ctx, keeper)
+
+	assert.NotNil(t, state, "ExportGenesis should have non-nil output")
+
+	// Verify CrosschainFeeReceiveAccount
 	assert.Equal(t, receiver, state.CrosschainFeeReceiveAccount)
-
-	key := keeper.GetEthereumLockBurnSequencePrefix(test.NetworkDescriptor, valAddress)
-	assert.Equal(t, lockBurnSequence, state.EthereumLockBurnSequence[string(key)])
-
-	// After set, global nonce will be +1
-	assert.Equal(t, globalNonce+1, state.GlobalNonce[uint32(test.NetworkDescriptor)])
-
-	key = keeper.GetGlobalSequenceToBlockNumberPrefix(ctx, test.NetworkDescriptor, globalNonce)
-	assert.Equal(t, blockNumber, state.GlobalNonceBlockNumber[string(key)])
+	// Verify EtehreumLockBurnSequence
+	assert.Equal(t, keeper.GetEthereumLockBurnSequences(ctx), state.EthereumLockBurnSequence)
+	// Verify GlobalNonce
+	assert.Equal(t, keeper.GetGlobalSequences(ctx), state.GlobalNonce)
+	// Verify GlobalSequenceBlockNumber
+	assert.Equal(t, keeper.GetGlobalSequenceToBlockNumbers(ctx), state.GlobalNonceBlockNumber)
 }
 
-func TestInitGenesis(t *testing.T) {
-	ctx1, keeper1 := test.CreateTestAppEthBridge(false)
-	ctx2, keeper2 := test.CreateTestAppEthBridge(false)
+// InitGenesis and ExportGenesis should be inverse of each other.
+// aka InitGenesis(ExportGenesis(keeper)) === keeper
+func TestInitGenesisWithExportGenesisDataSuccessful(t *testing.T) {
+	ctx1, oldKeeper := test.CreateTestAppEthBridge(false)
+	ctx2, newKeeper := test.CreateTestAppEthBridge(false)
 	// Generate State
-	receiver := CreateState(ctx1, keeper1, t)
-	state := ethbridge.ExportGenesis(ctx1, keeper1)
-	assert.Equal(t, state.CrosschainFeeReceiveAccount, receiver)
+	createState(ctx1, oldKeeper, t)
 
-	// not set for state2, so receiver account is empty
-	state2 := ethbridge.ExportGenesis(ctx2, keeper2)
-	assert.Equal(t, state2.CrosschainFeeReceiveAccount, "")
-
+	exportedState := ethbridge.ExportGenesis(ctx1, oldKeeper)
 	// no validator update from the module
-	valUpdates := ethbridge.InitGenesis(ctx2, keeper2, *state)
+	valUpdates := ethbridge.InitGenesis(ctx2, newKeeper, *exportedState)
 	assert.Equal(t, len(valUpdates), 0)
 
 	// after init the genesis from state, receive account is set
-	actualReceiver := keeper2.GetCrossChainFeeReceiverAccount(ctx2)
-	assert.Equal(t, receiver, actualReceiver.String())
+	assert.Equal(t, oldKeeper.GetCrossChainFeeReceiverAccount(ctx1), newKeeper.GetCrossChainFeeReceiverAccount(ctx2))
 
-	// check lock burn sequence from genesis
-	actualLockBurnSequence := keeper2.GetEthereumLockBurnSequence(ctx2, test.NetworkDescriptor, valAddress)
-	assert.Equal(t, lockBurnSequence, actualLockBurnSequence)
+	assert.Equal(t, oldKeeper.GetEthereumLockBurnSequences(ctx1), newKeeper.GetEthereumLockBurnSequences(ctx2))
 
 	// check block number for network
-	actualBlockNumber := keeper2.GetGlobalSequence(ctx2, test.NetworkDescriptor)
+	actualBlockNumber := newKeeper.GetGlobalSequence(ctx2, test.NetworkDescriptor)
 	assert.Equal(t, blockNumber, actualBlockNumber)
+
+	// TODO: Need to make the states more complex, Import actually fails, it is comparing default values.
+	assert.Equal(t, oldKeeper.GetGlobalSequences(ctx1), newKeeper.GetGlobalSequences(ctx2))
 
 	// check block number for network and global nonce
-	actualBlockNumber = keeper2.GetGlobalSequenceToBlockNumber(ctx2, test.NetworkDescriptor, globalNonce)
-	assert.Equal(t, blockNumber, actualBlockNumber)
+	// actualBlockNumber = keeper2.GetGlobalSequenceToBlockNumber(ctx2, test.NetworkDescriptor, globalNonce)
+	// assert.Equal(t, blockNumber, actualBlockNumber)
+
+	assert.Equal(t, oldKeeper.GetGlobalSequenceToBlockNumbers(ctx1), newKeeper.GetGlobalSequenceToBlockNumbers(ctx2))
 }
 
-func CreateState(ctx sdk.Context, keeper ethbridge.Keeper, t *testing.T) string {
+func TestInitGenesisWithExportGenesisNonEmptyEthereumLockBurnSequence(t *testing.T) {
+	ctx1, oldKeeper, _, _, _, _, _, validators := test.CreateTestKeepers(t, 0.7, []int64{3, 3}, "")
+	ctx2, newKeeper := test.CreateTestAppEthBridge(false)
+
+	testValidatorAddress := validators[0]
+
+	expectedEvmNetworkToLockBurnSequence := make(map[oracletypes.NetworkDescriptor]uint64)
+	expectedEvmNetworkToLockBurnSequence[oracletypes.NetworkDescriptor_NETWORK_DESCRIPTOR_BINANCE_SMART_CHAIN_TESTNET] = rand.Uint64()
+	expectedEvmNetworkToLockBurnSequence[oracletypes.NetworkDescriptor_NETWORK_DESCRIPTOR_GANACHE] = rand.Uint64()
+	expectedEvmNetworkToLockBurnSequence[oracletypes.NetworkDescriptor_NETWORK_DESCRIPTOR_HARDHAT] = rand.Uint64()
+
+	for networkDescriptor, sequence := range expectedEvmNetworkToLockBurnSequence {
+		oldKeeper.SetEthereumLockBurnSequence(ctx1, networkDescriptor, testValidatorAddress, sequence)
+	}
+
+	exportedState := ethbridge.ExportGenesis(ctx1, oldKeeper)
+	ethbridge.InitGenesis(ctx2, newKeeper, *exportedState)
+
+	assert.Equal(t, oldKeeper.GetEthereumLockBurnSequences(ctx1), newKeeper.GetEthereumLockBurnSequences(ctx2))
+	assert.Equal(t, len(expectedEvmNetworkToLockBurnSequence), len(newKeeper.GetEthereumLockBurnSequences(ctx2)))
+}
+
+func TestInitGenesisWithExportGenesisEthereumLockBurnSequenceMultipleValidators(t *testing.T) {
+	ctx1, oldKeeper, _, _, _, _, _, validators := test.CreateTestKeepers(t, 0.7, []int64{3, 3, 2}, "")
+	ctx2, newKeeper := test.CreateTestAppEthBridge(false)
+
+	for _, validator := range validators {
+		oldKeeper.SetEthereumLockBurnSequence(ctx1, oracletypes.NetworkDescriptor_NETWORK_DESCRIPTOR_GANACHE, validator, rand.Uint64())
+		oldKeeper.SetEthereumLockBurnSequence(ctx1, oracletypes.NetworkDescriptor_NETWORK_DESCRIPTOR_HARDHAT, validator, rand.Uint64())
+	}
+
+	exportedState := ethbridge.ExportGenesis(ctx1, oldKeeper)
+	ethbridge.InitGenesis(ctx2, newKeeper, *exportedState)
+
+	assert.Equal(t, oldKeeper.GetEthereumLockBurnSequences(ctx1), newKeeper.GetEthereumLockBurnSequences(ctx2))
+}
+
+func TestInitGenesisWithExportGenesisDataGlobalSequenceSuccess(t *testing.T) {
+	ctx1, oldKeeper := test.CreateTestAppEthBridge(false)
+	ctx2, newKeeper := test.CreateTestAppEthBridge(false)
+
+	// TODO: Cleanup
+	oldKeeper.UpdateGlobalSequence(ctx1, oracletypes.NetworkDescriptor_NETWORK_DESCRIPTOR_BINANCE_SMART_CHAIN_TESTNET, 5)
+	oldKeeper.UpdateGlobalSequence(ctx1, oracletypes.NetworkDescriptor_NETWORK_DESCRIPTOR_BINANCE_SMART_CHAIN_TESTNET, 34)
+	oldKeeper.UpdateGlobalSequence(ctx1, oracletypes.NetworkDescriptor_NETWORK_DESCRIPTOR_BINANCE_SMART_CHAIN_TESTNET, 51)
+
+	oldKeeper.UpdateGlobalSequence(ctx1, test.NetworkDescriptor, 13)
+	oldKeeper.UpdateGlobalSequence(ctx1, test.NetworkDescriptor, 53)
+
+	exportedState := ethbridge.ExportGenesis(ctx1, oldKeeper)
+	ethbridge.InitGenesis(ctx2, newKeeper, *exportedState)
+
+	assert.Equal(t, oldKeeper.GetGlobalSequences(ctx1), newKeeper.GetGlobalSequences(ctx2))
+
+}
+
+func createState(ctx sdk.Context, keeper ethbridge.Keeper, t *testing.T) string {
 	//Setting Receiver account
 	receiver := test.GenerateAddress("")
 	keeper.SetCrossChainFeeReceiverAccount(ctx, receiver)
@@ -78,7 +138,7 @@ func CreateState(ctx sdk.Context, keeper ethbridge.Keeper, t *testing.T) string 
 	assert.True(t, set)
 
 	ethereumLockBurnSequences := keeper.GetEthereumLockBurnSequences(ctx)
-	assert.Equal(t, len(ethereumLockBurnSequences), 0)
+	assert.Equal(t, len(ethereumLockBurnSequences), 0, "New instances should have 0")
 
 	globalNonces := keeper.GetGlobalSequences(ctx)
 	assert.Equal(t, len(globalNonces), 0)
