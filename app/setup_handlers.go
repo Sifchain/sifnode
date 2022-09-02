078b73e62b2e9528bbeb1cf7caafe8880e2efc1e
Caner Candan <caner@candan.fr>
2022-09-02 02:37:16 +0200

    fix: 🐛 add margin genesis logic to sifgen

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "1.0-beta.1"
+const releaseVersion = "1.0-beta.2"
 
 func SetupHandlers(app *SifchainApp) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm m.VersionMap) (m.VersionMap, error) {
