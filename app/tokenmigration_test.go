8ad15fa7e61a7d6e93eaf75071a46373e35bede1
James Moore <james@sifchain.finance>
2022-09-08 13:30:06 -0700

    Call SetConfig before tests

--- app/tokenmigration_test.go
+++ app/tokenmigration_test.go
@@ -1,17 +1,17 @@
 package app_test
 
 import (
-	"testing"
-
 	sifapp "github.com/Sifchain/sifnode/app"
 	"github.com/Sifchain/sifnode/x/ethbridge/test"
 	ethbridge "github.com/Sifchain/sifnode/x/ethbridge/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func TestBankMigration(t *testing.T) {
+	sifapp.SetConfig(true)
 	app, ctx := test.CreateTestApp(false)
 	addrs, _ := test.CreateTestAddrs(1000)
 	// Create Peggy 1 balances for all denoms in map
