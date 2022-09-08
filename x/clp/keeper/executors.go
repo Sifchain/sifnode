af48257b76d02f0428e508e7197b381d5f15eab1
James Moore <james@sifchain.finance>
2022-09-08 10:50:08 -0700

    GetRegistryEntry instead of GetRegistry

--- x/clp/keeper/executors.go
+++ x/clp/keeper/executors.go
@@ -236,8 +236,7 @@ func (k Keeper) ParseToInt(nu string) (sdk.Int, bool) {
 }
 
 func (k Keeper) ProcessRemoveLiquidityMsg(ctx sdk.Context, msg *types.MsgRemoveLiquidity) (sdk.Int, sdk.Int, sdk.Uint, error) {
-	registry := k.tokenRegistryKeeper.GetRegistry(ctx)
-	_, err := k.tokenRegistryKeeper.GetEntry(registry, msg.ExternalAsset.Symbol)
+	_, err := k.tokenRegistryKeeper.GetRegistryEntry(ctx, msg.ExternalAsset.Symbol)
 	if err != nil {
 		return sdk.ZeroInt(), sdk.ZeroInt(), sdk.ZeroUint(), types.ErrTokenNotSupported
 	}
