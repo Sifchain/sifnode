198c0db28829a5b3c30aea42922089b557ade689
Tim Lind <tim@incremental.co>
2022-09-22 18:31:52 +0200

    test with lpd and rewards

---
+++ scripts/clp/reward-period.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -x
+
+sifnoded tx clp reward-period \
+  --path reward-period.json \
+  --from $SIF_ACT \
+  --keyring-backend test \
+  --fees 100000000000000000rowan \
+  --node ${SIFNODE_NODE} \
+  --chain-id $SIFNODE_CHAIN_ID \
+  --broadcast-mode block \
+  -y
