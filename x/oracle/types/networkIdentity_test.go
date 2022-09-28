1cf755944b804894e85fbb02c56ad4f1f2697fcc
juniuszhou <junius.zhou@gmail.com>
2022-09-28 09:42:05 +0800

    fix unit test

--- x/oracle/types/networkIdentity_test.go
+++ x/oracle/types/networkIdentity_test.go
@@ -1,9 +1,12 @@
 package types
 
 import (
+	math "math"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -28,28 +31,28 @@ func TestNetworkDescriptorValid(t *testing.T) {
 
 func TestParseNetworkDescriptorValid(t *testing.T) {
 	input := "1"
-	output, err := parseNetworkDescriptor(input)
-	require.Equal(t, oracletypes.NetworkDescriptor(1), output)
+	output, err := ParseNetworkDescriptor(input)
+	require.Equal(t, NetworkDescriptor(1), output)
 	require.NoError(t, err)
 }
 
 func TestParseNetworkDescriptorNegative(t *testing.T) {
 	input := "-1"
-	output, err := parseNetworkDescriptor(input)
+	output, err := ParseNetworkDescriptor(input)
 	require.Equal(t, -1, int(output))
 	require.Error(t, err)
 }
 
 func TestParseNetworkDescriptorBeyondInt32(t *testing.T) {
 	outOfRangeInput := strconv.Itoa(int(math.Pow(2, 33)))
-	output, err := parseNetworkDescriptor(outOfRangeInput)
+	output, err := ParseNetworkDescriptor(outOfRangeInput)
 	require.Equal(t, -1, int(output))
 	require.Error(t, err)
 }
 
 func TestParseNetworkDescriptorNonExistNetworkDescriptor(t *testing.T) {
-	nonExistNetworkDescriptor := "555" //
-	output, err := parseNetworkDescriptor(nonExistNetworkDescriptor)
+	nonExistNetworkDescriptor := "555"
+	output, err := ParseNetworkDescriptor(nonExistNetworkDescriptor)
 	require.Equal(t, -1, int(output))
 	require.Error(t, err)
 }
