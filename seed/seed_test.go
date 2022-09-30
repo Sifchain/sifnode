e6511cb0325fd3f48239c6ae991a179015b7f16d
Tim Lind <tim@incremental.co>
2022-09-30 12:21:10 +0200

    randomise lps and use smaller add amounts

--- seed/seed_test.go
+++ seed/seed_test.go
@@ -15,36 +15,49 @@ func TestSeed(t *testing.T) {
 	app, ctx := test.CreateTestApp(false)
 	app.TokenRegistryKeeper.SetToken(ctx, &tokenregistrytypes.RegistryEntry{
 		Denom: "ceth", Permissions: []tokenregistrytypes.Permission{tokenregistrytypes.Permission_CLP},
+		Decimals: 6,
 	})
 	app.TokenRegistryKeeper.SetToken(ctx, &tokenregistrytypes.RegistryEntry{
 		Denom: "cusdc", Permissions: []tokenregistrytypes.Permission{tokenregistrytypes.Permission_CLP},
+		Decimals: 6,
 	})
 	app.TokenRegistryKeeper.SetToken(ctx, &tokenregistrytypes.RegistryEntry{
 		Denom: "clink", Permissions: []tokenregistrytypes.Permission{tokenregistrytypes.Permission_CLP},
+		Decimals: 6,
 	})
-	err := app.ClpKeeper.SetPool(ctx, &clptypes.Pool{
+	swapPriceExternal, err := sdk.NewDecFromStr("127.230711704805236001")
+	require.NoError(t, err)
+	swapPriceNative, err := sdk.NewDecFromStr("0.007859737531926673")
+	require.NoError(t, err)
+	err = app.ClpKeeper.SetPool(ctx, &clptypes.Pool{
 		ExternalAsset:        &clptypes.Asset{Symbol: "ceth"},
-		NativeAssetBalance:   sdk.NewUint(1000000000),
-		ExternalAssetBalance: sdk.NewUint(1000000000),
+		NativeAssetBalance:   sdk.NewUintFromString("75151690408573637255087195"),
+		ExternalAssetBalance: sdk.NewUintFromString("590672561692"),
+		SwapPriceNative:      &swapPriceNative,
+		SwapPriceExternal:    &swapPriceExternal,
 	})
 	require.NoError(t, err)
 	err = app.ClpKeeper.SetPool(ctx, &clptypes.Pool{
 		ExternalAsset:        &clptypes.Asset{Symbol: "cusdc"},
-		NativeAssetBalance:   sdk.NewUint(1000000000),
-		ExternalAssetBalance: sdk.NewUint(1000000000),
+		NativeAssetBalance:   sdk.NewUintFromString("75151690408573637255087195"),
+		ExternalAssetBalance: sdk.NewUintFromString("590672561692"),
+		SwapPriceNative:      &swapPriceNative,
+		SwapPriceExternal:    &swapPriceExternal,
 	})
 	require.NoError(t, err)
 	err = app.ClpKeeper.SetPool(ctx, &clptypes.Pool{
 		ExternalAsset:        &clptypes.Asset{Symbol: "clink"},
-		NativeAssetBalance:   sdk.NewUint(1000000000),
-		ExternalAssetBalance: sdk.NewUint(1000000000),
+		NativeAssetBalance:   sdk.NewUintFromString("75151690408573637255087195"),
+		ExternalAssetBalance: sdk.NewUintFromString("590672561692"),
+		SwapPriceNative:      &swapPriceNative,
+		SwapPriceExternal:    &swapPriceExternal,
 	})
 	require.NoError(t, err)
 
-	err = seed.Seed(app.ClpKeeper, app.BankKeeper, ctx, 10000, []string{"ceth", "cusdc", "clink"})
+	err = seed.Seed(app.ClpKeeper, app.BankKeeper, app.TokenRegistryKeeper, ctx, 10000, 3)
 	require.NoError(t, err)
 
 	lps, err := app.ClpKeeper.GetAllLiquidityProviders(ctx)
 	require.NoError(t, err)
-	require.Equal(t, 30000, len(lps))
+	require.GreaterOrEqual(t, 10000, len(lps))
 }
