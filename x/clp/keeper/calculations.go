af5414eee895afa6c93a3e7d1fde58881da8bb43
jedi2002 <kingpin.xd@gmail.com>
2022-10-05 00:30:04 -0400

    Changed RountInt to TruncateInt in calcualtions.go

--- x/clp/keeper/calculations.go
+++ x/clp/keeper/calculations.go
@@ -69,10 +69,10 @@ func CalculateWithdrawal(poolUnits sdk.Uint, nativeAssetBalance string,
 	//if asymmetry is 0 we don't need to swap
 	lpUnitsLeft := lpUnitsF.Sub(unitsToClaim)
 
-	return sdk.NewUintFromBigInt(withdrawNativeAssetAmount.RoundInt().BigInt()),
-		sdk.NewUintFromBigInt(withdrawExternalAssetAmount.RoundInt().BigInt()),
-		sdk.NewUintFromBigInt(lpUnitsLeft.RoundInt().BigInt()),
-		sdk.NewUintFromBigInt(swapAmount.RoundInt().BigInt())
+	return sdk.NewUintFromBigInt(withdrawNativeAssetAmount.TruncateInt().BigInt()),
+		sdk.NewUintFromBigInt(withdrawExternalAssetAmount.TruncateInt().BigInt()),
+		sdk.NewUintFromBigInt(lpUnitsLeft.TruncateInt().BigInt()),
+		sdk.NewUintFromBigInt(swapAmount.TruncateInt().BigInt())
 }
 
 // More details on the formula
