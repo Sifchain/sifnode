31a703365ea313b729fde0da04ba86a7c0f617cb
Tim Lind <tim@incremental.co>
2022-10-14 23:05:18 +0200

    1.0.14-rc.1

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "1.0.14-beta"
+const releaseVersion = "1.0.14-rc.1"
 
 func SetupHandlers(app *SifchainApp) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm m.VersionMap) (m.VersionMap, error) {
