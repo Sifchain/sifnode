9a9e2690bf6f6831088841d6031c4322c903b3fb
Kevin Jones <kevinj@sifchain.finance>
2022-10-15 19:53:39 +0100

    Bump version

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "1.0.13-rc.1"
+const releaseVersion = "1.0.13-beta"
 
 func SetupHandlers(app *SifchainApp) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm m.VersionMap) (m.VersionMap, error) {
