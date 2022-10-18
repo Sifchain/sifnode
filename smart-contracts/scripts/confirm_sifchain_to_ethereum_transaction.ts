073d74532730a9db917c9c6b8cb77e264e57c15a
James Moore <james@sifchain.finance>
2022-10-18 15:55:09 -0700

    Add tool to check prophecy completion

--- smart-contracts/scripts/confirm_sifchain_to_ethereum_transaction.ts
+++ smart-contracts/scripts/confirm_sifchain_to_ethereum_transaction.ts
@@ -6,13 +6,14 @@ import { setupDeployment } from "../src/hardhatFunctions";
 import { LogNewOracleClaimEvent } from "../build/CosmosBridge";
 import { BigNumber } from "ethers";
 
-const startingBlockNumber = 	15776327;
-const endingBlockNumber = startingBlockNumber + 6000;
+const startingBlockNumber = 15676218;
+const endingBlockNumber = startingBlockNumber + 1000000;
 
 interface ProphecyClaimState {
   [_index: string]: {
     count: number,
-    complete: boolean
+    complete: boolean,
+    validators: string[]
   };
 }
 
@@ -23,16 +24,20 @@ async function main() {
 
   const cosmosBridge = await container.resolve(DeployedCosmosBridge).contract;
 
+  const validatorToBlock: any = {};
+
   const newProphecyClaims = await cosmosBridge.queryFilter(cosmosBridge.filters.LogNewOracleClaim(), startingBlockNumber, endingBlockNumber);
   const prophecyClaimState = newProphecyClaims.reduce((acc: ProphecyClaimState, x: LogNewOracleClaimEvent) => {
     const prophecyId = BigNumber.from(x.args._prophecyID).toString();
     const item = {
       [prophecyId]: {
         count: ((acc[prophecyId] ?? {}).count ?? 0) + 1,
+        validators: ((acc[prophecyId] ?? {}).validators ?? []).concat(x.args._validatorAddress),
         complete: false,
         mostRecentBlockNumber: x.blockNumber
       }
     };
+    validatorToBlock[x.args._validatorAddress] = x.blockNumber;
     return {
       ...acc,
       ...item
@@ -43,9 +48,20 @@ async function main() {
   prophecyCompletedLogs.forEach(x => {
     const prophecyId = BigNumber.from((x.args as any)["_prophecyID"]).toString();
     prophecyClaimState[prophecyId].complete = true;
-  })
+  });
 
-  console.log(JSON.stringify({ prophecyClaimState }, undefined, 2));
+  const activeValidators = Object.entries(prophecyClaimState).reduce((acc, x: any) => {
+    const [prophecyId, data] = x;
+    for (const v of data["validators"])
+      acc.add(v);
+    return acc;
+  }, new Set());
+
+  console.log(JSON.stringify({
+    prophecyClaimState,
+    activeValidators: Array.from(activeValidators),
+    validatorToBlock
+  }, undefined, 2));
 }
 
 main()
