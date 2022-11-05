706a55f1205d96d706daebfb69318184e1b53027
Caner Candan <caner@candan.fr>
2022-11-05 23:21:33 +0100

    fix: 🐛 DLP discounted sent amount

--- x/clp/keeper/msg_server.go
+++ x/clp/keeper/msg_server.go
@@ -615,7 +615,8 @@ func (k msgServer) Swap(goCtx context.Context, msg *types.MsgSwap) (*types.MsgSw
 	if k.GetLiquidityProtectionParams(ctx).IsActive {
 		if types.StringCompare(sAsset.Denom, types.NativeSymbol) {
 			// selling rowan
-			k.MustUpdateLiquidityProtectionThreshold(ctx, true, msg.SentAmount, price)
+			discountedSentAmount := CalculateDiscountedSentAmount(msg.SentAmount, swapFeeRate)
+			k.MustUpdateLiquidityProtectionThreshold(ctx, true, discountedSentAmount, price)
 		}
 
 		if types.StringCompare(rAsset.Denom, types.NativeSymbol) {
