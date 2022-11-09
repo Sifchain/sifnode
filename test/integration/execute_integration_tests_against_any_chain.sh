087f66c210456f7cb7dabb4b04af68acfbecdbe1
Michael Ho <michaelho@sifchain.finance>
2022-11-09 12:05:35 -0600

    Revert "Add integration test to github action"
    This reverts commit bdd2d0e46602ed0adbadb2b128729ccabe410b25.

--- test/integration/execute_integration_tests_against_any_chain.sh
+++ test/integration/execute_integration_tests_against_any_chain.sh
@@ -15,4 +15,3 @@ loglevel=${LOG_LEVEL:-INFO}
 python3 -m pytest -olog_level=$loglevel -v -olog_file=/tmp/log.txt -v \
   ${TEST_INTEGRATION_PY_DIR}/test_eth_transfers.py \
   ${TEST_INTEGRATION_PY_DIR}/test_rowan_transfers.py \
-  ${TEST_INTEGRATION_PY_DIR}/test_sifnode_peggy_pause_unpause.py \
