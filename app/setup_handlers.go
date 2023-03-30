a283f1c5c37e9b76b88e3774459a3a966bcbbc18
atheesh <atheesh@vitwit.com>
2023-03-30 15:17:07 +0530

    upgrade handler

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -1,38 +1,17 @@
 package app
 
 import (
-	admintypes "github.com/Sifchain/sifnode/x/admin/types"
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	m "github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "1.1.0-beta"
+const releaseVersion = "1.2.0-beta"
 
 func SetupHandlers(app *SifchainApp) {
-	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm m.VersionMap) (m.VersionMap, error) {
+	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, _ types.Plan, vm m.VersionMap) (m.VersionMap, error) {
 		app.Logger().Info("Running upgrade handler for " + releaseVersion)
-
-		app.AdminKeeper.SetParams(ctx, &admintypes.Params{
-			SubmitProposalFee: sdk.NewUintFromString("5000000000000000000000"),
-		})
 
 		return app.mm.RunMigrations(ctx, app.configurator, vm)
 	})
-
-	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
-	if err != nil {
-		panic(err)
-	}
-	if upgradeInfo.Name == releaseVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		storeUpgrades := storetypes.StoreUpgrades{
-			// Added: []string{},
-		}
-		// Use upgrade store loader for the initial loading of all stores when app starts,
-		// it checks if version == upgradeHeight and applies store upgrades before loading the stores,
-		// so that new stores start with the correct version (the current height of chain),
-		// instead the default which is the latest version that store last committed i.e 0 for new stores.
-		app.SetStoreLoader(types.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
-	}
 }
