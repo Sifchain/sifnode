cbfba9425556880dcb6eaedf868dca91cea8e746
Snobbish Bee <125891987+snobbee@users.noreply.github.com>
2023-12-05 15:16:03 +0100

    ci: update version (#3434)

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -1,14 +1,13 @@
 package app
 
 import (
-	epochstypes "github.com/Sifchain/sifnode/x/epochs/types"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	m "github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "1.3.0-beta"
+const releaseVersion = "1.3.1-beta"
 
 func SetupHandlers(app *SifchainApp) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, _ types.Plan, vm m.VersionMap) (m.VersionMap, error) {
@@ -23,9 +22,7 @@ func SetupHandlers(app *SifchainApp) {
 	}
 	if upgradeInfo.Name == releaseVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
 		storeUpgrades := storetypes.StoreUpgrades{
-			Added: []string{
-				epochstypes.StoreKey,
-			},
+			Added: []string{},
 		}
 		// Use upgrade store loader for the initial loading of all stores when app starts,
 		// it checks if version == upgradeHeight and applies store upgrades before loading the stores,
